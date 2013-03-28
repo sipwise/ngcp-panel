f982cbbe0b4b0de3d553ceb56ebc79010b1cdb1a
Gerhard Jungwirth <gjungwirth@sipwise.com>
2013-03-28 16:33:03 +0100

    outsource model config to config file

--- lib/NGCP/Panel/Model/billing.pm
+++ lib/NGCP/Panel/Model/billing.pm
@@ -3,11 +3,4 @@ package NGCP::Panel::Model::billing;
 use strict;
 use base 'Catalyst::Model::DBIC::Schema';
 
-__PACKAGE__->config(
-    schema_class => 'NGCP::Schema::billing',
-    
-    connect_info => {
-        dsn => 'dbi:mysql:dbname=billing',
-        user => 'root',
-    }
-);
+1;
