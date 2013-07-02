b8a6b6b9f12484ec8d798ca14bf44b5fde414ddd
Andreas Granig <agranig@sipwise.com>
2013-07-02 21:53:35 +0200

    Enhance paging style.

--- share/static/js/libs/datatables-bootstrap-paging.js
+++ share/static/js/libs/datatables-bootstrap-paging.js
@@ -26,9 +26,9 @@ $.extend( $.fn.dataTableExt.oPagination, {
 			};
 
 			$(nPaging).addClass('pagination').append(
-				'<ul>'+
-					'<li class="prev disabled"><a href="#">&larr; '+oLang.sPrevious+'</a></li>'+
-					'<li class="next disabled"><a href="#">'+oLang.sNext+' &rarr; </a></li>'+
+				'<ul class="pull-right">'+
+					'<li class="prev disabled"><a href="#">&larr;</a></li>'+
+					'<li class="next disabled"><a href="#">&rarr;</a></li>'+
 				'</ul>'
 			);
 			var els = $('a', nPaging);
