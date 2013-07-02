f3f7f62868df6c4b30e22c4d7f8d4daaaded3455
Andreas Granig <agranig@sipwise.com>
2013-07-02 13:00:05 +0200

    Implement customer fraud levels.

--- lib/NGCP/Panel/Controller/Customer.pm
+++ lib/NGCP/Panel/Controller/Customer.pm
@@ -3,6 +3,8 @@ use Sipwise::Base;
 use namespace::sweep;
 BEGIN { extends 'Catalyst::Controller'; }
 use NGCP::Panel::Form::Customer;
+use NGCP::Panel::Form::CustomerMonthlyFraud;
+use NGCP::Panel::Form::CustomerDailyFraud;
 use NGCP::Panel::Utils;
 
 use Data::Printer;
@@ -50,13 +52,81 @@ sub base :Chained('list_customer') :PathPart('') :CaptureArgs(1) {
     my $contract = $c->model('billing')->resultset('contracts')
         ->find($contract_id);
 
+    $c->stash(fraud => $contract->contract_fraud_preference);
+    $c->stash(template => 'customer/details.tt'); 
     $c->stash(contract => $contract);
 }
 
 sub details :Chained('base') :PathPart('details') :Args(0) {
     my ($self, $c) = @_;
+}
 
-    $c->stash(template => 'customer/details.tt'); 
+sub edit_fraud :Chained('base') :PathPart('fraud/edit') :Args(1) {
+    my ($self, $c, $type) = @_;
+
+    my $posted = ($c->request->method eq 'POST');
+    my $form;
+    if($type eq "month") {
+        $form = NGCP::Panel::Form::CustomerMonthlyFraud->new;
+    } elsif($type eq "day") {
+        $form = NGCP::Panel::Form::CustomerDailyFraud->new;
+    } else {
+        $c->flash(messages => [{type => 'error', text => "Invalid fraud interval '$type'!"}]);
+        $c->response->redirect($c->uri_for_action("/customer/details", [$c->stash->{contract}->id]));
+        return;
+    }
+
+    my $fraud_prefs = $c->stash->{fraud} ||
+        $c->model('billing')->resultset('contract_fraud_preferences')
+            ->new_result({ contract_id => $c->stash->{contract}->id});
+    $form->process(
+        posted => $posted,
+        params => $c->request->params,
+        action => $c->uri_for_action("/customer/edit_fraud", $c->stash->{contract}->id, $type),
+        item => $fraud_prefs,
+    );
+    if($posted && $form->validated) {
+        $c->flash(messages => [{type => 'success', text => 'Fraud settings successfully changed!'}]);
+        $c->response->redirect($c->uri_for_action("/customer/details", [$c->stash->{contract}->id]));
+        return;
+    }
+
+    $c->stash(close_target => $c->uri_for_action("/customer/details", [$c->stash->{contract}->id]));
+    $c->stash(form => $form);
+    $c->stash(edit_flag => 1);
+}
+
+sub delete_fraud :Chained('base') :PathPart('fraud/delete') :Args(1) {
+    my ($self, $c, $type) = @_;
+
+    if($type eq "month") {
+        $type = "interval";
+    } elsif($type eq "day") {
+        $type = "daily";
+    } else {
+        $c->flash(messages => [{type => 'error', text => "Invalid fraud interval '$type'!"}]);
+        $c->response->redirect($c->uri_for_action("/customer/details", [$c->stash->{contract}->id]));
+        return;
+    }
+
+    my $fraud_prefs = $c->stash->{fraud};
+    if($fraud_prefs) {
+        try {
+            $fraud_prefs->update({
+                "fraud_".$type."_limit" => undef,
+                "fraud_".$type."_lock" => undef,
+                "fraud_".$type."_notify" => undef,
+            });
+        } catch($e) {
+            $c->log->error("Failed to clear fraud interval: $e");
+            $c->flash(messages => [{type => 'error', text => "Failed to clear fraud interval!"}]);
+            $c->response->redirect($c->uri_for_action("/customer/details", [$c->stash->{contract}->id]));
+            return;
+        }
+    }
+    $c->flash(messages => [{type => 'success', text => "Successfully cleared fraud interval!"}]);
+    $c->response->redirect($c->uri_for_action("/customer/details", [$c->stash->{contract}->id]));
+    return;
 }
 
 =head1 AUTHOR
