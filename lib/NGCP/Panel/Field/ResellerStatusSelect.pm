4cbf5be13fd55fb1382f5160f615a08d4b30ccb6
Andreas Granig <agranig@sipwise.com>
2013-07-04 13:25:18 +0200

    Make reseller status active by default.

--- lib/NGCP/Panel/Field/ResellerStatusSelect.pm
+++ lib/NGCP/Panel/Field/ResellerStatusSelect.pm
@@ -6,7 +6,6 @@ sub build_options {
     my ($self) = @_;
 
     return [ 
-        { label => 'Select...', value => '' },
         { label => 'active', value => 'active' },
         { label => 'locked', value => 'locked' },
         { label => 'terminated', value => 'terminated' },
