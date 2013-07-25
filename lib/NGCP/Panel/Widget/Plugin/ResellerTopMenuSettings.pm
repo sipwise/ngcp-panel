264a5460daea489b3afc3903e30a9627b04a9abe
Andreas Granig <agranig@sipwise.com>
2013-07-25 18:15:19 +0200

    Add reseller top menu.

---
+++ lib/NGCP/Panel/Widget/Plugin/ResellerTopMenuSettings.pm
@@ -0,0 +1,32 @@
+package NGCP::Panel::Widget::Plugin::ResellerTopMenuSettings;
+use Moose::Role;
+
+has 'template' => (
+    is  => 'ro',
+    isa => 'Str',
+    default => 'widgets/reseller_topmenu_settings.tt'
+);
+
+has 'type' => (
+    is  => 'ro',
+    isa => 'Str',
+    default => 'topmenu_widgets',
+);
+
+around handle => sub {
+    my ($foo, $self, $c) = @_;
+    return;
+};
+
+sub filter {
+    my ($self, $c, $type) = @_;
+
+    return $self if(
+        $type eq $self->type &&
+        $c->user_in_realm('reseller')
+    );
+    return;
+}
+
+1;
+# vim: set syntax=perl tabstop=4 expandtab:
