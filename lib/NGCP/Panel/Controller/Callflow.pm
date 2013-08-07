c8beecba9ae0782badde04c18628a191001c85cb
Andreas Granig <agranig@sipwise.com>
2013-08-07 10:31:15 +0200

    Only allow callflows if enabled in config.

--- lib/NGCP/Panel/Controller/Callflow.pm
+++ lib/NGCP/Panel/Controller/Callflow.pm
@@ -12,6 +12,8 @@ sub auto :Does(ACL) :ACLDetachTo('/denied_page') :AllowedRole(admin) {
     my ($self, $c) = @_;
     $c->log->debug(__PACKAGE__ . '::auto');
     NGCP::Panel::Utils::Navigation::check_redirect_chain(c => $c);
+    $c->detach('/denied_page')
+        unless($c->config->{features}->{callflow});
     return 1;
 }
 
