594f341ee8e739841751c333bf15a6bbbddd1b6b
Gerhard Jungwirth <gjungwirth@sipwise.com>
2013-09-19 17:35:51 +0200

    Preserve the product_id when updating a billing_mapping
    This had created a billing_mapping with product_id = NULL before, which
    is no longer a valid option and caused the panel to fail

--- lib/NGCP/Panel/Controller/Contract.pm
+++ lib/NGCP/Panel/Controller/Contract.pm
@@ -186,6 +186,7 @@ sub edit :Chained('base') :PathPart('edit') :Args(0) {
                     $contract->billing_mappings->create({
                         start_date => NGCP::Panel::Utils::DateTime::current_local,
                         billing_profile_id => $form->values->{billing_profile}{id},
+                        product_id => $billing_mapping->product_id,
                     });
                 }
                 my $old_status = $contract->status;
