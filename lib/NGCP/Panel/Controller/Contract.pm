bf3337b57a6e9762ec964f05517912c190016046
Gerhard Jungwirth <gjungwirth@sipwise.com>
2013-09-23 15:51:13 +0200

    Correctly identify customer contracts
    the product handle was compared to the class name by mistake

--- lib/NGCP/Panel/Controller/Contract.pm
+++ lib/NGCP/Panel/Controller/Contract.pm
@@ -164,7 +164,11 @@ sub base :Chained('contract_list') :PathPart('') :CaptureArgs(1) {
     }
 
     my $billing_mapping = $res->billing_mappings->find($res->get_column('bmid'));
-    if (! defined ($billing_mapping->product) ) {
+    if (! defined ($billing_mapping->product) || (
+        $billing_mapping->product->handle ne 'VOIP_RESELLER' &&
+        $billing_mapping->product->handle ne 'SIP_PEERING' &&
+        $billing_mapping->product->handle ne 'PSTN_PEERING')) {
+
         $c->stash(page_title => "Customer",
                   page_title_plural => "Customers");
     }
