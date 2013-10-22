d2b7acaf5ee596f2036b6173400913e1e8d50e3e
Andreas Granig <agranig@sipwise.com>
2013-10-22 17:17:58 +0200

    MT#4607 Replace UNION by simple OR
    MySQL 5.5 handles OR properly in terms of indices, no need for a UNION,
    which is completely broken with DBIx::Class anyways, killing the performance.

--- lib/NGCP/Panel/Controller/Subscriber.pm
+++ lib/NGCP/Panel/Controller/Subscriber.pm
@@ -1956,13 +1956,12 @@ sub ajax_calls :Chained('master') :PathPart('calls/ajax') :Args(0) {
     my ($self, $c) = @_;
 
     # CDRs
-    my $out_rs = $c->model('DB')->resultset('cdr')->search({
-        source_user_id => $c->stash->{subscriber}->uuid,
+    my $rs = $c->model('DB')->resultset('cdr')->search({
+        -or => [
+            source_user_id => $c->stash->{subscriber}->uuid,
+            destination_user_id => $c->stash->{subscriber}->uuid,
+        ],
     });
-    my $in_rs = $c->model('DB')->resultset('cdr')->search({
-        destination_user_id => $c->stash->{subscriber}->uuid,
-    });
-    my $rs = $out_rs->union($in_rs);
     NGCP::Panel::Utils::Datatables::process(
         $c, $rs, $c->stash->{calls_dt_columns},
         sub {
@@ -1999,6 +1998,7 @@ sub ajax_voicemails :Chained('master') :PathPart('voicemails/ajax') :Args(0) {
 
     my $vm_rs = $c->model('DB')->resultset('voicemail_spool')->search({
         mailboxuser => $c->stash->{subscriber}->uuid,
+        msgnum => { '>=' => 0 },
     });
     NGCP::Panel::Utils::Datatables::process($c, $vm_rs, $c->stash->{vm_dt_columns});
 
@@ -2008,15 +2008,12 @@ sub ajax_voicemails :Chained('master') :PathPart('voicemails/ajax') :Args(0) {
 sub ajax_captured_calls :Chained('master') :PathPart('callflow/ajax') :Args(0) {
     my ($self, $c) = @_;
 
-    my $caller_rs = $c->model('DB')->resultset('messages')->search({
-        'me.caller_uuid' => $c->stash->{subscriber}->uuid,
-    });
-    my $callee_rs = $c->model('DB')->resultset('messages')->search({
-        'me.callee_uuid' => $c->stash->{subscriber}->uuid,
-    });
-
-    # TODO: group_by or distinct on call_id!
-    my $rs = $caller_rs->union($callee_rs)->search(undef, {
+    my $rs = $c->model('DB')->resultset('messages')->search({
+        -or => [
+            'me.caller_uuid' => $c->stash->{subscriber}->uuid,
+            'me.callee_uuid' => $c->stash->{subscriber}->uuid,
+        ],
+    }, {
         order_by => { -asc => 'me.timestamp' },
     });
 
