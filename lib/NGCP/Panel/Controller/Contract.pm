54101eaa732463e66cf211b2d540fd9700fb3223
Andreas Granig <agranig@sipwise.com>
2013-10-29 16:49:33 +0100

    Revert "Show product name in customer details."
    This reverts commit b76042a4afb170edc57c02c8749cf751591361b1.

--- lib/NGCP/Panel/Controller/Contract.pm
+++ lib/NGCP/Panel/Controller/Contract.pm
@@ -28,7 +28,40 @@ sub contract_list :Chained('/') :PathPart('contract') :CaptureArgs(0) {
         { name => "status", search => 1, title => "Status" },
     ]);
 
-    my $rs = NGCP::Panel::Utils::Contract::get_contract_rs(c => $c);
+    my $mapping_rs = $c->model('DB')->resultset('billing_mappings');
+    my $rs = $c->model('DB')->resultset('contracts')
+        ->search({
+            'me.status' => { '!=' => 'terminated' },
+            'billing_mappings.id' => {
+                '=' => $mapping_rs->search({
+                    contract_id => { -ident => 'me.id' },
+                    start_date => [ -or =>
+                        { '<=' => NGCP::Panel::Utils::DateTime::current_local },
+                        { -is  => undef },
+                    ],
+                    end_date => [ -or =>
+                        { '>=' => NGCP::Panel::Utils::DateTime::current_local },
+                        { -is  => undef },
+                    ],
+                },{
+                    alias => 'bilmap',
+                    rows => 1,
+                    order_by => {-desc => ['bilmap.start_date', 'bilmap.id']},
+                })->get_column('id')->as_query,
+            },
+        },{
+            'join' => 'billing_mappings',
+            '+select' => [
+                'billing_mappings.id',
+                'billing_mappings.start_date',
+            ],
+            '+as' => [
+                'billing_mapping_id',
+                'billing_mapping_start_date',
+            ],
+            alias => 'me',
+        });
+
     unless($c->user->is_superuser) {
         $rs = $rs->search({
             'contact.reseller_id' => $c->user->reseller_id,
