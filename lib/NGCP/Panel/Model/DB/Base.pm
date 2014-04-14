c4a4ee2d6db058db4c3811d3009112711ec9ad1b
Irina Peshinskaya <ipeshinskaya@gmail.com>
2014-04-14 15:28:06 +0300

    MT#5879 Temporary: avoid using external module Catalyst::Model::Adaptor.
    Catalyst dependencies config should be used for this later, if it is correct way.

--- lib/NGCP/Panel/Model/DB/Base.pm
+++ lib/NGCP/Panel/Model/DB/Base.pm
@@ -1,7 +1,66 @@
 package NGCP::Panel::Model::DB::Base;
-use base 'Catalyst::Model::Adaptor';
+#use base 'Catalyst::Model::Adaptor';
+use base 'Catalyst::Model';
 use NGCP::Panel::Model::DB;
 use Moose;
+
+
+use Carp;
+use MRO::Compat;
+
+
+sub COMPONENT {
+    my ($class, $app, @rest) = @_;
+    my $arg = {};
+    if ( scalar @rest ) {
+        if ( ref($rest[0]) eq 'HASH' ) {
+            $arg = $rest[0];
+        }
+        else {
+            $arg = { @rest };
+        }
+    }
+    my $self = $class->next::method($app, $arg);
+
+    $self->_load_adapted_class;
+    return $self->_create_instance(
+        $app, $class->merge_config_hashes($class->config || {}, $arg)
+    );
+}
+
+
+sub _load_adapted_class {
+    my ($self) = @_;
+
+    croak 'need class' unless $self->{class};
+    my $adapted_class = $self->{class};
+    Catalyst::Utils::ensure_class_loaded($adapted_class);
+
+    return $adapted_class;
+}
+
+sub _create_instance {
+    my ($self, $app, $rest) = @_;
+
+    my $constructor = $self->{constructor} || 'new';
+    my $arg = $self->prepare_arguments($app, $rest);
+    my $adapted_class = $self->{class};
+
+    return $adapted_class->$constructor($self->mangle_arguments($arg));
+}
+
+sub prepare_arguments {
+    my ($self, $app, $arg) = @_;
+    return exists $self->{args} ? {
+        %{$self->{args}},
+        %$arg,
+    } : $arg;
+}
+
+sub mangle_arguments {
+    my ($self, $args) = @_;
+    return $args;
+}
 
 __PACKAGE__->config( 
     class => __PACKAGE__ ,
