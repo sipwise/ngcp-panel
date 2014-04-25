02610faa68566f37b97bf00a18b0c18a74a5c5bd
Andreas Granig <agranig@sipwise.com>
2014-04-25 14:20:23 +0200

    MT#6789 Implement email template management.
    Basic functionality to create, edit and delete email templates.

---
+++ lib/NGCP/Panel/Form/EmailTemplate/Admin.pm
@@ -0,0 +1,20 @@
+package NGCP::Panel::Form::EmailTemplate::Admin;
+
+use HTML::FormHandler::Moose;
+extends 'NGCP::Panel::Form::EmailTemplate::Reseller';
+use Moose::Util::TypeConstraints;
+
+has_field 'reseller' => (
+    type => '+NGCP::Panel::Field::Reseller',
+    validate_when_empty => 1,
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/reseller name from_email subject body/],
+);
+
+1;
+
+# vim: set tabstop=4 expandtab:
