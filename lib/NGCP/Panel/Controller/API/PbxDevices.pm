816fbcefb69c206b247cb8b274eb7729aa549c1c
Gerhard Jungwirth <gjungwirth@sipwise.com>
2014-04-29 13:29:24 +0200

    MT#6827 API pbxdevices GET
    wip

---
+++ lib/NGCP/Panel/Controller/API/PbxDevices.pm
@@ -0,0 +1,222 @@
+package NGCP::Panel::Controller::API::PbxDevices;
+use Sipwise::Base;
+use namespace::sweep;
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
+use MooseX::ClassAttribute qw(class_has);
+use NGCP::Panel::Utils::DateTime;
+BEGIN { extends 'Catalyst::Controller::ActionRole'; }
+require Catalyst::ActionRole::ACL;
+require Catalyst::ActionRole::CheckTrailingSlash;
+require Catalyst::ActionRole::HTTPMethods;
+require Catalyst::ActionRole::RequireSSL;
+
+class_has 'api_description' => (
+    is => 'ro',
+    isa => 'Str',
+    default => 
+        'TODO',
+);
+
+class_has 'query_params' => (
+    is => 'ro',
+    isa => 'ArrayRef',
+    default => sub {[
+    ]},
+);
+
+
+with 'NGCP::Panel::Role::API::PbxDevices';
+
+class_has('resource_name', is => 'ro', default => 'pbxdevices');
+class_has('dispatch_path', is => 'ro', default => '/api/pbxdevices/');
+class_has('relation', is => 'ro', default => 'http://purl.org/sipwise/ngcp-api/#rel-pbxdevices');
+
+__PACKAGE__->config(
+    action => {
+        map { $_ => {
+            ACLDetachTo => '/api/root/invalid_user',
+            AllowedRole => [qw/admin reseller/],
+            Args => 0,
+            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
+            Method => $_,
+            Path => __PACKAGE__->dispatch_path,
+        } } @{ __PACKAGE__->allowed_methods },
+    },
+    action_roles => [qw(HTTPMethods)],
+);
+
+sub auto :Private {
+    my ($self, $c) = @_;
+
+    $self->set_body($c);
+    $self->log_request($c);
+    return 1;
+}
+
+sub GET :Allow {
+    my ($self, $c) = @_;
+    my $page = $c->request->params->{page} // 1;
+    my $rows = $c->request->params->{rows} // 10;
+    {
+        my $field_devs = $self->item_rs($c);
+
+        my $total_count = int($field_devs->count);
+        $field_devs = $field_devs->search(undef, {
+            page => $page,
+            rows => $rows,
+        });
+        my (@embedded, @links);
+        for my $dev ($field_devs->search({}, {order_by => {-asc => 'me.id'}})->all) {
+            push @embedded, $self->hal_from_item($c, $dev);
+            push @links, Data::HAL::Link->new(
+                relation => 'ngcp:'.$self->resource_name,
+                href     => sprintf('%s%d', $self->dispatch_path, $dev->id),
+            );
+        }
+        push @links,
+            Data::HAL::Link->new(
+                relation => 'curies',
+                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
+                name => 'ngcp',
+                templated => true,
+            ),
+            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
+            Data::HAL::Link->new(relation => 'self', href => sprintf('%s?page=%s&rows=%s', $self->dispatch_path, $page, $rows));
+        if(($total_count / $rows) > $page ) {
+            push @links, Data::HAL::Link->new(relation => 'next', href => sprintf('%s?page=%d&rows=%d', $self->dispatch_path, $page + 1, $rows));
+        }
+        if($page > 1) {
+            push @links, Data::HAL::Link->new(relation => 'prev', href => sprintf('%s?page=%d&rows=%d', $self->dispatch_path, $page - 1, $rows));
+        }
+
+        my $hal = Data::HAL->new(
+            embedded => [@embedded],
+            links => [@links],
+        );
+        $hal->resource({
+            total_count => $total_count,
+        });
+        my $response = HTTP::Response->new(HTTP_OK, undef, 
+            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
+        $c->response->headers($response->headers);
+        $c->response->body($response->content);
+        return;
+    }
+    return;
+}
+
+sub HEAD :Allow {
+    my ($self, $c) = @_;
+    $c->forward(qw(GET));
+    $c->response->body(q());
+    return;
+}
+
+sub OPTIONS :Allow {
+    my ($self, $c) = @_;
+    my $allowed_methods = $self->allowed_methods;
+    $c->response->headers(HTTP::Headers->new(
+        Allow => $allowed_methods->join(', '),
+        Accept_Post => 'application/hal+json; profile=http://purl.org/sipwise/ngcp-api/#rel-'.$self->resource_name,
+    ));
+    $c->response->content_type('application/json');
+    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
+    return;
+}
+
+sub POST :Allow {
+    my ($self, $c) = @_;
+
+    my $guard = $c->model('DB')->txn_scope_guard;
+    {
+        my $schema = $c->model('DB');
+        my $resource = $self->get_valid_post_data(
+            c => $c,
+            media_type => 'application/json',
+        );
+        last unless $resource;
+
+        my $form = $self->get_form($c);
+        last unless $self->validate_form(
+            c => $c,
+            resource => $resource,
+            form => $form,
+        );
+
+        my $dset;
+
+        unless(defined $resource->{subscriber_id}) {
+            $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Required: 'subscriber_id'");
+            last;
+        }
+
+        my $b_subscriber = $schema->resultset('voip_subscribers')->find({
+                id => $resource->{subscriber_id},
+            });
+        unless($b_subscriber) {
+            $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid 'subscriber_id'.");
+            last;
+        }
+        my $subscriber = $b_subscriber->provisioning_voip_subscriber;
+        unless($subscriber) {
+            $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid subscriber.");
+            last;
+        }
+        if (! exists $resource->{destinations} ) {
+            $resource->{destinations} = [];
+        }
+        if (ref $resource->{destinations} ne "ARRAY") {
+            $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid field 'destinations'. Must be an array.");
+            last;
+        }
+        try {
+            my $primary_nr_rs = $b_subscriber->primary_number;
+            my $number;
+            if ($primary_nr_rs) {
+                $number = $primary_nr_rs->cc . ($primary_nr_rs->ac //'') . $primary_nr_rs->sn;
+            } else {
+                $number = ''
+            }
+            my $domain = $subscriber->domain->domain // '';
+
+            $dset = $schema->resultset('voip_cf_destination_sets')->create({
+                    name => $resource->{name},
+                    subscriber_id => $subscriber->id,
+                });
+            for my $d ( @{$resource->{destinations}} ) {
+                delete $d->{destination_set_id};
+                $d->{destination} = NGCP::Panel::Utils::Subscriber::field_to_destination(
+                        destination => $d->{destination},
+                        number => $number,
+                        domain => $domain,
+                        uri => $d->{destination},
+                    );
+                $dset->create_related("voip_cf_destinations", $d);
+            }
+        } catch($e) {
+            $c->log->error("failed to create cfdestinationset: $e");
+            $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to create cfdestinationset.");
+            last;
+        }
+
+        $guard->commit;
+
+        $c->response->status(HTTP_CREATED);
+        $c->response->header(Location => sprintf('/%s%d', $c->request->path, $dset->id));
+        $c->response->body(q());
+    }
+    return;
+}
+
+sub end : Private {
+    my ($self, $c) = @_;
+
+    $self->log_response($c);
+    return 1;
+}
+
+# vim: set tabstop=4 expandtab:
---
+++ lib/NGCP/Panel/Role/API/PbxDevices.pm
@@ -0,0 +1,160 @@
+package NGCP::Panel::Role::API::PbxDevices;
+use Moose::Role;
+use Sipwise::Base;
+with 'NGCP::Panel::Role::API' => {
+    -alias       =>{ item_rs  => '_item_rs', },
+    -excludes    => [ 'item_rs' ],
+};
+
+use boolean qw(true);
+use TryCatch;
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Status qw(:constants);
+use JSON::Types;
+
+sub get_form {
+    my ($self, $c) = @_;
+    return NGCP::Panel::Form::Customer::PbxFieldDevice->new;
+}
+
+sub hal_from_item {
+    my ($self, $c, $item) = @_;
+    my $form;
+    my $type = 'pbxdevices';
+
+    my %resource = $item->get_inflated_columns;
+    my @lines;
+    for my $line ($item->autoprov_field_device_lines->all) {
+        my $p_subs = $line->provisioning_voip_subscriber;
+        my $b_subs = $p_subs ? $p_subs->voip_subscriber : undef;
+
+        push @lines, {
+            ($line->get_inflated_columns),
+            $b_subs ? (subscriber_id => $b_subs->id) : (),
+        };
+    }
+
+    my $hal = Data::HAL->new(
+        links => [
+            Data::HAL::Link->new(
+                relation => 'curies',
+                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
+                name => 'ngcp',
+                templated => true,
+            ),
+            Data::HAL::Link->new(relation => 'collection', href => sprintf("%s", $self->dispatch_path)),
+            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
+            Data::HAL::Link->new(relation => 'self', href => sprintf("%s%d", $self->dispatch_path, $item->id)),
+            Data::HAL::Link->new(relation => "ngcp:$type", href => sprintf("/api/%s/%d", $type, $item->id)),
+            Data::HAL::Link->new(relation => 'ngcp:pbxdeviceprofiles', href => sprintf("/api/pbxdeviceprofiles/%d", $item->profile_id)),
+            Data::HAL::Link->new(relation => 'ngcp:customers', href => sprintf("/api/customers/%d", $item->contract_id)),
+        ],
+        relation => 'ngcp:'.$self->resource_name,
+    );
+
+#    $form //= $self->get_form($c);
+#    return unless $self->validate_form(
+#        c => $c,
+#        form => $form,
+#        resource => \%resource,
+#        run => 0,
+#    );
+    $resource{lines} = \@lines;
+    $hal->resource(\%resource);
+    return $hal;
+}
+
+sub item_rs {
+    my ($self, $c) = @_;
+    my $item_rs = $c->model('DB')->resultset('autoprov_field_devices');
+
+    if($c->user->roles eq "admin") {
+    } elsif ($c->user->roles eq "reseller") {
+        $item_rs = $item_rs->search({ reseller_id => $c->user->reseller_id });
+    }
+
+    return $item_rs;
+}
+
+sub item_by_id {
+    my ($self, $c, $id) = @_;
+
+    my $item_rs = $self->item_rs($c);
+    return $item_rs->find($id);
+}
+
+sub update_item {
+    my ($self, $c, $item, $old_resource, $resource, $form) = @_;
+
+    delete $resource->{id};
+    my $schema = $c->model('DB');
+
+    return unless $self->validate_form(
+        c => $c,
+        form => $form,
+        resource => $resource,
+    );
+
+    if (! exists $resource->{destinations} ) {
+        $resource->{destinations} = [];
+    }
+    if (ref $resource->{destinations} ne "ARRAY") {
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid field 'destinations'. Must be an array.");
+        return;
+    }
+    for my $d (@{ $resource->{destinations} }) {
+        if (exists $d->{timeout} && ! $d->{timeout}->is_integer) {
+            $c->log->error("Invalid field 'timeout'.");
+            $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid field 'timeout'.");
+            return;
+        }
+    }
+
+    my $b_subscriber = $schema->resultset('voip_subscribers')->find($resource->{subscriber_id});
+    unless ($b_subscriber) {
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid 'subscriber_id'.");
+        return;
+    }
+    my $subscriber = $b_subscriber->provisioning_voip_subscriber;
+    unless($subscriber) {
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid subscriber.");
+        last;
+    }
+
+    try {
+        my $primary_nr_rs = $b_subscriber->primary_number;
+        my $number;
+        if ($primary_nr_rs) {
+            $number = $primary_nr_rs->cc . ($primary_nr_rs->ac //'') . $primary_nr_rs->sn;
+        } else {
+            $number = ''
+        }
+        my $domain = $subscriber->domain->domain // '';
+
+        $item->update({
+                name => $resource->{name},
+                subscriber_id => $subscriber->id,
+            })->discard_changes;
+        $item->voip_cf_destinations->delete;
+        for my $d ( @{$resource->{destinations}} ) {
+            delete $d->{destination_set_id};
+            $d->{destination} = NGCP::Panel::Utils::Subscriber::field_to_destination(
+                    destination => $d->{destination},
+                    number => $number,
+                    domain => $domain,
+                    uri => $d->{destination},
+                );
+            $item->create_related("voip_cf_destinations", $d);
+        }
+    } catch($e) {
+        $c->log->error("failed to create cfdestinationset: $e");
+        $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to create cfdestinationset.");
+        return;
+    };
+
+    return $item;
+}
+
+1;
+# vim: set tabstop=4 expandtab:
