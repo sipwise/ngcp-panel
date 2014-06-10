e0bb62f82bf09c24f6ed5dfd002ce7111479ed15
Andreas Granig <agranig@sipwise.com>
2014-06-10 15:21:36 +0200

    MT#7323 API: fetch sound handles via REST

---
+++ lib/NGCP/Panel/Form/Sound/HandleAPI.pm
@@ -0,0 +1,33 @@
+package NGCP::Panel::Form::Sound::HandleAPI;
+
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+use Moose::Util::TypeConstraints;
+
+use HTML::FormHandler::Widget::Block::Bootstrap;
+
+has '+enctype' => ( default => 'multipart/form-data');
+has '+widget_wrapper' => ( default => 'Bootstrap' );
+has_field 'submitid' => ( type => 'Hidden' );
+
+has_field 'handle' => (
+    type => 'Text',
+    required => 1,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The sound handle.'],
+    },
+);
+
+has_field 'group' => (
+    type => 'Text',
+    required => 1,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The group this handle belongs to.'],
+    },
+);
+
+1;
+
+# vim: set tabstop=4 expandtab:
---
+++ lib/NGCP/Panel/Role/API/SoundHandles.pm
@@ -0,0 +1,72 @@
+package NGCP::Panel::Role::API::SoundHandles;
+use Moose::Role;
+use Sipwise::Base;
+with 'NGCP::Panel::Role::API' => {
+    -alias       =>{ item_rs  => '_item_rs', },
+    -excludes    => [ 'item_rs' ],
+};
+
+use boolean qw(true);
+use TryCatch;
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Status qw(:constants);
+use NGCP::Panel::Form::Sound::HandleAPI;
+
+sub item_rs {
+    my ($self, $c) = @_;
+
+    my $item_rs = $c->model('DB')->resultset('voip_sound_handles')->search({}, {
+        select => [qw/me.id me.name group.name/],
+        as => [qw/id handle group/],
+        join => 'group',
+    });
+    return $item_rs;
+}
+
+sub get_form {
+    my ($self, $c) = @_;
+    return NGCP::Panel::Form::Sound::HandleAPI->new(ctx => $c);
+}
+
+sub item_by_id {
+    my ($self, $c, $id) = @_;
+    my $item_rs = $self->item_rs($c);
+    return $item_rs->find($id);
+}
+
+sub hal_from_item {
+    my ($self, $c, $item, $form) = @_;
+    my %resource = $item->get_inflated_columns;
+
+    my $hal = Data::HAL->new(
+        links => [
+            Data::HAL::Link->new(
+                relation => 'curies',
+                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
+                name => 'ngcp',
+                templated => true,
+            ),
+            Data::HAL::Link->new(relation => 'collection', href => sprintf("/api/%s/", $self->resource_name)),
+            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
+            Data::HAL::Link->new(relation => 'self', href => sprintf("%s%d", $self->dispatch_path, $item->id)),
+        ],
+        relation => 'ngcp:'.$self->resource_name,
+    );
+
+    $form //= $self->get_form($c);
+
+    $self->validate_form(
+        c => $c,
+        resource => \%resource,
+        form => $form,
+        run => 0,
+    );
+
+    $resource{id} = int($item->id);
+    $hal->resource({%resource});
+    return $hal;
+}
+
+1;
+# vim: set tabstop=4 expandtab:
