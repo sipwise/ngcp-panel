1c0a2bfa2f6485cacddf612abcab0cdadb61ed89
Andreas Granig <agranig@sipwise.com>
2014-07-21 14:10:37 +0200

    Fix positioning inside figure container.
    Use absolute positioning within relative placed container, so
    put annotations inside container instead of appending them to
    the body.

--- sandbox/annotate.js
+++ sandbox/annotate.js
@@ -1,6 +1,8 @@
 (function( $ ) {
 
-  $.fn.annotate = function(el) {
+  $.fn.annotate = function(ctx) {
+
+    console.log("ctx is ", ctx);
 
     var $annotate,
       $body = $('body'),
@@ -13,29 +15,29 @@
       if(pos == undefined) pos = "top";
       console.log("pos at " + i + " is ", pos);
 
-      var $annotate = $('<div class="annotate ' + $el.attr("class") + '" data-annotate="' + i + '">' + $el.html() + '<div class="arrow-' + pos + '"></div></div>').appendTo("body");
+      var $annotate = $('<div class="annotate ' + $el.attr("class") + '" data-annotate="' + i + '">' + $el.html() + '<div class="arrow-' + pos + '"></div></div>');
+      $(ctx).append($annotate);
+
       $el.html('');
 
       var linkPosition = $el.position();
-      console.log(linkPosition);
-      console.log("w="+$annotate.outerWidth()+",h="+$annotate.outerHeight());
       var top, left;
       switch(pos) {
       	case "top":
-          top = linkPosition.top + 10 - $annotate.outerHeight();
-          left = linkPosition.left + 48 - $annotate.outerWidth()/2;
+          top = linkPosition.top - 5 - $annotate.outerHeight();
+          left = linkPosition.left - 9 - $annotate.outerWidth()/2;
 	  break;
       	case "bottom":
-          top = linkPosition.top + 3 + $annotate.outerHeight();
-          left = linkPosition.left + 48 - $annotate.outerWidth()/2;
+          top = linkPosition.top - 11 + $annotate.outerHeight();
+          left = linkPosition.left - 9 - $annotate.outerWidth()/2;
 	  break;
       	case "left":
-          top = linkPosition.top + 17 - ($annotate.outerHeight()/2);
-          left = linkPosition.left + 41 - $annotate.outerWidth();
+          top = linkPosition.top - ($annotate.outerHeight()/2);
+          left = linkPosition.left - 25 - $annotate.outerWidth();
 	  break;
       	case "right":
-          top = linkPosition.top + 17 - ($annotate.outerHeight()/2);
-          left = linkPosition.left + 55;
+          top = linkPosition.top - ($annotate.outerHeight()/2);
+          left = linkPosition.left + 5;
 	  break;
       }
       $annotate.css({
