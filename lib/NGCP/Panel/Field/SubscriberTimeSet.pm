c288771b2114492a47438a4cb8a9282adeba2f47
Andreas Granig <agranig@sipwise.com>
2014-09-25 15:56:21 +0200

    MT#9237 Fix editing advanced CF map with n>1 recs
    (cherry picked from commit f2a00bad7823156ca6407e66aa07529df6d4f104)

--- lib/NGCP/Panel/Field/SubscriberTimeSet.pm
+++ lib/NGCP/Panel/Field/SubscriberTimeSet.pm
@@ -8,24 +8,17 @@ sub build_options {
     my $form = $self->form;
     return [] unless $form->ctx;
 
-    my $active_time_set = $form->ctx->stash->{cf_active_time_set};
     my $time_sets = $form->ctx->stash->{cf_time_sets};
-
     my @all;
     return \@all unless($time_sets);
+
     push @all, { label => '<always>', value => undef};
     foreach my $set($time_sets->all) {
         my $entry = {};
         $entry->{label} = $set->name;
         $entry->{value} = $set->id;
-
-        if($active_time_set && 
-           $set->id == $active_time_set->id) {
-            $entry->{selected} = 1;
-        }
         push @all, $entry;
     }
-
     return \@all;
 }
 
