70784411090f614a197da8e7051d95c22a17958f
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2014-09-25 14:00:36 +0300

    Vsyakoy tvari po pare. Before cleaning.

--- share/tools/mech1.pl
+++ share/tools/mech1.pl
@@ -23,20 +23,20 @@ my $cfg = {
     }
 };
 
-#$dbh->do('drop table urltemplate');
-#$dbh->do('drop table url');
-#$dbh->do('drop table urlcontent');
-#$dbh->do('drop table urlvariant');
-#$dbh->do('drop table control');
-#$dbh->do('drop table url_control');
-#$dbh->do('drop table url_template');
-#$dbh->do('create table urltemplate(id integer(11) auto_increment primary key, template varchar(512))');
-#$dbh->do('create table url(id integer(11) unsigned not null auto_increment primary key, url varchar(512), urltemplate_id integer(11), lastvisit timestamp default 0)');
-#$dbh->do('create table urlvariant(id integer(11) unsigned not null, url varchar(512), container_url_id integer(11))');
-#$dbh->do('create table urlcontent(id integer(11) unsigned not null, content text)');
-#$dbh->do('create table control(id integer(11) unsigned not null auto_increment primary key, label text, goto_url_id integer(11) unsigned)');
-#$dbh->do('create table url_control(url_id integer(11) unsigned, control_id integer(11) unsigned)');
-#$dbh->do('create table url_template(url_id integer(11) unsigned, urltemplate_id integer(11) unsigned)');
+$dbh->do('drop table urltemplate');
+$dbh->do('drop table url');
+$dbh->do('drop table urlcontent');
+$dbh->do('drop table urlvariant');
+$dbh->do('drop table control');
+$dbh->do('drop table url_control');
+$dbh->do('drop table url_template');
+$dbh->do('create table urltemplate(id integer(11) auto_increment primary key, template varchar(512))');
+$dbh->do('create table url(id integer(11) unsigned not null auto_increment primary key, url varchar(512), urltemplate_id integer(11), lastvisit timestamp default 0)');
+$dbh->do('create table urlvariant(id integer(11) unsigned not null, url varchar(512), container_url_id integer(11))');
+$dbh->do('create table urlcontent(id integer(11) unsigned not null, content text)');
+$dbh->do('create table control(id integer(11) unsigned not null auto_increment primary key, label text, goto_url_id integer(11) unsigned)');
+$dbh->do('create table url_control(url_id integer(11) unsigned, control_id integer(11) unsigned)');
+$dbh->do('create table url_template(url_id integer(11) unsigned, urltemplate_id integer(11) unsigned)');
 
 # Xvfb :99 &
 # DISPLAY=:99 firefox --display=:99 &
@@ -45,8 +45,17 @@ my $cfg = {
 # DISPLAY=:99 ~/fillform.pl 
 
 
-our $host = 'https://192.168.56.7:1444';
+our $host = 'http://192.168.56.7:1444';
 
+
+#        $mech->get_local("./test.html");
+#        #my $contentDiv = $mech->xpath('//div[@id="content"]', single => 1);
+#        my $contentDiv = $mech->xpath('//div[@id="testrow"]', single => 1);
+#        #my @links = $mech->find_link_dom( node => $contentDiv, n => 'all' );
+#        my @links = $mech->find_all_links_dom( node => $contentDiv );
+#        print $#links;
+#        die();
+ 
 
 get_data($mech,$host.'/dashboard');
 #while (my $url = $dbh->selectrow_array('select url from url where unix_timestamp(lastvisit) < ? limit 1', undef, $datetime->epoch)){
@@ -101,11 +110,14 @@ sub get_data{
         #foreach my $link ( $mech->links()  ) {
         #foreach my $link ( $mech->find_all_links_dom()  ) {
         #my $repl = $mech->repl();
-        my $contentDiv = $mech->xpath('//div[@id="content"]', single => 1);
-        my @links = $mech->find_all_links_dom( node => $contentDiv );
-        #my @links = $mech->find_all_links_dom( );
-        print $#links;
-        die();
+        my $contentDiv;
+        eval{
+            $contentDiv = $mech->xpath('//div[@id="content"]', single => 1);
+        };
+        my @links = $mech->find_all_links_dom( $contentDiv ? ( node => $contentDiv )  : () );
+        ##my @links = $mech->find_all_links_dom( );
+        #print $#links;
+        #die();
         foreach my $link ( @links ) {
             if(!check_url_toadd($link->{href})){
                 next;
@@ -144,9 +156,10 @@ sub get_data{
                 if('link_timed_out' eq $@){
                     print "link hanged\n";
                 }else{
-                    #get_data($mechRecursion,$goto_url->{url},$url,1);
                     print "get_data\n";
-                    get_data($mech,$goto_url->{url},$url,0);
+                    get_data($mech,$goto_url->{url},$url,1);
+                    #get_data($mechRecursion,$goto_url->{url},$url,1);
+                    #get_data($mech,$goto_url->{url},$url,0);
                     eval {
                         alarm (10);
                         $mech->back();
