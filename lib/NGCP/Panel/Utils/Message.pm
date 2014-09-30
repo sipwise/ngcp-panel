71eadae6ead292fe937eb1f9ecb2ec825f711b26
Kirill Solomko <ksolomko@sipwise.com>
2014-09-30 17:00:38 +0200

    MT#7705 - Message.pm further edited the pod doc

--- lib/NGCP/Panel/Utils/Message.pm
+++ lib/NGCP/Panel/Utils/Message.pm
@@ -285,7 +285,7 @@ Params:
              (by default $c->request->params is used for the data source)
     $cname - Custom function name for CALLED= e.g. Controller::Domain::create
              as the caller and if you need to something else
-             $cname=custom_create leads to
+             $cname => 'custom_create' leads to
              CALLED=Controller:Domain::custom_create in the log
 
 =head3 C<info>
@@ -297,7 +297,6 @@ Params:
     $c (required)
     $desc (required) - main log message (will apear on the GUI).
     $log   - additional information that will be added to LOG= only (no GUI).
-    $error - error log message, will be parsed and may also appear on the GUI
     $type  - 'panel' by default, means the message will go to both the log and the GUI,
              anything else but 'panel' is written only to the log.
              Expandable with new types if neccessary.
@@ -305,7 +304,7 @@ Params:
              (by default $c->request->params is used for the data source)
     $cname - Custom function name for CALLED= e.g. Controller::Domain::create
              as the caller and if you need to something else
-             $cname=custom_create leads to
+             $cname => 'custom_create' leads to
              CALLED=Controller:Domain::custom_create in the log
 
 =head1 AUTHOR
