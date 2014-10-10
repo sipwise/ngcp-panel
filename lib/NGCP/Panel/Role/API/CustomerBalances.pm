b9aa3be4ff136a3d7e62f6a6f76aa90d976e454c
Andreas Granig <agranig@sipwise.com>
2014-10-10 13:18:25 +0200

    MT#9481 API: fix customerbalance link to self

--- lib/NGCP/Panel/Role/API/CustomerBalances.pm
+++ lib/NGCP/Panel/Role/API/CustomerBalances.pm
@@ -51,7 +51,7 @@ sub hal_from_item {
             ),
             Data::HAL::Link->new(relation => 'collection', href => sprintf("/api/%s/", $self->resource_name)),
             Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            Data::HAL::Link->new(relation => 'self', href => sprintf("%s%d", $self->dispatch_path, $item->id)),
+            Data::HAL::Link->new(relation => 'self', href => sprintf("%s%d", $self->dispatch_path, $item->contract->id)),
             Data::HAL::Link->new(relation => 'ngcp:customers', href => sprintf("/api/customers/%d", $item->contract->id)),
         ],
         relation => 'ngcp:'.$self->resource_name,
