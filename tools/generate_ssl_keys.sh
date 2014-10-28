264a09911300dae2408def9893a5c40237acfb7f
Victor Seva <vseva@sipwise.com>
2014-10-28 17:50:48 +0100

    MT#9547 generate_ssl_keys.sh: add third option to select the name of the certs produced
    Change-Id: I2f842376ad0eb904354879b39af24285ffde438a

--- tools/generate_ssl_keys.sh
+++ tools/generate_ssl_keys.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 DEST=${1:-/etc/ngcp-panel/api_ssl}
 BASE=${2:-/usr/share/ngcp-panel-tools}
+FILE=${3:-api_ca}
 
 mkdir -p ${DEST}
 
 /usr/bin/openssl req -x509 -config ${BASE}/opensslcnf.cnf \
-	-newkey rsa:4096 -keyout ${DEST}/api_ca.key -out ${DEST}/api_ca.crt \
+	-newkey rsa:4096 -keyout ${DEST}/${FILE}.key -out ${DEST}/${FILE}.crt \
 	-days 999 -nodes -batch
