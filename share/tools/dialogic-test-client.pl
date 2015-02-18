693ea5a8c7772dc35556829b47fce5627582efc3
Gerhard Jungwirth <gjungwirth@sipwise.com>
2015-02-18 14:40:10 +0100

    MT#11501 dialogic API formatting, more config
    Change-Id: I29bb77410cb79f3e89664a641e03509382754f6c

--- share/tools/dialogic-test-client.pl
+++ share/tools/dialogic-test-client.pl
@@ -17,7 +17,7 @@ my $test = NGCP::Panel::Utils::DialogicImg->new(
 	},
 );
 
-p $test;
+#p $test;
 
 p $test->login( 'dialogic', 'Dial0gic' );
 
@@ -25,25 +25,15 @@ my $resp
     = $test->get( '/oamp/configuration/objects', { appid => $test->appid } );
 
 $resp = $test->obtain_lock();
-p $resp;
+p $resp->code;
 
 print "LOGGED IN, LOCK OBTAINED ############################\n";
 
-# $resp
-#     = $test->get(
-#     '/oamp/configuration/objects/Node/NULL?detaillevel=1&pid=10000&appid='
-#         . $test->appid );
-# p $resp;
-# #p $resp->data;
 
-# print "GOT DATA, SEND NOW ###################################\n";
 
-# $resp
-#     = $test->post(
-#     '/oamp/configuration/objects/Node/NULL?detaillevel=1&pid=10000&appid='
-#         . $test->appid, $resp->data );
+# $resp = $test->reboot_and_wait;
 # p $resp;
-# #p $resp->data;
+# exit 0;
 
 $resp = $test->create_bn2020;
 p $resp->code;
@@ -69,18 +59,56 @@ p $resp->code;
 #p $resp->data;
 
 $resp = $test->create_ip_address({
-		NIIPAddress => '10.2.3.4',
+		NIIPAddress => '11.2.3.4',
 		NIIPPhy => 'Services',
 	});
 p $resp->code;
 #p $resp->data;
 
-$resp = $test->create_interface;  # DataA
+$resp = $test->create_interface;  # DataA by default
 p $resp->code;
 #p $resp->data;
 
-$resp = $test->create_ip_address({NIIPAddress => '10.6.7.8',
+$resp = $test->create_ip_address({NIIPAddress => '11.6.7.8',
 	NIIPPhy => 'Media 0'});
+p $resp->code;
+#p $resp->data;
+
+print "CREATE FACILITY ###################################\n";
+
+$resp = $test->create_facility;
+p $resp->code;
+#p $resp->data;
+
+print "CREATE PACKET FACILITY COLLECTION ###################################\n";
+
+$resp = $test->create_packet_facility_collection;
+p $resp->code;
+#p $resp->data;
+
+print "CREATE PACKET FACILITY ###################################\n";
+
+$resp = $test->create_packet_facility({
+	ChannelCount => 5, # our licence has 128 or so
+	});
+p $resp->code;
+#p $resp->data;
+
+print "CREATE SIGNALING ###################################\n";
+
+$resp = $test->create_signaling({});
+p $resp->code;
+#p $resp->data;
+
+print "CREATE SIP ###################################\n";
+
+$resp = $test->create_sip;
+p $resp->code;
+#p $resp->data;
+
+print "CREATE SIP IP ###################################\n";
+
+$resp = $test->create_sip_ip;
 p $resp->code;
 #p $resp->data;
 
