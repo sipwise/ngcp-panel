485832a6859e9b4248aeaeba76735b7491ffcfd8
Andreas Granig <agranig@sipwise.com>
2015-02-25 11:00:41 +0100

    MT#11741 API: fix pbxdevicemodelimages props doc
    Change-Id: Iae529c693f7f909e568083b6983b4a792b76b8f0
    (cherry picked from commit bc32830ad9c9affd522d607e9f77dcf5ceba4b58)

---
+++ lib/NGCP/Panel/Role/API/PbxDeviceModelImages.pm
@@ -0,0 +1,11 @@
+package NGCP::Panel::Role::API::PbxDeviceModelImages;
+use Moose::Role;
+use Sipwise::Base;
+
+sub get_form {
+    my ($self, $c) = @_;
+    return;
+}
+
+1;
+# vim: set tabstop=4 expandtab:
