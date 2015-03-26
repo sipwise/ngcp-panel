4525b0cedda44d6690d19f001e0ca261df7b065f
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2015-03-26 20:29:03 +0200

    MT#11147 Create fake data for the devicemodels and pbxdevices tests
    Change-Id: I782bd536fe6dc18d35857519f463329fba0c9da2
    (cherry picked from commit 11979e04adb683b97807e0f79e37fb27ee9e6ac4)
    
    Conflicts:
    	t/Test/Collection.pm

--- lib/NGCP/Panel/Controller/API/SubscriberProfileSets.pm
+++ lib/NGCP/Panel/Controller/API/SubscriberProfileSets.pm
@@ -38,6 +38,17 @@ class_has 'query_params' => (
                 second => sub {},
             },
         },
+        {
+            param => 'name',
+            description => 'Filter for profile sets with a specific name',
+            query => {
+                first => sub {
+                    my $q = shift;
+                    { 'me.name' => { like => $q } };
+                },
+                second => sub {},
+            },
+        },
     ]},
 );
 
