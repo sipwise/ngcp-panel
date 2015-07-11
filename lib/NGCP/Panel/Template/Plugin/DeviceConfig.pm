2c71698c8de3b00b592127daab8659f2f6ff3443
Andreas Granig <agranig@sipwise.com>
2015-07-11 13:55:44 +0200

    MT#13919 Move raw perl from dev config to plugin
    Change-Id: I7c52c36bbf59b2229b960f977563c0bd089278ac
    (cherry picked from commit b003cea498539a47231bff7c181595b696ab36f2)

---
+++ lib/NGCP/Panel/Template/Plugin/DeviceConfig.pm
@@ -0,0 +1,31 @@
+package NGCP::Panel::Template::Plugin::DeviceConfig;
+use base 'Template::Plugin';
+use Crypt::RC4;
+
+sub new {
+	my ($class, $context) = @_;
+
+	# set error via base ::error()
+	# return $class->error("foo") if($foo)
+
+	bless {
+		_CONTEXT => $context,
+	}, $class;	
+}
+
+sub innovaphone_pwdgen {
+	my ($self, $user, $pass, $plain) = @_;
+	my $key = $user;
+	my $pad = 16 - (length $user) % 16;
+	$key .= "\0" x $pad;
+	$key .= $pass;
+	$pad = 16 - (length $pass) % 16;
+	$key .= "\0" x $pad;
+	my $rc4 = Crypt::RC4->new($key);
+	my $cipher = $rc4->RC4($plain);
+	my $hexcipher = unpack("H*", $cipher);
+	return $hexcipher;	
+}
+
+
+1;
