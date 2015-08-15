1ab9051ba52d4df594c5f0266f508574a15a46b8
Kirill Solomko <ksolomko@sipwise.com>
2015-08-15 18:45:54 +0200

    MT#12707 - add "malicious calls" feature
    Change-Id: I53138d7ba591f8e138712eb7e6252007fc20d671

---
+++ lib/NGCP/Panel/Form/MaliciousCall/Admin.pm
@@ -0,0 +1,20 @@
+package NGCP::Panel::Form::MaliciousCall::Admin;
+
+use HTML::FormHandler::Moose;
+extends 'NGCP::Panel::Form::MaliciousCall::Reseller';
+use Moose::Util::TypeConstraints;
+
+has_field 'reseller' => (
+    type => '+NGCP::Panel::Field::Reseller',
+    validate_when_empty => 1,
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/reseller call_id caller callee start_time duration reported_at/],
+);
+
+1;
+
+# vim: set tabstop=4 expandtab:
