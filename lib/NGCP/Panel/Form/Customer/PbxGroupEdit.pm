b6004d594642f90ff0ec8b52dadd5fcf3537eda7
Gerhard Jungwirth <gjungwirth@sipwise.com>
2015-10-05 19:05:45 +0200

    MT#15203 Allow adding alias numbers to pbx group
    Change-Id: If1fa5574d28cd18df1790e0dd2eccff76b255894
    (cherry picked from commit 8c45fc5a7c48a132e352534a13ffbe823e763406)

--- lib/NGCP/Panel/Form/Customer/PbxGroupEdit.pm
+++ lib/NGCP/Panel/Form/Customer/PbxGroupEdit.pm
@@ -3,25 +3,29 @@ package NGCP::Panel::Form::Customer::PbxGroupEdit;
 use HTML::FormHandler::Moose;
 extends 'NGCP::Panel::Form::Customer::PbxGroupBase';
 
-has_field 'pbx_extension' => (
-    type => 'Text',
-    required => 1,
-    label => 'Extension',
+has_field 'alias_number' => (
+    type => '+NGCP::Panel::Field::AliasNumber',
+    setup_for_js => 1,
+    do_wrapper => 1,
+    do_label => 0,
+    tags => {
+        controls_div => 1,
+    },
+    wrapper_class => [qw/hfh-rep/],
+);
+
+has_field 'alias_number_add' => (
+    type => 'AddElement',
+    repeatable => 'alias_number',
+    value => 'Add another number',
+    element_class => [qw/btn btn-primary pull-right/],
 );
 
 has_block 'fields' => (
     tag => 'div',
     class => [qw/modal-body/],
-    render_list => [qw/pbx_extension pbx_hunt_policy pbx_hunt_timeout/],
+    render_list => [qw/pbx_extension pbx_hunt_policy pbx_hunt_timeout alias_number alias_number_add/],
 );
-
-sub validate_username {
-    my ($self, $field) = @_;
-
-    unless($field->value =~ /^[a-zA-Z0-9_\-]+$/) {
-        $field->add_error("Invalid group name, must only contain letters, digits, - and _");
-    }
-}
 
 1;
 # vim: set tabstop=4 expandtab:
