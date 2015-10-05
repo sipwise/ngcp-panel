873ba2e5f77a7368c7c7b5be7e52f64093aab1a2
Gerhard Jungwirth <gjungwirth@sipwise.com>
2015-10-05 19:05:45 +0200

    MT#15203 wip alias to pbx group
    Change-Id: If1fa5574d28cd18df1790e0dd2eccff76b255894

--- lib/NGCP/Panel/Form/Customer/PbxGroupBase.pm
+++ lib/NGCP/Panel/Form/Customer/PbxGroupBase.pm
@@ -5,12 +5,19 @@ extends 'HTML::FormHandler';
 use Moose::Util::TypeConstraints;
 
 use HTML::FormHandler::Widget::Block::Bootstrap;
-use NGCP::Panel::Field::PosInteger;
 
 has '+widget_wrapper' => ( default => 'Bootstrap' );
 has_field 'submitid' => ( type => 'Hidden' );
 sub build_render_list {[qw/submitid fields actions/]}
-sub build_form_element_class {[qw(form-horizontal)]}
+sub build_form_element_class { [qw(form-horizontal)] }
+
+with 'NGCP::Panel::Render::RepeatableJs';
+
+has_field 'pbx_extension' => (
+    type => 'Text',
+    required => 1,
+    label => 'Extension',
+);
 
 has_field 'pbx_hunt_policy' => (
     type => 'Select',
@@ -32,6 +39,24 @@ has_field 'pbx_hunt_timeout' => (
     default => 10,
 );
 
+has_field 'alias_number' => (
+    type => '+NGCP::Panel::Field::AliasNumber',
+    setup_for_js => 1,
+    do_wrapper => 1,
+    do_label => 0,
+    tags => {
+        controls_div => 1,
+    },
+    wrapper_class => [qw/hfh-rep/],
+);
+
+has_field 'alias_number_add' => (
+    type => 'AddElement',
+    repeatable => 'alias_number',
+    value => 'Add another number',
+    element_class => [qw/btn btn-primary pull-right/],
+);
+
 has_field 'save' => (
     type => 'Submit',
     value => 'Save',
@@ -42,7 +67,7 @@ has_field 'save' => (
 has_block 'fields' => (
     tag => 'div',
     class => [qw/modal-body/],
-    render_list => [qw/pbx_hunt_policy pbx_hunt_timeout/],
+    render_list => [qw/pbx_extension pbx_hunt_policy pbx_hunt_timeout alias_number alias_number_add/],
 );
 
 has_block 'actions' => (
