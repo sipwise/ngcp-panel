e14432e7e97682be6acdd9baa84f77cde0948fe4
Gerhard Jungwirth <gjungwirth@sipwise.com>
2015-10-05 19:05:45 +0200

    MT#15203 Allow adding alias numbers to pbx group
    Conflicts:
            lib/NGCP/Panel/Controller/Customer.pm
    
    Change-Id: If1fa5574d28cd18df1790e0dd2eccff76b255894
    (cherry picked from commit 8c45fc5a7c48a132e352534a13ffbe823e763406)

--- lib/NGCP/Panel/Form/Customer/PbxGroupBase.pm
+++ lib/NGCP/Panel/Form/Customer/PbxGroupBase.pm
@@ -5,12 +5,19 @@ extends 'HTML::FormHandler';
 use Moose::Util::TypeConstraints;
 
 use HTML::FormHandler::Widget::Block::Bootstrap;
-use NGCP::Panel::Field::PosInteger;
 
 has '+widget_wrapper' => ( default => 'Bootstrap' );
 has_field 'submitid' => ( type => 'Hidden' );
 sub build_render_list {[qw/submitid fields actions/]}
-sub build_form_element_class {[qw(form-horizontal)]}
+sub build_form_element_class { [qw(form-horizontal)] }
+
+with 'NGCP::Panel::Render::RepeatableJs';
+
+has_field 'pbx_extension' => (
+    type => 'Text',
+    required => 1,
+    label => 'Extension',
+);
 
 has_field 'pbx_hunt_policy' => (
     type => 'Select',
@@ -42,7 +49,7 @@ has_field 'save' => (
 has_block 'fields' => (
     tag => 'div',
     class => [qw/modal-body/],
-    render_list => [qw/pbx_hunt_policy pbx_hunt_timeout/],
+    render_list => [qw/pbx_extension pbx_hunt_policy pbx_hunt_timeout/],
 );
 
 has_block 'actions' => (
