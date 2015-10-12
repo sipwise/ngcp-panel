bf3cd23556e0ee7cab41aeddcb239bee87a758fa
Kirill Solomko <ksolomko@sipwise.com>
2015-10-12 12:33:54 +0200

    MT#14679 add mail2fax for GUI and API interfaces
    Change-Id: I856f08393e09249f481f7acd02f3972bf946dc6d

---
+++ lib/NGCP/Panel/Form/MailToFax/SecretKey.pm
@@ -0,0 +1,45 @@
+package NGCP::Panel::Form::MailToFax::SecretKey;
+
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+use Moose::Util::TypeConstraints;
+use NGCP::Panel::Utils::Form;
+
+use HTML::FormHandler::Widget::Block::Bootstrap;
+
+has '+widget_wrapper' => ( default => 'Bootstrap' );
+has_field 'submitid' => ( type => 'Hidden' );
+sub build_render_list {[qw/submitid fields actions/]}
+sub build_form_element_class { [qw/form-horizontal/] }
+
+has_field 'secret_key' => (
+    type => 'Text',
+    label => 'Secret Key',
+    required => 0,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Enable strict mode that requires all mail2fax emails to have the secret key as the very first line of the email + an empty line. The key is removed from the email once matched.']
+    },
+);
+
+has_field 'save' => (
+    type => 'Submit',
+    value => 'Save',
+    element_class => [qw/btn btn-primary/],
+    label => '',
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/secret_key/],
+);
+
+has_block 'actions' => (
+    tag => 'div',
+    class => [qw/modal-footer/],
+    render_list => [qw/save/],
+);
+
+1;
+# vim: set tabstop=4 expandtab:
