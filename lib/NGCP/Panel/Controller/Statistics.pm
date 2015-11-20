a79be94415ca850c8a7e19dc22624f5dd7011f6c
Gerhard Jungwirth <gjungwirth@sipwise.com>
2015-11-20 17:02:20 +0100

    MT#16355 wip panel version
    Change-Id: Ia26f669c21f37e7263e806f32d1cb547549fd57f

--- lib/NGCP/Panel/Controller/Statistics.pm
+++ lib/NGCP/Panel/Controller/Statistics.pm
@@ -125,6 +125,21 @@ sub rrd : Chained('/') :PathPart('statistics/rrd') :Args() {
     return;
 }
 
+sub versions :Chained('/') :PathPart('statistics/versions') :Args() {
+    my ( $self, $c ) = @_;
+    my $versions_info = NGCP::Panel::Utils::Statistics::get_dpkg_versions();
+    use DDP; p $versions_info;
+    $c->response->body($versions_info);
+    $c->response->headers->content_type('text/plain');
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
 =head1 AUTHOR
 
 Andreas Granig,,,
@@ -135,9 +150,5 @@ This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
-
-__PACKAGE__->meta->make_immutable;
-
-1;
 
 # vim: set tabstop=4 expandtab:
