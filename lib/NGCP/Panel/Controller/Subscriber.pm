e84c2a803e1ed36aff3f9daed3ec8494a3eda2c9
Andreas Granig <agranig@sipwise.com>
2015-12-10 13:33:50 +0100

    MT#16883 Set vmb prefix for cfb to voicemail.
    API needs to be checked also!
    
    Change-Id: Ia2368e25ffcdf7a667fe173359c54cffc8577330
    (cherry picked from commit e456d6d8c6ce17ac47b1d0ea4e4ec52ebca756e9)

--- lib/NGCP/Panel/Controller/Subscriber.pm
+++ lib/NGCP/Panel/Controller/Subscriber.pm
@@ -1090,6 +1090,7 @@ sub preferences_callforward :Chained('base') :PathPart('preferences/callforward'
                         domain => $c->stash->{subscriber}->domain->domain,
                         destination => $d,
                         uri => $dest->field('uri')->field('destination')->value,
+                        cf_type => $cf_type,
                     );
 
                 $dest_set->voip_cf_destinations->create({
@@ -1433,6 +1434,7 @@ sub preferences_callforward_destinationset_create :Chained('base') :PathPart('pr
                                 domain => $c->stash->{subscriber}->domain->domain,
                                 destination => $d,
                                 uri => $dest->field('uri')->field('destination')->value,
+                                cf_type => $cf_type,
                             );
 
                         $set->voip_cf_destinations->create({
@@ -1601,6 +1603,7 @@ sub preferences_callforward_destinationset_edit :Chained('preferences_callforwar
                             domain => $c->stash->{subscriber}->domain->domain,
                             destination => $d,
                             uri => $dest->field('uri')->field('destination')->value,
+                            cf_type => $cf_type,
                         );
 
                     $set->voip_cf_destinations->create({
