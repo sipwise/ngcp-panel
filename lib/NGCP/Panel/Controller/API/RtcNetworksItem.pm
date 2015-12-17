8c823708a22c5e0e78ad84666b9ae1e261487cb7
Gerhard Jungwirth <gjungwirth@sipwise.com>
2015-12-17 11:02:01 +0100

    MT#15883 implement rtcnetworks API
    GET,item PUT, item PATCH implemented
    this covers CRUD of rtcnetworks
    
    Change-Id: If64034161886b5c7f943fd5e9a4d651e131c5a52

---
+++ lib/NGCP/Panel/Controller/API/RtcNetworksItem.pm
@@ -0,0 +1,228 @@
+package NGCP::Panel::Controller::API::RtcNetworksItem;
+use NGCP::Panel::Utils::Generic qw(:all);
+use Moose;
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
+use MooseX::ClassAttribute qw(class_has);
+
+BEGIN { extends 'Catalyst::Controller::ActionRole'; }
+require Catalyst::ActionRole::ACL;
+require Catalyst::ActionRole::HTTPMethods;
+require Catalyst::ActionRole::RequireSSL;
+
+with 'NGCP::Panel::Role::API::RtcNetworks';
+
+class_has('resource_name', is => 'ro', default => 'rtcnetworks');
+class_has('dispatch_path', is => 'ro', default => '/api/rtcnetworks/');
+class_has('relation', is => 'ro', default => 'http://purl.org/sipwise/ngcp-api/#rel-rtcnetworks');
+
+class_has(@{ __PACKAGE__->get_journal_query_params() });
+
+__PACKAGE__->config(
+    action => {
+        (map { $_ => {
+            ACLDetachTo => '/api/root/invalid_user',
+            AllowedRole => [qw/admin reseller/],
+            Args => 1,
+            Does => [qw(ACL RequireSSL)],
+            Method => $_,
+            Path => __PACKAGE__->dispatch_path,
+        } } @{ __PACKAGE__->allowed_methods }),
+        @{ __PACKAGE__->get_journal_action_config(__PACKAGE__->resource_name,{
+            ACLDetachTo => '/api/root/invalid_user',
+            AllowedRole => [qw/admin reseller/],
+            Does => [qw(ACL RequireSSL)],
+        }) }
+    },
+    action_roles => [qw(HTTPMethods)],
+);
+
+sub auto :Private {
+    my ($self, $c) = @_;
+
+    $self->set_body($c);
+    $self->log_request($c);
+    return 1;
+}
+
+sub GET :Allow {
+    my ($self, $c, $id) = @_;
+    {
+        last unless $self->valid_id($c, $id);
+        my $item = $self->item_by_id($c, $id);
+        last unless $self->resource_exists($c, reseller => $item);
+
+        my $hal = $self->hal_from_item($c, $item);
+
+        my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
+            (map { # XXX Data::HAL must be able to generate links with multiple relations
+                s|rel="(http://purl.org/sipwise/ngcp-api/#rel-resellers)"|rel="item $1"|r
+                =~ s/rel=self/rel="item self"/r;
+            } $hal->http_headers),
+        ), $hal->as_json);
+        $c->response->headers($response->headers);
+        $c->response->body($response->content);
+        return;
+    }
+    return;
+}
+
+sub HEAD :Allow {
+    my ($self, $c, $id) = @_;
+    $c->forward(qw(GET));
+    $c->response->body(q());
+    return;
+}
+
+sub OPTIONS :Allow {
+    my ($self, $c, $id) = @_;
+    my $allowed_methods = $self->allowed_methods_filtered($c);
+    $c->response->headers(HTTP::Headers->new(
+        Allow => join(', ', @{ $allowed_methods }),
+        Accept_Patch => 'application/json-patch+json',
+    ));
+    $c->response->content_type('application/json');
+    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
+    return;
+}
+
+sub PUT :Allow {
+    my ($self, $c, $id) = @_;
+    my $schema = $c->model('DB');
+    my $guard = $schema->txn_scope_guard;
+    {
+        my $preference = $self->require_preference($c);
+        last unless $preference;
+
+        my $subscriber = $self->item_by_id($c, $id);
+        last unless $self->resource_exists($c, subscriber => $subscriber);
+        my $resource = $self->get_valid_put_data(
+            c => $c,
+            id => $id,
+            media_type => 'application/json',
+        );
+        last unless $resource;
+
+        my $form = $self->get_form($c);
+        my $old_resource = $self->hal_from_item($c, $subscriber, 1)->resource;
+        $subscriber = $self->update_item($c, $subscriber, $old_resource, $resource, $form);
+        last unless $subscriber;
+
+        my $hal = $self->hal_from_item($c, $subscriber);
+        last unless $self->add_update_journal_item_hal($c,{ hal => $hal, id => $subscriber->id });
+
+        $guard->commit;
+
+        if ('minimal' eq $preference) {
+            $c->response->status(HTTP_NO_CONTENT);
+            $c->response->header(Preference_Applied => 'return=minimal');
+            $c->response->body(q());
+        } else {
+            #my $hal = $self->hal_from_item($c, $subscriber);
+            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
+                $hal->http_headers,
+            ), $hal->as_json);
+            $c->response->headers($response->headers);
+            $c->response->header(Preference_Applied => 'return=representation');
+            $c->response->body($response->content);
+        }
+    }
+    return;
+}
+
+sub PATCH :Allow {
+    my ($self, $c, $id) = @_;
+    my $schema = $c->model('DB');
+    my $guard = $schema->txn_scope_guard;
+    {
+        my $preference = $self->require_preference($c);
+        last unless $preference;
+
+        my $subscriber = $self->item_by_id($c, $id);
+        last unless $self->resource_exists($c, subscriber => $subscriber);
+        my $json = $self->get_valid_patch_data(
+            c => $c,
+            id => $id,
+            media_type => 'application/json-patch+json',
+            ops => ["add", "replace", "copy", "remove"],
+        );
+        last unless $json;
+
+        my $form = $self->get_form($c);
+        my $old_resource = $self->hal_from_item($c, $subscriber, 1)->resource;
+        my $resource = $self->apply_patch($c, $old_resource, $json);
+        last unless $resource;
+
+        $subscriber = $self->update_item($c, $subscriber, $old_resource, $resource, $form);
+        last unless $subscriber;
+
+        my $hal = $self->hal_from_item($c, $subscriber);
+        last unless $self->add_update_journal_item_hal($c,{ hal => $hal, id => $subscriber->id });
+
+        $guard->commit;
+
+        if ('minimal' eq $preference) {
+            $c->response->status(HTTP_NO_CONTENT);
+            $c->response->header(Preference_Applied => 'return=minimal');
+            $c->response->body(q());
+        } else {
+            #my $hal = $self->hal_from_item($c, $subscriber);
+            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
+                $hal->http_headers,
+            ), $hal->as_json);
+            $c->response->headers($response->headers);
+            $c->response->header(Preference_Applied => 'return=representation');
+            $c->response->body($response->content);
+        }
+    }
+    return;
+}
+
+sub item_base_journal :Journal {
+    my $self = shift @_;
+    return $self->handle_item_base_journal(@_);
+}
+
+sub journals_get :Journal {
+    my $self = shift @_;
+    return $self->handle_journals_get(@_);
+}
+
+sub journalsitem_get :Journal {
+    my $self = shift @_;
+    return $self->handle_journalsitem_get(@_);
+}
+
+sub journals_options :Journal {
+    my $self = shift @_;
+    return $self->handle_journals_options(@_);
+}
+
+sub journalsitem_options :Journal {
+    my $self = shift @_;
+    return $self->handle_journalsitem_options(@_);
+}
+
+sub journals_head :Journal {
+    my $self = shift @_;
+    return $self->handle_journals_head(@_);
+}
+
+sub journalsitem_head :Journal {
+    my $self = shift @_;
+    return $self->handle_journalsitem_head(@_);
+}
+
+sub end : Private {
+    my ($self, $c) = @_;
+
+    $self->log_response($c);
+    return 1;
+}
+
+no Moose;
+1;
+
+# vim: set tabstop=4 expandtab:
--- lib/NGCP/Panel/Utils/Rtc.pm
+++ lib/NGCP/Panel/Utils/Rtc.pm
@@ -102,9 +102,9 @@ sub _create_rtc_user {
                 {xms => JSON::false},
                 $user->{data}{id},
             );
-        if ($user->{code} != 201) {
+        if ($n_response->{code} != 201) {
             return unless &{$err_code}(
-                'Creating rtc network failed. Error code: ' . $user->{code});
+                'Creating rtc network failed. Error code: ' . $n_response->{code});
         }
     }
 }
@@ -141,6 +141,146 @@ sub _delete_rtc_user {
         return unless &{$err_code}(
             'Deleting rtc user failed. Error code: ' . $delete_resp->{code});
     }
+}
+
+sub get_rtc_networks {
+    my ($rtc_user_id, $config, $reseller_item, $include_id, $err_code) = @_;
+
+    if (!defined $err_code || ref $err_code ne 'CODE') {
+        $err_code = sub { return 0; };
+    }
+
+    my $comx = NGCP::Panel::Utils::ComxAPIClient->new(
+        host => $config->{rtc}{schema}.'://'.
+        $config->{rtc}{host}.':'.$config->{rtc}{port}.
+        $config->{rtc}{path},
+    );
+    $comx->login(
+        $config->{rtc}{user},
+        $config->{rtc}{pass},
+        $config->{rtc}{host}.':'.$config->{rtc}{port});
+    if ($comx->login_status->{code} != 200) {
+        return unless &{$err_code}(
+            'Rtc Login failed. Check config settings.');
+    }
+
+    my $networks_resp = $comx->get_networks_by_user_id($rtc_user_id);
+    my $networks = $networks_resp->{data};
+    unless (defined $networks  && 'ARRAY' eq ref $networks && @{ $networks }) {
+        return unless &{$err_code}(
+            'Fetching networks failed. Code: ' . $networks_resp->{code});
+    }
+
+    my $res = [map {{
+            config =>$_->{config},
+            connector => $_->{connector},
+            tag => $_->{tag},
+            $include_id ? (id => $_->{id}) : (),
+        }} @{ $networks }];
+
+    return $res;
+}
+
+sub modify_rtc_networks {
+    my ($old_resource, $resource, $config, $reseller_item, $err_code) = @_;
+
+    if (!defined $err_code || ref $err_code ne 'CODE') {
+        $err_code = sub { return 0; };
+    }
+
+    if ((!defined $old_resource) || (!defined $resource)) { # can only modify (no create/delete) the whole resource
+        return unless &{$err_code}(
+            'Cannot Modify rtc network. Old or new resource missing.');
+    }
+
+    my $comx = NGCP::Panel::Utils::ComxAPIClient->new(
+        host => $config->{rtc}{schema}.'://'.
+        $config->{rtc}{host}.':'.$config->{rtc}{port}.
+        $config->{rtc}{path},
+    );
+    $comx->login(
+        $config->{rtc}{user},
+        $config->{rtc}{pass},
+        $config->{rtc}{host}.':'.$config->{rtc}{port});
+    if ($comx->login_status->{code} != 200) {
+        return unless &{$err_code}(
+            'Rtc Login failed. Check config settings.');
+    }
+
+    my (@deleted, @new);
+    for my $nw (@{ $resource->{networks} }) {
+        my $nw_tag = $nw->{tag};
+        my ($old_nw) = grep {$nw_tag eq $_->{tag}} @{ $old_resource->{networks} };
+        if (!defined $old_nw) {
+            push @new, $nw;
+        } else {
+            if ($nw->{connector} ne $old_nw->{connector}
+                    || !_recursive_compare_hashref($nw->{config}, $old_nw->{config})
+                ) {
+                push @deleted, $old_nw;
+                push @new, $nw;
+            }
+        }
+    }
+    for my $nw (@{ $old_resource->{networks} }) {
+        my $nw_tag = $nw->{tag};
+
+        my ($new_nw) = grep {$nw_tag eq $_->{tag}} @{ $resource->{networks} };
+        if (!defined $new_nw) {
+            push @deleted, $nw;
+        }
+    }
+
+    for my $nw (@deleted) {
+        my $n_response = $comx->delete_network($nw->{id});
+        if ($n_response->{code} != 200) {
+            return unless &{$err_code}(
+                'Deleting rtc network failed. Error code: ' . $n_response->{code});
+        }
+    }
+    for my $nw (@new) {
+        my $n_response = $comx->create_network(
+                $nw->{tag},
+                $nw->{connector},
+                $nw->{config} // {},
+                $old_resource->{rtc_user_id},
+            );
+        if ($n_response->{code} != 201) {
+            return unless &{$err_code}(
+                'Creating rtc network failed. Error code: ' . $n_response->{code});
+        }
+    }
+}
+
+# 1 if equal, 0 if different
+sub _recursive_compare_hashref {
+    my ($hash1, $hash2) = @_;
+
+    my $ref1 = ref $hash1;
+    my $ref2 = ref $hash2;
+    if ($ref1) {
+        if ($ref2) {
+            return 0 if ($ref1 ne $ref2);
+            if ($ref1 eq "HASH") {
+                return 0 if keys %{$hash1} != keys %{$hash2};
+                for my $k (keys %{$hash1}) {
+                    return 0 if ( !_recursive_compare_hashref($hash1->{$k}, $hash2->{$k}) );
+                }
+                return 1;
+            } elsif ($ref1 =~ m/json.*bool/i) {
+                return $hash1 == $hash2;
+            }
+        } else {
+            return 0;
+        }
+    } else {
+        if ($ref2) {
+            return 0;
+        } else {
+            return $hash1 eq $hash2;
+        }
+    }
+    return 0;  # arrays not supported
 }
 
 1;
