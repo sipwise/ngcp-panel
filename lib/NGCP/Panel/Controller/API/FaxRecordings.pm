748f80379e493b210c3fc67bd884e637283f201b
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2015-12-24 09:56:39 +0200

    MT#11731 Consider different content types for API actions
    Change-Id: I3edc66cf091ac1cf4575fd12302c1f6befe71e6d

--- lib/NGCP/Panel/Controller/API/FaxRecordings.pm
+++ lib/NGCP/Panel/Controller/API/FaxRecordings.pm
@@ -22,7 +22,7 @@ class_has 'api_description' => (
     is => 'ro',
     isa => 'Str',
     default => 
-        'Defines the actual recording of fax messages. It is referred to by the <a href="#faxs">Faxs</a> relation. A GET on an item returns the binary blob of the recording with Content-Type "audio/x-wav".',
+        'Defines the actual recording of fax messages. It is referred to by the <a href="#faxes">Faxes</a> relation. A GET on an item returns the binary blob of the recording with Content-Type "audio/x-wav".',
 );
 
 with 'NGCP::Panel::Role::API::FaxRecordings';
