10760269b41386c17586cdbba4f2af7556c15ddc
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-01-04 05:13:12 +0200

    MT#15633 API for peering groups, servers, rules
    Change-Id: Ic39751a622240f12c5d790ee15ea3a329d731807

---
+++ lib/NGCP/Panel/Controller/API/PeeringServerPreferencesItem.pm
@@ -0,0 +1,234 @@
+package NGCP::Panel::Controller::API::PeeringServerPreferencesItem;
+use NGCP::Panel::Utils::Generic qw(:all);
+use Sipwise::Base;
+use Moose;
+#use namespace::sweep;
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
+use MooseX::ClassAttribute qw(class_has);
+use NGCP::Panel::Utils::ValidateJSON qw();
+use NGCP::Panel::Utils::DateTime;
+use Path::Tiny qw(path);
+use Safe::Isa qw($_isa);
+BEGIN { extends 'Catalyst::Controller::ActionRole'; }
+require Catalyst::ActionRole::ACL;
+require Catalyst::ActionRole::HTTPMethods;
+require Catalyst::ActionRole::RequireSSL;
+
+with 'NGCP::Panel::Role::API::Preferences';
+
+class_has('resource_name', is => 'ro', default => 'peeringserverpreferences');
+class_has('dispatch_path', is => 'ro', default => '/api/peeringserverpreferences/');
+class_has('relation', is => 'ro', default => 'http://purl.org/sipwise/ngcp-api/#rel-peeringserverpreferences');
+
+class_has(@{ __PACKAGE__->get_journal_query_params() });
+
+__PACKAGE__->config(
+    action => {
+        (map { $_ => {
+            ACLDetachTo => '/api/root/invalid_user',
+            AllowedRole => [qw/admin reseller/],
+            Args => 1,
+            Does => [qw(ACL RequireSSL)],
+            Method => $_,
+            Path => __PACKAGE__->dispatch_path,
+        } } @{ __PACKAGE__->allowed_methods }),
+        @{ __PACKAGE__->get_journal_action_config(__PACKAGE__->resource_name,{
+            ACLDetachTo => '/api/root/invalid_user',
+            AllowedRole => [qw/admin reseller/],
+            Does => [qw(ACL RequireSSL)],
+        }) }
+    },
+    action_roles => [qw(HTTPMethods)],
+);
+
+sub auto :Private {
+    my ($self, $c) = @_;
+
+    $self->set_body($c);
+    $self->log_request($c);
+}
+
+sub GET :Allow {
+    my ($self, $c, $id) = @_;
+    {
+        last unless $self->valid_id($c, $id);
+        my $profile = $self->item_by_id($c, $id, "profiles");
+        last unless $self->resource_exists($c, profilepreference => $profile);
+
+        my $hal = $self->hal_from_item($c, $profile, "profiles");
+
+        my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
+            (map { # XXX Data::HAL must be able to generate links with multiple relations
+                s|rel="(http://purl.org/sipwise/ngcp-api/#rel-resellers)"|rel="item $1"|;
+                s/rel=self/rel="item self"/;
+                $_
+            } $hal->http_headers),
+        ), $hal->as_json);
+        $c->response->headers($response->headers);
+        $c->response->body($response->content);
+        return;
+    }
+    return;
+}
+
+sub HEAD :Allow {
+    my ($self, $c, $id) = @_;
+    $c->forward(qw(GET));
+    $c->response->body(q());
+    return;
+}
+
+sub OPTIONS :Allow {
+    my ($self, $c, $id) = @_;
+    my $allowed_methods = $self->allowed_methods_filtered($c);
+    $c->response->headers(HTTP::Headers->new(
+        Allow => join(', ', @{ $allowed_methods }),
+        Accept_Patch => 'application/json-patch+json',
+    ));
+    $c->response->content_type('application/json');
+    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
+    return;
+}
+
+sub PATCH :Allow {
+    my ($self, $c, $id) = @_;
+    my $guard = $c->model('DB')->txn_scope_guard;
+    {
+        my $preference = $self->require_preference($c);
+        last unless $preference;
+
+        my $json = $self->get_valid_patch_data(
+            c => $c,
+            id => $id,
+            media_type => 'application/json-patch+json',
+            ops => [qw/add replace remove copy/],
+        );
+        last unless $json;
+
+        my $profile = $self->item_by_id($c, $id, "profiles");
+        last unless $self->resource_exists($c, peeringserverpreferences => $profile);
+        my $old_resource = $self->get_resource($c, $profile, "profiles");
+        my $resource = $self->apply_patch($c, $old_resource, $json);
+        last unless $resource;
+
+        # last param is "no replace" to NOT delete existing prefs
+        # for proper PATCH behavior
+        $profile = $self->update_item($c, $profile, $old_resource, $resource, 0, "profiles");
+        last unless $profile;
+
+        my $hal = $self->hal_from_item($c, $profile, "profiles");
+        last unless $self->add_update_journal_item_hal($c,$hal);
+        
+        $guard->commit; 
+
+        if ('minimal' eq $preference) {
+            $c->response->status(HTTP_NO_CONTENT);
+            $c->response->header(Preference_Applied => 'return=minimal');
+            $c->response->body(q());
+        } else {
+            #my $hal = $self->hal_from_item($c, $profile, "profiles");
+            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
+                $hal->http_headers,
+            ), $hal->as_json);
+            $c->response->headers($response->headers);
+            $c->response->header(Preference_Applied => 'return=representation');
+            $c->response->body($response->content);
+        }
+    }
+    return;
+}
+
+sub PUT :Allow {
+    my ($self, $c, $id) = @_;
+    my $guard = $c->model('DB')->txn_scope_guard;
+    {
+        my $preference = $self->require_preference($c);
+        last unless $preference;
+
+        my $profile = $self->item_by_id($c, $id, "profiles");
+        # TODO: systemcontact?
+        last unless $self->resource_exists($c, systemcontact => $profile);
+        my $resource = $self->get_valid_put_data(
+            c => $c,
+            id => $id,
+            media_type => 'application/json',
+        );
+        last unless $resource;
+        my $old_resource = $self->get_resource($c, $profile, "profiles");
+
+        # last param is "replace" to delete all existing prefs
+        # for proper PUT behavior
+        $profile = $self->update_item($c, $profile, $old_resource, $resource, 1, "profiles");
+        last unless $profile;
+
+        my $hal = $self->hal_from_item($c, $profile, "profiles");
+        last unless $self->add_update_journal_item_hal($c,$hal);
+        
+        $guard->commit; 
+
+        if ('minimal' eq $preference) {
+            $c->response->status(HTTP_NO_CONTENT);
+            $c->response->header(Preference_Applied => 'return=minimal');
+            $c->response->body(q());
+        } else {
+            #my $hal = $self->hal_from_item($c, $profile, "profiles");
+            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
+                $hal->http_headers,
+            ), $hal->as_json);
+            $c->response->headers($response->headers);
+            $c->response->header(Preference_Applied => 'return=representation');
+            $c->response->body($response->content);
+        }
+    }
+    return;
+}
+
+sub item_base_journal :Journal {
+    my $self = shift @_;
+    return $self->handle_item_base_journal(@_);
+}
+    
+sub journals_get :Journal {
+    my $self = shift @_;
+    return $self->handle_journals_get(@_);
+}
+
+sub journalsitem_get :Journal {
+    my $self = shift @_;
+    return $self->handle_journalsitem_get(@_);
+}
+
+sub journals_options :Journal {
+    my $self = shift @_;
+    return $self->handle_journals_options(@_);
+}
+
+sub journalsitem_options :Journal {
+    my $self = shift @_;
+    return $self->handle_journalsitem_options(@_);
+}
+
+sub journals_head :Journal {
+    my $self = shift @_;
+    return $self->handle_journals_head(@_);
+}
+
+sub journalsitem_head :Journal {
+    my $self = shift @_;
+    return $self->handle_journalsitem_head(@_);
+} 
+
+sub end : Private {
+    my ($self, $c) = @_;
+
+    $self->log_response($c);
+}
+
+no Moose;
+1;
+
+# vim: set tabstop=4 expandtab:
