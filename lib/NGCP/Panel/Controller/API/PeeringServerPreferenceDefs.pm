001f62e608650ddd0f9ecfd1bd6c7822ed8e5ad7
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-01-04 05:13:12 +0200

    MT#15633 API for peering groups, servers, rules
    Change-Id: Ic39751a622240f12c5d790ee15ea3a329d731807

---
+++ lib/NGCP/Panel/Controller/API/PeeringServerPreferenceDefs.pm
@@ -0,0 +1,107 @@
+package NGCP::Panel::Controller::API::PeeringServerPreferenceDefs;
+use NGCP::Panel::Utils::Generic qw(:all);
+use Sipwise::Base;
+use Moose;
+#use namespace::sweep;
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
+use MooseX::ClassAttribute qw(class_has);
+use NGCP::Panel::Utils::DateTime;
+use NGCP::Panel::Utils::Preferences;
+use Path::Tiny qw(path);
+use Safe::Isa qw($_isa);
+use JSON::Types qw();
+BEGIN { extends 'Catalyst::Controller::ActionRole'; }
+require Catalyst::ActionRole::ACL;
+require Catalyst::ActionRole::CheckTrailingSlash;
+require Catalyst::ActionRole::HTTPMethods;
+require Catalyst::ActionRole::RequireSSL;
+
+with 'NGCP::Panel::Role::API';
+
+class_has('resource_name', is => 'ro', default => 'peeringserverpreferencedefs');
+class_has('dispatch_path', is => 'ro', default => '/api/peeringserverpreferencedefs/');
+class_has('relation', is => 'ro', default => 'http://purl.org/sipwise/ngcp-api/#rel-peeringserverpreferencedefs');
+
+__PACKAGE__->config(
+    action => {
+        map { $_ => {
+            ACLDetachTo => '/api/root/invalid_user',
+            AllowedRole => [qw/admin reseller/],
+            Args => 0,
+            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
+            Method => $_,
+            Path => __PACKAGE__->dispatch_path,
+        } } @{ __PACKAGE__->allowed_methods }
+    },
+    action_roles => [qw(HTTPMethods)],
+);
+
+sub auto :Private {
+    my ($self, $c) = @_;
+
+    $self->set_body($c);
+    $self->log_request($c);
+}
+
+sub GET :Allow {
+    my ($self, $c) = @_;
+    {
+        my @links;
+        push @links,
+            Data::HAL::Link->new(
+                relation => 'curies',
+                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
+                name => 'ngcp',
+                templated => true,
+            ),
+            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
+            Data::HAL::Link->new(relation => 'self', href => sprintf('%s', $self->dispatch_path));
+
+        my $hal = Data::HAL->new(
+            links => [@links],
+        );
+        my $resource = NGCP::Panel::Utils::Preferences::api_preferences_defs( c => $c, preferences_group => 'peer_pref' );
+        $hal->resource($resource);
+
+        my $response = HTTP::Response->new(HTTP_OK, undef, 
+            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
+        $c->response->headers($response->headers);
+        $c->response->body($response->content);
+        return;
+    }
+    return;
+}
+
+sub HEAD :Allow {
+    my ($self, $c) = @_;
+    $c->forward(qw(GET));
+    $c->response->body(q());
+    return;
+}
+
+sub OPTIONS :Allow {
+    my ($self, $c) = @_;
+    my $allowed_methods = $self->allowed_methods_filtered($c);
+    $c->response->headers(HTTP::Headers->new(
+        Allow => join(', ', @{ $allowed_methods }),
+        Accept_Post => 'application/hal+json; profile=http://purl.org/sipwise/ngcp-api/#rel-'.$self->resource_name,
+    ));
+    $c->response->content_type('application/json');
+    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
+    return;
+}
+
+sub end : Private {
+    my ($self, $c) = @_;
+
+    $self->log_response($c);
+}
+
+no Moose;
+1;
+
+# vim: set tabstop=4 expandtab:
---
+++ lib/NGCP/Panel/Form/Peering/ServerAPI.pm
@@ -0,0 +1,44 @@
+package NGCP::Panel::Form::Peering::ServerAPI;
+use HTML::FormHandler::Moose;
+extends 'NGCP::Panel::Form::Peering::Server';
+
+has_field 'group_id' => (
+    type => 'PosInteger',
+    required => 1,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The peering group this server belongs to.']
+    },
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/group_id name ip host port transport weight via_route enabled/],
+);
+
+1;
+__END__
+
+=head1 NAME
+
+NGCP::Panel::Form::Peering::ServerAPI
+
+=head1 DESCRIPTION
+
+-
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+Irina Peshinskaya
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+# vim: set tabstop=4 expandtab:
