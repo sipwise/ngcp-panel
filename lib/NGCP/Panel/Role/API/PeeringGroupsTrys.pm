5873952de48795d6d4535dbd11b912a09576ace9
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-01-18 20:11:08 +0200

    MT#17173 Copy files before change
    Change-Id: I20de771f791e0edf75cd0a3822b421b1b48076bd

---
+++ lib/NGCP/Panel/Role/API/PeeringGroupsTrys.pm
@@ -0,0 +1,95 @@
+package NGCP::Panel::Role::API::PeeringGroups;
+use NGCP::Panel::Utils::Generic qw(:all);
+use Moose::Role;
+use Sipwise::Base;
+with 'NGCP::Panel::Role::API' => {
+    -alias       =>{ item_rs  => '_item_rs', },
+    -excludes    => [ 'item_rs' ],
+};
+
+use boolean qw(true);
+use TryCatch;
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Status qw(:constants);
+use NGCP::Panel::Form::Peering::Group;
+use NGCP::Panel::Utils::Peering;
+
+sub item_rs {
+    my ($self, $c) = @_;
+    my $item_rs = $c->model('DB')->resultset('voip_peer_groups');
+    return $item_rs;
+}
+
+sub get_form {
+    my ($self, $c) = @_;
+    return NGCP::Panel::Form::Peering::Group->new;
+}
+
+sub hal_from_item {
+    my ($self, $c, $item, $form) = @_;
+    my %resource = $item->get_inflated_columns;
+    $resource{contract_id} = delete $resource{peering_contract_id};
+    my $hal = Data::HAL->new(
+        links => [
+            Data::HAL::Link->new(
+                relation => 'curies',
+                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
+                name => 'ngcp',
+                templated => true,
+            ),
+            Data::HAL::Link->new(relation => 'collection', href => sprintf("/api/%s/", $self->resource_name)),
+            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
+            Data::HAL::Link->new(relation => 'self', href => sprintf("%s%d", $self->dispatch_path, $item->id)),
+        ],
+        relation => 'ngcp:'.$self->resource_name,
+    );
+
+    $form //= $self->get_form($c);
+
+    $self->validate_form(
+        c => $c,
+        resource => \%resource,
+        form => $form,
+        run => 0,
+    );
+
+    $resource{id} = int($item->id);
+    $hal->resource({%resource});
+    return $hal;
+}
+
+sub item_by_id {
+    my ($self, $c, $id) = @_;
+    my $item_rs = $self->item_rs($c);
+    return $item_rs->find($id);
+}
+
+sub update_item {
+    my ($self, $c, $item, $old_resource, $resource, $form) = @_;
+
+    $form //= $self->get_form($c);
+    return unless $self->validate_form(
+        c => $c,
+        form => $form,
+        resource => $resource,
+    );
+    $resource = $form->custom_get_values;
+    last unless $resource;
+
+    my $dup_item = $c->model('DB')->resultset('voip_peer_groups')->find({
+        name => $resource->{name},
+    });
+    if($dup_item && $dup_item->id != $item->id) {
+        $c->log->error("peering group with name '$$resource{name}' already exists"); # TODO: user, message, trace, ...
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "peering group with this name already exists");
+        return;
+    }
+
+    $item->update($resource);
+    NGCP::Panel::Utils::Peering::_sip_lcr_reload(c => $c);
+    return $item;
+}
+
+1;
+# vim: set tabstop=4 expandtab:
