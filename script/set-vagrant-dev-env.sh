af5833db7f9c9755671a8af553b5842c921e1e2d
Andreas Granig <agranig@sipwise.com>
2016-01-25 12:24:47 +0100

    MT#17505 Link git paths to deb paths
    Change-Id: I80ae20b04112e37774d17af68466dc095747a9ba

---
+++ script/set-vagrant-dev-env.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+VAGRANT_MNT=${VAGRANT_MNT:-/usr/local/devel}
+
+for i in Panel Panel.pm; do
+  if [ -L /usr/share/perl5/NGCP/$i ]; then
+    echo "/usr/share/perl5/NGCP/$i is already a link, ignoring..."
+  else
+    mv /usr/share/perl5/NGCP/$i /usr/share/perl5/NGCP/${i}.orig
+    ln -s $VAGRANT_MNT/ngcp-panel/lib/NGCP/$i /usr/share/perl5/NGCP/$i
+  fi
+done
+
+for i in ngcp_panel_fastcgi.pl; do
+  if [ -L /usr/share/ngcp-panel/$i ]; then
+    echo "/usr/share/ngcp-panel/$i is already a link, ignoring..."
+  else
+    mv /usr/share/ngcp-panel/$i /usr/share/ngcp-panel/${i}.orig
+    ln -s $VAGRANT_MNT/ngcp-panel/script/$i /usr/share/ngcp-panel/$i
+  fi
+done
+
+for i in script ngcp_panel.psgi; do
+  if [ -L /usr/share/ngcp-panel/$i ]; then
+    echo "/usr/share/ngcp-panel/$i is already a link, ignoring..."
+  else
+    mv /usr/share/ngcp-panel/$i /usr/share/ngcp-panel/${i}.orig
+    ln -s $VAGRANT_MNT/ngcp-panel/$i /usr/share/ngcp-panel/$i
+  fi
+done
+
+for i in layout static templates tools; do
+  if [ -L /usr/share/ngcp-panel/$i ]; then
+    echo "/usr/share/ngcp-panel/$i is already a link, ignoring..."
+  else
+    mv /usr/share/ngcp-panel/$i /usr/share/ngcp-panel/${i}.orig
+    ln -s $VAGRANT_MNT/ngcp-panel/share/$i /usr/share/ngcp-panel/$i
+  fi
+done
+
+
+for i in Schema Schema.pm InterceptSchema.pm; do
+  if [ -L /usr/share/ngcp-panel/$i ]; then
+    echo "/usr/share/perl5/NGCP/$i is already a link, ignoring..."
+  else
+    mv /usr/share/perl5/NGCP/$i /usr/share/perl5/NGCP/${i}.orig
+    ln -s $VAGRANT_MNT/ngcp-schema/lib/NGCP/$i /usr/share/perl5/NGCP/$i
+  fi
+done
