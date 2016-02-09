4a34f7e6f309392bf43ab5ca33d73f518897a4ff
Kirill Solomko <ksolomko@sipwise.com>
2016-02-09 16:36:22 +0100

    MT#17801 added api_admin_system authentication method
    Change-Id: Id02d7d764bf69de0d39cd5cb75eda1ecd2f552cf

---
+++ lib/NGCP/Panel/AuthenticationStore/SystemRole.pm
@@ -0,0 +1,15 @@
+package NGCP::Panel::AuthenticationStore::SystemRole;
+use Sipwise::Base;
+use base 'Catalyst::Authentication::User::Hash';
+
+sub roles  {
+    my $self = shift;
+
+    # return only first role for now
+    return ref($self->{roles}) eq "ARRAY" ? $self->{roles}[0]
+                                          : $self->{roles};
+}
+
+1;
+
+# vim ts=4 sw=4 et
