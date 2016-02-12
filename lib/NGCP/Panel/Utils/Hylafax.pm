d707ec189a777b13d5b743b885f792b0da5001cb
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-02-12 08:42:32 +0200

    MT#15491 Adapt Panel to new NGCP::Fax fax server
    Change-Id: Iec1e667ff74cbdec93162a1094c6adaa1301ba67

--- lib/NGCP/Panel/Utils/Hylafax.pm
+++ lib/NGCP/Panel/Utils/Hylafax.pm
@@ -1,6 +1,6 @@
 package NGCP::Panel::Utils::Hylafax;
 
-use Sipwise::Base;
+use NGCP::Fax;
 use File::Temp qw/tempfile/;
 use TryCatch;
 use IPC::System::Simple qw/capture/;
@@ -8,116 +8,38 @@ use Data::Dumper;
 
 sub send_fax {
     my (%args) = @_;
-    #%args =>{
-    #    c => $c,
-    #    subscriber => $subscriber,
-    #    [notify => 1,]
-    #    [coverpage => 1,]
-    #    [resolution => ['low'|'medium'|'extended'],]
-    #    [destination => '',]
-    #    [data => '',]
-    #    [upload => '',]
-    #};
     my $c = $args{c};
+
     my $subscriber = $args{subscriber};
     my $prov_subscriber = $subscriber->provisioning_voip_subscriber;
 
-    my $sendfax = $c->config->{faxserver}->{sendfax} // '/usr/bin/sendfax';
-    my @sendfax_args = ();
+    my %sendfax_args = ();
 
     my $sender = 'webfax';
     my $number;
     if($subscriber->primary_number) {
         $number = $subscriber->primary_number->cc .
-                  ($subscriber->primary_number->ac // '').
-                  $subscriber->primary_number->sn;
+            ($subscriber->primary_number->ac // '').
+            $subscriber->primary_number->sn;
     } else {
         $number = $sender;
     }
-    if($prov_subscriber->voip_fax_preference) {
-        $sender = $prov_subscriber->voip_fax_preference->name // $sender;
-        if($prov_subscriber->voip_fax_preference->password) {
-            push @sendfax_args, '-o '.$number.':'.$prov_subscriber->voip_fax_preference->password;
-        } else {
-            push @sendfax_args, '-o '.$number;
-        }
-    } else {
-        push @sendfax_args, '-o '.$number;
+    $sendfax_args{caller} = $number;
+    $sendfax_args{callee} = $args{destination};
+
+    if($args{resolution}) {#low|medium|extended
+        $sendfax_args{resolution} = $args{resolution};
     }
 
-    my $hosts_config = $c->config->{faxserver}->{ip};
-    my @hosts;
-    if(ref($hosts_config) eq "ARRAY") {
-        @hosts = @{$hosts_config};
-    } else {
-        @hosts = ($hosts_config // '127.0.0.1',);
-    }
-
-    if($args{notify}) {
-        push @sendfax_args, '-D';
-        push @sendfax_args, "-f '$sender <$args{notify}>'";
-    } else {
-        push @sendfax_args, "-f '$sender'";
-    }
-    unless($args{coverpage}) {
-        push @sendfax_args, '-n';
-    }
-    if($args{resolution}) {
-        if($args{resolution} eq 'low') {
-            push @sendfax_args, '-l';
-        } elsif($args{resolution} eq 'medium') {
-            push @sendfax_args, '-m';
-        } elsif($args{resolution} eq 'extended') {
-            push @sendfax_args, '-G';
-        }
-    }
-
-    push @sendfax_args, '-d '.$args{destination};
-
-    my (@filenames);
-    if($args{data}) {
-        my($fh, $filename) = tempfile;
-        unless(print $fh $args{data}) {
-            my $err = $!;
-            close $fh;
-            unlink $filename;
-            die $c->loc("Failed to write fax data to temporary file: [_1]", $err);
-        }
-        close $fh;
-        my $filename_ps = $filename.'.ps';
-        my $cmd = "textfmt -O UTF8:Yes -B -f Courier-Bold -p 11 -s A4 > $filename_ps < $filename";
-        my $err_ps = capture($cmd);
-        $c and $c->log->debug( "$cmd: $err_ps;" );
-        if(!$err_ps){
-            unlink $filename;
-            $filename = $filename_ps;
-        }else{
-            unlink $filename;
-            unlink $filename_ps;
-            die $c->loc("Failed to convert text to ps: [_1]", $err_ps);
-        }
-        push @filenames, $filename;
-    }
+    $sendfax_args{files} = [];
     if($args{upload}){
-        push @filenames, eval { $args{upload}->tempname };
+        push @{$sendfax_args{files}}, eval { $args{upload}->tempname };
     }
-
-    my $sa = join(' ', @sendfax_args);
-    my ($exit,$output);
-    for my $host (@hosts) {
-        my $cmd = "$sendfax $sa -h $host ".join(" ",@filenames);
-        $output = `$cmd 2>&1`;
-        $exit = $?;
-        $c->log->debug("faxserver command: $cmd; output=$output; exit=$exit;");
-        last if $exit eq '0';
+    if($args{data}){
+        $sendfax_args{input} = $args{data};
     }
-    foreach my $filename(@filenames){
-        unlink $filename;
-    }
-    if($exit ne '0') {
-        chomp $output;
-        die $output."\n";
-    }
+    my $client = new NGCP::Fax;
+    $client->send_fax(\%data);
 }
 
 1;
