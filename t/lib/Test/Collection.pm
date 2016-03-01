f4e621e5aa1f8081adb824929747d0a48282ec7e
Rene Krenn <rkrenn@sipwise.com>
2016-03-01 14:53:52 +0100

    MT#16855 backport 17323 (union_all for call lists) to 3.8
    Change-Id: I1e8750a6e7f1e806c133f7d2c0831f8001681a50
    (cherry picked from commit 37506aad4bee07266d6df29f8ef572f7f34f2085)

--- t/lib/Test/Collection.pm
+++ t/lib/Test/Collection.pm
@@ -44,8 +44,8 @@ has 'base_uri' => (
     is => 'ro',
     isa => 'Str',
     default => sub {
-        $_[0]->{local_test} 
-        ? ( length($_[0]->{local_test})>1 ? $_[0]->{local_test} : 'https://127.0.0.1:4443' ) 
+        $_[0]->{local_test}
+        ? ( length($_[0]->{local_test})>1 ? $_[0]->{local_test} : 'https://127.0.0.1:4443' )
         : $ENV{CATALYST_SERVER} || ('https://'.hostfqdn.':4443')},
 );
 has 'name' => (
@@ -165,7 +165,7 @@ sub get_catalyst_config{
             }
         }
         $panel_config //= '../ngcp_panel.conf';
-        $catalyst_config = Config::General->new($panel_config);   
+        $catalyst_config = Config::General->new($panel_config);
     } else {
         #taken 1:1 from /lib/NGCP/Panel.pm
         for my $path(qw#/etc/ngcp-panel/ngcp_panel.conf etc/ngcp_panel.conf ngcp_panel.conf#) {
@@ -175,7 +175,7 @@ sub get_catalyst_config{
             }
         }
         $panel_config //= 'ngcp_panel.conf';
-        $catalyst_config = Config::General->new($panel_config);   
+        $catalyst_config = Config::General->new($panel_config);
     }
     my %config = $catalyst_config->getall();
     $self->{catalyst_config} = \%config;
@@ -577,6 +577,7 @@ sub check_created_listed{
 sub check_item_get{
     my($self,$uri) = @_;
     $uri ||= $self->get_uri_current;
+    $uri = $self->normalize_uri($uri);
     my $req = HTTP::Request->new('GET', $uri);
     my $res = $self->request($req);
     #print Dumper $res;
@@ -643,12 +644,12 @@ sub check_get2put{
 
 sub check_put2get{
     my($self, $put_data_in, $put_data_cb, $uri) = @_;
-    
+
     my $item_put_data = $self->process_data($put_data_cb, $put_data_in);
     $item_put_data = JSON::to_json($item_put_data);
     my ($res_put,$result_item_put,$req_put) = $self->request_put( $item_put_data, $uri );
     $self->http_code_msg(200, "check_put2get: check put successful",$res_put, $result_item_put);
-    
+
     my ($res_get, $result_item_get, $req_get) = $self->check_item_get($uri);
     delete $result_item_get->{_links};
     delete $result_item_get->{_embedded};
@@ -661,11 +662,11 @@ sub check_put2get{
 
 sub check_post2get{
     my($self, $post_data_in, $post_data_cb) = @_;
-    
+
     my ($res_post, $result_item_post, $req_post, $item_post_data ) = $self->request_post( $post_data_cb, $post_data_in );
     $self->http_code_msg(201, "check_post2get: POST item '".$self->name."' for check_post2get", $res_post, $result_item_post);
     my $location_post = $self->base_uri.($res_post->header('Location') // '');
-    
+
     my ($res_get, $result_item_get, $req_get) = $self->request_get( $location_post );
     $self->http_code_msg(200, "check_post2get: fetch POSTed test '".$self->name."'", $res_get, $result_item_get);
     delete $result_item_get->{_links};
