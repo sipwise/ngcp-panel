b99b3ffbd8a8cb0ffdf603f1271a70067c77a730
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-03-02 16:16:28 +0200

    MT#14659 Move calls from subscribers master to separate interface
    (cherry picked from commit 74333c94ee9a8bd53462281086cb390fce23f625)
    
    Conflicts:
    	lib/NGCP/Panel/Controller/Subscriber.pm
    	share/templates/subscriber/master.tt
    Change-Id: I70373df92bd2e9a7d76a9b4e14139cddda974d6d

--- lib/NGCP/Panel/Controller/Subscriber.pm
+++ lib/NGCP/Panel/Controller/Subscriber.pm
@@ -2062,25 +2062,6 @@ sub load_preference_list :Private {
 sub master :Chained('base') :PathPart('details') :CaptureArgs(0) {
     my ($self, $c) = @_;
 
-    my $call_cols = [
-        { name => "id", title => $c->loc('#')  },
-        { name => "direction", search => 1, literal_sql => 'if(source_user_id = "'.$c->stash->{subscriber}->uuid.'", "outcoming", "incoming")' },
-        { name => "source_user", search => 1, title => $c->loc('Caller') },
-        { name => "destination_user", search => 1, title => $c->loc('Callee') },
-        { name => "source_customer_billing_zones_history.detail", search => 1, title => $c->loc('Billing zone') },
-        { name => "call_status", search => 1, title => $c->loc('Status') },
-        { name => "start_time", search_from_epoch => 1, search_to_epoch => 1, title => $c->loc('Start Time') },
-        { name => "duration", search => 1, title => $c->loc('Duration'), show_total => 'sum' },
-    ];
-    push @{ $call_cols }, (
-        { name => "call_id", search => 1, title => $c->loc('Call-ID') },
-    ) if($c->user->roles eq "admin" || $c->user->roles eq "reseller");
-
-    push @{ $call_cols }, (
-        { name => "source_customer_cost", search => 1, title => $c->loc('Source Cust Cost (cents)'), show_total => 'sum' },
-    ) ;
-    $c->stash->{calls_dt_columns} = NGCP::Panel::Utils::Datatables::set_columns($c, $call_cols);
-
     $c->stash->{vm_dt_columns} = NGCP::Panel::Utils::Datatables::set_columns($c, [
         { name => "id", search => 1, title => $c->loc('#') },
         { name => "read", search => 1, title => $c->loc('Read'), literal_sql => 'if(dir like "%/INBOX", "new", "read")' },
@@ -2132,7 +2113,38 @@ sub voicemails :Chained('master') :PathPart('voicemails') :Args(0) {
     );
 }
 
-sub calllist :Chained('master') :PathPart('calls') :Args(0) {
+
+sub calllist_master :Chained('base') :PathPart('calls') :CaptureArgs(0) {
+    my ($self, $c) = @_;
+
+    my $call_cols = [
+        { name => "id", title => $c->loc('#')  },
+        { name => "direction", search => 1, literal_sql => 'if(source_user_id = "'.$c->stash->{subscriber}->uuid.'", "outgoing", "incoming")' },
+        { name => "source_user", search => 1, title => $c->loc('Caller') },
+        { name => "destination_user", search => 1, title => $c->loc('Callee') },
+        { name => "source_customer_billing_zones_history.detail", search => 1, title => $c->loc('Billing zone') },
+        { name => "call_status", search => 1, title => $c->loc('Status') },
+        { name => "start_time", search_from_epoch => 1, search_to_epoch => 1, title => $c->loc('Start Time') },
+        { name => "duration", search => 1, title => $c->loc('Duration'), show_total => 'sum' },
+    ];
+    push @{ $call_cols }, (
+        { name => "call_id", search => 1, title => $c->loc('Call-ID') },
+    ) if($c->user->roles eq "admin" || $c->user->roles eq "reseller");
+
+    my $vat_factor = $c->config->{appearance}{cdr_apply_vat} && $c->stash->{subscriber}->contract->add_vat
+        ? "* " . (1 + $c->stash->{subscriber}->contract->vat_rate / 100)
+        : "";
+    $c->log->debug("using vat_factor '$vat_factor'");
+
+    push @{ $call_cols }, (
+        { name => "source_customer_cost", search => 1, title => $c->loc('Cost'), show_total => 'sum',
+            literal_sql => 'if(source_user_id = "'.$c->stash->{subscriber}->uuid.'", source_customer_cost, destination_customer_cost)'.$vat_factor },
+    ) ;
+    $c->stash->{calls_dt_columns} = NGCP::Panel::Utils::Datatables::set_columns($c, $call_cols);
+}
+
+
+sub calllist :Chained('calllist_master') :PathPart('') :Args(0) {
     my ($self, $c) = @_;
 
     $c->stash(
@@ -3075,7 +3087,7 @@ sub _process_calls_rows {
     );
 }
 
-sub ajax_calls :Chained('master') :PathPart('list/ajax') :Args(0) {
+sub ajax_calls :Chained('calllist_master') :PathPart('list/ajax') :Args(0) {
     my ($self, $c) = @_;
 
     my $out_rs = $c->model('DB')->resultset('cdr')->search({
@@ -3091,7 +3103,7 @@ sub ajax_calls :Chained('master') :PathPart('list/ajax') :Args(0) {
     $c->detach( $c->view("JSON") );
 }
 
-sub ajax_calls_in :Chained('master') :PathPart('list/ajax/in') :Args(0) {
+sub ajax_calls_in :Chained('calllist_master') :PathPart('list/ajax/in') :Args(0) {
     my ($self, $c) = @_;
 
     my $rs = $c->model('DB')->resultset('cdr')->search({
@@ -3103,7 +3115,7 @@ sub ajax_calls_in :Chained('master') :PathPart('list/ajax/in') :Args(0) {
     $c->detach( $c->view("JSON") );
 }
 
-sub ajax_calls_out :Chained('master') :PathPart('list/ajax/out') :Args(0) {
+sub ajax_calls_out :Chained('calllist_master') :PathPart('list/ajax/out') :Args(0) {
     my ($self, $c) = @_;
 
     my $rs = $c->model('DB')->resultset('cdr')->search({
