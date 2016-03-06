085a6cc335da169fabbfed4decce1e4de6c1b099
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-03-06 03:52:09 +0200

    MT#18407 Consider null in emailtemplate.reseller_id
    Change-Id: I3e2f4606697c04052ed8a976e5c78dbe953261b3

--- lib/NGCP/Panel/Controller/API/EmailTemplates.pm
+++ lib/NGCP/Panel/Controller/API/EmailTemplates.pm
@@ -79,7 +79,7 @@ sub GET :Allow {
         (my $total_count, $items) = $self->paginate_order_collection($c, $items);
         my (@embedded, @links);
         my $form = $self->get_form($c);
-        for my $item ($items->search({}, {prefetch => ['reseller']})->all) {
+        for my $item ($items->all) {
             push @embedded, $self->hal_from_item($c, $item, $form);
             push @links, Data::HAL::Link->new(
                 relation => 'ngcp:'.$self->resource_name,
