a24bc59bc58b32218bd49a4226e8cb7dc052d941
Rene Krenn <rkrenn@sipwise.com>
2016-03-07 11:46:30 +0100

    MT#18395 lazy load dashboard values
    csc dashboard recent calls cover last 24h
    
    (cherry picked from commit 4438756017eb637da93008854b2cae71a471668b)
    
    Conflicts:
    	lib/NGCP/Panel/Controller/Root.pm
    
    Change-Id: I375ad9f1ab65e56f2c7747378f674e3e03777023

--- lib/NGCP/Panel/Widget/Plugin/SubscriberCFOverview.pm
+++ lib/NGCP/Panel/Widget/Plugin/SubscriberCFOverview.pm
@@ -22,31 +22,13 @@ has 'priority' => (
 around handle => sub {
     my ($foo, $self, $c) = @_;
 
-
-    my $prov_subscriber = $c->user;
-    my $cfs = {};
-
-    foreach my $type(qw/cfu cfna cft cfb/) {
-        my $maps = $prov_subscriber->voip_cf_mappings
-            ->search({ type => $type });
-        $cfs->{$type} = [];
-        foreach my $map($maps->all) {
-            my @dset = map { { $_->get_columns } } $map->destination_set->voip_cf_destinations->search({},
-                { order_by => { -asc => 'priority' }})->all;
-            foreach my $d(@dset) {
-                $d->{as_string} = NGCP::Panel::Utils::Subscriber::destination_as_string($c, $d);
-            }
-            my @tset = ();
-            if($map->time_set) {
-                @tset = map { { $_->get_columns } } $map->time_set->voip_cf_periods->all;
-                foreach my $t(@tset) {
-                    $t->{as_string} = NGCP::Panel::Utils::Subscriber::period_as_string($t);
-                }
-            }
-            push @{ $cfs->{$type} }, { destinations => \@dset, periods => \@tset };
-        }
+    unless ($c->stash->{subscriber}) {
+        $c->stash(
+            subscriber => $c->model('DB')->resultset('voip_subscribers')->find({
+                uuid => $c->user->uuid,
+            }),
+        );
     }
-    $c->stash(cf_destinations => $cfs);
 
     return;
 };
@@ -60,6 +42,56 @@ sub filter {
         ref $c->controller eq 'NGCP::Panel::Controller::Dashboard'
     );
     return;
+}
+
+sub _get_cf_type_descriptions {
+    my ($self, $c) = @_;
+    return { cfu  => $c->loc("Call Forward Unconditional"),
+             cfb  => $c->loc("Call Forward Busy"),
+             cft  => $c->loc("Call Forward Timeout"),
+             cfna => $c->loc("Call Forward Unavailable") };
+}
+
+sub cfs {
+    my ($self, $c) = @_;
+
+    my $prov_subscriber = $c->user;
+    my $cfs = {};
+    my $descriptions = $self->_get_cf_type_descriptions($c);
+
+    foreach my $type (qw/cfu cfna cft cfb/) {
+        my $maps = $prov_subscriber->voip_cf_mappings
+            ->search({ type => $type });
+        my @mappings = ();
+        foreach my $map ($maps->all) {
+            my @dset = map { { $_->get_columns } } $map->destination_set->voip_cf_destinations->search({},
+                { order_by => { -asc => 'priority' }})->all;
+            foreach my $d (@dset) {
+                my $as_string = NGCP::Panel::Utils::Subscriber::destination_as_string($c, $d);
+                delete @$d{keys %$d};
+                $d->{as_string} = $as_string;
+            }
+            my @tset = ();
+            if ($map->time_set) {
+                @tset = map { { $_->get_columns } } $map->time_set->voip_cf_periods->all;
+                foreach my $t (@tset) {
+                    my $as_string = NGCP::Panel::Utils::Subscriber::period_as_string($t);
+                    delete @$t{keys %$t};
+                    $t->{as_string} = $as_string;
+                }
+            }
+            push @mappings, {
+                destinations => \@dset,
+                periods => \@tset,
+            };
+        }
+        $cfs->{$type} = {
+            mappings => \@mappings,
+            desc => $descriptions->{$type},
+        };
+    }
+
+    return $cfs;
 }
 
 1;
