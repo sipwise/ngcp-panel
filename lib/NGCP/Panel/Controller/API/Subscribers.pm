b0be8e2aaccd76954832c7b8313ffffa3c43fdd4
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-03-09 14:39:47 +0200

    MT#17495 Fix API doc for subscriber - no customer_id for update
    Change-Id: I9f60a476d33a992ec8d31db75ed4bcf98207bcdb

--- lib/NGCP/Panel/Controller/API/Subscribers.pm
+++ lib/NGCP/Panel/Controller/API/Subscribers.pm
@@ -29,6 +29,12 @@ sub api_description {
         'XMPP and place and receive calls via SIP. A subscriber always belongs to a '.
         '<a href="#customers">Customer</a> and is placed inside a <a href="#domains">Domain</a>.';
 }
+sub documentation_sample_update {
+    return { "domain_id" => 4,
+       "password" => "test",
+       "username" => "test",
+    };
+}
 
 sub query_params {
     return [
