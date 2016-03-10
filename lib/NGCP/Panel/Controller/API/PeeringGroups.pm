a86c91ca322273c778c8e5f83f4b748a1d5de59f
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-03-10 16:58:06 +0200

    MT#17173 Code reduction on peeringgroups example
    Change-Id: Id74847757c34493fd35c2fbf1d139fc6578c3d90

--- lib/NGCP/Panel/Controller/API/PeeringGroups.pm
+++ lib/NGCP/Panel/Controller/API/PeeringGroups.pm
@@ -1,199 +1,50 @@
 package NGCP::Panel::Controller::API::PeeringGroups;
-use NGCP::Panel::Utils::Generic qw(:all);
+
 
 use Sipwise::Base;
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::PeeringGroups/;
 
-use boolean qw(true);
-use Data::HAL qw();
-use Data::HAL::Link qw();
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
-#
-use NGCP::Panel::Utils::DateTime;
+
 use NGCP::Panel::Utils::Peering;
-use Path::Tiny qw(path);
-use Safe::Isa qw($_isa);
-use parent qw/Catalyst::Controller NGCP::Panel::Role::API::PeeringGroups/;#Catalyst::Controller
-require Catalyst::ActionRole::ACL;
-require Catalyst::ActionRole::CheckTrailingSlash;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
 
-sub allowed_methods{
+
+__PACKAGE__->set_config();
+
+sub allowed_methods {
     return [qw/GET POST OPTIONS HEAD/];
 }
 
 sub api_description {
     return 'Defines peering groups.';
-};
+}
 
 sub query_params {
     return [
         {
             param => 'name',
             description => 'Filter for peering group name',
-            query => {
-                first => sub {
-                    my $q = shift;
-                    { name => { like => $q } };
-                },
-                second => sub {},
-            },
+            query_type => 'string_like',
         },
         {
             param => 'description',
             description => 'Filter for peering group description',
-            query => {
-                first => sub {
-                    my $q = shift;
-                    { description => { like => $q } };
-                },
-                second => sub {},
-            },
+            query_type => 'string_like',
         },
     ];
 }
 
-__PACKAGE__->config(
-    action => {
-        map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin reseller/],
-            Args => 0,
-            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods }
-    },
-    action_roles => [qw(+NGCP::Panel::Role::HTTPMethods)],
-);
-
-sub auto :Private {
-    my ($self, $c) = @_;
-
-    $self->set_body($c);
-    $self->log_request($c);
-}
-
-sub GET :Allow {
-    my ($self, $c) = @_;
-    my $page = $c->request->params->{page} // 1;
-    my $rows = $c->request->params->{rows} // 10;
-    {
-        my $items = $self->item_rs($c);
-        (my $total_count, $items) = $self->paginate_order_collection($c, $items);
-        my (@embedded, @links);
-        my $form = $self->get_form($c);
-        for my $item ($items->all) {
-            push @embedded, $self->hal_from_item($c, $item, $form);
-            push @links, Data::HAL::Link->new(
-                relation => 'ngcp:'.$self->resource_name,
-                href     => sprintf('/%s%d', $c->request->path, $item->id),
-            );
-        }
-        push @links,
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            Data::HAL::Link->new(relation => 'self', href => sprintf('/%s?page=%s&rows=%s', $c->request->path, $page, $rows));
-        if(($total_count / $rows) > $page ) {
-            push @links, Data::HAL::Link->new(relation => 'next', href => sprintf('/%s?page=%d&rows=%d', $c->request->path, $page + 1, $rows));
-        }
-        if($page > 1) {
-            push @links, Data::HAL::Link->new(relation => 'prev', href => sprintf('/%s?page=%d&rows=%d', $c->request->path, $page - 1, $rows));
-        }
-
-        my $hal = Data::HAL->new(
-            embedded => [@embedded],
-            links => [@links],
-        );
-        $hal->resource({
-            total_count => $total_count,
-        });
-        my $response = HTTP::Response->new(HTTP_OK, undef, 
-            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
+sub create_item :Private {
+    my ($self, $c, $resource, $form) = @_;
+    my $item;
+    try {
+        $item = $c->model('DB')->resultset('voip_peer_groups')->create($resource);
+        NGCP::Panel::Utils::Peering::_sip_lcr_reload(c => $c);
+    } catch($e) {
+        $c->log->error("failed to create peering group: $e"); # TODO: user, message, trace, ...
+        $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to create peering group.");
+        last;
     }
-    return;
-}
-
-sub HEAD :Allow {
-    my ($self, $c) = @_;
-    $c->forward(qw(GET));
-    $c->response->body(q());
-    return;
-}
-
-sub OPTIONS :Allow {
-    my ($self, $c) = @_;
-    my $allowed_methods = $self->allowed_methods_filtered($c);
-    $c->response->headers(HTTP::Headers->new(
-        Allow => join(', ', @{ $allowed_methods }),
-        Accept_Post => 'application/hal+json; profile=http://purl.org/sipwise/ngcp-api/#rel-'.$self->resource_name,
-    ));
-    $c->response->content_type('application/json');
-    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
-    return;
-}
-
-sub POST :Allow {
-    my ($self, $c) = @_;
-
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $resource = $self->get_valid_post_data(
-            c => $c, 
-            media_type => 'application/json',
-        );
-        last unless $resource;
-
-        my $form = $self->get_form($c);
-        
-        last unless $self->validate_form(
-            c => $c,
-            resource => $resource,
-            form => $form,
-        );
-        $resource = $form->custom_get_values;
-        last unless $resource;
-        my $item;
-        my $dup_item = $c->model('DB')->resultset('voip_peer_groups')->find({
-            name => $resource->{name},
-        });
-        if($dup_item) {
-            $c->log->error("peering group with name '$$resource{name}' already exists"); # TODO: user, message, trace, ...
-            $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Peering group with this name already exists");
-            last;
-        }
-
-        try {
-            $item = $c->model('DB')->resultset('voip_peer_groups')->create($resource);
-            NGCP::Panel::Utils::Peering::_sip_lcr_reload(c => $c);
-        } catch($e){
-            $c->log->error("failed to create peering group: $e"); # TODO: user, message, trace, ...
-            $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to create peering group.");
-            last;
-        }
-
-        $guard->commit;
-
-        $c->response->status(HTTP_CREATED);
-        $c->response->header(Location => sprintf('/%s%d', $c->request->path, $item->id));
-        $c->response->body(q());
-    }
-    return;
-}
-
-sub end : Private {
-    my ($self, $c) = @_;
-
-    $self->log_response($c);
+    return $item;
 }
 
 1;
