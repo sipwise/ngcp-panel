04b21a99a1e4f311dcd23558fb272ffea02d3f51
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-03-10 16:58:06 +0200

    MT#17173 Code reduction on peeringgroups example
    Change-Id: Id74847757c34493fd35c2fbf1d139fc6578c3d90
    (cherry picked from commit a86c91ca322273c778c8e5f83f4b748a1d5de59f)

--- lib/NGCP/Panel/Controller/API/PeeringGroupsItem.pm
+++ lib/NGCP/Panel/Controller/API/PeeringGroupsItem.pm
@@ -1,197 +1,28 @@
 package NGCP::Panel::Controller::API::PeeringGroupsItem;
 
+use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::PeeringGroups/;
 
 use Sipwise::Base;
 
-use NGCP::Panel::Utils::Generic qw(:all);
-use HTTP::Headers qw();
+
 use HTTP::Status qw(:constants);
+use NGCP::Panel::Utils::Peering;
 
-use NGCP::Panel::Utils::DateTime;
-use NGCP::Panel::Utils::ValidateJSON qw();
-use Path::Tiny qw(path);
-use Safe::Isa qw($_isa);
-use parent qw/Catalyst::Controller NGCP::Panel::Role::API::PeeringGroups/;
-require Catalyst::ActionRole::ACL;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
 
-sub allowed_methods{
+__PACKAGE__->set_config();
+
+sub allowed_methods {
     return [qw/GET OPTIONS HEAD PATCH PUT DELETE/];
 }
 
-__PACKAGE__->config(
-    action => {
-        map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin reseller/],
-            Args => 1,
-            Does => [qw(ACL RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods }
-    },
-    action_roles => [qw(+NGCP::Panel::Role::HTTPMethods)],
-);
-
-sub auto :Private {
-    my ($self, $c) = @_;
-
-    $self->set_body($c);
-    $self->log_request($c);
-}
-
-sub GET :Allow {
-    my ($self, $c, $id) = @_;
-    {
-        last unless $self->valid_id($c, $id);
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, peeringgroup => $item);
-
-        my $hal = $self->hal_from_item($c, $item);
-
-        my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-            (map { # XXX Data::HAL must be able to generate links with multiple relations
-                s|rel="(http://purl.org/sipwise/ngcp-api/#rel-resellers)"|rel="item $1"|;
-                s/rel=self/rel="item self"/;
-                $_
-            } $hal->http_headers),
-        ), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
+sub delete_item {
+    my($self, $c, $item, $old_resource, $resource, $form) = @_;
+    foreach my $p ($item->voip_peer_hosts->all) {
+        $p->voip_peer_preferences->delete_all;
+        $p->delete;
     }
-    return;
-}
-
-sub HEAD :Allow {
-    my ($self, $c, $id) = @_;
-    $c->forward(qw(GET));
-    $c->response->body(q());
-    return;
-}
-
-sub OPTIONS :Allow {
-    my ($self, $c, $id) = @_;
-    my $allowed_methods = $self->allowed_methods_filtered($c);
-    $c->response->headers(HTTP::Headers->new(
-        Allow => join(', ', @{ $allowed_methods }),
-        Accept_Patch => 'application/json-patch+json',
-    ));
-    $c->response->content_type('application/json');
-    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
-    return;
-}
-
-sub PATCH :Allow {
-    my ($self, $c, $id) = @_;
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $preference = $self->require_preference($c);
-        last unless $preference;
-
-        my $json = $self->get_valid_patch_data(
-            c => $c, 
-            id => $id,
-            media_type => 'application/json-patch+json',
-        );
-        last unless $json;
-
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, peeringgroup => $item);
-        my $old_resource = { $item->get_inflated_columns };
-        my $resource = $self->apply_patch($c, $old_resource, $json);
-        last unless $resource;
-
-        my $form = $self->get_form($c);
-        $item = $self->update_item($c, $item, $old_resource, $resource, $form);
-        last unless $item;
-        
-        $guard->commit;
-
-        if ('minimal' eq $preference) {
-            $c->response->status(HTTP_NO_CONTENT);
-            $c->response->header(Preference_Applied => 'return=minimal');
-            $c->response->body(q());
-        } else {
-            my $hal = $self->hal_from_item($c, $item, $form);
-            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-                $hal->http_headers,
-            ), $hal->as_json);
-            $c->response->headers($response->headers);
-            $c->response->header(Preference_Applied => 'return=representation');
-            $c->response->body($response->content);
-        }
-    }
-    return;
-}
-
-sub PUT :Allow {
-    my ($self, $c, $id) = @_;
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $preference = $self->require_preference($c);
-        last unless $preference;
-
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, peeringgroup => $item);
-        my $resource = $self->get_valid_put_data(
-            c => $c,
-            id => $id,
-            media_type => 'application/json',
-        );
-        last unless $resource;
-        my $old_resource = { $item->get_inflated_columns };
-
-        my $form = $self->get_form($c);
-        $item = $self->update_item($c, $item, $old_resource, $resource, $form);
-        last unless $item;
-
-        $guard->commit; 
-
-        if ('minimal' eq $preference) {
-            $c->response->status(HTTP_NO_CONTENT);
-            $c->response->header(Preference_Applied => 'return=minimal');
-            $c->response->body(q());
-        } else {
-            my $hal = $self->hal_from_item($c, $item, $form);
-            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-                $hal->http_headers,
-            ), $hal->as_json);
-            $c->response->headers($response->headers);
-            $c->response->header(Preference_Applied => 'return=representation');
-            $c->response->body($response->content);
-        }
-    }
-    return;
-}
-
-sub DELETE :Allow {
-    my ($self, $c, $id) = @_;
-
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, peeringgroup => $item);
-
-        foreach my $p ($item->voip_peer_hosts->all) {
-            $p->voip_peer_preferences->delete_all;
-            $p->delete;
-        }
-        $item->delete;
-        NGCP::Panel::Utils::Peering::_sip_lcr_reload(c => $c);
-        $guard->commit;
-
-        $c->response->status(HTTP_NO_CONTENT);
-        $c->response->body(q());
-    }
-    return;
-}
-
-sub end : Private {
-    my ($self, $c) = @_;
-
-    $self->log_response($c);
+    $item->delete;
+    NGCP::Panel::Utils::Peering::_sip_lcr_reload(c => $c);
 }
 
 1;
