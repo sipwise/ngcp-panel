5283bedee54fe3225d3c3d4fcba4c37ad29e6590
Gerhard Jungwirth <gjungwirth@sipwise.com>
2016-03-18 14:10:52 +0100

    MT#17805 WIP callflow complex query
    Change-Id: I0e7ea23891dde9f491e648594622964a4ba0b1da

--- lib/NGCP/Panel/Utils/Callflow.pm
+++ lib/NGCP/Panel/Utils/Callflow.pm
@@ -4,6 +4,44 @@ use warnings;
 
 use NGCP::Panel::Utils::GD::Simple;
 
+sub get_packet_rs {
+    my ($schema, $callid, $config) = @_;
+
+    if ( $config->{callflow}{complex_callid_match} ) {
+        my $quoted_callid = quotemeta($callid);
+        return $schema->resultset('packets')->search({
+                'message.call_id' => { regexp => "^$quoted_callid(.*(_pbx-1|_b2b-1))?\$" },
+            }, {
+                join => { message_packets => 'message' },
+            });
+    } else {
+        return $schema->resultset('packets')->search({
+                'message.call_id' => { -in => [ $callid, $callid.'_b2b-1', $callid.'_pbx-1' ] },
+            }, {
+                join => { message_packets => 'message' },
+            });
+    }
+}
+
+sub get_message_rs {
+    my ($schema, $callid, $config) = @_;
+
+    if ( $config->{callflow}{complex_callid_match} ) {
+        my $quoted_callid = quotemeta($callid);
+        return $schema->resultset('messages')->search({
+                'me.call_id' => { regexp => "^$quoted_callid(.*(_pbx-1|_b2b-1))?\$" },
+            }, {
+                order_by => { -asc => 'timestamp' },
+            });
+    } else {
+        return $schema->resultset('messages')->search({
+                'me.call_id' => { -in => [ $callid, $callid.'_b2b-1', $callid.'_pbx-1' ] },
+            }, {
+                order_by => { -asc => 'timestamp' },
+            });
+    }
+}
+
 sub generate_pcap {
     my $packets = shift;
 
