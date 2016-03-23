f3c7845fb29e5053cbcc2eb1cffb00e5e1fdfff7
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-03-24 00:25:59 +0200

    MT#18601 Fix getting pbx relations
    Change-Id: Ie9f844d73b082abc74d989bc3da5fb41c62aa793

---
+++ sandbox/ngcp_panel.pl
@@ -0,0 +1,6 @@
+use NGCP::Panel;
+
+#my $model = MyApp->model('MyModel'); 
+#my $c = NGCP::Panel->prepare();
+my $controller = NGCP::Panel->controller('MyController'); 
+my $c = NGCP::Panel->prepare();
---
+++ share/tools/ngcp_panel.pl
@@ -0,0 +1,4 @@
+use NGCP::Panel;
+
+#my $model = MyApp->model('MyModel'); 
+my $c = NGCP::Panel->prepare();
