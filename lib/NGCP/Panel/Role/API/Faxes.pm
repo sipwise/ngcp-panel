d388ff2ca3de335fd9cd11b1ea8121aed9c412f9
Kirill Solomko <ksolomko@sipwise.com>
2016-04-02 17:13:49 +0200

    MT#18865 adapt use of provisioning.voip_fax_journal
    Change-Id: I55078355e15ad692bfe69d985b89d7c099679b0a

--- lib/NGCP/Panel/Role/API/Faxes.pm
+++ lib/NGCP/Panel/Role/API/Faxes.pm
@@ -9,23 +9,24 @@ use TryCatch;
 use Data::HAL qw();
 use Data::HAL::Link qw();
 use HTTP::Status qw(:constants);
+use DateTime::Format::Strptime;
 use NGCP::Panel::Form::Subscriber::WebfaxAPI;
 use NGCP::Panel::Utils::Subscriber;
 
 sub _item_rs {
     my ($self, $c) = @_;
 
-    my $item_rs = $c->model('DB')->resultset('fax_journal')->search({
+    my $item_rs = $c->model('DB')->resultset('voip_fax_journal')->search({
         'voip_subscriber.id' => { '!=' => undef },
     },{
-        join => { subscriber => { provisioning_voip_subscriber => 'voip_subscriber' } }
+        join => { 'provisioning_voip_subscriber' => 'voip_subscriber' }
     });
     if($c->user->roles eq "admin") {
     } elsif($c->user->roles eq "reseller") {
         $item_rs = $item_rs->search({
             'contact.reseller_id' => $c->user->reseller_id
         },{
-            join => { subscriber => { provisioning_voip_subscriber => { voip_subscriber => { contract => 'contact' } } } }
+            join => { provisioning_voip_subscriber => { voip_subscriber => { contract => 'contact' } } }
         });
     }
     return $item_rs;
@@ -50,7 +51,7 @@ sub hal_from_item {
             Data::HAL::Link->new(relation => 'collection', href => sprintf("/api/%s/", $self->resource_name)),
             Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
             Data::HAL::Link->new(relation => 'self', href => sprintf("%s%d", $self->dispatch_path, $item->id)),
-            Data::HAL::Link->new(relation => 'ngcp:subscribers', href => sprintf("/api/subscribers/%d", $item->subscriber->provisioning_voip_subscriber->voip_subscriber->id)),
+            Data::HAL::Link->new(relation => 'ngcp:subscribers', href => sprintf("/api/subscribers/%d", $item->provisioning_voip_subscriber->voip_subscriber->id)),
             Data::HAL::Link->new(relation => 'ngcp:faxrecordings', href => sprintf("/api/faxrecordings/%d", $item->id)),
         ],
         relation => 'ngcp:'.$self->resource_name,
@@ -66,11 +67,15 @@ sub resource_from_item {
 
     $form //= $self->get_form($c);
 
+   my $datetime_fmt = DateTime::Format::Strptime->new(
+        pattern => '%F %T',
+    );
+
     my %resource = ();
     $resource{id} = int($item->id);
-    $resource{time} = "" . $item->the_timestamp;
-    $resource{subscriber_id} = int($item->subscriber->provisioning_voip_subscriber->voip_subscriber->id);
-    foreach(qw/direction peer_name peer_number reason status quality filename/){
+    $resource->{time} = $datetime_fmt->format_datetime($item->time);
+    $resource{subscriber_id} = int($item->provisioning_voip_subscriber->voip_subscriber->id);
+    foreach(qw/direction caller callee reason status quality filename/){
         $resource{$_} = $item->$_;
     }
     foreach(qw/duration pages signal_rate/){
