e1d4371244d2a0e796f65dec5f25c57bc865a1e9
Gerhard Jungwirth <gjungwirth@sipwise.com>
2016-04-06 10:32:37 +0200

    MT#17461 validate max_subscribers with sipaccount
    Change-Id: Ib14086dea0b45dfa2097ef96267578a91d9aa168
    (cherry picked from commit 651b772538f59453572a049a77a3c7d0e03bdf55)

--- lib/NGCP/Panel/Form/Contract/ProductSelect.pm
+++ lib/NGCP/Panel/Form/Contract/ProductSelect.pm
@@ -25,5 +25,24 @@ has_block 'fields' => (
     render_list => [qw/contact billing_profile_definition billing_profile billing_profiles profile_add profile_package product max_subscribers status external_id subscriber_email_template passreset_email_template invoice_email_template invoice_template vat_rate add_vat/],
 );
 
+sub validate {
+    my $self = shift;
+
+    my $product = $self->field('product');
+    my $max_subscribers = $self->field('max_subscribers');
+    my $c = $self->ctx;
+
+    return unless $c;
+
+    my $sipaccount = $c->model('DB')->resultset('products')->find({class => 'sipaccount'});
+    return unless $sipaccount;
+    my $sipaccount_id = $sipaccount->id // 0;
+
+    if($max_subscribers->value && $product->field('id')->value == $sipaccount_id) {
+        $max_subscribers->add_error('Max Subscribers should not be set when the Product is "Basic SIP Account"');
+    }
+    return;
+}
+
 1;
 # vim: set tabstop=4 expandtab:
