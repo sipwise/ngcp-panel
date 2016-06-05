8a312f66cb4e2a7554287d4f20dba2199374e4f2
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-06-05 04:00:06 +0300

    MT#20315 Add email templates and invoice template ids...
    ...as a form "id" processing exception in update_item method
    
    Change-Id: I33167a17b6bad645c2ad9a32c13f58924b52a80e

--- lib/NGCP/Panel/Role/API/Customers.pm
+++ lib/NGCP/Panel/Role/API/Customers.pm
@@ -160,7 +160,7 @@ sub update_customer {
         c => $c,
         form => $form,
         resource => $resource,
-        exceptions => [ "contact_id", "billing_profile_id", "profile_package_id" ],
+        exceptions => [ "contact_id", "billing_profile_id", "profile_package_id", "invoice_email_template_id", "invoice_template_id", "passreset_email_template_id", "subscriber_email_template_id" ],
     );
     #$resource->{profile_package_id} = undef unless NGCP::Panel::Utils::ProfilePackages::ENABLE_PROFILE_PACKAGES;
 
