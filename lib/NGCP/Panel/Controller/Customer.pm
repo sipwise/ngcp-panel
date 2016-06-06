c6732830dbf199c71acb9099532e26a3bbc8d4b5
Kirill Solomko <ksolomko@sipwise.com>
2016-06-06 12:11:30 +0200

    MT#20325 - TryCatch syntax fix
    Change-Id: I00eb2f03032f064a6031447e2a0a0e80048c88f8
    (cherry picked from commit 84d4624bf8157d59f7d5c13c92312afa98b13b60)
    (cherry picked from commit 21e3897fecb4fcd15c727743dde833b3a40da2ba)

--- lib/NGCP/Panel/Controller/Customer.pm
+++ lib/NGCP/Panel/Controller/Customer.pm
@@ -990,8 +990,7 @@ sub edit_balance :Chained('base_restricted') :PathPart('balance/edit') :Args(0) 
                 c => $c,
                 desc => $c->loc('Account balance successfully changed!'),
             );
-        }
-        catch($e) {
+        } catch($e) {
             NGCP::Panel::Utils::Message::error(
                 c => $c,
                 error => $e,
@@ -1067,8 +1066,7 @@ sub topup_cash :Chained('base_restricted') :PathPart('balance/topupcash') :Args(
                 c => $c,
                 desc => $c->loc('Top-up using cash performed successfully!'),
             );
-        }
-        catch($e) {
+        } catch($e) {
             NGCP::Panel::Utils::Message::error(
                 c => $c,
                 error => $e,
@@ -1089,8 +1087,7 @@ sub topup_cash :Chained('base_restricted') :PathPart('balance/topupcash') :Args(
                     request_token => NGCP::Panel::Utils::ProfilePackages::PANEL_TOPUP_REQUEST_TOKEN,
                 );                
             });
-        }
-        catch($e) {
+        } catch($e) {
             $c->log->error("failed to create topup log record: $e");
         }
         
@@ -1163,8 +1160,7 @@ sub topup_voucher :Chained('base_restricted') :PathPart('balance/topupvoucher') 
                 c => $c,
                 desc => $c->loc('Top-up using voucher performed successfully!'),
             );
-        }
-        catch($e) {
+        } catch($e) {
             NGCP::Panel::Utils::Message::error(
                 c => $c,
                 error => $e,
@@ -1185,8 +1181,7 @@ sub topup_voucher :Chained('base_restricted') :PathPart('balance/topupvoucher') 
                     request_token => NGCP::Panel::Utils::ProfilePackages::PANEL_TOPUP_REQUEST_TOKEN,
                 );                
             });
-        }
-        catch($e) {
+        } catch($e) {
             $c->log->error("failed to create topup log record: $e");
         }        
         
