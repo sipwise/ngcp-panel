6f45f09f800503f062aeb43f92853da31a2341b0
Gerhard Jungwirth <gjungwirth@sipwise.com>
2016-06-17 12:36:43 +0200

    MT#19795 create rtcapps REST api endpoint
    Change-Id: Ic3a7d41865f464e3f51b08c5506e1ea59d78d29b

---
+++ lib/NGCP/Panel/Controller/API/RtcApps.pm
@@ -0,0 +1,137 @@
+package NGCP::Panel::Controller::API::RtcApps;
+use NGCP::Panel::Utils::Generic qw(:all);
+use TryCatch;
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
+require Catalyst::ActionRole::ACL;
+require Catalyst::ActionRole::CheckTrailingSlash;
+require NGCP::Panel::Role::HTTPMethods;
+require Catalyst::ActionRole::RequireSSL;
+
+
+sub allowed_methods{
+    return [qw/GET OPTIONS HEAD/];
+}
+
+sub api_description {
+    return 'Show a collection of RTC apps, belonging to a specific reseller.';
+};
+
+sub query_params {
+    return [];
+}
+
+use parent qw/Catalyst::Controller NGCP::Panel::Role::API::RtcApps/;
+
+sub resource_name{
+    return 'rtcapps';
+}
+sub dispatch_path{
+    return '/api/rtcapps/';
+}
+sub relation{
+    return 'http://purl.org/sipwise/ngcp-api/#rel-rtcapps';
+}
+
+__PACKAGE__->config(
+    action => {
+        map { $_ => {
+            ACLDetachTo => '/api/root/invalid_user',
+            AllowedRole => [qw/admin reseller/],
+            Args => 0,
+            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
+            Method => $_,
+            Path => __PACKAGE__->dispatch_path,
+        } } @{ __PACKAGE__->allowed_methods },
+    },
+    action_roles => [qw(+NGCP::Panel::Role::HTTPMethods)],
+);
+
+sub auto :Private {
+    my ($self, $c) = @_;
+
+    $self->set_body($c);
+    $self->log_request($c);
+    return 1;
+}
+
+sub GET :Allow {
+    my ($self, $c) = @_;
+    my $page = $c->request->params->{page} // 1;
+    my $rows = $c->request->params->{rows} // 10;
+    {
+        my $resellers = $self->item_rs($c);
+        (my $total_count, $resellers) = $self->paginate_order_collection($c, $resellers);
+        my (@embedded, @links);
+        for my $reseller ($resellers->all) {
+            push @embedded, $self->hal_from_item($c, $reseller);
+            push @links, Data::HAL::Link->new(
+                relation => 'ngcp:'.$self->resource_name,
+                href     => sprintf('%s%d', $self->dispatch_path, $reseller->id),
+            );
+        }
+        push @links,
+            Data::HAL::Link->new(
+                relation => 'curies',
+                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
+                name => 'ngcp',
+                templated => true,
+            ),
+            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
+            Data::HAL::Link->new(relation => 'self', href => sprintf('%s?page=%s&rows=%s', $self->dispatch_path, $page, $rows));
+        if(($total_count / $rows) > $page ) {
+            push @links, Data::HAL::Link->new(relation => 'next', href => sprintf('%s?page=%d&rows=%d', $self->dispatch_path, $page + 1, $rows));
+        }
+        if($page > 1) {
+            push @links, Data::HAL::Link->new(relation => 'prev', href => sprintf('/%s?page=%d&rows=%d', $c->request->path, $page - 1, $rows));
+        }
+
+        my $hal = Data::HAL->new(
+            embedded => [@embedded],
+            links => [@links],
+        );
+        $hal->resource({
+            total_count => $total_count,
+        });
+        my $response = HTTP::Response->new(HTTP_OK, undef,
+            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
+        $c->response->headers($response->headers);
+        $c->response->body($response->content);
+        return;
+    }
+    return;
+}
+
+sub HEAD :Allow {
+    my ($self, $c) = @_;
+    $c->forward(qw(GET));
+    $c->response->body(q());
+    return;
+}
+
+sub OPTIONS :Allow {
+    my ($self, $c) = @_;
+    my $allowed_methods = $self->allowed_methods_filtered($c);
+    $c->response->headers(HTTP::Headers->new(
+        Allow => join(', ', @{ $allowed_methods }),
+        Accept_Post => 'application/hal+json; profile=http://purl.org/sipwise/ngcp-api/#rel-'.$self->resource_name,
+    ));
+    $c->response->content_type('application/json');
+    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
+    return;
+}
+
+sub end : Private {
+    my ($self, $c) = @_;
+
+    $self->log_response($c);
+    return 1;
+}
+
+no Moose;
+1;
+
+# vim: set tabstop=4 expandtab:
