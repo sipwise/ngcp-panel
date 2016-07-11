082edb874cc813e41e6fda3816f76fbcb8ae241c
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-07-11 23:52:52 +0300

    MT#21097 Start of HTTP methods
    Change-Id: Ib943d886b24139c17ba0506abd6070cd21af9dc9

---
+++ lib/NGCP/Panel/Utils/DeviceBootstrap/Innovaphone.pm
@@ -0,0 +1,83 @@
+package NGCP::Panel::Utils::DeviceBootstrap::Panasonic;
+
+use strict;
+use Moose;
+use Data::Dumper;
+extends 'NGCP::Panel::Utils::DeviceBootstrap::VendorRPC';
+
+sub rpc_server_params{
+    my $self = shift;
+    my $cfg  = {
+        proto    => 'https',
+        host     => 'my.innovaphone.com',
+        port     => '443',
+        path     => '/redirect/xmlrpc',
+    };
+    #$cfg->{headers} = { %{$self->get_basic_authorization($self->params->{credentials})} };
+    --innovaphone-user='office@sipwise.com' --innovaphone-password='innova4Sipwise\!'
+    $cfg->{headers} = { %{$self->get_basic_authorization( {user => 'office@sipwise.com', password => 'innova4Sipwise!' } )} };
+    $self->{rpc_server_params} = $cfg;
+    return $self->{rpc_server_params};
+}
+
+sub register_content {
+    my $self = shift;
+#<param><value><string>".URI::Escape::uri_escape($self->content_params->{uri})."</string></value></param> 
+    $self->{register_content} = "<?xml version=\"1.0\"?> 
+<methodCall> 
+<methodName>ipredirect.registerPhone</methodName> 
+<params> 
+<param><value><string>".$self->content_params->{mac}."</string></value></param> 
+<param><value><string><![CDATA[".$self->content_params->{uri}."]]></string></value></param> 
+</params> 
+</methodCall>";
+    return $self->{register_content};
+}
+
+sub unregister_content {
+    my $self = shift;
+    $self->{unregister_content} =  "<?xml version=\"1.0\"?> 
+<methodCall> 
+<methodName>ipredirect.unregisterPhone</methodName> 
+<params> 
+<param><value><string>".$self->content_params->{mac_old}."</string></value></param> 
+</params> 
+</methodCall>";
+    return $self->{unregister_content};
+}
+
+override 'process_bootstrap_uri' => sub {
+    my($self,$uri) = @_;
+    $uri = super($uri);
+    $uri = $self->bootstrap_uri_mac($uri);
+    $self->content_params->{uri} = $uri;
+    return $self->content_params->{uri};
+};
+
+1;
+
+=head1 NAME
+
+NGCP::Panel::Utils::DeviceBootstrap
+
+=head1 DESCRIPTION
+
+Make API requests to configure remote redirect servers for requested MAC with autorpov uri.
+
+=head1 METHODS
+
+=head2 bootstrap
+
+Dispatch to proper vendor API call.
+
+=head1 AUTHOR
+
+Irina Peshinskaya C<< <ipeshinskaya@sipwise.com> >>
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+# vim: set tabstop=4 expandtab:
