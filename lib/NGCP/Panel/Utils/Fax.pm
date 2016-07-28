a4543893b9cc588f02707fbbc76ae270b24d6fcf
Kirill Solomko <ksolomko@sipwise.com>
2016-07-28 12:44:55 +0200

    MT#18171 use spool_dir+filename for faxrecordings retrieval
    Change-Id: I5e4edbf72613e82d74a6c829247108457a45eea8

--- lib/NGCP/Panel/Utils/Fax.pm
+++ lib/NGCP/Panel/Utils/Fax.pm
@@ -1,6 +1,8 @@
 package NGCP::Panel::Utils::Fax;
 
+use English;
 use File::Temp qw/tempfile/;
+use File::Slurp;
 use TryCatch;
 use IPC::System::Simple qw/capture/;
 use Data::Dumper;
@@ -14,7 +16,7 @@ sub send_fax {
     if ($@) {
         if ($@ =~ m#Can't locate NGCP/Fax.pm#) {
             $c->log->debug("Fax features are not supported in the Community Edition");
-            return; 
+            return;
         } else {
             die $@;
         }
@@ -53,6 +55,55 @@ sub send_fax {
     my $client = new NGCP::Fax;
     $client->send_fax(\%sendfax_args);
     $c->log->debug("webfax: res=$res;");
+}
+
+sub get_fax {
+    my (%args) = @_;
+    my $c = $args{c};
+
+    #moved here due to CE, as it doesn't carry NGCP::fax
+    eval { require NGCP::Fax; };
+    if ($@) {
+        if ($@ =~ m#Can't locate NGCP/Fax.pm#) {
+            $c->log->debug("Fax features are not supported in the Community Edition");
+            return;
+        } else {
+            die $@;
+        }
+    }
+
+    my ($filename, $format) = @{args}{qw(filename format)};
+    return unless $filename;
+    my $spool = $c->config->{faxserver}{spool_dir} || return;
+    my $filepath;
+    foreach my $dir (qw(done failed)) {
+        my $check_path = sprintf "%s/%s/%s", $spool, $dir, $filename;
+        if (-e $check_path) {
+            $filepath = $check_path;
+            last;
+        }
+    }
+    return unless $filepath;
+
+
+    my $content;
+    my $ext = 'tif';
+
+    if ($format) {
+        my $client = new NGCP::Fax;
+        my $fh = $client->convert_file({}, $filepath, $format);
+        my $rs_old = $RS;
+        local $RS = undef;
+        $content = <$fh>;
+        local $RS = $rs_old;
+        close $fh;
+        $ext = $client->formats->{$format}->{extension};
+    } else {
+        eval { $content = read_file($filepath, binmode => ':raw'); };
+        return if $@;
+    }
+
+    return ($content, $ext);
 }
 
 1;
