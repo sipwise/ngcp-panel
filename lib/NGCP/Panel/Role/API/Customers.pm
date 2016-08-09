201a39e8757d6f4d0b43dfc9a6a68da56774c30c
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-08-10 00:28:21 +0300

    Revert "MT#20315 Add email templates and invoice template ids..."
    MT#21819 Fix incorrect backporting - old form didn't need exceptions
    
    This reverts commit 68d7b8d58d3772dbfc3c443811434248dbce226d.
    
    Change-Id: Ibfe4ada802d6c4a7801e3dea48a7b06078819137

--- lib/NGCP/Panel/Role/API/Customers.pm
+++ lib/NGCP/Panel/Role/API/Customers.pm
@@ -145,7 +145,6 @@ sub update_customer {
         c => $c,
         form => $form,
         resource => $resource,
-        exceptions => [ "contact_id", "billing_profile_id", "profile_package_id", "invoice_email_template_id", "invoice_template_id", "passreset_email_template_id", "subscriber_email_template_id" ],
     );
 
     my $now = NGCP::Panel::Utils::DateTime::current_local;
