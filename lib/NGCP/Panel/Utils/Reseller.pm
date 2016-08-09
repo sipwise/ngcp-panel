1f1860d613b5513e2e4593b623e7b92ee3e8aab6
Gerhard Jungwirth <gjungwirth@sipwise.com>
2016-08-09 14:51:49 +0200

    MT#21543 fix reseller terminate emergency mapping
    Change-Id: I542fcca4914902f51523079b85bf945fa9c6998a
    (cherry picked from commit 31e54a8be59350f25f20a30f47973707579362ea)

--- lib/NGCP/Panel/Utils/Reseller.pm
+++ lib/NGCP/Panel/Utils/Reseller.pm
@@ -149,7 +149,7 @@ sub _handle_reseller_status_change {
         #delete autoprov_devices
         $reseller->autoprov_devices->delete_all;
         $reseller->email_templates->delete_all;
-        $reseller->emergency_containers->emergency_mappings->delete_all;
+        $reseller->emergency_containers->search_related_rs('emergency_mappings')->delete_all;
         $reseller->emergency_containers->delete_all;
     }
 }
