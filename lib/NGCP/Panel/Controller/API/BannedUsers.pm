e961a661f353e85990e1c373e1584be5cc713e78
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-10-06 14:06:52 +0300

    TT#3648 Add banned ips and users API interface
    Change-Id: Ifbaafdfaf2194f84729bfb91c79453fd186dc5c6

---
+++ lib/NGCP/Panel/Controller/API/BannedUsers.pm
@@ -0,0 +1,28 @@
+package NGCP::Panel::Controller::API::BannedUsers;
+
+
+use Sipwise::Base;
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::BannedUsers/;
+
+
+use NGCP::Panel::Utils::Peering;
+use HTTP::Status qw(:constants);
+use NGCP::Panel::Utils::Security;
+
+__PACKAGE__->set_config();
+
+sub allowed_methods {
+    return [qw/GET OPTIONS HEAD/];
+}
+
+sub api_description {
+    return 'Defines banned users.';
+}
+
+sub get_list{
+    my ($self, $c) = @_;
+    return NGCP::Panel::Utils::Security::list_banned_users($c, data_for_json => 1);
+}
+1;
+
+# vim: set tabstop=4 expandtab:
--- lib/NGCP/Panel/Controller/Security.pm
+++ lib/NGCP/Panel/Controller/Security.pm
@@ -4,10 +4,8 @@ use Sipwise::Base;
 
 use parent 'Catalyst::Controller';
 
-use XML::LibXML;
-use URI::Encode;
+use NGCP::Panel::Utils::Security;
 use NGCP::Panel::Utils::Navigation;
-use NGCP::Panel::Utils::XMLDispatcher;
 use NGCP::Panel::Utils::DateTime;
 
 sub auto :Does(ACL) :ACLDetachTo('/denied_page') :AllowedRole(admin) {
@@ -23,74 +21,14 @@ sub root :PathPart('/') :CaptureArgs(0) {
 
 sub index :Chained('/') :PathPart('security') :Args(0) {
     my ( $self, $c ) = @_;
-    my $dispatcher = NGCP::Panel::Utils::XMLDispatcher->new;
-    my $xml_parser = XML::LibXML->new();
 
-    my $ip_xml = <<'EOF';
-<?xml version="1.0" ?>
-<methodCall>
-    <methodName>htable.dump</methodName>
-    <params>
-        <param><value><string>ipban</string></value></param>
-    </params>
-</methodCall>
-EOF
-
-    my $ip_res = $dispatcher->dispatch($c, "loadbalancer", 1, 1, $ip_xml);
-
-    my @ips = ();
-    for my $host (grep {$$_[1]} @$ip_res) {
-        my $xmlDoc = $xml_parser->parse_string($host->[2]);
-        @ips = map { { ip => $_->to_literal } }
-                    $xmlDoc->findnodes('//member/value/string');
-    }
-
-
-    my $user_xml = <<'EOF';
-<?xml version="1.0" ?>
-<methodCall>
-    <methodName>htable.dump</methodName>
-    <params>
-        <param><value><string>auth</string></value></param>
-    </params>
-</methodCall>
-EOF
-
-    my $user_res = $dispatcher->dispatch($c, "loadbalancer", 1, 1, $user_xml);
-    my @users = ();
-    my $usr = {};
-    for my $host (grep {$$_[1]} @$user_res) {
-        my $xmlDoc = $xml_parser->parse_string($host->[2]);
-        my $username = '';
-        my $key = '';
-        foreach my $node ($xmlDoc->findnodes('//member')) {
-            my $name = $node->findvalue('./name');
-            my $value = $node->findvalue('./value/string') ||
-                        $node->findvalue('./value/int');
-            if ($name eq 'name') {
-                $value =~ m/(?<user>.*)::(?<key>.*)/;
-                $username = $+{user};
-                $key = $+{key};
-            } elsif ($name eq 'value' && $username && $key) {
-                # there souldn't be any other keys
-                $key eq 'auth_count' and $usr->{$username}->{auth_count} = $value;
-                $key eq 'last_auth' and $usr->{$username}->{last_auth} = $value;
-            }
-        }
-    }
-
-    for my $key (keys %{ $usr }) {
-        push @users, {
-            username => $key,
-            auth_count => $usr->{$key}->{auth_count},
-            last_auth => NGCP::Panel::Utils::DateTime::epoch_local($usr->{$key}->{last_auth}),
-        } if($usr->{$key}->{auth_count} >= $c->config->{security}->{failed_auth_attempts});
-    }
+    my $ips = NGCP::Panel::Utils::Security::list_banned_ips($c);
+    my $users = NGCP::Panel::Utils::Security::list_banned_users($c);
 
     $c->stash(
         template => 'security/list.tt',
-        banned_ips => \@ips,
-        banned_users => \@users,
+        banned_ips => $ips,
+        banned_users => $users,
     );
 }
 
@@ -102,21 +40,8 @@ sub ip_base :Chained('/') :PathPart('security/ip') :CaptureArgs(1) {
 
 sub ip_unban :Chained('ip_base') :PathPart('unban') :Args(0) {
     my ( $self, $c ) = @_;
-    my $dispatcher = NGCP::Panel::Utils::XMLDispatcher->new;
     my $ip = $c->stash->{ip};
-
-    my $xml = <<"EOF";
-<?xml version="1.0" ?>
-<methodCall>
-    <methodName>htable.delete</methodName>
-    <params>
-        <param><value><string>ipban</string></value></param>
-        <param><value><string>$ip</string></value></param>
-    </params>
-</methodCall>
-EOF
-
-    $dispatcher->dispatch($c, "loadbalancer", 1, 1, $xml);
+    NGCP::Panel::Utils::Security::ip_unban($c, $ip);
     NGCP::Panel::Utils::Message::info(
         c    => $c,
         data => { ip => $ip },
@@ -133,24 +58,8 @@ sub user_base :Chained('/') :PathPart('security/user') :CaptureArgs(1) {
 
 sub user_unban :Chained('user_base') :PathPart('unban') :Args(0) {
     my ( $self, $c ) = @_;
-    my $dispatcher = NGCP::Panel::Utils::XMLDispatcher->new;
     my $user = $c->stash->{user};
-
-    my @keys = ($user.'::auth_count', $user.'::last_auth');
-    foreach my $key (@keys) {
-        my $xml = <<"EOF";
-<?xml version="1.0" ?>
-<methodCall>
-    <methodName>htable.delete</methodName>
-    <params>
-        <param><value><string>auth</string></value></param>
-        <param><value><string>$key</string></value></param>
-    </params>
-</methodCall>
-EOF
-
-        $dispatcher->dispatch($c, "loadbalancer", 1, 1, $xml);
-    }
+    NGCP::Panel::Utils::Security::ip_unban($c, $user);
     NGCP::Panel::Utils::Message::info(
         c    => $c,
         data => { user => $user },
