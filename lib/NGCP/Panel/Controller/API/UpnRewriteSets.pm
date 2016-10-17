8bf49f65819e3eab5fbb339f9623ee55f2cdb822
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-10-18 02:56:14 +0300

    TT#28460 Remove copy-pasted sub HEAD and sub end
    Change-Id: Ia06320ead891945eebe715fa34012a14d5529f17

--- lib/NGCP/Panel/Controller/API/UpnRewriteSets.pm
+++ lib/NGCP/Panel/Controller/API/UpnRewriteSets.pm
@@ -123,12 +123,7 @@ sub GET :Allow {
     return;
 }
 
-sub HEAD :Allow {
-    my ($self, $c) = @_;
-    $c->forward(qw(GET));
-    $c->response->body(q());
-    return;
-}
+
 
 
 
@@ -203,11 +198,7 @@ sub POST :Allow {
     return;
 }
 
-sub end : Private {
-    my ($self, $c) = @_;
 
-    $self->log_response($c);
-}
 
 1;
 
