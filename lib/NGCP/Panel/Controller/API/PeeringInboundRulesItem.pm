8be2e6747e124bd2417e738aef06f96eb2381e4c
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-10-18 02:56:14 +0300

    TT#28460 Remove copy-pasted sub HEAD and sub end
    Change-Id: Ia06320ead891945eebe715fa34012a14d5529f17

--- lib/NGCP/Panel/Controller/API/PeeringInboundRulesItem.pm
+++ lib/NGCP/Panel/Controller/API/PeeringInboundRulesItem.pm
@@ -71,12 +71,7 @@ sub GET :Allow {
     return;
 }
 
-sub HEAD :Allow {
-    my ($self, $c, $id) = @_;
-    $c->forward(qw(GET));
-    $c->response->body(q());
-    return;
-}
+
 
 
 
@@ -184,11 +179,7 @@ sub DELETE :Allow {
     return;
 }
 
-sub end : Private {
-    my ($self, $c) = @_;
 
-    $self->log_response($c);
-}
 
 1;
 
