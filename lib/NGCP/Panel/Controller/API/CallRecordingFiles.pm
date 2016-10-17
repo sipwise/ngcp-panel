8f9ef0313a0af2f27c85e491bf2458348e3acdcd
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-10-18 02:13:18 +0300

    TT#28460 Remove copy-pasted sub OPTIONS
    Change-Id: I2b00ea267edcc3caa00eaac5753638794a13398c

--- lib/NGCP/Panel/Controller/API/CallRecordingFiles.pm
+++ lib/NGCP/Panel/Controller/API/CallRecordingFiles.pm
@@ -52,16 +52,6 @@ sub gather_default_action_roles {
     push @roles, 'NGCP::Panel::Role::HTTPMethods' if $args{attributes}->{Method};
     return @roles;
 }
-sub OPTIONS :Allow {
-    my ($self, $c) = @_;
-    my $allowed_methods = $self->allowed_methods_filtered($c);
-    $c->response->headers(HTTP::Headers->new(
-        Allow => join(', ', @{ $allowed_methods }),
-    ));
-    $c->response->content_type('application/json');
-    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
-    return;
-}
 
 sub end : Private {
     my ($self, $c) = @_;
