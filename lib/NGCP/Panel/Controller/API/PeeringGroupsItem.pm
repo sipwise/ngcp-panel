c236d002a87581d5bf5505613eb5cf632d2ec579
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-10-18 02:18:08 +0300

    TT#28460 Change parents in API controllers
    Use Entities and EntitiesItem respectively
    
    Change-Id: I5d32b010cc51810acee42d14c5c24ce3d4267602

--- lib/NGCP/Panel/Controller/API/PeeringGroupsItem.pm
+++ lib/NGCP/Panel/Controller/API/PeeringGroupsItem.pm
@@ -8,7 +8,7 @@ use HTTP::Headers qw();
 use HTTP::Status qw(:constants);
 
 use NGCP::Panel::Utils::ValidateJSON qw();
-use parent qw/Catalyst::Controller NGCP::Panel::Role::API::PeeringGroups/;
+use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::PeeringGroups/;
 require Catalyst::ActionRole::ACL;
 require NGCP::Panel::Role::HTTPMethods;
 require Catalyst::ActionRole::RequireSSL;
