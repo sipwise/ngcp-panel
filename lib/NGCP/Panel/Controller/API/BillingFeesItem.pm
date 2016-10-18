f64c21039db263b71295800535a934b8d56df71d
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-10-18 15:42:06 +0300

    TT#28460 Introduce return_representation in item packages
    Change-Id: Ibc2afdffd8a70245b6656f78bad2c4fc5273526e

--- lib/NGCP/Panel/Controller/API/BillingFeesItem.pm
+++ lib/NGCP/Panel/Controller/API/BillingFeesItem.pm
@@ -101,20 +101,8 @@ sub PATCH :Allow {
         last unless $fee;
 
         $guard->commit;
-
-        if ('minimal' eq $preference) {
-            $c->response->status(HTTP_NO_CONTENT);
-            $c->response->header(Preference_Applied => 'return=minimal');
-            $c->response->body(q());
-        } else {
-            my $hal = $self->hal_from_fee($c, $fee, $form);
-            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-                $hal->http_headers,
-            ), $hal->as_json);
-            $c->response->headers($response->headers);
-            $c->response->header(Preference_Applied => 'return=representation');
-            $c->response->body($response->content);
-        }
+        my $hal = $self->hal_from_fee($c, $fee, $form);
+        $self->return_representation($c, 'hal' => $hal, 'preference' => $preference );
     }
     return;
 }
@@ -141,20 +129,8 @@ sub PUT :Allow {
         last unless $fee;
 
         $guard->commit;
-
-        if ('minimal' eq $preference) {
-            $c->response->status(HTTP_NO_CONTENT);
-            $c->response->header(Preference_Applied => 'return=minimal');
-            $c->response->body(q());
-        } else {
-            my $hal = $self->hal_from_fee($c, $fee, $form);
-            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-                $hal->http_headers,
-            ), $hal->as_json);
-            $c->response->headers($response->headers);
-            $c->response->header(Preference_Applied => 'return=representation');
-            $c->response->body($response->content);
-        }
+        my $hal = $self->hal_from_fee($c, $fee, $form);
+        $self->return_representation($c, 'hal' => $hal, 'preference' => $preference );
     }
     return;
 }
