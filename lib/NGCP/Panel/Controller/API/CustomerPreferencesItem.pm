b03bd969ed1b8cd77352f72a49f5c3de9032c09b
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-10-18 15:42:06 +0300

    MT#17173 Return_representation in item packages
    Change-Id: Ibc2afdffd8a70245b6656f78bad2c4fc5273526e

--- lib/NGCP/Panel/Controller/API/CustomerPreferencesItem.pm
+++ lib/NGCP/Panel/Controller/API/CustomerPreferencesItem.pm
@@ -110,19 +110,7 @@ sub PATCH :Allow {
 
         $guard->commit; 
 
-        if ('minimal' eq $preference) {
-            $c->response->status(HTTP_NO_CONTENT);
-            $c->response->header(Preference_Applied => 'return=minimal');
-            $c->response->body(q());
-        } else {
-            #my $hal = $self->hal_from_item($c, $customer, "contracts");
-            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-                $hal->http_headers,
-            ), $hal->as_json);
-            $c->response->headers($response->headers);
-            $c->response->header(Preference_Applied => 'return=representation');
-            $c->response->body($response->content);
-        }
+        $self->return_representation($c, 'hal' => $hal, 'preference' => $preference );
     }
     return;
 }
@@ -154,19 +142,7 @@ sub PUT :Allow {
 
         $guard->commit; 
 
-        if ('minimal' eq $preference) {
-            $c->response->status(HTTP_NO_CONTENT);
-            $c->response->header(Preference_Applied => 'return=minimal');
-            $c->response->body(q());
-        } else {
-            #my $hal = $self->hal_from_item($c, $customer, "contracts");
-            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-                $hal->http_headers,
-            ), $hal->as_json);
-            $c->response->headers($response->headers);
-            $c->response->header(Preference_Applied => 'return=representation');
-            $c->response->body($response->content);
-        }
+        $self->return_representation($c, 'hal' => $hal, 'preference' => $preference );
     }
     return;
 }
