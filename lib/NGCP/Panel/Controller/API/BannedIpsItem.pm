1b131b2b5f0b73508efc90cb931f0bc5f4aac751
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-10-26 19:58:04 +0300

    TT#5203 Add banned ips and users API interface
    (cherry picked from commit e0dd0f0f036c5c247ed66d8bfccc609ea9ecf4be)
    
    Change-Id: Icdd27129dc39b5b8fe39d2aba69820ac0a5a0671

---
+++ lib/NGCP/Panel/Controller/API/BannedIpsItem.pm
@@ -0,0 +1,105 @@
+package NGCP::Panel::Controller::API::BannedIpsItem;
+use NGCP::Panel::Utils::Generic qw(:all);
+
+use Sipwise::Base;
+
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
+
+use NGCP::Panel::Utils::ValidateJSON qw();
+use NGCP::Panel::Utils::DateTime;
+use Path::Tiny qw(path);
+use Safe::Isa qw($_isa);
+require Catalyst::ActionRole::ACL;
+require NGCP::Panel::Role::HTTPMethods;
+require Catalyst::ActionRole::RequireSSL;
+
+sub allowed_methods{
+    return [qw/OPTIONS HEAD DELETE/];
+}
+
+use parent qw/Catalyst::Controller NGCP::Panel::Role::API::BannedIps/;
+
+sub resource_name{
+    return 'bannedips';
+}
+sub dispatch_path{
+    return '/api/bannedips/';
+}
+sub relation{
+    return 'http://purl.org/sipwise/ngcp-api/#rel-bannedips';
+}
+__PACKAGE__->config(
+    action => {
+        (map { $_ => {
+            ACLDetachTo => '/api/root/invalid_user',
+            AllowedRole => [qw/admin reseller/],
+            Args => 1,
+            Does => [qw(ACL RequireSSL)],
+            Method => $_,
+            Path => __PACKAGE__->dispatch_path,
+        } } @{ __PACKAGE__->allowed_methods }),
+    },
+    action_roles => [qw(+NGCP::Panel::Role::HTTPMethods)],
+);
+
+sub auto :Private {
+    my ($self, $c) = @_;
+
+    $self->set_body($c);
+    $self->log_request($c);
+    return 1;
+}
+sub HEAD :Allow {
+    my ($self, $c, $id) = @_;
+    $c->forward(qw(GET));
+    $c->response->body(q());
+    return;
+}
+
+sub OPTIONS :Allow {
+    my ($self, $c, $id) = @_;
+    my $allowed_methods = $self->allowed_methods_filtered($c);
+    $c->response->headers(HTTP::Headers->new(
+        Allow => join(', ', @{ $allowed_methods }),
+        Accept_Patch => 'application/json-patch+json',
+    ));
+    $c->response->content_type('application/json');
+    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
+    return;
+}
+sub end : Private {
+    my ($self, $c) = @_;
+
+    $self->log_response($c);
+    return 1;
+}
+
+sub delete_item {
+    my($self, $c, $item, $old_resource, $resource, $form) = @_;
+    my $ip = $item;
+    NGCP::Panel::Utils::Security::ip_unban($c, $ip);
+}
+
+sub DELETE :Allow {
+    my ($self, $c, $id) = @_;
+
+    my $guard = $c->model('DB')->txn_scope_guard;
+    {
+        my $item = $self->item_by_id_valid($c, $id);
+        last unless $item;
+
+        $self->delete_item($c, $item );
+        $guard->commit;
+
+        $c->response->status(HTTP_NO_CONTENT);
+        $c->response->body(q());
+    }
+    return;
+}
+1;
+
+# vim: set tabstop=4 expandtab:
