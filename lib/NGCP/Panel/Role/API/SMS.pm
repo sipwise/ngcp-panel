b7bc8e24ad8328b925768b7b9b84a2726e0dd967
Gerhard Jungwirth <gjungwirth@sipwise.com>
2016-10-28 15:20:12 +0200

    TT#2369 API for sending SMS and journal
    Change-Id: Ia1e09771bc14cd9e8f240de5b86f17563525a565

---
+++ lib/NGCP/Panel/Role/API/SMS.pm
@@ -0,0 +1,92 @@
+package NGCP::Panel::Role::API::SMS;
+use NGCP::Panel::Utils::Generic qw(:all);
+
+use Sipwise::Base;
+
+use parent 'NGCP::Panel::Role::API';
+
+
+use Data::HAL::Link qw();
+use HTTP::Status qw(:constants);
+use NGCP::Panel::Utils::SMS;
+use NGCP::Panel::Form::SMSAPI;
+
+sub item_name {
+    return 'sms';
+}
+
+sub resource_name{
+    return 'sms';
+}
+
+sub get_form {
+    my ($self, $c) = @_;
+    return (NGCP::Panel::Form::SMSAPI->new, ['subscriber_id']);
+}
+
+sub hal_links {
+    my($self, $c, $item, $resource, $form) = @_;
+    my $b_subs_id = $item->provisioning_voip_subscriber->voip_subscriber->id;
+    return [
+        Data::HAL::Link->new(relation => "ngcp:subscribers", href => sprintf("/api/subscribers/%d", $b_subs_id)),
+    ];
+}
+
+sub _item_rs {
+    my ($self, $c) = @_;
+    my $item_rs;
+
+    if($c->user->roles eq "admin") {
+        $item_rs = $c->model('DB')->resultset('sms_journal');
+    } elsif ($c->user->roles eq "reseller") {
+        my $reseller_id = $c->user->reseller_id;
+        $item_rs = $c->model('DB')->resultset('sms_journal')
+            ->search_rs({
+                    'reseller_id' => $reseller_id,
+                } , {
+                    join => { provisioning_voip_subscriber => {'subscriber' => {'contract' => 'contact'} } },
+                });
+    } elsif ($c->user->roles eq 'subscriber' || $c->user->roles eq 'subscriberadmin') {
+        my $subscriber_uuid = $c->user->uuid;
+        $item_rs = $c->model('DB')->resultset('sms_journal')
+            ->search_rs({
+                    'provisioning_voip_subscriber.uuid' => $subscriber_uuid,
+                } , {
+                    join => 'provisioning_voip_subscriber',
+                });
+    }
+
+    return $item_rs;
+}
+
+sub check_resource{
+    my($self, $c, $item, $old_resource, $resource, $form) = @_;
+
+    unless(defined $resource->{subscriber_id}) { # TODO: might check in form
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Missing mandatory field 'subscriber_id'");
+        return;
+    }
+    my $b_subscriber = $c->model('DB')->resultset('voip_subscribers')->find({
+            id => $resource->{subscriber_id},
+        });
+    unless($b_subscriber) {
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid 'subscriber_id'.");
+        return;
+    }
+    my $subscriber = $b_subscriber->provisioning_voip_subscriber;
+    unless($subscriber) {
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid subscriber.");
+        return;
+    }
+    $resource->{subscriber_id} = $subscriber->id;
+
+    return unless NGCP::Panel::Utils::SMS::check_numbers($c, $resource, $subscriber, sub {
+            my ($err) = @_;
+            $self->error($c, HTTP_UNPROCESSABLE_ENTITY, $err);
+        });
+
+    return 1;
+}
+
+1;
+# vim: set tabstop=4 expandtab:
---
+++ lib/NGCP/Panel/Utils/SMS.pm
@@ -0,0 +1,139 @@
+package NGCP::Panel::Utils::SMS;
+
+use Sipwise::Base;
+use LWP::UserAgent;
+use URI;
+
+sub send_sms {
+    my (%args) = @_;
+    my $c = $args{c};
+    my $caller = $args{caller};
+    my $callee = $args{callee};
+    my $text = $args{text};
+    my $err_code = $args{err_code};
+
+    if (!defined $err_code || ref $err_code ne 'CODE') {
+        $err_code = sub { return; };
+    }
+
+    my $schema = $c->config->{sms}{schema};
+    my $host = $c->config->{sms}{host};
+    my $port = $c->config->{sms}{port};
+    my $path = $c->config->{sms}{path};
+    my $user = $c->config->{sms}{user};
+    my $pass = $c->config->{sms}{pass};
+
+    my $fullpath = "$schema://$host:$port$path";
+    my $ua = LWP::UserAgent->new(
+            #ssl_opts => { verify_hostname => 0, SSL_verify_mode => 0 },
+            timeout => 5,
+        );
+    my $uri = URI->new($fullpath);
+    $uri->query_form(
+            charset => "utf8",
+            coding => "2",
+            user => "$user",
+            pass => "$pass",
+            text => $text,
+            to => $callee,
+            from => $caller,
+        );
+    my $res = $ua->get($uri);
+    if ($res->is_success) {
+        return 1;
+    } else {
+        &{$err_code}("Error with send_sms: " . $res->status_line);
+        return;
+    }
+}
+
+# false if error, true if ok
+# TODOs: normalization?
+sub check_numbers {
+    my ($c, $resource, $prov_subscriber, $err_code) = @_;
+
+    if (!defined $err_code || ref $err_code ne 'CODE') {
+        $err_code = sub { return; };
+    }
+
+    my $pref_rs_allowed_clis = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
+            c => $c, attribute => "allowed_clis",
+            prov_subscriber => $prov_subscriber,
+        );
+    my @allowed_clis = $pref_rs_allowed_clis->get_column('value')->all;
+    my $pref_rs_user_cli = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
+            c => $c, attribute => "user_cli",
+            prov_subscriber => $prov_subscriber,
+        );
+    my $user_cli = defined $pref_rs_user_cli->first ? $pref_rs_user_cli->first->value : undef;
+    my $pref_rs_cli = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
+            c => $c, attribute => "cli",
+            prov_subscriber => $prov_subscriber,
+        );
+    my $cli = defined $pref_rs_cli->first ? $pref_rs_cli->first->value : undef;
+
+    if ($resource->{caller}) {
+        my $anumber_ok = 0;
+        for my $number (@allowed_clis, $user_cli, $cli) {
+            next unless $number;
+            if ( _glob_matches($number, $resource->{caller}) ) {
+                $anumber_ok = 1;
+            }
+        }
+        unless ($anumber_ok) {
+            return unless &{$err_code}("Invalid 'caller'", 'caller');
+        }
+    } else {
+        if ($user_cli) {
+            $resource->{caller} = $user_cli;
+        } elsif ($cli) {
+            $resource->{caller} = $cli;
+        } else {
+            return unless &{$err_code}("Could not set value for 'caller'", 'caller');
+        }
+    }
+
+    # done setting/checking anumber
+    # checking bnumber
+    for my $adm ('adm_', '') {
+        my $pref_rs_block_out_list = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
+                c => $c, attribute => $adm."block_out_list",
+                prov_subscriber => $prov_subscriber,
+            );
+        my @block_out_list = $pref_rs_block_out_list->all;
+        my $pref_rs_block_out_mode = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
+                c => $c, attribute => $adm."block_out_mode",
+                prov_subscriber => $prov_subscriber,
+            );
+        my $block_out_mode = defined $pref_rs_block_out_mode->first ? $pref_rs_block_out_mode->first->value : undef;
+
+        if ($block_out_mode) {  # whitelist
+            my $bnumber_ok = 0;
+            for my $number (@block_out_list) {
+                if (_glob_matches($number->value, $resource->{callee})) {
+                    $bnumber_ok = 1;
+                }
+            }
+            unless ($bnumber_ok) {
+                return unless &{$err_code}("Callee Number is not on whitelist for outgoing calls (${adm}block_out_list)", 'callee');
+            }
+        } else {  # blacklist
+            for my $number (@block_out_list) {
+                if (_glob_matches($number->value, $resource->{callee})) {
+                    return unless &{$err_code}("Callee Number is on blocklist for outgoing calls (${adm}block_out_list)", 'callee');
+                }
+            }
+        }
+    }
+
+    return 1;
+}
+
+sub _glob_matches {
+    my ($glob, $string) = @_;
+
+    use Text::Glob;
+    return !!Text::Glob::match_glob($glob, $string);
+}
+
+1;
