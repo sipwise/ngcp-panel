a66ff5ed6279025150c63ad417e4f0cbd39b310d
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-11-21 08:34:50 +0200

    TT#5415 Fix typo
    Change-Id: Iae347183b54459b3d510319ad845eb218bbf7b82

--- lib/NGCP/Panel/Utils/Subscriber.pm
+++ lib/NGCP/Panel/Utils/Subscriber.pm
@@ -569,7 +569,7 @@ sub manage_pbx_groups{
                     );
                     $preferences_rs->create({ value => $subscriber_uri });
                 }
-            }{
+            }else{
                 $c->log->debug('No groups were added.');
             }
         }else{
