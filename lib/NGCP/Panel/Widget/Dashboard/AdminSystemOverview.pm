e64979d87d9c74f9a716641a001e68a8512fa340
Andreas Granig <agranig@sipwise.com>
2016-11-24 14:00:31 +0100

    TT#6638 Show emergency mode in Application Status
    If emergency mode is enabled for one or more domains, show
    application status "Emergency Mode".
    
    Change-Id: Id7f8e3cc8a02d21ba76691905d3d98c2cd3d00ad
    (cherry picked from commit d984d92db0d78c8030718b7a27ca7b579cae94e1)

--- lib/NGCP/Panel/Widget/Dashboard/AdminSystemOverview.pm
+++ lib/NGCP/Panel/Widget/Dashboard/AdminSystemOverview.pm
@@ -3,6 +3,8 @@ package NGCP::Panel::Widget::Dashboard::AdminSystemOverview;
 use warnings;
 use strict;
 
+use NGCP::Panel::Utils::Preferences;
+
 sub template {
     return 'widgets/admin_system_overview.tt';
 }
@@ -14,6 +16,23 @@ sub filter {
         $c->user->roles eq 'admin'
     );
     return;
+}
+
+sub emergency_mode {
+    my ($self, $c) = @_;
+    my $em_count = 0;
+    foreach my $prov_dom($c->model('DB')->resultset('voip_domains')->all) {
+        my $em_rs = NGCP::Panel::Utils::Preferences::get_dom_preference_rs(
+            c => $c,
+            attribute => 'emergency_mode_enabled',
+            prov_domain => $prov_dom,
+        );
+        if($em_rs && $em_rs->first) {
+            $c->log->debug("+++++ domain ".$prov_dom->domain." has emergency mode " . $em_rs->first->value);
+            $em_count++;
+        }
+    }
+    return $em_count;
 }
 
 1;
