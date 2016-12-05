137a492a4efb877968e2e4bf9210163f34388f59
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-12-05 09:18:41 +0200

    TT#5954 Readonly SoundGroups interface
    Change-Id: I06cdb731bbfea459a43344aa66c61735bd156f1a

---
+++ lib/NGCP/Panel/Controller/API/SoundGroupsItem.pm
@@ -0,0 +1,16 @@
+package NGCP::Panel::Controller::API::SoundGroupsItem;
+
+use Sipwise::Base;
+use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::SoundGroups/;
+
+use HTTP::Status qw(:constants);
+
+__PACKAGE__->set_config();
+
+sub allowed_methods{
+    return [qw/GET OPTIONS HEAD/];
+}
+
+1;
+
+# vim: set tabstop=4 expandtab:
