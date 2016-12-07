452d930c0df57e358f4a06c50992a43f9890868c
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-12-07 15:30:18 +0200

    devicemodelimages version
    Change-Id: Ie7610a90774d5e7e8018ef15887b713c7ec32e67

--- lib/NGCP/Panel/Controller/API/PbxDeviceModelImages.pm
+++ lib/NGCP/Panel/Controller/API/PbxDeviceModelImages.pm
@@ -1,24 +1,12 @@
 package NGCP::Panel::Controller::API::PbxDeviceModelImages;
-use NGCP::Panel::Utils::Generic qw(:all);
-
 use Sipwise::Base;
 
-use boolean qw(true);
-use Data::HAL qw();
-use Data::HAL::Link qw();
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
 
-use NGCP::Panel::Utils::DateTime;
-use Path::Tiny qw(path);
-use Safe::Isa qw($_isa);
-require Catalyst::ActionRole::ACL;
-require Catalyst::ActionRole::CheckTrailingSlash;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
+use parent qw/NGCP::Panel::Role::EntitiesFiles NGCP::Panel::Role::API::PbxDeviceModelImages NGCP::Panel::Role::API::PbxDeviceModels/;
+
 
 sub allowed_methods{
-    return [qw/OPTIONS/];
+    return [qw/OPTIONS POST/];
 }
 
 sub api_description {
@@ -39,58 +27,20 @@ sub query_params {
     ];
 }
 
-use parent qw/Catalyst::Controller NGCP::Panel::Role::API::PbxDeviceModelImages NGCP::Panel::Role::API::PbxDeviceModels/;
-
-sub resource_name{
-    return 'pbxdevicemodelimages';
-}
-sub dispatch_path{
-    return '/api/pbxdevicemodelimages/';
-}
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-pbxdevicemodelimages';
-}
-
-__PACKAGE__->config(
-    action => {
-        map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin reseller/],
-            Args => 0,
-            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods },
-    },
-    action_roles => [qw(+NGCP::Panel::Role::HTTPMethods)],
-);
-
-sub auto :Private {
-    my ($self, $c) = @_;
-
-    $self->set_body($c);
-    #$self->log_request($c);
-    return 1;
-}
-
-sub OPTIONS :Allow {
-    my ($self, $c) = @_;
-    my $allowed_methods = $self->allowed_methods_filtered($c);
-    $c->response->headers(HTTP::Headers->new(
-        Allow => join(', ', @{ $allowed_methods }),
-        Accept_Post => 'application/hal+json; profile=http://purl.org/sipwise/ngcp-api/#rel-'.$self->resource_name,
-    ));
-    $c->response->content_type('application/json');
-    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
-    return;
-}
-
-sub end : Private {
-    my ($self, $c) = @_;
-
-    $self->log_response($c);
-    return;
-}
+#sub create_item :Private {
+#    my ($self, $c, $resource, $form, $process_extras) = @_;
+#    my $item;
+#    my $model = $process_extras->{model};
+#    try {
+#        $item = $model->autoprov_firmwares->create($resource);
+#        NGCP::Panel::Utils::Peering::_sip_lcr_reload(c => $c);
+#    } catch($e) {
+#        $c->log->error("failed to create peering group: $e"); # TODO: user, message, trace, ...
+#        $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to create peering group.");
+#        last;
+#    }
+#    return $item;
+#}
 
 1;
 
