154ea4d32927a10624bd2589abed06100a426da2
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-12-07 15:30:42 +0200

    Revert "devicemodelimages version"
    This reverts commit 1d00909836f849b95e07dcfe284015945b8ebf84.
    
    Change-Id: I09f7b956693a7dfff3109a15992a49a12da423b1

--- lib/NGCP/Panel/Controller/API/PbxDeviceModelImagesItem.pm
+++ lib/NGCP/Panel/Controller/API/PbxDeviceModelImagesItem.pm
@@ -1,51 +1,111 @@
 package NGCP::Panel::Controller::API::PbxDeviceModelImagesItem;
-
-use parent qw/NGCP::Panel::Role::EntitiesFilesItem NGCP::Panel::Role::API::PbxDeviceModelImages/;
-
+use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
+
+use HTTP::Headers qw();
 use HTTP::Status qw(:constants);
 
-__PACKAGE__->set_config();
+use NGCP::Panel::Utils::DateTime;
+use NGCP::Panel::Utils::ValidateJSON qw();
+use Path::Tiny qw(path);
+use Safe::Isa qw($_isa);
+require Catalyst::ActionRole::ACL;
+require NGCP::Panel::Role::HTTPMethods;
+require Catalyst::ActionRole::RequireSSL;
 
 sub allowed_methods{
-    return [qw/GET OPTIONS HEAD PUT/];
+    return [qw/GET OPTIONS HEAD/];
 }
 
-sub _set_config{
-    my ($self, $method) = @_;
-    $method //='';
-    if ('PUT' eq $method){
-        return { 'ContentType' => 'image/jpeg' };
-    }
-    return {};
+use parent qw/Catalyst::Controller NGCP::Panel::Role::API::PbxDeviceModelImages NGCP::Panel::Role::API::PbxDeviceModels/;
+
+sub resource_name{
+    return 'pbxdevicemodelimages';
+}
+sub dispatch_path{
+    return '/api/pbxdevicemodelimages/';
+}
+sub relation{
+    return 'http://purl.org/sipwise/ngcp-api/#rel-pbxdevicemodelimages';
 }
 
-sub get_item_file {
-    my ($self, $c, $id, $item) = @_;
-    
+__PACKAGE__->config(
+    action => {
+        map { $_ => {
+            ACLDetachTo => '/api/root/invalid_user',
+            AllowedRole => [qw/admin reseller/],
+            Args => 1,
+            Does => [qw(ACL RequireSSL)],
+            Method => $_,
+            Path => __PACKAGE__->dispatch_path,
+        } } @{ __PACKAGE__->allowed_methods }
+    },
+    action_roles => [qw(+NGCP::Panel::Role::HTTPMethods)],
+);
 
-    my $type = $c->req->param('type') // 'front';
-    my $data;
-    my $ctype;
-    if($type eq 'mac') {
-        $data = $item->mac_image;
-        $ctype = $item->mac_image_type;
-    } else {
-        $data = $item->front_image;
-        $ctype = $item->front_image_type;
-    }
-    unless(defined $data) {
-        $self->error($c, HTTP_NOT_FOUND, "Image type '$type' is not uploaded");
-        last;
-    }
+sub auto :Private {
+    my ($self, $c) = @_;
 
-    my $fext = $ctype; 
-    $fext =~ s/^.*?([a-zA-Z0-9]+)$/$1/;
-    my $fname = $item->vendor . ' ' . $item->model . ".$fext";
-    return $fname,$ctype,$data;
+    $self->set_body($c);
+    $self->log_request($c);
 }
 
+sub GET :Allow {
+    my ($self, $c, $id) = @_;
+    {
+        last unless $self->valid_id($c, $id);
+        my $item = $self->item_by_id($c, $id);
+        last unless $self->resource_exists($c, pbxdevicemodelimages => $item);
+        my $type = $c->req->param('type') // 'front';
+        my $data;
+        my $ctype;
+        if($type eq 'mac') {
+            $data = $item->mac_image;
+            $ctype = $item->mac_image_type;
+        } else {
+            $data = $item->front_image;
+            $ctype = $item->front_image_type;
+        }
+        unless(defined $data) {
+            $self->error($c, HTTP_NOT_FOUND, "Image type '$type' is not uploaded");
+            last;
+        }
+
+        my $fext = $ctype; $fext =~ s/^.*?([a-zA-Z0-9]+)$/$1/;
+        my $fname = $item->vendor . ' ' . $item->model . ".$fext";
+        $c->response->header ('Content-Disposition' => 'attachment; filename="' . $fname . '"');
+        $c->response->content_type($ctype);
+        $c->response->body($data);
+        return;
+    }
+    return;
+}
+
+sub HEAD :Allow {
+    my ($self, $c, $id) = @_;
+    $c->forward(qw(GET));
+    $c->response->body(q());
+    return;
+}
+
+sub OPTIONS :Allow {
+    my ($self, $c, $id) = @_;
+    my $allowed_methods = $self->allowed_methods_filtered($c);
+    $c->response->headers(HTTP::Headers->new(
+        Allow => join(', ', @{ $allowed_methods }),
+        Accept_Patch => 'application/json-patch+json',
+    ));
+    $c->response->content_type('application/json');
+    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
+    return;
+}
+
+sub end : Private {
+    my ($self, $c) = @_;
+
+    #$self->log_response($c);
+}
 
 1;
 
