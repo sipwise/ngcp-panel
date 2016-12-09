3b2dc001f2785fd01d728c0499816f7069a3f8e6
Gerhard Jungwirth <gjungwirth@sipwise.com>
2016-12-09 14:18:43 +0100

    TT#5812 fix calllist direction processing
    the conditional setting of type "call" got overwritten by later
    code.
    
    Change-Id: I297670ed9391e6336d3c7ecefbab1c790b0b89e8
    (cherry picked from commit 1a25aa3eb147e0e8faeb9813a57087da2a4dff34)

--- lib/NGCP/Panel/Utils/CallList.pm
+++ lib/NGCP/Panel/Utils/CallList.pm
@@ -166,6 +166,8 @@ sub process_cdr_item {
     # a call forward
     if($resource->{direction} eq "in") {
         $resource->{type} = "call";
+    } else {
+        $resource->{type} = $item->call_type;
     }
 
     # strip any _b2b-1 and _pbx-1 to allow grouping of calls
@@ -209,7 +211,6 @@ sub process_cdr_item {
 
     $resource->{status} = $item->call_status;
     $resource->{rating_status} = $item->rating_status;
-    $resource->{type} = $item->call_type;
 
     $resource->{start_time} = $item->start_time;
     $resource->{duration} = NGCP::Panel::Utils::DateTime::sec_to_hms($c,$item->duration,3);
