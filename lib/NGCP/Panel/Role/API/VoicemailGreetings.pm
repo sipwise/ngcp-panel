43732e3eca077fa3e26a42ba573016cf8b19a351
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2016-12-09 08:59:02 +0200

    TT#5953 Voicemail greetings
    Change-Id: I6024de5a9264b765097dfce3f7ab4b99637eabe3

---
+++ lib/NGCP/Panel/Role/API/VoicemailGreetings.pm
@@ -0,0 +1,60 @@
+package NGCP::Panel::Role::API::VoicemailGreetings;
+
+use parent qw/NGCP::Panel::Role::API/;
+
+use Sipwise::Base;
+use NGCP::Panel::Utils::Generic qw(:all);
+
+use NGCP::Panel::Form::Voicemail::GreetingAPI;
+
+sub resource_name{
+    return 'voicemailgreetings';
+}
+
+sub dispatch_path{
+    return '/api/voicemailgreetings/';
+}
+
+sub relation{
+    return 'http://purl.org/sipwise/ngcp-api/#rel-voicemailgreetings';
+}
+
+sub _item_rs {
+    my ($self, $c) = @_;
+
+    my $item_rs = $c->model('DB')->resultset('voicemail_spool')->search({
+        'duration' => { '!=' => '' },
+        'msgnum' => '-1',
+        'dir' => {-in => [qw/unavail busy/]},
+        'voip_subscriber.id' => { '!=' => undef },
+    },{
+        join => { 'mailboxuser' => { provisioning_voip_subscriber => 'voip_subscriber' } }
+    });
+    if($c->user->roles eq "admin") {
+    } elsif($c->user->roles eq "reseller") {
+        $item_rs = $item_rs->search({ 
+            'contact.reseller_id' => $c->user->reseller_id 
+        },{
+            join => { mailboxuser => { 
+                provisioning_voip_subscriber => { 
+                    voip_subscriber => { 
+                        contract => 'contact' 
+                    } 
+                } 
+            } }
+        });
+    }
+    return $item_rs;
+}
+
+sub get_form {
+    my ($self, $c) = @_;
+    return (NGCP::Panel::Form::Voicemail::GreetingAPI->new, ['subscriber_id'] );
+}
+
+sub update_item{
+
+}
+
+1;
+# vim: set tabstop=4 expandtab:
