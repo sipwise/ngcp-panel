0ccff2474c618ac8057c66ee47fdff4d344da18c
Rene Krenn <rkrenn@sipwise.com>
2016-12-19 08:23:07 +0100

    TT#6735 TT#7030 TT#5850 record event subscriber numbers
    Change-Id: I509b4c2e9db72374fe33ea74cf0f8334daf0543f

--- lib/NGCP/Panel/Utils/Events.pm
+++ lib/NGCP/Panel/Utils/Events.pm
@@ -2,6 +2,8 @@ package NGCP::Panel::Utils::Events;
 
 use Sipwise::Base;
 
+use NGCP::Panel::Utils::DateTime qw();
+
 sub insert {
     my %params = @_;
     my $c = $params{c};
@@ -11,19 +13,76 @@ sub insert {
     my $old = $params{old};
     my $new = $params{new};
 
+    my $now_hires = NGCP::Panel::Utils::DateTime::current_local_hires;
+    my $customer = $subscriber->contract;
 
-    $schema->resultset('events')->create({
+    my $event = $schema->resultset('events')->create({
         type => $type,
         subscriber_id => $subscriber->id,
-        reseller_id => $subscriber->contract->contact->reseller_id,
+        reseller_id => $customer->contact->reseller_id,
         old_status => $old // '',
         new_status => $new // '',
-        timestamp => NGCP::Panel::Utils::DateTime::current_local_hires,
+        timestamp => $now_hires,
         export_status => 'unexported',
         exported_at => undef,
     });
+
+    save_e164(event => $event, number => $subscriber->primary_number, types_prefix => 'number_', now_hires => $now_hires);
+
+    my $bm_actual = get_actual_billing_mapping(c => $c,schema => $schema, contract => $customer, now => $now_hires);
+    if ($bm_actual->billing_mappings->first->product->product_class eq 'pbxaccount') {
+        my $pilot = $customer->voip_subscribers->search({
+            'provisioning_voip_subscriber.is_pbx_pilot' => 1,
+        },{
+            join => 'provisioning_voip_subscriber',
+        })->first;
+
+        if ($pilot) {
+            save_e164(event => $event, number => $pilot->primary_number, types_prefix => 'pilot_number_', now_hires => $now_hires);
+        }
+    }
 }
 
+sub save_e164 {
+    my %params = @_;
+    my ($event,$number,$types_prefix,$now_hires) = @params{qw/event number types_prefix now_hires/};
+    if ($number) {
+        my $tags_rs = $schema->resultset('events_tag');
+        #my $relations_rs = $schema->resultset('events_relation');
+        if (length(my $cc = $number->cc) > 0) {
+            $event->create_related("tag_data", {
+                tag_id => $tags_rs->find({ type => $types_prefix.'cc' })->id,
+                val => $cc,
+                event_timestamp => $now_hires,
+            });
+        }
+        if (length(my $ac = $number->ac) > 0) {
+            $event->create_related("tag_data", {
+                tag_id => $tags_rs->find({ type => $types_prefix.'ac' })->id,
+                val => $ac,
+                event_timestamp => $now_hires,
+            });
+        }
+        if (length(my $sn = $number->sn) > 0) {
+            $event->create_related("tag_data", {
+                tag_id => $tags_rs->find({ type => $types_prefix.'sn' })->id,
+                val => $sn,
+                event_timestamp => $now_hires,
+            });
+        }
+    }
+}
+
+sub get_actual_billing_mapping {
+    my %params = @_;
+    my ($c,$schema,$contract,$now) = @params{qw/c schema contract now/};
+    $schema //= $c->model('DB');
+    $now //= NGCP::Panel::Utils::DateTime::current_local;
+    my $contract_create = NGCP::Panel::Utils::DateTime::set_local_tz($contract->create_timestamp // $contract->modify_timestamp);
+    my $dtf = $schema->storage->datetime_parser;
+    $now = $contract_create if $now < $contract_create; #if there is no mapping starting with or before $now, it would returns the mapping with max(id):
+    return $schema->resultset('billing_mappings_actual')->search({ contract_id => $contract->id },{bind => [ ( $dtf->format_datetime($now) ) x 2, ($contract->id) x 2 ],})->first;
+}
 
 1;
 
