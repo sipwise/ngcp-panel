e67d15481a271909d13e1c81e326433e21feff63
Rene Krenn <rkrenn@sipwise.com>
2016-12-19 08:23:07 +0100

    TT#6735 TT#7030 TT#5850 record event subscriber numbers
    Change-Id: I509b4c2e9db72374fe33ea74cf0f8334daf0543f

--- lib/NGCP/Panel/Utils/Events.pm
+++ lib/NGCP/Panel/Utils/Events.pm
@@ -11,17 +11,40 @@ sub insert {
     my $old = $params{old};
     my $new = $params{new};
 
+    my $now_hires = NGCP::Panel::Utils::DateTime::current_local_hires;
 
-    $schema->resultset('events')->create({
+    my $event = $schema->resultset('events')->create({
         type => $type,
         subscriber_id => $subscriber->id,
         reseller_id => $subscriber->contract->contact->reseller_id,
         old_status => $old // '',
         new_status => $new // '',
-        timestamp => NGCP::Panel::Utils::DateTime::current_local_hires,
+        timestamp => $now_hires,
         export_status => 'unexported',
         exported_at => undef,
     });
+
+    my $tags_rs = $schema->resultset('events_tag');
+    my $relations_rs = $schema->resultset('events_relation');
+
+    my $primary_number = $subscriber->primary_number;
+    if ($primary_number) {
+        $event->create_related("tag_data", {
+            tag_id => $tags_rs->find({ type => 'number_cc' })->id,
+            val => $primary_number->cc,
+            event_timestamp => $now_hires,
+        });
+        $event->create_related("tag_data", {
+            tag_id => $tags_rs->find({ type => 'number_ac' })->id,
+            val => $primary_number->ac,
+            event_timestamp => $now_hires,
+        });
+        $event->create_related("tag_data", {
+            tag_id => $tags_rs->find({ type => 'number_sn' })->id,
+            val => $primary_number->sn,
+            event_timestamp => $now_hires,
+        });
+    }
 }
 
 
