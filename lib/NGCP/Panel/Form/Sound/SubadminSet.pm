5bbd6d3f626450ee6abc7c442a0e8129bd55ce6b
Gerhard Jungwirth <gjungwirth@sipwise.com>
2016-12-22 17:03:22 +0100

    TT#4334 adapt soundsets and autoattendants for subadmin
    Change-Id: I62f5d4d6227f209ebe1e8949fe0d5db5303c30e9

---
+++ lib/NGCP/Panel/Form/Sound/SubadminSet.pm
@@ -0,0 +1,61 @@
+package NGCP::Panel::Form::Sound::SubadminSet;
+
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+
+use HTML::FormHandler::Widget::Block::Bootstrap;
+
+has '+widget_wrapper' => ( default => 'Bootstrap' );
+has_field 'submitid' => ( type => 'Hidden' );
+sub build_render_list {[qw/submitid fields actions/]}
+sub build_form_element_class {[qw(form-horizontal)]}
+
+has_field 'name' => (
+    type => 'Text',
+    label => 'Name',
+    required => 1,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The name of the sound set'],
+    },
+);
+
+has_field 'description' => (
+    type => 'Text',
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The description of the sound set'],
+    },
+);
+
+has_field 'contract_default' => (
+    type => 'Boolean',
+    label => 'Default for Subscribers',
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['If active (and a customer is selected), this sound set is used for all existing and new subscribers within this customer if no specific sound set is specified for the subscribers'],
+    },
+);
+
+has_field 'save' => (
+    type => 'Submit',
+    value => 'Save',
+    element_class => [qw/btn btn-primary/],
+    label => '',
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/name description contract_default/],
+);
+
+has_block 'actions' => (
+    tag => 'div',
+    class => [qw/modal-footer/],
+    render_list => [qw/save/],
+);
+
+# TODO: inheritance?
+
+1;
