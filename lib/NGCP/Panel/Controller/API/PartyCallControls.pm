582500bf687883859be2036a8fd3483a630f0fe0
Kirill Solomko <ksolomko@sipwise.com>
2016-12-23 12:00:01 +0100

    TT#2651 add /partycallcontrols API
        * /partycallcontrols is for handling incoming
          call control requests from external API sources
    
    Change-Id: I4d886f941f19f659017e32504fdb10e8ae02ba8b

---
+++ lib/NGCP/Panel/Controller/API/PartyCallControls.pm
@@ -0,0 +1,122 @@
+package NGCP::Panel::Controller::API::PartyCallControls;
+use NGCP::Panel::Utils::Generic qw(:all);
+
+use Sipwise::Base;
+
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
+
+use NGCP::Panel::Utils::DateTime;
+use Path::Tiny qw(path);
+use Safe::Isa qw($_isa);
+
+use NGCP::Panel::Utils::Sems;
+
+require Catalyst::ActionRole::ACL;
+require Catalyst::ActionRole::CheckTrailingSlash;
+require NGCP::Panel::Role::HTTPMethods;
+require Catalyst::ActionRole::RequireSSL;
+
+sub allowed_methods{
+    return [qw/POST OPTIONS/];
+}
+
+sub api_description {
+    return 'Allows to place calls via the API.';
+};
+
+sub query_params {
+    return [
+    ];
+}
+
+use parent qw/Catalyst::Controller NGCP::Panel::Role::API::PartyCallControls/;
+
+sub resource_name{
+    return 'partycallcontrols';
+}
+sub dispatch_path{
+    return '/api/partycallcontrols/';
+}
+sub relation{
+    return 'http://purl.org/sipwise/ngcp-api/#rel-callcontrols';
+}
+
+__PACKAGE__->config(
+    action => {
+        map { $_ => {
+            ACLDetachTo => '/api/root/invalid_user',
+            AllowedRole => [qw/admin reseller/],
+            Args => 0,
+            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
+            Method => $_,
+            Path => __PACKAGE__->dispatch_path,
+        } } @{ __PACKAGE__->allowed_methods }
+    },
+    action_roles => [qw(+NGCP::Panel::Role::HTTPMethods)],
+);
+
+sub auto :Private {
+    my ($self, $c) = @_;
+
+    $self->set_body($c);
+    $self->log_request($c);
+}
+
+sub OPTIONS :Allow {
+    my ($self, $c) = @_;
+    my $allowed_methods = $self->allowed_methods_filtered($c);
+    $c->response->headers(HTTP::Headers->new(
+        Allow => join(', ', @{ $allowed_methods }),
+        Accept_Post => 'application/hal+json; profile=http://purl.org/sipwise/ngcp-api/#rel-'.$self->resource_name,
+    ));
+    $c->response->content_type('application/json');
+    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
+    return;
+}
+
+sub POST :Allow {
+    my ($self, $c) = @_;
+    my $guard = $c->model('DB')->txn_scope_guard;
+    {
+        my $resource = $self->get_valid_post_data(
+            c => $c, 
+            media_type => 'application/json',
+        );
+        last unless $resource;
+
+        my $form = $self->get_form($c);
+        last unless $self->validate_form(
+            c => $c,
+            resource => $resource,
+            form => $form,
+        );
+
+        try {
+            NGCP::Panel::Utils::Sems::party_call_control($c, $resource);
+        } catch($e) {
+            $c->log->error("failed to handle party call control request: $e");
+            $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to handle party call control request.");
+            last;
+        }
+
+        $guard->commit;
+
+        $c->response->status(HTTP_OK);
+        $c->response->body(q());
+    }
+    return;
+}
+
+sub end : Private {
+    my ($self, $c) = @_;
+
+    $self->log_response($c);
+}
+
+1;
+
+# vim: set tabstop=4 expandtab:
