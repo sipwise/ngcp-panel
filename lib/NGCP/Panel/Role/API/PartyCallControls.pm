be4ab66be75d3ba8ca9f67d430ce26a3205fa1f0
Kirill Solomko <ksolomko@sipwise.com>
2016-12-23 12:00:01 +0100

    TT#2651 add /partycallcontrols API
    Used to notify NGCP about pending calls and sms.
    
        * /partycallcontrols is for handling incoming
          call control requests from external API sources
    
    Change-Id: I4d886f941f19f659017e32504fdb10e8ae02ba8b

---
+++ lib/NGCP/Panel/Role/API/PartyCallControls.pm
@@ -0,0 +1,25 @@
+package NGCP::Panel::Role::API::PartyCallControls;
+use NGCP::Panel::Utils::Generic qw(:all);
+
+use Sipwise::Base;
+
+use parent 'NGCP::Panel::Role::API';
+
+
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Status qw(:constants);
+
+use NGCP::Panel::Form::PartyCallControl::API;
+
+sub _item_rs {
+}
+
+sub get_form {
+    my ($self, $c) = @_;
+    return NGCP::Panel::Form::PartyCallControl::API->new;
+}
+
+1;
+# vim: set tabstop=4 expandtab:
