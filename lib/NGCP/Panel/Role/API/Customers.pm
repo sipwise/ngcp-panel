25eb8b700768eb3a990fc5b485d23e2675dee14c
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2017-01-03 22:29:55 +0200

    TT#7453 Add  readonly time fields to the customers api
    Change-Id: I46e2c5d6def84b2cb5bab99d15838671274ce44b
    (cherry picked from commit 63958c3edb215363fbc2c52f0d8ffc7b657250cd)
    (cherry picked from commit ad8808929b7512bd2b6c86f72ddeb1d54bf0ab27)

--- lib/NGCP/Panel/Role/API/Customers.pm
+++ lib/NGCP/Panel/Role/API/Customers.pm
@@ -119,6 +119,9 @@ sub hal_from_customer {
         exceptions => [ "contact_id", "billing_profile_id", "profile_package_id", "invoice_template_id", "invoice_email_template_id", "passreset_email_template_id", "subscriber_email_template_id" ],
     );
 
+    foreach my $field (qw/create_timestamp activate_timestamp modify_timestamp terminate_timestamp/){
+        $resource{$field} =  defined $resource{$field} ? NGCP::Panel::Utils::DateTime::to_string(NGCP::Panel::Utils::DateTime::from_string($resource{$field})) : undef ;
+    }
     # return the virtual "type" instead of the actual product id
     $resource{type} = $billing_mapping->product->class;
     $resource{billing_profiles} = $future_billing_profiles;
