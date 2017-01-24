8281d1be3665e1dfaef850771d130d171a2b24da
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2017-01-24 13:08:45 +0200

    clear_errors and erros failed delegation
    Change-Id: I040563efa179b17af6a2c98cd20637763624d821

--- lib/NGCP/Panel/Field/PosInteger.pm
+++ lib/NGCP/Panel/Field/PosInteger.pm
@@ -5,6 +5,12 @@ extends 'HTML::FormHandler::Field::Integer';
 
 sub validate {
     my ( $self ) = @_;
+    #use Data::Dumper;
+    #use irka;
+    #my $ctx = $self->ctx;
+    #$ctx->log->debug(Dumper(["AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"]));
+    #irka::loglong(Dumper([caller]));
+    #$ctx->log->debug(Dumper([caller]));
     my $value = $self->value;
     $self->add_error('Value must be a positive integer')
         if(!$self->has_errors && $value < 0);
