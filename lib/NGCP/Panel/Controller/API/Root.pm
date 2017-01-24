4ab62a659efeea30fe5faa99b234f8512395a0f9
Gerhard Jungwirth <gjungwirth@sipwise.com>
2017-01-24 16:11:39 +0100

    TT#28827 implement initial OpenAPI documentation
    formerly known as swagger
    already working: collection GET/POST, item GET
    
    Change-Id: Icd6ba3f60fd51fb948a957801a963d134fe6e127

--- lib/NGCP/Panel/Controller/API/Root.pm
+++ lib/NGCP/Panel/Controller/API/Root.pm
@@ -195,6 +195,7 @@ sub GET : Allow {
 
         $c->stash->{collections}->{$rel} = {
             name => $mod,
+            entity_name => $mod =~ s/s$//r,
             description => $full_mod->api_description,
             fields => $form_fields,
             uploads => $form_fields_upload,
@@ -235,6 +236,8 @@ sub GET : Allow {
             Content_Language => 'en',
             Content_Type => 'application/json',
         ));
+    } elsif ($c->req->params->{swagger}) {
+        $c->detach('swagger');
     } else {
         $c->stash(template => 'api/root.tt');
         $c->forward($c->view);
@@ -244,6 +247,275 @@ sub GET : Allow {
             #$self->collections_link_headers,
         ));
     }
+
+    return;
+}
+
+sub swagger :Private {
+    my ($self, $c) = @_;
+
+    my %paths;
+    my %schemas;
+    my %responses = (
+        ErrorResponse => {
+            description => 'An error',
+            content => {
+                "application/json" => {
+                    "schema" => {
+                        type => "object",
+                        properties => {
+                            code => { type => "integer" },
+                            message => { type => "string" },
+                        }
+                    }
+                }
+            }
+        }
+    );
+    my %parameters = (
+        PageParameter => {
+            name => 'page',
+            in => 'query',
+            description => 'Pagination page which should be displayed (default: 1)',
+            example => 1,
+        },
+        RowsParameter => {
+            name => 'rows',
+            in => 'query',
+            description => 'Number of rows in one pagination page (default: 10)',
+            example => 10,
+        },
+        ItemIdParameter => {
+            "name" => "id",
+            "in" => "path",
+            "required" => JSON::true,
+            "schema" => { type => "integer" },
+        },
+    );
+
+    my @chapters = sort (keys %{ $c->stash->{collections} });
+
+    for my $chapter (@chapters) {
+        my $col = $c->stash->{collections}{$chapter};
+        my $p = {}; # Path Item Object
+        my $item_p = {}; # Path Item Object for "NGCP Item"
+        my $title = $col->{name};
+        my $entity = $col->{entity_name};
+
+        if (grep {m/^GET$/} @{ $col->{actions} }) {
+            $p->{get} = {
+                summary => "Get $entity items",
+                tags => ['Collection Get'],
+                responses => {
+                    "200" => {
+                        description => "$title",
+                        content => {
+                            "application/json" => {
+                                schema => {
+                                    type => "array", # I want an Array to $entity objects here
+                                    items => {
+                                        '$ref' => "#/components/schemas/$entity",
+                                    }
+                                }
+                            },
+                        },
+                    }
+                }
+            };
+
+            for my $query_param (@{ $col->{query_params} // [] }) {
+                push @{$p->{get}{parameters} }, {
+                    name => $query_param->{param},
+                    description => $query_param->{description},
+                    in => 'query',
+                };
+            }
+            if ($col->{sorting_cols} && @{ $col->{sorting_cols} }) {
+                push @{$p->{get}{parameters} }, {
+                        name => 'order_by',
+                        description => 'Order collection by a specific attribute.',
+                        in => 'query',
+                        schema => {
+                            type => 'string',
+                            enum => [ @{ $col->{sorting_cols} } ],
+                        }
+                    },{
+                        name => 'order_by_direction',
+                        description => 'Direction which the collection should be ordered by. Possible values are: asc (default), desc.',
+                        in => 'query',
+                        example => 'asc',
+                        schema => {
+                            type => 'string',
+                            enum => [ 'asc', 'desc' ],
+                        }
+                    };
+            }
+            push @{ $p->{get}{parameters} }, {
+                    '$ref' => '#/components/parameters/PageParameter',
+                },{
+                    '$ref' => '#/components/parameters/RowsParameter',
+                };
+        }
+
+        if (grep {m/^POST$/} @{ $col->{actions} }) {
+            $p->{post} = {
+                # description => "Creates a new item of $title",
+                summary => "Create a new $entity",
+                tags => ['Collection Post'],
+                requestBody => {
+                    required => JSON::true,
+                    content => {
+                        "application/json" => {
+                            schema => {
+                                '$ref' => "#/components/schemas/$entity",
+                            }
+                        }
+                    }
+                },
+                responses => {
+                    "201" => {
+                        description => "The newly created item or empty",
+                        content => {
+                            "application/json" => {
+                                schema => {
+                                    type => "array",
+                                    items => {
+                                        '$ref' => "#/components/schemas/$entity",
+                                    }
+                                }
+                            },
+                            # "*/*" => {
+                            #     schema => { type => "string", maxLength => 0 }
+                            # }
+                        },
+                        headers => {
+                            "Location" => {
+                                "description" => "Location of the newly created item (as a relative path)",
+                                "schema" => {
+                                    "type" => "string",
+                                }
+                            }
+                        }
+                    },
+                    "422" => { '$ref' => "#/components/responses/ErrorResponse" },
+                }
+            };
+        }
+
+        if (grep {m/^GET$/} @{ $col->{item_actions} }) {
+            $item_p->{get} = {
+                summary => "Get a specific $entity",
+                tags => ['Item Get'],
+                responses => {
+                    "200" => {
+                        description => "$title",
+                        content => {
+                            "application/json" => {
+                                schema => {
+                                    '$ref' => "#/components/schemas/$entity",
+                                }
+                            },
+                        },
+                    }
+                }
+            };
+        }
+
+        # common description for all methods
+        $p->{description} = $col->{description};
+
+        #push @paths, $p;
+        $paths{'/'.$chapter.'/'} = $p;
+        if (keys %{ $item_p }) {
+            $item_p->{description} = $col->{description};
+            $item_p->{parameters} = [
+                { '$ref' => '#/components/parameters/ItemIdParameter' },
+            ];
+            $paths{'/'.$chapter.'/{id}'} = $item_p;
+        }
+
+
+        # ---------------------------------------------
+
+        my $e = {
+            type => "object",
+            properties => {},
+            required => [],
+        };
+
+        # possible values for types: null, (select options), Number, Boolean, Array, Object, String
+        for my $f (@{ $col->{fields} }) {
+            my $p = {};
+            if ($f->{type_original} eq "Select" ||
+                ($f->{type_original} =~ m/\+NGCP::Panel::Field::.*Select$/ && $f->{enum})) {
+                $p->{type} = "string";
+                $p->{enum} = [ map {$_->{value}} @{ $f->{enum} // [] } ];
+            } elsif ($f->{type_original} eq 'IntRange') {
+                $p->{type} = "number";
+                $p->{enum} = [ map {$_->{value}} @{ $f->{enum} // [] } ];
+            } elsif ($f->{type_original} eq "Boolean") {
+                $p->{type} = "boolean";
+            } elsif (grep {m/^Number$/} @{$f->{types}}) {
+                $p->{type} = "number";
+            } elsif ($f->{type_original} eq '+NGCP::Panel::Field::EmailList' ||
+                     $f->{type_original} eq 'Email') {
+                $p->{type} = "string";
+                $p->{format} = "email"; # not the same as emaillist but that's nit-picky
+            } elsif ($f->{type_original} eq '+NGCP::Panel::Field::DateTime') {
+                $p->{type} = "string";
+                $p->{format} = "date-time"; # actually a slightly different format
+            } elsif ($f->{type_original} eq "Text" || grep {m/^String$/} @{$f->{types}}) {
+                $p->{type} = "string";
+            } elsif ($f->{type_original} eq "Repeatable" || grep {m/^Array$/} @{$f->{types}}) {
+                $p->{type} = "array";
+                $p->{items}{type} = "object"; # content of array basically unspecified
+            } else {
+                $p->{type} = "object"; # object or uncategorizable
+            }
+
+            $p->{description} = $f->{description};
+            if (grep {m/^null$/} @{ $f->{types} // [] }) {
+                push @{ $e->{required} }, $f->{name};
+            }
+
+            $e->{properties}{$f->{name}} = $p;
+        }
+        unless (@{ $e->{required} }) {
+            delete $e->{required}; # empty required is not allowed
+        }
+        unless (keys %{ $e->{properties} }) {
+            delete $e->{properties}; # try delete empty properties (then it's a valid Free Form Object)
+        }
+
+        $schemas{$entity} = $e;
+    }
+
+    my $role = "".$c->user->roles;
+    my $result = {
+        "openapi" => "3.0.0",
+        "info"    => {
+            "title"       => "NGCP API",
+            "description" => "Sipwise NGCP API (role $role)",
+            "version"     => "1.0.1",
+        },
+        "servers" => [ { "url" => "/api" } ],
+
+        "paths" => \%paths,
+        "components" => {
+            "schemas" => \%schemas,
+            "responses" => \%responses,
+            "parameters" => \%parameters,
+        },
+    };
+
+    use JSON qw/encode_json/;
+
+    $c->response->headers(HTTP::Headers->new(
+        Content_Language => 'en',
+        Content_Type => 'application/json',
+    ));
+    $c->response->body(encode_json($result));
+    $c->response->code(200);
     return;
 }
 
@@ -386,7 +658,11 @@ sub get_field_poperties :Private{
             $name .= '_id';
         }
     }
+    my $enum;
     push(@types, $self->field_to_json($field));
+    if ($field->$_isa('HTML::FormHandler::Field::Select')) {
+        $enum = $field->options;
+    }
     my $desc = undef;
     if($field->element_attr) {
         $desc = $field->element_attr->{title}->[0];
@@ -397,7 +673,8 @@ sub get_field_poperties :Private{
     unless (defined $desc && length($desc) > 0) {
         $desc = 'to be described ...';
     }
-    return { name => $name, description => $desc, types => \@types, type_original => $field->type, readonly => $field->readonly };
+    return { name => $name, description => $desc, types => \@types, type_original => $field->type,
+        readonly => $field->readonly, ($enum ? (enum => $enum) : ()) };
 }
 sub get_collection_properties {
     my ($self, $form) = @_;
