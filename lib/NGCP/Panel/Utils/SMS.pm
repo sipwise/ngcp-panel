0c85dd58ed7e41f9fe787a8cbc19b13dcc719252
Andreas Granig <agranig@sipwise.com>
2017-01-26 09:47:10 +0100

    TT#9702 Implement I-New SMS charging.
    Change-Id: I3db0f45fcd071577f0799f8682788fa626dccabc
    (cherry picked from commit b81b067eacdb8365f1299415e022413f8fd25950)

--- lib/NGCP/Panel/Utils/SMS.pm
+++ lib/NGCP/Panel/Utils/SMS.pm
@@ -3,6 +3,11 @@ package NGCP::Panel::Utils::SMS;
 use Sipwise::Base;
 use LWP::UserAgent;
 use URI;
+use POSIX;
+use UUID;
+
+use NGCP::Rating::Inew::SmsSession;
+use NGCP::Panel::Utils::Utf8;
 
 sub send_sms {
     my (%args) = @_;
@@ -15,6 +20,17 @@ sub send_sms {
 
     if (!defined $err_code || ref $err_code ne 'CODE') {
         $err_code = sub { return; };
+    }
+
+    unless(defined $coding) {
+        # if unicode, we have to use utf8 encoding, limiting our
+        # text length to 70; otherwise send as default
+        # encoding, allowing 160 chars
+        if(NGCP::Panel::Utils::Utf8::is_within_ascii($text)) {
+            $coding = 0;
+        } else {
+            $coding = 2;
+        }
     }
 
     my $schema = $c->config->{sms}{schema};
@@ -32,7 +48,7 @@ sub send_sms {
     my $uri = URI->new($fullpath);
     $uri->query_form(
             charset => "utf-8",
-            coding => $coding // "2",
+            coding => $coding,
             user => "$user",
             pass => "$pass",
             text => $text,
@@ -135,6 +151,110 @@ sub _glob_matches {
 
     use Text::Glob;
     return !!Text::Glob::match_glob($glob, $string);
+}
+
+sub get_number_of_parts {
+    my $text = shift;
+    my $maxlen; 
+    if(NGCP::Panel::Utils::Utf8::is_within_ascii($text)) {
+        $maxlen = 160;
+    } else {
+        $maxlen = 70;
+    }
+    return ceil(length($text) / $maxlen);
+}
+
+sub perform_prepaid_billing {
+    my (%args) = @_;
+    my $c = $args{c};
+    my $prov_subscriber = $args{prov_subscriber};
+    my $parts = $args{parts};
+    my $caller = $args{caller};
+    my $callee = $args{callee};
+
+    my ($uuid, $session_id);
+    UUID::generate($uuid);
+    UUID::unparse($uuid, $session_id);
+
+    my ($prepaid_lib, $is_prepaid);
+    my $prepaid_pref_rs = NGCP::Panel::Utils::Preferences::get_dom_preference_rs(
+        c => $c, attribute => 'prepaid_library',
+        prov_domain => $prov_subscriber->domain,
+    );
+    if($prepaid_pref_rs && $prepaid_pref_rs->first) {
+        $prepaid_lib = $prepaid_pref_rs->first->value;
+    }
+
+    $prepaid_pref_rs = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
+        c => $c, attribute => 'prepaid',
+        prov_subscriber => $prov_subscriber,
+    );
+    if($prepaid_pref_rs && $prepaid_pref_rs->first && $prepaid_pref_rs->first->value) {
+        $is_prepaid = 1;
+    } else {
+        $is_prepaid = 0;
+    }
+
+    # currently only inew rating supported, let others pass
+    return 1 unless($is_prepaid && $prepaid_lib eq "libinewrate");
+
+    my $amqr = NGCP::Rating::Inew::SmsSession::init(
+        $c->config->{libinewrate}->{soap_uri},
+        $c->config->{libinewrate}->{openwire_uri},
+    );
+    unless($amqr) {
+        $c->log->error("Failed to create sms amqr handle from $caller to $callee");
+        return;
+    }
+    # Reserve credit for each part, and then commit each reservation.
+    # If we can charge multiple times within one session - perfect.
+    # Otherwise we have to create one session per part, store it in an
+    # array, then after all reservations were successful, commit each
+    # of them!
+    my @sessions = ();
+    my @failed_sessions = ();
+    for(my $i = 0; $i < $parts; ++$i) {
+        my $has_credit = 1;
+        my $this_session_id = $session_id."-".$i;
+        my $sess = NGCP::Rating::Inew::SmsSession::session_create(
+            $amqr, $this_session_id, $caller, $callee, sub {
+                $has_credit = 0;
+        });
+        unless($sess) {
+            $c->log->error("Failed to create sms rating session from $caller to $callee with session id $this_session_id");
+            last;
+        }
+        unless(NGCP::Rating::Inew::SmsSession::session_sms_reserve($sess)) {
+            $c->log->error("Failed to reserve sms session from $caller to $callee with session id $this_session_id");
+            push @failed_sessions, $sess;
+            last;
+        }
+        unless($has_credit) {
+            $c->log->info("No credit for sms from $caller to $callee with session id $this_session_id");
+            push @failed_sessions, $sess;
+            last;
+        }
+        push @sessions, $sess;
+    }
+    if(@sessions == $parts) {
+        foreach my $sess(@sessions) {
+            NGCP::Rating::Inew::SmsSession::session_sms_commit($sess);
+            NGCP::Rating::Inew::SmsSession::session_destroy($sess);
+        }
+        NGCP::Rating::Inew::SmsSession::destroy($amqr);
+        return 1;
+    } else {
+        foreach my $sess(@sessions) {
+            NGCP::Rating::Inew::SmsSession::session_sms_discard($sess);
+            NGCP::Rating::Inew::SmsSession::session_destroy($sess);
+        }
+        foreach my $sess(@failed_sessions) {
+            NGCP::Rating::Inew::SmsSession::session_sms_discard($sess);
+            NGCP::Rating::Inew::SmsSession::session_destroy($sess);
+        }
+        NGCP::Rating::Inew::SmsSession::destroy($amqr);
+        return;
+    }
 }
 
 1;
