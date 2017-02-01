33af1fc6cf38a2104857df4bd45072421896333f
Kirill Solomko <ksolomko@sipwise.com>
2017-02-01 16:59:52 +0100

    TT#7818 add CallLists own_cli parameter
        * enables a filter for calls that were initated or received
          by the specified subscriber/custoemr
        * set direction for internal subscriber calls to "out"
    
    Change-Id: I2f3b543b57352f92dc5a67bc82779cf371b6f23c

--- lib/NGCP/Panel/Controller/API/CallLists.pm
+++ lib/NGCP/Panel/Controller/API/CallLists.pm
@@ -222,6 +222,46 @@ sub query_params {
                 },
             },
         },
+        {
+            param => 'own_cli',
+            description => 'Filter calls by a specific number that is a part of in our out calls.',
+            new_rs => sub {
+                my ($c,$q,$rs) = @_;
+                my $owner = $c->stash->{owner} // {};
+                return unless $owner;
+                if ($owner->{subscriber}) {
+                    return $rs->search_rs({
+                        -or => [
+                            { source_cli => $q,
+                              -or => [
+                                       source_user_id => $owner->{subscriber}->uuid,
+                                     ],
+                            },
+                            { destination_user_in => $q,
+                              -or => [
+                                       destination_user_id => $owner->{subscriber}->uuid,
+                                     ],
+                            },
+                        ],
+                    });
+                } elsif ($owner->{customer}) {
+                    return $rs->search_rs({
+                        -or => [
+                            { source_cli => $q,
+                              -or => [
+                                       source_account_id => $owner->{customer}->id,
+                                     ],
+                            },
+                            { destination_user_in => $q,
+                              -or => [
+                                       destination_account_id => $owner->{customer}->id,
+                                     ],
+                            },
+                        ],
+                    });
+                }
+            },
+        },
     ];
 }
 
