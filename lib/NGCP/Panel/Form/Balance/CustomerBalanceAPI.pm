ef3cbfb00336be6ed35bd09f17c1d06cb1bddc8e
Rene Krenn <rkrenn@sipwise.com>
2017-02-14 15:44:38 +0100

    TT#10963 cash_balance_interval not returned in euro
    + as stated per apidoc.
    + fields renamed to align with /api/balanceintervals
    
    Change-Id: Iaba062f1ed8336861bc29f6e38ac6be8df5df91c
    (cherry picked from commit 22bd45df4bd0717fc267f5668f50af6bc2daf23b)

--- lib/NGCP/Panel/Form/Balance/CustomerBalanceAPI.pm
+++ lib/NGCP/Panel/Form/Balance/CustomerBalanceAPI.pm
@@ -4,32 +4,30 @@ use HTML::FormHandler::Moose;
 extends 'NGCP::Panel::Form::Balance::CustomerBalance';
 use Moose::Util::TypeConstraints;
 
-has_field 'cash_balance_interval' => (
+has_field 'cash_debit' => (
     type => 'Money',
-    label => 'Cash Balance (Interval)',
-    required => 1,
-    inflate_method => sub { return $_[1] * 100 },
-    deflate_method => sub { return $_[1] / 100 },
+    #label => 'Cash Balance (Interval)',
+    #required => 1,
     element_attr => {
         rel => ['tooltip'],
-        title => ['The current cash balance of the customer in EUR/USD/etc for the current interval (Read-only).'],
+        title => ['The amount spent during the current interval in EUR/USD/etc (read-only).'],
     },
 );
 
-has_field 'free_time_balance_interval' => (
+has_field 'free_time_spent' => (
     type => 'Integer',
-    label => 'Free-Time Balance',
-    required => 1,
+    #label => 'Free-Time Balance',
+    #required => 1,
     element_attr => {
         rel => ['tooltip'],
-        title => ['The current free-time balance of the customer for the current interval in seconds (Read-only).'],
+        title => ['The free-time spent during the current interval in seconds (read-only).'],
     },
 );
 
 has_block 'fields' => (
     tag => 'div',
     class => [qw/modal-body/],
-    render_list => [qw/cash_balance cash_balance_interval free_time_balance free_time_balance_interval/],
+    render_list => [qw/cash_balance cash_debit free_time_balance free_time_spent/],
 );
 
 1;
