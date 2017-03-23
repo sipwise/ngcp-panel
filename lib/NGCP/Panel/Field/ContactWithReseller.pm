d4746c5019b2017eb7c03e47227683939c77a01d
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2017-03-23 17:38:39 +0200

    TT#12197 Use contacts connected to the resellers for the customers creation
    Change-Id: I6fc8fc49b5c7dc7d6ca78c4dbc1ab75f4891437a

---
+++ lib/NGCP/Panel/Field/ContactWithReseller.pm
@@ -0,0 +1,28 @@
+package NGCP::Panel::Field::ContactWithReseller;
+use Moose;
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler::Field::Compound';
+
+has_field 'id' => (
+    type => '+NGCP::Panel::Field::DataTable',
+    label => 'Contact',
+    do_label => 0,
+    do_wrapper => 0,
+    required => 1,
+    template => 'helpers/datatables_field.tt',
+    ajax_src => '/contact/ajax_reseller',
+    table_titles => ['#', 'Reseller', 'First Name', 'Last Name', 'Email'],
+    table_fields => ['id', 'reseller_name', 'firstname', 'lastname', 'email'],
+);
+
+has_field 'create' => (
+    type => 'Button',
+    do_label => 0,
+    value => 'Create Contact',
+    element_class => [qw/btn btn-tertiary pull-right/],
+);
+
+no Moose;
+1;
+
+# vim: set tabstop=4 expandtab:
