3f47a37a48a0b7fb137841b14716effaaf69df1a
Kirill Solomko <ksolomko@sipwise.com>
2017-04-05 20:48:58 +0200

    TT#7332 add "Peering Overview"
        - a new feature, "Peering Overview" under the "Tools" menu that
          shows a consolidated table of peering rules, peering groups and
          peering hosts with a quick links to the related edit dialogs
        - added a group selection for "peering rule edit" dialogs that
          enables a possibility to move a peering rule into another
          peering group
        - changed the "back" uri in the "peering host" and "peering rule"
          dialogs to return back to the previous uri instead of forcing it
          to /peering, that is needed to call those dialogs from
          /peeringoverview and then get back to the original uri upon
          completion
        - dynamic columns support
        - separate outbound and inbound peering rules respresentation
        - export to "CSV", also with the "Search" filter support
        - add dynamic column join detection excluding excessive joins
          if no related columns are selected
    
    Change-Id: I71b4c62c4583989baacfc166f08e965c4464e4b2
    (cherry picked from commit fc9593175f705552828cf0a74cac4118b48bae2d)

---
+++ lib/NGCP/Panel/Form/Peering/RuleEditAdmin.pm
@@ -0,0 +1,47 @@
+package NGCP::Panel::Form::Peering::RuleEditAdmin;
+
+use HTML::FormHandler::Moose;
+extends 'NGCP::Panel::Form::Peering::Rule';
+
+has_field 'group' => (
+    type => '+NGCP::Panel::Field::PeeringGroupSelect',
+    label => 'Peering Group',
+    not_nullable => 1,
+    required => 1,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['A peering group the rule belongs to.']
+    },
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/callee_prefix callee_pattern caller_pattern description enabled group/],
+);
+
+1;
+__END__
+
+=head1 NAME
+
+NGCP::Panel::Form::Peering::RuleEdit
+
+=head1 DESCRIPTION
+
+-
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+Irina Peshinskaya
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+# vim: set tabstop=4 expandtab:
