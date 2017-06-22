9bd9b05d4c6df40b0d46e7ed26b12c62f99b5e6a
Rene Krenn <rkrenn@sipwise.com>
2017-06-22 20:28:33 +0200

    TT#14755 call list suppressions WIP
    Change-Id: Ie1a75f5ee2465e68d62d6ad4f77c6a0d5bd729b6

--- lib/NGCP/Panel/Utils/CallList.pm
+++ lib/NGCP/Panel/Utils/CallList.pm
@@ -7,6 +7,13 @@ use JSON qw();
 use NGCP::Panel::Utils::DateTime;
 use NGCP::Panel::Utils::Subscriber;
 
+use constant SUPPRESS_OUT => 1;
+use constant SUPPRESS_IN => 2;
+use constant SUPPRESS_INOUT => 3;
+
+my $source_cli_suppression_id_colname = 'source_cli_suppression_id';
+my $destination_user_in_suppression_id_colname = 'destination_user_in_suppression_id';
+
 # owner:
 #     * subscriber (optional)
 #     * customer
@@ -70,6 +77,13 @@ sub process_cdr_item {
     $source_cli = $anonymize ? 'anonymous@anonymous.invalid' : $source_cli;
     $resource->{clir} = $item->source_clir;
 
+    my ($source_cli_suppression,$destination_user_in_suppression);
+    my $supressions_rs = $c->model('DB')->resultset('call_list_suppressions');
+    $source_cli_suppression = $supressions_rs->find($item->get_column($source_cli_suppression_id_colname))
+        if defined $item->get_column($source_cli_suppression_id_colname);
+    $destination_user_in_suppression = $supressions_rs->find($item->get_column($destination_user_in_suppression_id_colname))
+        if defined $item->get_column($destination_user_in_suppression_id_colname);
+
     my ($src_sub, $dst_sub);
     my $billing_src_sub = $item->source_subscriber;
     my $billing_dst_sub = $item->destination_subscriber;
@@ -79,7 +93,7 @@ sub process_cdr_item {
     if($billing_dst_sub && $billing_dst_sub->provisioning_voip_subscriber) {
         $dst_sub = $billing_dst_sub->provisioning_voip_subscriber;
     }
-    my ($own_normalize, $other_normalize, $own_domain, $other_domain);
+    my ($own_normalize, $other_normalize, $own_domain, $other_domain, $own_suppression, $other_suppression);
     my $other_skip_domain = 0;
 
     if($resource->{direction} eq "out") {
@@ -101,6 +115,7 @@ sub process_cdr_item {
             $own_normalize = 1;
         }
         $own_domain = $item->source_domain;
+        $own_suppression = $source_cli_suppression;
 
         # for intra pbx out calls, use extension as other cli
         if($intra && $dst_sub && $dst_sub->pbx_extension) {
@@ -120,6 +135,7 @@ sub process_cdr_item {
             $other_normalize = 1;
         }
         $other_domain = $item->destination_domain;
+        $other_suppression = $destination_user_in_suppression;
     } else {
         # for pbx in calls, use extension as own cli
         if($dst_sub && $dst_sub->pbx_extension) {
@@ -139,6 +155,7 @@ sub process_cdr_item {
             $own_normalize = 1;
         }
         $own_domain = $item->destination_domain;
+        $own_suppression = $destination_user_in_suppression;
 
         # rewrite cf to voicemail to "voicemail"
         if($item->destination_user_in =~ /^vm[ub]/ &&
@@ -161,22 +178,25 @@ sub process_cdr_item {
             $other_normalize = 0;
             $other_skip_domain = 1;
             $resource->{direction} = "out";
-        # for intra pbx in calls, use extension as other cli
-        } elsif($intra && $src_sub && $src_sub->pbx_extension) {
-            $resource->{other_cli} = $src_sub->pbx_extension;
-        # for termianted subscribers if there is an alias field (e.g. gpp0), use this
-        } elsif($intra && $item->source_account_id && $params->{'intra_alias_field'}) {
-            my $alias = $item->get_column('source_'.$params->{'intra_alias_field'});
-            $resource->{other_cli} = $alias // $source_cli;
-            $other_normalize = 0;
-        # if there is an alias field (e.g. gpp0), use this
-        } elsif($item->source_account_id && $params->{'alias_field'}) {
-            my $alias = $item->get_column('source_'.$params->{'alias_field'});
-            $resource->{other_cli} = $alias // $source_cli;
-            $other_normalize = 1;
         } else {
-            $resource->{other_cli} = $source_cli;
-            $other_normalize = 1;
+            # for intra pbx in calls, use extension as other cli
+            if($intra && $src_sub && $src_sub->pbx_extension) {
+                $resource->{other_cli} = $src_sub->pbx_extension;
+            # for termianted subscribers if there is an alias field (e.g. gpp0), use this
+            } elsif($intra && $item->source_account_id && $params->{'intra_alias_field'}) {
+                my $alias = $item->get_column('source_'.$params->{'intra_alias_field'});
+                $resource->{other_cli} = $alias // $source_cli;
+                $other_normalize = 0;
+            # if there is an alias field (e.g. gpp0), use this
+            } elsif($item->source_account_id && $params->{'alias_field'}) {
+                my $alias = $item->get_column('source_'.$params->{'alias_field'});
+                $resource->{other_cli} = $alias // $source_cli;
+                $other_normalize = 1;
+            } else {
+                $resource->{other_cli} = $source_cli;
+                $other_normalize = 1;
+            }
+            $other_suppression = $source_cli_suppression;
         }
         $other_domain = $item->source_domain;
     }
@@ -221,13 +241,36 @@ sub process_cdr_item {
             $resource->{other_cli} = $normalized_cli;
         }
     }
+
+    my @own_details = ();
+    if ($own_suppression) {
+        if (_is_show_suppressions($c)) {
+            push(@own_details,'obfuscated: ' . $own_suppression->label) if 'obfuscate' eq $own_suppression->mode;
+            push(@own_details,'filtered: ' . $own_suppression->label) if 'filter' eq $own_suppression->mode;
+        } else {
+            $resource->{own_cli} = $own_suppression->label;
+        }
+    }
     if ( (!($sub // $own_sub)) || (($sub // $own_sub)->status eq "terminated") ) {
-        $resource->{own_cli} .= " (terminated)";
+        push(@own_details,'terminated');
+        #$resource->{own_cli} .= " (terminated)";
+    }
+    $resource->{own_cli} .= ' (' . join(', ',@own_details) . ')' if (scalar @own_details) > 0;
+    my @other_details = ();
+    if ($other_suppression) {
+        if (_is_show_suppressions($c)) {
+            push(@other_details,'obfuscated: ' . $other_suppression->label) if 'obfuscate' eq $other_suppression->mode;
+            push(@other_details,'filtered: ' . $other_suppression->label) if 'filter' eq $other_suppression->mode;
+        } else {
+            $resource->{other_cli} = $other_suppression->label;
+        }
     }
     if ($other_sub && $other_sub->status eq "terminated" &&
             $own_sub && $own_sub->contract_id == $other_sub->contract_id) {
-        $resource->{other_cli} .= " (terminated)";
+        push(@other_details,'terminated');
+        #$resource->{other_cli} .= " (terminated)";
     }
+    $resource->{other_cli} .= ' (' . join(', ',@other_details) . ')' if (scalar @other_details) > 0;
 
     $resource->{status} = $item->call_status;
     $resource->{rating_status} = $item->rating_status;
@@ -247,7 +290,114 @@ sub process_cdr_item {
     return $resource;
 }
 
+sub _is_show_suppressions {
+    my $c = shift;
+    return ($c->user->roles eq "admin" or $c->user->roles eq "reseller");
+}
 
+sub call_list_suppressions_rs {
+    my ($c,$rs,$mode) = @_;
+    my %search_cond = ();
+    my %search_xtra = ();
+    if (_is_show_suppressions($c)) {
+        if (defined $mode and SUPPRESS_OUT == $mode) {
+            $search_xtra{'+select'} = [
+                #{ '' => \[ 'me.source_cli' ] , -as => $source_cli_suppression_id_colname },
+                { '' => \[ 'NULL' ] , -as => $source_cli_suppression_id_colname },
+                { '' => \[ _get_call_list_suppression_sq('outgoing',qw(filter obfuscate)) ] , -as => $destination_user_in_suppression_id_colname },
+            ];
+        } elsif (defined $mode and SUPPRESS_IN == $mode) {
+            $search_xtra{'+select'} = [
+                { '' => \[ _get_call_list_suppression_sq('incoming',qw(filter obfuscate)) ] , -as => $source_cli_suppression_id_colname },
+                #{ '' => \[ 'me.destination_user_in' ] , -as => $destination_user_in_suppression_id_colname },
+                { '' => \[ 'NULL' ] , -as => $destination_user_in_suppression_id_colname },
+            ];
+        } elsif (defined $mode and SUPPRESS_INOUT == $mode) {
+            $search_xtra{'+select'} = [
+                { '' => \[ _get_call_list_suppression_sq('incoming',qw(filter obfuscate)) ] , -as => $source_cli_suppression_id_colname },
+                { '' => \[ _get_call_list_suppression_sq('outgoing',qw(filter obfuscate)) ] , -as => $destination_user_in_suppression_id_colname },
+            ];
+        } else {
+            $search_xtra{'+select'} = [
+                #{ '' => \[ 'me.source_cli' ] , -as => $source_cli_suppression_id_colname },
+                { '' => \[ 'NULL' ] , -as => $source_cli_suppression_id_colname },
+                #{ '' => \[ 'me.destination_user_in' ] , -as => $destination_user_in_suppression_id_colname },
+                { '' => \[ 'NULL' ] , -as => $destination_user_in_suppression_id_colname },
+            ];
+        }
+    } else {
+        if (defined $mode and SUPPRESS_OUT == $mode) {
+            $search_xtra{'+select'} = [
+                #{ '' => \[ 'me.source_cli' ] , -as => $source_cli_suppression_id_colname },
+                { '' => \[ 'NULL' ] , -as => $source_cli_suppression_id_colname },
+                { '' => \[ _get_call_list_suppression_sq('outgoing',qw(obfuscate)) ] , -as => $destination_user_in_suppression_id_colname },
+            ];
+            $search_cond{'-not exists'} = \[ '('._get_call_list_suppression_sq('outgoing',qw(filter)).')' ];
+        } elsif (defined $mode and SUPPRESS_IN == $mode) {
+            $search_xtra{'+select'} = [
+                { '' => \[ _get_call_list_suppression_sq('incoming',qw(obfuscate)) ] , -as => $source_cli_suppression_id_colname },
+                #{ '' => \[ 'me.destination_user_in' ] , -as => $destination_user_in_suppression_id_colname },
+                { '' => \[ 'NULL' ] , -as => $destination_user_in_suppression_id_colname },
+            ];
+            $search_cond{'-not exists'} = \[ '('._get_call_list_suppression_sq('incoming',qw(filter)).')' ];
+        } elsif (defined $mode and SUPPRESS_INOUT == $mode) {
+            $search_xtra{'+select'} = [
+                { '' => \[ _get_call_list_suppression_sq('incoming',qw(obfuscate)) ] , -as => $source_cli_suppression_id_colname },
+                { '' => \[ _get_call_list_suppression_sq('outgoing',qw(obfuscate)) ] , -as => $destination_user_in_suppression_id_colname },
+            ];
+            $search_cond{'-not exists'} => \[ '('._get_call_list_suppression_sq('incoming',qw(filter)).')' ];
+            $search_cond{'-not exists'} => \[ '('._get_call_list_suppression_sq('outgoing',qw(filter)).')' ];
+        } else {
+            $search_xtra{'+select'} = [
+                #{ '' => \[ 'me.source_cli' ] , -as => $source_cli_suppression_id_colname },
+                { '' => \[ 'NULL' ] , -as => $source_cli_suppression_id_colname },
+                #{ '' => \[ 'me.destination_user_in' ] , -as => $destination_user_in_suppression_id_colname },
+                { '' => \[ 'NULL' ] , -as => $destination_user_in_suppression_id_colname },
+            ];
+        }
+    }
+    return $rs->search_rs(\%search_cond,\%search_xtra);
+}
+
+sub _get_call_list_suppression_sq {
+    my ($direction,@modes) = @_;
+    my $domain_col;
+    my $number_col;
+    if ('incoming' eq $direction) {
+        $domain_col = 'destination_domain';
+        $number_col = 'source_cli';
+    } else {
+        $domain_col = 'source_domain';
+        $number_col = 'destination_user_in';
+    }
+    return "select id from billing.call_list_suppressions where direction = \"$direction\" and mode in (".join(',',map { '"'.$_.'"'; } @modes).")".
+        " and (domain = \"\" or domain = me.$domain_col) and me.$number_col regexp pattern limit 1";
+
+}
+
+#sub _get_call_list_suppressions_rs {
+#    my ($c,$direction,@modes) = @_;
+#    my $domain_col;
+#    my $number_col;
+#    if ('incoming' eq $direction) {
+#        $domain_col = 'destination_domain';
+#        $number_col = 'source_cli';
+#    } else {
+#        $domain_col = 'source_domain';
+#        $number_col = 'destination_user_in';
+#    }
+#    return $c->model('DB')->resultset('call_list_suppressions')->search_rs({
+#        direction => { '=' => $direction },
+#        mode => { 'in' => \@modes },
+#        '-or' => [{
+#            domain => '',
+#            },{
+#            domain => \"me.$domain_col",
+#        }],
+#        "me.$number_col" => { 'regexp' => \'pattern' },
+#    });
+#
+#}
 
 1;
 
