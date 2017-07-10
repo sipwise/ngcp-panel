8e45311f4928239161a6fee215a61fa061186d64
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2017-07-11 01:20:20 +0300

    TT#22494 TT#17638 Rewrite Rule API to set priority
    Conflicts:
    	lib/NGCP/Panel/Controller/API/RewriteRuleSets.pm
    	lib/NGCP/Panel/Controller/API/RewriteRuleSetsItem.pm
    	lib/NGCP/Panel/Controller/API/RewriteRules.pm
    	lib/NGCP/Panel/Controller/API/RewriteRulesItem.pm
    	lib/NGCP/Panel/Role/API/RewriteRuleSets.pm
    	lib/NGCP/Panel/Role/API/RewriteRules.pm
    	lib/NGCP/Panel/Role/Entities.pm
    	lib/NGCP/Panel/Role/EntitiesItem.pm
    
    Change-Id: I84099ab6d4869ee232532668fa15f1cc40501fb8
    (cherry picked from commit c0940df9db663fba9b52d1df912aa25d185d7cec)

--- lib/NGCP/Panel/Role/API/RewriteRules.pm
+++ lib/NGCP/Panel/Role/API/RewriteRules.pm
@@ -1,110 +1,104 @@
 package NGCP::Panel::Role::API::RewriteRules;
-use NGCP::Panel::Utils::Generic qw(:all);
+
+use parent qw/NGCP::Panel::Role::API/;
 
 use Sipwise::Base;
-
-use parent 'NGCP::Panel::Role::API';
-
-
-use boolean qw(true);
+use NGCP::Panel::Utils::Generic qw(:all);
+use HTTP::Status qw(:constants);
 use Data::HAL qw();
 use Data::HAL::Link qw();
-use HTTP::Status qw(:constants);
-use JSON::Types;
-use NGCP::Panel::Form::RewriteRule::AdminSet;
-use NGCP::Panel::Form::RewriteRule::ResellerSet;
 use NGCP::Panel::Form::RewriteRule::RuleAPI;
 
-sub get_form {
-    my ($self, $c) = @_;
-    return NGCP::Panel::Form::RewriteRule::RuleAPI->new;
+sub item_name{
+    return 'rewriterule';
 }
 
-sub hal_from_item {
-    my ($self, $c, $item, $type) = @_;
-    my $form;
+sub resource_name{
+    return 'rewriterules';
+}
 
-    my %resource = $item->get_inflated_columns;
+sub dispatch_path{
+    return '/api/rewriterules/';
+}
 
-    my $hal = Data::HAL->new(
-        links => [
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            Data::HAL::Link->new(relation => 'collection', href => sprintf("%s", $self->dispatch_path)),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            Data::HAL::Link->new(relation => 'self', href => sprintf("%s%d", $self->dispatch_path, $item->id)),
-            Data::HAL::Link->new(relation => "ngcp:$type", href => sprintf("/api/%s/%d", $type, $item->id)),
-            Data::HAL::Link->new(relation => "ngcp:rewriterulesets", href => sprintf("/api/rewriterulesets/%d", $item->set_id)),
-        ],
-        relation => 'ngcp:'.$self->resource_name,
-    );
+sub relation{
+    return 'http://purl.org/sipwise/ngcp-api/#rel-rewriterules';
+}
 
-    $form //= $self->get_form($c);
-    return unless $self->validate_form(
-        c => $c,
-        form => $form,
-        resource => \%resource,
-        run => 0,
-        exceptions => [qw/set_id/],
-    );
-
-    $resource{match_pattern} = $form->inflate_match_pattern($resource{match_pattern});
-    $resource{replace_pattern} = $form->inflate_replace_pattern($resource{replace_pattern});
-
-    $hal->resource(\%resource);
-    return $hal;
+sub config_allowed_roles {
+    return [qw/admin reseller/];
 }
 
 sub _item_rs {
     my ($self, $c, $type) = @_;
     my $item_rs;
 
-    if($type eq "rules") {
-        if($c->user->roles eq "admin") {
-            $item_rs = $c->model('DB')->resultset('voip_rewrite_rules');
-        } elsif ($c->user->roles eq "reseller") {
-            $item_rs = $c->model('DB')->resultset('voip_rewrite_rules')->search_rs({
-                    'ruleset.reseller_id' => $c->user->reseller_id,
-                },{
-                    join => 'ruleset'
-                });
-        }
-    } else {
-        die "You should not reach this";
+    if($c->user->roles eq "admin") {
+        $item_rs = $c->model('DB')->resultset('voip_rewrite_rules');
+    } elsif ($c->user->roles eq "reseller") {
+        $item_rs = $c->model('DB')->resultset('voip_rewrite_rules')->search_rs({
+                'ruleset.reseller_id' => $c->user->reseller_id,
+            },{
+                join => 'ruleset'
+            });
     }
     return $item_rs;
 }
 
-sub item_by_id {
-    my ($self, $c, $id, $type) = @_;
-
-    my $item_rs = $self->item_rs($c, $type);
-    return $item_rs->find($id);
+sub get_form {
+    my ($self, $c) = @_;
+    return ( NGCP::Panel::Form::RewriteRule::RuleAPI->new, ['set_id'] );
 }
 
-sub update_item {
-    my ($self, $c, $item, $old_resource, $resource, $form) = @_;
+sub hal_links{
+    my($self, $c, $item, $resource, $form) = @_;
+    return [
+        Data::HAL::Link->new(relation => "ngcp:rewriterulesets", href => sprintf("/api/rewriterulesets/%d", $item->set_id)),
+    ];
+}
 
-    delete $resource->{id};
+sub post_process_hal_resource {
+    my($self, $c, $item, $resource, $form) = @_;
+    $resource->{match_pattern} = $form->inflate_match_pattern($resource->{match_pattern});
+    $resource->{replace_pattern} = $form->inflate_replace_pattern($resource->{replace_pattern});
+    return $resource;
+}
 
-    return unless $self->validate_form(
-        c => $c,
-        form => $form,
-        resource => $resource,
-        exceptions => [qw/set_id/],
-    );
-
+sub process_form_resource{
+    my($self,$c, $item, $old_resource, $resource, $form, $process_extras) = @_;
     $resource->{match_pattern} = $form->values->{match_pattern};
     $resource->{replace_pattern} = $form->values->{replace_pattern};
-
-    $item->update($resource);
-
-    return $item;
+    return $resource;
 }
+
+sub check_resource{
+    my($self, $c, $item, $old_resource, $resource, $form, $process_extras) = @_;
+    my $schema = $c->model('DB');
+
+    unless(defined $resource->{set_id}) {
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Required: 'set_id'");
+        return;
+    }
+
+    my $reseller_id;
+    if($c->user->roles eq "reseller") {
+        $reseller_id = $c->user->reseller_id;
+    }
+
+    my $ruleset = $schema->resultset('voip_rewrite_rule_sets')->find({
+        id => $resource->{set_id},
+        ($reseller_id ? (reseller_id => $reseller_id) : ()),
+    });
+    unless($ruleset) {
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid 'set_id'.");
+        return;
+    }
+
+    $c->stash->{checked}->{ruleset} = $ruleset;
+
+    return 1;
+}
+
 
 1;
 # vim: set tabstop=4 expandtab:
