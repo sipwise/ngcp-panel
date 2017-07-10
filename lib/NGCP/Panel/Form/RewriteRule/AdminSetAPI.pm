39273a71d453eeb94aeb06d21f640ccd9c35463c
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2017-07-11 01:20:20 +0300

    TT#17638 Rewrite Rule API to set priority. Draft.
    Change-Id: I84099ab6d4869ee232532668fa15f1cc40501fb8

---
+++ lib/NGCP/Panel/Form/RewriteRule/AdminSetAPI.pm
@@ -0,0 +1,23 @@
+package NGCP::Panel::Form::RewriteRule::AdminSetAPI;
+
+use HTML::FormHandler::Moose;
+extends 'NGCP::Panel::Form::RewriteRule::ResellerSetAPI';
+
+has_field 'reseller_id' => (
+    type => 'PosInteger',
+    validate_when_empty => 1,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The reseller who can use the Ruleset.'],
+    },
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/reseller_id name description rewriterules/],
+);
+
+1;
+
+# vim: set tabstop=4 expandtab:
