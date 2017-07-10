4cca1ca293f432c35b7440d2289a836d2579b15b
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2017-07-11 01:20:20 +0300

    TT#17638 Rewrite Rule API to set priority. Draft.
    Change-Id: I84099ab6d4869ee232532668fa15f1cc40501fb8

---
+++ lib/NGCP/Panel/Field/RewriteRuleAPI.pm
@@ -0,0 +1,18 @@
+package NGCP::Panel::Field::RewriteRuleAPI;
+use Moose;
+use HTML::FormHandler::Moose;
+extends 'NGCP::Panel::Field::RewriteRule';
+
+has_field 'priority' => (
+    type => 'PosInteger',
+    required => 0,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The rewrite rule priority.']
+    },
+);
+
+no Moose;
+1;
+
+# vim: set tabstop=4 expandtab:
