3fe26b6d2a7d883db9bd0326744b3e6b06d85ff7
Andreas Granig <agranig@sipwise.com>
2017-07-14 13:43:21 +0200

    TT#18754 Implement json response for api-root
    When requested with an "Accept: application/json" header, return
    the response in json format instead of xhtml.
    
    Change-Id: Iba4135d8013fd200af426f2eb3d67b97282c034b
    (cherry picked from commit abe78df43095d753f0010f379a5cdaad1ef87366)

--- lib/NGCP/Panel/Controller/API/Root.pm
+++ lib/NGCP/Panel/Controller/API/Root.pm
@@ -200,13 +200,30 @@ sub GET : Allow {
 
     }
 
-    $c->stash(template => 'api/root.tt');
-    $c->forward($c->view);
-    $c->response->headers(HTTP::Headers->new(
-        Content_Language => 'en',
-        Content_Type => 'application/xhtml+xml',
-        #$self->collections_link_headers,
-    ));
+    if($c->req->header('Accept') && $c->req->header('Accept') eq 'application/json') {
+        my $body = {};
+        foreach my $rel(keys %{ $c->stash->{collections} }) {
+            my $r = $c->stash->{collections}->{$rel};
+            foreach my $k(qw/
+                    actions item_actions fields query_params sorting_cols
+            /) {
+                $body->{$rel}->{$k} = $r->{$k};
+            }
+        }
+        $c->response->body(JSON::to_json($body, { pretty => 1 }));
+        $c->response->headers(HTTP::Headers->new(
+            Content_Language => 'en',
+            Content_Type => 'application/json',
+        ));
+    } else {
+        $c->stash(template => 'api/root.tt');
+        $c->forward($c->view);
+        $c->response->headers(HTTP::Headers->new(
+            Content_Language => 'en',
+            Content_Type => 'application/xhtml+xml',
+            #$self->collections_link_headers,
+        ));
+    }
     return;
 }
 
