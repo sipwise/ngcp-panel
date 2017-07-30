0a4683bfcb15dfc089aa3032555c117fcf2a2ef2
Kirill Solomko <ksolomko@sipwise.com>
2017-07-30 21:31:24 +0200

    TT#19459 add DND mode WIP
    Change-Id: I826138d8cdbce737c09c3b98bc7bdd905d09e81c

--- lib/NGCP/Panel/Form/SubscriberCFSimple.pm
+++ lib/NGCP/Panel/Form/SubscriberCFSimple.pm
@@ -56,6 +56,9 @@ sub build_destinations {
             { label => 'Custom Announcement', value => 'customhours' };
          push @options, { label => 'Local Subscriber', value => 'localuser' }
             if($c->config->{features}->{callthrough} || $c->config->{features}->{callingcard} );
+
+        push @options, { label => 'Do not Disturb', value => 'dnd' }
+            if($c->config->{features}->{dnd});
     }
     push @options, { label => 'URI/Number', value => 'uri' };
 
@@ -119,7 +122,7 @@ sub validate_destination{
     my ( $self, $field ) = @_;
     my $result = 0;
     (my($uri_field)) = grep {'destination' eq $_->name} ($field->field('uri')->fields);
-    if( 'uri' eq $field->field('destination')->value 
+    if( 'uri' eq $field->field('destination')->value
         && !$uri_field->value ){
         $uri_field->add_error($uri_field->label . " is empty");
         $result = 0;
