483f462ff7b73c2cfaa18da920a75ce3ebb1aeae
Rene Krenn <rkrenn@sipwise.com>
2017-08-25 06:51:36 +0200

    TT#20322 TT#20344 /api/conversations WIP
    + basic calls rs added
    + basic voicemails rs added
    + basic sms rs added
    + basic faxes rs added
    
    + calls+voicemails union-compatible
    + sms union-compatible
    + make call list suppressions work
    
    + add Tie::IxHash dependency
    
    + fax union-compatible
    + xmpp/sipwise_mam union-compatible
    
    + separate "timestamp" column for sorting
    
    + support explicity order-by column definitions
    + order by "timestamp" working
    + order by "type" working
    
    + "subscriber_id" filter parameter
    + "customer_id" filter parameter
    + "type" filter parameter
    + timestamp "from" filter parameter
    + timestamp "to" filter parameter
    + "direction" filter parameter
    
    + cleanups for preliminary commit.
    
    Change-Id: I7697635079b49bf2724731dee1c78e1801146b94

---
+++ lib/NGCP/Panel/Controller/API/Conversations.pm
@@ -0,0 +1,64 @@
+package NGCP::Panel::Controller::API::Conversations;
+
+use Sipwise::Base;
+#use NGCP::Panel::Utils::Generic qw(:all);
+use HTTP::Status qw(:constants);
+
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::Conversations/;
+
+__PACKAGE__->set_config();
+
+sub allowed_methods{
+    return [qw/GET OPTIONS HEAD/];
+}
+
+sub api_description {
+    return 'Combined collection of calls, voice mails, sms and faxes.';
+};
+
+sub query_params {
+    return [
+        {
+            param => 'subscriber_id',
+            description => 'Filter for calls for a specific subscriber. Either this or customer_id is mandatory if called by admin, reseller or subscriberadmin to filter list down to a specific subscriber in order to properly determine the direction of calls.',
+        },
+        {
+            param => 'customer_id',
+            description => 'Filter for calls for a specific customer. Either this or subscriber_id is mandatory if called by admin, reseller or subscriberadmin to filter list down to a specific customer. For calls within the same customer_id, the direction will always be "out".',
+        },
+
+        {
+            param => 'direction',
+            description => 'todo',
+        },
+
+        {
+            param => 'status',
+            description => 'todo',
+        },
+
+        {
+            param => 'type',
+            description => 'todo',
+        },
+
+        {
+            param => 'from',
+            description => 'Filter for calls starting greater or equal the specified time stamp.',
+        },
+        {
+            param => 'to',
+            description => 'Filter for calls starting lower or equal the specified time stamp.',
+        },
+
+    ];
+}
+
+sub order_by_cols {
+    return {
+        'timestamp' => 'me.timestamp',
+        'type' => 'me.type',
+    };
+}
+
+1;
