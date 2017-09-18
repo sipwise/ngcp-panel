05e631ae1f65bf310cbc907934e9278fb57cb823
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2017-09-18 17:23:56 +0300

    TT#21819 Remove excrescent code and add provisioning fields to the resource
    Change-Id: Ia0515b3d071d45435db7c9138ea9de9447775143

--- lib/NGCP/Panel/Controller/API/PbxDeviceModelsItem.pm
+++ lib/NGCP/Panel/Controller/API/PbxDeviceModelsItem.pm
@@ -1,243 +1,27 @@
 package NGCP::Panel::Controller::API::PbxDeviceModelsItem;
-use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
+use NGCP::Panel::Utils::Generic qw(:all);
 
-use boolean qw(true);
-use NGCP::Panel::Utils::DataHal qw();
-use NGCP::Panel::Utils::DataHalLink qw();
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
+use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::PbxDeviceModels/;
 
-use NGCP::Panel::Utils::ValidateJSON qw();
-use NGCP::Panel::Utils::DateTime;
-use Path::Tiny qw(path);
-use Safe::Isa qw($_isa);
-use Clone qw/clone/;
-require Catalyst::ActionRole::ACL;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
+__PACKAGE__->set_config();
+sub _set_config{
+    my ($self, $method) = @_;
+    $method //='';
+    if ('PUT' eq $method || 'PATCH' eq $method){
+        return {
+            'ContentType' => ['multipart/form-data'],#,
+            'Uploads'     => [qw/front_image mac_image/],
+#            Also correct way for the allowed_roles, and really the last word. Will be applied over all others.
+#            'AllowedRole' => [qw/admin reseller/],
+        };
+    }
+    return {};
+}
 
 sub allowed_methods{
     return [qw/GET OPTIONS HEAD PATCH PUT/];
-}
-
-use parent qw/Catalyst::Controller NGCP::Panel::Role::API::PbxDeviceModels/;
-
-sub resource_name{
-    return 'pbxdevicemodels';
-}
-sub dispatch_path{
-    return '/api/pbxdevicemodels/';
-}
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-pbxdevicemodels';
-}
-
-__PACKAGE__->config(
-    action => {
-        map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin reseller subscriberadmin/],
-            Args => 1,
-            Does => [qw(ACL RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods },
-    },
-);
-
-sub gather_default_action_roles {
-    my ($self, %args) = @_; my @roles = ();
-    push @roles, 'NGCP::Panel::Role::HTTPMethods' if $args{attributes}->{Method};
-    return @roles;
-}
-
-sub auto :Private {
-    my ($self, $c) = @_;
-
-    $self->set_body($c);
-    $self->log_request($c);
-    return 1;
-}
-
-sub GET :Allow {
-    my ($self, $c, $id) = @_;
-    {
-        last unless $self->valid_id($c, $id);
-        my $field_dev = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, devicemodel => $field_dev);
-
-        my $hal = $self->hal_from_item($c, $field_dev);
-
-        my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-            (map { # XXX Data::HAL must be able to generate links with multiple relations
-                s|rel="(http://purl.org/sipwise/ngcp-api/#rel-resellers)"|rel="item $1"|r
-                =~ s/rel=self/rel="item self"/r;
-            } $hal->http_headers),
-        ), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
-}
-
-sub HEAD :Allow {
-    my ($self, $c, $id) = @_;
-    $c->forward(qw(GET));
-    $c->response->body(q());
-    return;
-}
-
-sub OPTIONS :Allow {
-    my ($self, $c, $id) = @_;
-    my $allowed_methods = $self->allowed_methods_filtered($c);
-    $c->response->headers(HTTP::Headers->new(
-        Allow => join(', ', @{ $allowed_methods }),
-        Accept_Patch => 'application/json-patch+json',
-    ));
-    $c->response->content_type('application/json');
-    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
-    return;
-}
-
-sub PATCH :Allow {
-    my ($self, $c, $id) = @_;
-
-    if ($c->user->roles eq 'subscriberadmin') {
-        $c->log->error("role subscriberadmin cannot edit pbxdevicemodel");
-        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid role. Cannot edit pbxdevicemodel.");
-        return;
-    }
-
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $preference = $self->require_preference($c);
-        last unless $preference;
-
-        my $json = $self->get_valid_patch_data(
-            c => $c, 
-            id => $id,
-            media_type => 'application/json-patch+json',
-        );
-        last unless $json;
-
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, pbxdevicemodel => $item);
-        my $old_resource = $self->resource_from_item($c, $item);
-        #without it error: The entity could not be processed: Modification of a read-only value attempted at /usr/share/perl5/JSON/Pointer.pm line 200, <$fh> line 1.\n
-        $old_resource = clone($old_resource);
-        my $resource = $self->apply_patch($c, $old_resource, $json);
-        last unless $resource;
-
-        my $form = $self->get_form($c);
-        $item = $self->update_item($c, $item, $old_resource, $resource, $form);
-        last unless $item;
-        
-        $guard->commit;
-
-        if ('minimal' eq $preference) {
-            $c->response->status(HTTP_NO_CONTENT);
-            $c->response->header(Preference_Applied => 'return=minimal');
-            $c->response->body(q());
-        } else {
-            my $hal = $self->hal_from_item($c, $item, $form);
-            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-                $hal->http_headers,
-            ), $hal->as_json);
-            $c->response->headers($response->headers);
-            $c->response->header(Preference_Applied => 'return=representation');
-            $c->response->body($response->content);
-        }
-    }
-    return;
-}
-
-sub PUT :Allow {
-    my ($self, $c, $id) = @_;
-
-    if ($c->user->roles eq 'subscriberadmin') {
-        $c->log->error("role subscriberadmin cannot edit pbxdevicemodel");
-        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid role. Cannot edit pbxdevicemodel.");
-        return;
-    }
-
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $preference = $self->require_preference($c);
-        last unless $preference;
-
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, pbxdevicemodel => $item);
-
-        last unless $self->forbid_link_header($c);
-        last unless $self->valid_media_type($c, 'multipart/form-data');
-        last unless $self->require_wellformed_json($c, 'application/json', $c->req->param('json'));
-        my $resource = JSON::from_json($c->req->param('json'), { utf8 => 1 });
-        $resource->{front_image} = $self->get_upload($c, 'front_image');
-        last unless $resource->{front_image};
-        # optional, don't set error
-        $resource->{mac_image} = $c->req->upload('mac_image');
-
-
-        my $old_resource = $self->resource_from_item($c, $item);
-
-        my $form = $self->get_form($c);
-        $item = $self->update_item($c, $item, $old_resource, $resource, $form);
-        last unless $item;
-
-        $guard->commit; 
-
-        if ('minimal' eq $preference) {
-            $c->response->status(HTTP_NO_CONTENT);
-            $c->response->header(Preference_Applied => 'return=minimal');
-            $c->response->body(q());
-        } else {
-            my $hal = $self->hal_from_item($c, $item, $form);
-            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-                $hal->http_headers,
-            ), $hal->as_json);
-            $c->response->headers($response->headers);
-            $c->response->header(Preference_Applied => 'return=representation');
-            $c->response->body($response->content);
-        }
-    }
-    return;
-}
-
-=pod
-sub DELETE :Allow {
-    my ($self, $c, $id) = @_;
-
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, emailtemplate => $item);
-        foreach(qw/subscriber_email_template_id passreset_email_template_id invoice_email_template_id/){
-            $c->model('DB')->resultset('contracts')->search({
-                $_ => $item->id,
-            })->update({
-                $_ => undef,
-            });        
-        }
-
-        $item->delete;
-
-        $guard->commit;
-
-        $c->response->status(HTTP_NO_CONTENT);
-        $c->response->body(q());
-    }
-    return;
-}
-=cut
-
-sub end : Private {
-    my ($self, $c) = @_;
-
-    $self->log_response($c);
-    return 1;
 }
 
 1;
