3051650f2c14862768e44c07d16e562427e427db
Gerhard Jungwirth <gjungwirth@sipwise.com>
2017-10-24 16:01:21 +0200

    TT#23774 remove old config-override method in Model
    was used to allow specifying a custom config file in order
    to quickly test manually with a different db
    
    Change-Id: I03db8ca922037f9d342a81236e6dfbfe79575c36

--- lib/NGCP/Panel/Model/InterceptDB.pm
+++ lib/NGCP/Panel/Model/InterceptDB.pm
@@ -1,23 +1,9 @@
 package NGCP::Panel::Model::InterceptDB;
 
 use strict;
-use File::ShareDir 'dist_file';
-use Moose;
-use MooseX::Types::Moose;
-extends 'Catalyst::Model::DBIC::Schema';
+use warnings;
 
-use Module::Runtime qw(use_module);
-
-has 'testing' => (is => 'rw', isa => 'Bool', default => 0);
-
-sub setup {
-    my ($self) = @_;
-    if ($self->testing) {
-        my $config_location = dist_file('NGCP-Schema', 'test.conf');
-        use_module('NGCP::Schema::Config')->instance
-            ->config_file($config_location);
-    }
-}
+use base 'Catalyst::Model::DBIC::Schema';
 
 __PACKAGE__->config(
     connect_info => [],
