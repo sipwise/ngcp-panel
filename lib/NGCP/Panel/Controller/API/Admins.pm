154fde705cdf6ac638e7f7a9844e795cb86e5a15
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2017-11-06 15:20:15 +0200

    TT# Rewrite admins to Entities. Draft.
    Change-Id: I3bf47038bfb48c09f95030eed5d83550e392ff0c

--- lib/NGCP/Panel/Controller/API/Admins.pm
+++ lib/NGCP/Panel/Controller/API/Admins.pm
@@ -3,17 +3,11 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use boolean qw(true);
-use NGCP::Panel::Utils::DataHal qw();
-use NGCP::Panel::Utils::DataHalLink qw();
-use HTTP::Headers qw();
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::Admins/;
+
 use HTTP::Status qw(:constants);
 
-use NGCP::Panel::Utils::Admin;
-require Catalyst::ActionRole::ACL;
-require Catalyst::ActionRole::CheckTrailingSlash;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
+__PACKAGE__->set_config();
 
 sub api_description {
     return'Defines admins to log into the system via panel or api.';
@@ -27,197 +21,31 @@ sub query_params {
         {
             param => 'reseller_id',
             description => 'Filter for admins belonging to a specific reseller',
-            query => {
-                first => sub {
-                    my $q = shift;
-                    { reseller_id => $q };
-                },
-                second => sub {},
-            },
+            query_type => 'string_eq',
         },
         {
             param => 'login',
             description => 'Filter for admins with a specific login (wildcards possible)',
-            query => {
-                first => sub {
-                    my $q = shift;
-                    { login => { like => $q  } };
-                },
-                second => sub {},
-            },
+            query_type => 'string_like',
         },
     ];
 }
 
-use parent qw/Catalyst::Controller NGCP::Panel::Role::API::Admins/;
-
-sub resource_name{
-    return 'admins';
-}
-sub dispatch_path{
-    return '/api/admins/';
-}
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-admins';
-}
-
-__PACKAGE__->config(
-    action => {
-        map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin reseller/],
-            Args => 0,
-            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods }
-    },
-);
-
-sub gather_default_action_roles {
-    my ($self, %args) = @_; my @roles = ();
-    push @roles, 'NGCP::Panel::Role::HTTPMethods' if $args{attributes}->{Method};
-    return @roles;
-}
-
-sub auto :Private {
-    my ($self, $c) = @_;
-
-    $self->set_body($c);
-    $self->log_request($c);
-}
-
-sub GET :Allow {
-    my ($self, $c) = @_;
-    my $page = $c->request->params->{page} // 1;
-    my $rows = $c->request->params->{rows} // 10;
-    {
-        my $items = $self->item_rs($c);
-        (my $total_count, $items) = $self->paginate_order_collection($c, $items);
-        my (@embedded, @links);
-        my $form = $self->get_form($c);
-        for my $item ($items->all) {
-            push @embedded, $self->hal_from_item($c, $item, $form);
-            push @links, NGCP::Panel::Utils::DataHalLink->new(
-                relation => 'ngcp:'.$self->resource_name,
-                href     => sprintf('/%s%d', $c->request->path, $item->id),
-            );
-        }
-        push @links,
-            NGCP::Panel::Utils::DataHalLink->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            NGCP::Panel::Utils::DataHalLink->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            NGCP::Panel::Utils::DataHalLink->new(relation => 'self', href => sprintf('/%s?page=%s&rows=%s', $c->request->path, $page, $rows));
-        if(($total_count / $rows) > $page ) {
-            push @links, NGCP::Panel::Utils::DataHalLink->new(relation => 'next', href => sprintf('/%s?page=%d&rows=%d', $c->request->path, $page + 1, $rows));
-        }
-        if($page > 1) {
-            push @links, NGCP::Panel::Utils::DataHalLink->new(relation => 'prev', href => sprintf('/%s?page=%d&rows=%d', $c->request->path, $page - 1, $rows));
-        }
-
-        my $hal = NGCP::Panel::Utils::DataHal->new(
-            embedded => [@embedded],
-            links => [@links],
-        );
-        $hal->resource({
-            total_count => $total_count,
-        });
-        my $response = HTTP::Response->new(HTTP_OK, undef, 
-            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
+sub create_item {
+    my ($self, $c, $resource, $form, $process_extras) = @_;
+    unless($c->user->is_master) {
+        $self->error($c, HTTP_FORBIDDEN, "Cannot create admin without master permissions");
+        last;
     }
-    return;
-}
-
-sub HEAD :Allow {
-    my ($self, $c) = @_;
-    $c->forward(qw(GET));
-    $c->response->body(q());
-    return;
-}
-
-sub OPTIONS :Allow {
-    my ($self, $c) = @_;
-    my $allowed_methods = $self->allowed_methods_filtered($c);
-    $c->response->headers(HTTP::Headers->new(
-        Allow => join(', ', @{ $allowed_methods }),
-        Accept_Post => 'application/hal+json; profile=http://purl.org/sipwise/ngcp-api/#rel-'.$self->resource_name,
-    ));
-    $c->response->content_type('application/json');
-    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
-    return;
-}
-
-sub POST :Allow {
-    my ($self, $c) = @_;
-
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        unless($c->user->is_master) {
-            $self->error($c, HTTP_FORBIDDEN, "Cannot create admin without master permissions");
-            last;
-        }
-        
-        my $resource = $self->get_valid_post_data(
-            c => $c, 
-            media_type => 'application/json',
-        );
-        last unless $resource;
-
-        my $form = $self->get_form($c);
-        my $pass = $resource->{password};
-        last unless $self->validate_form(
-            c => $c,
-            resource => $resource,
-            form => $form,
-        );
-        delete $resource->{password};
-        if(defined $pass) {
-            $resource->{md5pass} = undef;
-            $resource->{saltedpass} = NGCP::Panel::Utils::Admin::generate_salted_hash($pass);
-        }
-        if($c->user->roles eq "admin") {
-        } elsif($c->user->roles eq "reseller") {
-            $resource->{reseller_id} = $c->user->reseller_id;
-        }
-
-        my $item;
-        $item = $c->model('DB')->resultset('admins')->find({
-            login => $resource->{login},
-        });
-        if($item) {
-            $c->log->error("admin with login '$$resource{login}' already exists");
-            $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Admin with this login already exists");
-            last;
-        }
-
-        try {
-            $item = $c->model('DB')->resultset('admins')->create($resource);
-        } catch($e) {
-            $c->log->error("failed to create admin: $e");
-            $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to create admin.");
-            last;
-        }
-
-        $guard->commit;
-
-        $c->response->status(HTTP_CREATED);
-        $c->response->header(Location => sprintf('/%s%d', $c->request->path, $item->id));
-        $c->response->body(q());
+    my $item;
+    try {
+        $item = $c->model('DB')->resultset('admins')->create($resource);
+    } catch($e) {
+        $c->log->error("failed to create admin: $e");
+        $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to create admin.");
+        last;
     }
-    return;
-}
-
-sub end : Private {
-    my ($self, $c) = @_;
-
-    $self->log_response($c);
+    return $item;
 }
 
 1;
