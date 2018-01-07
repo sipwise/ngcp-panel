f8a5cdfcf85daaf9361c9ee9154ee898e988bf41
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-01-07 20:40:23 +0200

    TT#28456,TT#28462 Rewrite PbxDeviceImages API to Entities
    Change-Id: Ic1d5ad3d54f933d6e6b4a274f80cf67cdf4bb2c6

--- lib/NGCP/Panel/Controller/API/PbxDeviceModelImages.pm
+++ lib/NGCP/Panel/Controller/API/PbxDeviceModelImages.pm
@@ -1,18 +1,15 @@
 package NGCP::Panel::Controller::API::PbxDeviceModelImages;
-use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
+use NGCP::Panel::Utils::Generic qw(:all);
 
-use boolean qw(true);
-use NGCP::Panel::Utils::DataHal qw();
-use NGCP::Panel::Utils::DataHalLink qw();
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::PbxDeviceModelImages NGCP::Panel::Role::API::PbxDeviceModels/;
 
-require Catalyst::ActionRole::ACL;
-require Catalyst::ActionRole::CheckTrailingSlash;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
+__PACKAGE__->set_config();
+
+sub config_allowed_roles {
+    return [qw/admin reseller subscriberadmin subscriber/];
+}
 
 sub allowed_methods{
     return [qw/OPTIONS/];
@@ -34,64 +31,6 @@ sub query_params {
             }
         }
     ];
-}
-
-use parent qw/Catalyst::Controller NGCP::Panel::Role::API::PbxDeviceModelImages NGCP::Panel::Role::API::PbxDeviceModels/;
-
-sub resource_name{
-    return 'pbxdevicemodelimages';
-}
-sub dispatch_path{
-    return '/api/pbxdevicemodelimages/';
-}
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-pbxdevicemodelimages';
-}
-
-__PACKAGE__->config(
-    action => {
-        map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin reseller subscriberadmin subscriber/],
-            Args => 0,
-            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods },
-    },
-);
-
-sub gather_default_action_roles {
-    my ($self, %args) = @_; my @roles = ();
-    push @roles, 'NGCP::Panel::Role::HTTPMethods' if $args{attributes}->{Method};
-    return @roles;
-}
-
-sub auto :Private {
-    my ($self, $c) = @_;
-
-    $self->set_body($c);
-    #$self->log_request($c);
-    return 1;
-}
-
-sub OPTIONS :Allow {
-    my ($self, $c) = @_;
-    my $allowed_methods = $self->allowed_methods_filtered($c);
-    $c->response->headers(HTTP::Headers->new(
-        Allow => join(', ', @{ $allowed_methods }),
-        Accept_Post => 'application/hal+json; profile=http://purl.org/sipwise/ngcp-api/#rel-'.$self->resource_name,
-    ));
-    $c->response->content_type('application/json');
-    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
-    return;
-}
-
-sub end : Private {
-    my ($self, $c) = @_;
-
-    $self->log_response($c);
-    return;
 }
 
 1;
