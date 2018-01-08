8eddb4a774ffdfbded898ceb3a57001c84d8293a
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-01-08 20:12:41 +0200

    TT#28508 Fix tests
    Change-Id: Id4bf415aa4e5e3f59e559e528427db0c5614f1cc

--- t/lib/Test/FakeData.pm
+++ t/lib/Test/FakeData.pm
@@ -72,6 +72,11 @@ has 'data_init' => (
     isa => 'HashRef',
 #    builder => 'build_data',
 );
+has 'use_uniquizer' => (
+    is => 'rw',
+    isa => 'Bool',
+    default => sub { 0 },
+);
 has 'FLAVOUR' => (
     is => 'rw',
     isa => 'Str',
@@ -220,7 +225,8 @@ sub build_data{
             'data' => {
                 sources => [{source => "test",}],
                 subscriber_id => sub { return shift->get_id('subscribers',@_); },
-                name => "from_test"
+                name => "from_test",
+                mode => "whitelist",
             },
             'query' => ['name','subscriber_id'],
             'uniquizer_cb' => sub { Test::FakeData::string_uniquizer(\$_[0]->{name}); },
@@ -242,6 +248,7 @@ sub build_data{
             'data' => {
                 ncos_level_id  => sub { return shift->get_id('ncoslevels',@_); },
                 description => "test_api",
+                carrier_id => sub { return shift->get_id('lnpcarriers',@_); },
             },
         },
         'ncospatterns' => {
@@ -696,7 +703,12 @@ sub create{
     if(exists $self->data->{$collection_name}->{create_special} && 'CODE' eq ref $self->data->{$collection_name}->{create_special}){
         $self->data->{$collection_name}->{create_special}->($self,$collection_name,$test_machine);
     }else{
-        $test_machine->check_create_correct(1);
+        $test_machine->check_create_correct(1,
+            $self->{use_uniquizer}
+            ?
+            $self->data->{$collection_name}->{uniquizer_cb}
+            :
+            undef);
     }
     $self->created->{$collection_name} = {values=>[values %{$test_machine->DATA_CREATED->{ALL}}], order => scalar keys %{$self->created}};
 
