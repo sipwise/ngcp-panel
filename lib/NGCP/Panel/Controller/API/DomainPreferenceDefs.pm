6a1c7e2915f2c037fc0b302fd7b500db963388cd
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-01-13 00:54:27 +0200

    TT#28460 Adapt PreferenceDefs APIs to new parent
    Change-Id: Ib19e2026632eb4bfe18f9ce42781a1a8bd8483a8

--- lib/NGCP/Panel/Controller/API/DomainPreferenceDefs.pm
+++ lib/NGCP/Panel/Controller/API/DomainPreferenceDefs.pm
@@ -1,88 +1,19 @@
 package NGCP::Panel::Controller::API::DomainPreferenceDefs;
-use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use boolean qw(true);
-use Data::HAL qw();
-use Data::HAL::Link qw();
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
+use parent qw/NGCP::Panel::Role::EntityPreferenceDefs NGCP::Panel::Role::API/;
 
 use NGCP::Panel::Utils::Preferences;
-use JSON::Types qw();
-require Catalyst::ActionRole::ACL;
-require Catalyst::ActionRole::CheckTrailingSlash;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
 
 sub allowed_methods{
     return [qw/GET OPTIONS HEAD/];
 }
 
-use parent qw/NGCP::Panel::Role::Entities/;
-
-sub resource_name{
-    return 'domainpreferencedefs';
-}
-sub dispatch_path{
-    return '/api/domainpreferencedefs/';
-}
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-domainpreferencedefs';
-}
-
-__PACKAGE__->config(
-    action => {
-        map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin reseller/],
-            Args => 0,
-            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods }
-    },
-);
-
-
-
-
-
-sub GET :Allow {
-    my ($self, $c) = @_;
-    {
-        my @links;
-        push @links,
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            Data::HAL::Link->new(relation => 'self', href => sprintf('%s', $self->dispatch_path));
-
-        my $hal = Data::HAL->new(
-            links => [@links],
-        );
-        my $resource = NGCP::Panel::Utils::Preferences::api_preferences_defs( c => $c, preferences_group => 'dom_pref' );
-        $hal->resource($resource);
-
-        my $response = HTTP::Response->new(HTTP_OK, undef, 
-            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
-}
-
-
-
-
-
-
+__PACKAGE__->set_config({
+    preferences_group => 'dom_pref',
+    allowed_roles    => [qw/admin reseller/],
+});
 
 1;
 
