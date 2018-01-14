9fa2a19b4f4591bca6258a7647e4471c0f1cf831
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-01-14 13:10:30 +0200

    TT#28460 Replace configs - not journalized
    Change-Id: I9843a780817922f23faa91041d3fab9b7336b2a3

--- lib/NGCP/Panel/Controller/API/PeeringGroups.pm
+++ lib/NGCP/Panel/Controller/API/PeeringGroups.pm
@@ -51,18 +51,9 @@ sub query_params {
     ];
 }
 
-__PACKAGE__->config(
-    action => {
-        map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin/],
-            Args => 0,
-            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods }
-    },
-);
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin/],
+});
 
 
 
