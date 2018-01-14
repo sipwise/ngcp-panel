88c5ae962bc76fe8afad73b24a931f62d359bf92
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-01-14 13:10:30 +0200

    TT#28460 Replace configs - not journalized
    Change-Id: I9843a780817922f23faa91041d3fab9b7336b2a3

--- lib/NGCP/Panel/Controller/API/CustomerFraudEvents.pm
+++ lib/NGCP/Panel/Controller/API/CustomerFraudEvents.pm
@@ -54,18 +54,9 @@ sub relation{
 }
 
 
-__PACKAGE__->config(
-    action => {
-        map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin reseller/],
-            Args => 0,
-            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods }
-    },
-);
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin reseller/],
+});
 
 
 
