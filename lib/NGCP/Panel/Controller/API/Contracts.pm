00099c631367ef519be819560810ceedb10ea48c
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-01-14 13:10:30 +0200

    TT#28460 Replace configs - not journalized
    Change-Id: I9843a780817922f23faa91041d3fab9b7336b2a3

--- lib/NGCP/Panel/Controller/API/Contracts.pm
+++ lib/NGCP/Panel/Controller/API/Contracts.pm
@@ -75,18 +75,9 @@ sub relation{
     return 'http://purl.org/sipwise/ngcp-api/#rel-contracts';
 }
 
-__PACKAGE__->config(
-    action => {
-        map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => 'admin',
-            Args => 0,
-            Does => [qw(ACL CheckTrailingSlash RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods },
-    },
-);
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin/],
+});
 
 
 sub GET :Allow {
