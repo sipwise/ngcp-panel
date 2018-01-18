cd133226ae3a38e37bafb659f6783f1e7219cf8f
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-01-18 17:18:32 +0200

    TT#28462 Rewrite admins API to Entities
    Change-Id: I3bf47038bfb48c09f95030eed5d83550e392ff0c

--- lib/NGCP/Panel/Controller/API/AdminsItem.pm
+++ lib/NGCP/Panel/Controller/API/AdminsItem.pm
@@ -3,32 +3,13 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
+use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::Admins/;
+
 use NGCP::Panel::Utils::Admin;
-
-use boolean qw(true);
-use Data::HAL qw();
-use Data::HAL::Link qw();
-use HTTP::Headers qw();
 use HTTP::Status qw(:constants);
-
-require Catalyst::ActionRole::ACL;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
 
 sub allowed_methods{
     return [qw/GET OPTIONS HEAD DELETE/];
-}
-
-use parent qw/Catalyst::Controller NGCP::Panel::Role::API::Admins/;
-
-sub resource_name{
-    return 'admins';
-}
-sub dispatch_path{
-    return '/api/admins/';
-}
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-admins';
 }
 
 sub journal_query_params {
@@ -36,128 +17,52 @@ sub journal_query_params {
     return $self->get_journal_query_params($query_params);
 }
 
-__PACKAGE__->config(
-    action => {
-        (map { $_ => {
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin reseller/],
-            Args => 1,
-            Does => [qw(ACL RequireSSL)],
-            Method => $_,
-            Path => __PACKAGE__->dispatch_path,
-        } } @{ __PACKAGE__->allowed_methods }),
-        @{ __PACKAGE__->get_journal_action_config(__PACKAGE__->resource_name,{
-            ACLDetachTo => '/api/root/invalid_user',
-            AllowedRole => [qw/admin reseller/],
-            Does => [qw(ACL RequireSSL)],
-        }) },
-    },
-);
-
-sub gather_default_action_roles {
-    my ($self, %args) = @_; my @roles = ();
-    push @roles, 'NGCP::Panel::Role::HTTPMethods' if $args{attributes}->{Method};
-    return @roles;
-}
-
-sub auto :Private {
-    my ($self, $c) = @_;
-
-    $self->set_body($c);
-    $self->log_request($c);
-    return 1;
-}
-
-sub GET :Allow {
-    my ($self, $c, $id) = @_;
-    {
-        last unless $self->valid_id($c, $id);
-        my $admin = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, admin => $admin);
-
-        my $hal = $self->hal_from_item($c, $admin);
-
-        my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-            (map { # XXX Data::HAL must be able to generate links with multiple relations
-                s|rel="(http://purl.org/sipwise/ngcp-api/#rel-resellers)"|rel="item $1"|r =~
-                s/rel=self/rel="item self"/r;
-            } $hal->http_headers),
-        ), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
-}
-
-sub HEAD :Allow {
-    my ($self, $c, $id) = @_;
-    $c->forward(qw(GET));
-    $c->response->body(q());
-    return;
-}
-
-sub OPTIONS :Allow {
-    my ($self, $c, $id) = @_;
-    my $allowed_methods = $self->allowed_methods_filtered($c);
-    $c->response->headers(HTTP::Headers->new(
-        Allow => join(', ', @{ $allowed_methods }),
-        Accept_Patch => 'application/json-patch+json',
-    ));
-    $c->response->content_type('application/json');
-    $c->response->body(JSON::to_json({ methods => $allowed_methods })."\n");
-    return;
-}
-
-sub DELETE :Allow {
-    my ($self, $c, $id) = @_;
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $admin = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, admin => $admin);
-
-        $c->log->error("++++++ trying to delete admin #$id as #" . $c->user->id);
-        
-        my $special_user_login = NGCP::Panel::Utils::Admin::get_special_admin_login();
-        if($admin->login eq $special_user_login) {
-            $self->error($c, HTTP_FORBIDDEN, "Cannot delete special user '$special_user_login'");
-            last;
-        }
-        if($c->user->id == $id) {
-            $self->error($c, HTTP_FORBIDDEN, "Cannot delete own user");
-            last;
-        }
-        if($c->user->read_only) {
-            $self->error($c, HTTP_FORBIDDEN, "Insufficient permissions");
-            last;
-        }
-
-        # reseller association is checked in item_rs of role
-
-        last unless $self->add_delete_journal_item_hal($c,sub {
-            my $self = shift;
-            my ($c) = @_;
-            return $self->hal_from_item($c,$admin); });
-        
-        $admin->delete;
-
-        $guard->commit;
-
-        $c->response->status(HTTP_NO_CONTENT);
-        $c->response->body(q());
-    }
-    return;
-}
-
 sub get_journal_methods{
     return [qw/handle_item_base_journal handle_journals_get handle_journalsitem_get handle_journals_options handle_journalsitem_options handle_journals_head handle_journalsitem_head/];
 }   
 
-sub end : Private {
-    my ($self, $c) = @_;
+__PACKAGE__->set_config();
 
-    $self->log_response($c);
+sub delete_item {
+    my ($self, $c, $item) = @_;
+
+    my $special_user_login = NGCP::Panel::Utils::Admin::get_special_admin_login();
+
+    if($item->login eq $special_user_login) {
+        $self->error($c, HTTP_FORBIDDEN, "Cannot delete special user '$special_user_login'");
+        last;
+    }
+    if($c->user->id == $item->id) {
+        $self->error($c, HTTP_FORBIDDEN, "Cannot delete own user");
+        last;
+    }
+    if($c->user->read_only) {
+        $self->error($c, HTTP_FORBIDDEN, "Insufficient permissions");
+        last;
+    }
+
+    # reseller association is checked in item_rs of role
+
+    last unless $self->add_delete_journal_item_hal($c,sub {
+        my $self = shift;
+        my ($c) = @_;
+        return $self->hal_from_item($c, $item); });
+    
+    $item->delete;
     return;
+}
+
+#we don't use update_item for the admins now, as we dont allo PUT and PATCH
+sub update_item_model {
+    my ($self, $c, $item, $old_resource, $resource, $form) = @_;
+
+    if($old_resource->{login} eq NGCP::Panel::Utils::Admin::get_special_admin_login()) {
+        my $active = $resource->{is_active};
+        $resource = $old_resource;
+        $resource->{is_active} = $active;
+    }
+    $item->update($resource);
+    return $item;
 }
 
 1;
