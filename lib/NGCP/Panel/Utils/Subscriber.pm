5df8cf4b7f21606c21e8d5428a150ff87bc7debd
Kirill Solomko <ksolomko@sipwise.com>
2018-01-18 15:17:05 +0100

    TT#30653 apply_rewrite() strict checks for $subscriber variable type
        * returns if $subscriber is not a reference
        * fix a check wether $subscriber is the provisioning object
    
    Change-Id: I02410b1f34a7761bbcbe7a6bd005336211a5e0b9

--- lib/NGCP/Panel/Utils/Subscriber.pm
+++ lib/NGCP/Panel/Utils/Subscriber.pm
@@ -1394,10 +1394,10 @@ sub apply_rewrite {
         unless ($rwr_rs->count) {
             return $callee;
         }
-    } elsif (not $subscriber) {
+    } elsif (!$subscriber || !ref($subscriber)) {
         $c->log->warn('could not apply rewrite: no subscriber found.');
         return $callee;
-    } elsif ($subscriber->provisioning_voip_subscriber) {
+    } elsif ($subscriber->can('provisioning_voip_subscriber')) {
         $rwr_rs = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
             c => $c, attribute => $dir,
             prov_subscriber => $subscriber->provisioning_voip_subscriber,
