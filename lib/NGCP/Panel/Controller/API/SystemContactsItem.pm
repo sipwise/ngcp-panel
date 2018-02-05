2fda730f1d4dc79f406dc10d3a5f47c5ea057916
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-02-05 14:38:46 +0200

    remove unnecessary libraries from item
    Change-Id: I82c9964eda1251410939ff6d15ed1ab7871cb6b7

--- lib/NGCP/Panel/Controller/API/SystemContactsItem.pm
+++ lib/NGCP/Panel/Controller/API/SystemContactsItem.pm
@@ -7,10 +7,7 @@ use HTTP::Headers qw();
 use HTTP::Status qw(:constants);
 
 use NGCP::Panel::Utils::DateTime;
-use NGCP::Panel::Utils::ValidateJSON qw();
-require Catalyst::ActionRole::ACL;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
+
 
 sub allowed_methods{
     return [qw/GET OPTIONS HEAD PATCH PUT DELETE/];
