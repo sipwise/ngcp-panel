a817c065be790409d02d61a1a788bd6ce08a220f
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-03-19 16:48:13 +0100

    TT#34015 Introduce /api/phonebookentries
        * /api/phonebookentries is used for the
          phonebook_reseller
          phonebook_contract
          phonebook_subscriber data storage interaction
    
    Change-Id: If45390fd3080886e03602216cfbacc33296f1558

---
+++ lib/NGCP/Panel/Form/Phonebook/CustomerAPI.pm
@@ -0,0 +1,46 @@
+package NGCP::Panel::Form::Phonebook::CustomerAPI;
+
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+
+has_field 'id' => (
+    type => 'PosInteger',
+    required => 0,
+    label => 'Phonebook entry id',
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Phonebook entry id'],
+    },
+);
+
+has_field 'contract_id' => (
+    type => 'PosInteger',
+    required => 0,
+    label => 'Phonebook entry contract id',
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Phonebook entry contract id'],
+    },
+);
+
+has_field 'name' => (
+    type => 'Text',
+    required => 1,
+    label => 'Phonebook entry name',
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The full entry name "e.g. John Smith".'],
+    },
+);
+
+has_field 'number' => (
+    type => 'Text',
+    required => 1,
+    label => 'Phonebook number',
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The phonebook number, can be either as a numeric or a SIP number.' ],
+    },
+);
+
+1;
