f9347452ef62fb71ab926fe1fd742fb5501e2ef6
Rene Krenn <rkrenn@sipwise.com>
2018-03-29 04:29:26 +0200

    TT#34751 TT#34752 /api/callqueues
    including:
    + call_id
    + start_time
    + duration
    + caller (from_uri)
    + callee (to_uri)
    
    Change-Id: I1b0aea9bf578a71fd3e1a5433c9801ebde173b65

---
+++ lib/NGCP/Panel/Controller/API/CallQueues.pm
@@ -0,0 +1,61 @@
+package NGCP::Panel::Controller::API::CallQueues;
+use NGCP::Panel::Utils::Generic qw(:all);
+
+use Sipwise::Base;
+
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
+
+sub allowed_methods{
+    return [qw/GET OPTIONS HEAD/];
+}
+
+sub api_description {
+    return 'The queue of waiting calls per subscriber.';
+};
+
+sub query_params {
+    return [
+        {
+            param => 'reseller_id',
+            description => 'Filter for callqueues of subscribers belonging to a specific reseller',
+            query => {
+                first => sub {
+                    my $q = shift;
+                    { 'domain_resellers.reseller_id' => $q };
+                },
+                second => sub {
+                    { join => 'domain_resellers' };
+                },
+            },
+        },
+        {
+            # we handle that separately/manually in the role
+            param => 'number',
+            description => 'Filter for callqueues of subscribers with numbers matching the given pattern.',
+        },
+    ];
+}
+
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::CallQueues/;
+
+sub resource_name{
+    return 'callqueues';
+}
+
+sub dispatch_path{
+    return '/api/callqueues/';
+}
+
+sub relation{
+    return 'http://purl.org/sipwise/ngcp-api/#rel-callqueues';
+}
+
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
+});
+
+1;
---
+++ lib/NGCP/Panel/Form/CallQueue/API.pm
@@ -0,0 +1,32 @@
+package NGCP::Panel::Form::CallQueue::API;
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+
+has_field 'id' => (
+    type => 'Hidden',
+    noupdate => 1,
+);
+
+has_field 'queue_length' => (
+    type => 'Integer',
+    required => 0,
+    default => '0',
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The length of the call queue.']
+    },
+);
+
+has_field 'queue' => (
+    type => 'Repeatable',
+    do_wrapper => 1,
+    do_label => 0,
+    required => 0,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Number of Objects, each containing the keys ' .
+                  '"start_time", "duration", "callee" and "caller".'],
+    },
+);
+
+1;
