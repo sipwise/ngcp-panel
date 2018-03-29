8c911f28117cf814e6e0d14dcede8ccf3c7063de
Rene Krenn <rkrenn@sipwise.com>
2018-03-29 04:29:26 +0200

    TT#34751 TT#34752 /api/callqueues
    Change-Id: I1b0aea9bf578a71fd3e1a5433c9801ebde173b65

---
+++ lib/NGCP/Panel/Controller/API/CallQueues.pm
@@ -0,0 +1,112 @@
+package NGCP::Panel::Controller::API::CallQueues;
+use NGCP::Panel::Utils::Generic qw(:all);
+
+use Sipwise::Base;
+
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
+
+sub allowed_methods{
+    return [qw/GET OPTIONS HEAD/];
+}
+
+sub api_description {
+    return 'The queue of waiting calls per subscriber.';
+};
+
+sub query_params {
+    return [
+        {
+            param => 'reseller_id',
+            description => 'Filter for callrecordings belonging to a specific reseller',
+            query => {
+                first => sub {
+                    my $q = shift;
+                    { 'domain_resellers.reseller_id' => $q };
+                },
+                second => sub {
+                    { join => 'domain_resellers' };
+                },
+            },
+        },
+        {
+            # we handle that separately/manually in the role
+            param => 'number',
+            description => 'Filter for callrecordings where the subscriber with the given id is involved.',
+        },
+    ];
+}
+
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::CallQueues/;
+
+sub resource_name{
+    return 'callqueues';
+}
+
+sub dispatch_path{
+    return '/api/callqueues/';
+}
+
+sub relation{
+    return 'http://purl.org/sipwise/ngcp-api/#rel-callqueues';
+}
+
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
+});
+
+sub GET :Allow {
+    my ($self, $c) = @_;
+    my $page = $c->request->params->{page} // 1;
+    my $rows = $c->request->params->{rows} // 10;
+    {
+        my $callqueues = $self->item_rs($c);
+        (my $total_count, $callqueues) = $self->paginate_order_collection($c, $callqueues);
+        my (@embedded, @links);
+        my $form = $self->get_form($c);
+        for my $item ($callqueues->all) {
+            push @embedded, $self->hal_from_item($c, $item, $form);
+            push @links, Data::HAL::Link->new(
+                relation => 'ngcp:'.$self->resource_name,
+                href     => sprintf('/%s%d', $c->request->path, $item->id),
+            );
+        }
+        push @links,
+            Data::HAL::Link->new(
+                relation => 'curies',
+                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
+                name => 'ngcp',
+                templated => true,
+            ),
+            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
+            Data::HAL::Link->new(relation => 'self', href => sprintf('/%s?page=%s&rows=%s', $c->request->path, $page, $rows));
+        if(($total_count / $rows) > $page ) {
+            push @links, Data::HAL::Link->new(relation => 'next', href => sprintf('/%s?page=%d&rows=%d', $c->request->path, $page + 1, $rows));
+        }
+        if($page > 1) {
+            push @links, Data::HAL::Link->new(relation => 'prev', href => sprintf('/%s?page=%d&rows=%d', $c->request->path, $page - 1, $rows));
+        }
+
+        my $hal = Data::HAL->new(
+            embedded => [@embedded],
+            links => [@links],
+        );
+        $hal->resource({
+            total_count => $total_count,
+        });
+        my $rname = $self->resource_name;
+
+        my $response = HTTP::Response->new(HTTP_OK, undef,
+            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
+        $c->response->headers($response->headers);
+        $c->response->body($response->content);
+        return;
+    }
+    return;
+}
+
+1;
+
