5680c8ffc1205e13ce80d58b9544e9cf6ac8aeb3
Rene Krenn <rkrenn@sipwise.com>
2018-03-29 04:29:26 +0200

    TT#34751 TT#34752 /api/callqueues
    Change-Id: I1b0aea9bf578a71fd3e1a5433c9801ebde173b65

---
+++ lib/NGCP/Panel/Form/CallQueue.pm/API.pm
@@ -0,0 +1,26 @@
+package NGCP::Panel::Form::CallQueue::API;
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+
+has_field 'id' => (
+    type => 'Hidden',
+    noupdate => 1,
+);
+
+has_field 'queue' => (
+    type => 'Repeatable',
+    do_wrapper => 1,
+    do_label => 0,
+    required => 0,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Number of Objects, each containing the keys ' .
+                  '"destinationset", "timeset" and "sourceset". The values must be the name of ' .
+                  'a corresponding set which belongs to the same subscriber.'],
+    },
+);
+
+
+1;
+
+# vim: set tabstop=4 expandtab:
