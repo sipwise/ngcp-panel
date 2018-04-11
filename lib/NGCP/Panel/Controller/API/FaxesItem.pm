e62628a872b956602a4ac61c598ba416db94149a
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-04-11 12:32:47 +0200

    TT#35538 Faxes allow application/json and multipart/form-data
    Change-Id: Ic08995e096581c1b0f3dc7d6abce1fe15720cde7

--- lib/NGCP/Panel/Controller/API/FaxesItem.pm
+++ lib/NGCP/Panel/Controller/API/FaxesItem.pm
@@ -3,82 +3,21 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
+use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::Faxes/;
 
-use NGCP::Panel::Utils::ValidateJSON qw();
-use NGCP::Panel::Utils::Subscriber;
-require Catalyst::ActionRole::ACL;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
+    dont_validate_hal => 1,
+});
 
 sub allowed_methods{
     return [qw/GET OPTIONS HEAD/];
 }
 
-use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::Faxes/;
-
-sub resource_name{
-    return 'faxes';
-}
-
-sub dispatch_path{
-    return '/api/faxes/';
-}
-
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-faxes';
-}
-
-__PACKAGE__->set_config({
-    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
-});
-
-sub GET :Allow {
-    my ($self, $c, $id) = @_;
-    {
-        last unless $self->valid_id($c, $id);
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, fax => $item);
-
-        my $hal = $self->hal_from_item($c, $item);
-
-        my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-            (map { # XXX Data::HAL must be able to generate links with multiple relations
-                s|rel="(http://purl.org/sipwise/ngcp-api/#rel-resellers)"|rel="item $1"|;
-                s/rel=self/rel="item self"/;
-                $_
-            } $hal->http_headers),
-        ), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
-}
-
-
-
-
-
-
-
-#sub DELETE :Allow {
-#    my ($self, $c, $id) = @_;
-#
-#    my $guard = $c->model('DB')->txn_scope_guard;
-#    {
-#        my $item = $self->item_by_id($c, $id);
-#        last unless $self->resource_exists($c, fax => $item);
-#
-#        $item->delete;
-#        NGCP::Panel::Utils::Subscriber::vmnotify( 'c' => $c, 'fax' => $item );
-#        $guard->commit;
-#
-#        $c->response->status(HTTP_NO_CONTENT);
-#        $c->response->body(q());
-#    }
-#    return;
+#sub delete_item {
+#    my ($self, $c, $item) = @_;
+#    $item->delete;
+#    NGCP::Panel::Utils::Subscriber::vmnotify( 'c' => $c, 'fax' => $item );
 #}
 
 1;
