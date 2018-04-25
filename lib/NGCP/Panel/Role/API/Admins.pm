5916584a2000d4eb26bc603a792feebe17e0a0ce
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-04-25 21:55:22 +0200

    TT#35429 Fix using boolean json strings in /api/admins
    Change-Id: If60ef0e75042c23771294315bd62513ee6d7678d
    (cherry picked from commit 17d36cc3393a838ad12bef7a52bff644553c2eda)

--- lib/NGCP/Panel/Role/API/Admins.pm
+++ lib/NGCP/Panel/Role/API/Admins.pm
@@ -80,8 +80,7 @@ sub process_form_resource{
         $resource->{saltedpass} = NGCP::Panel::Utils::Admin::generate_salted_hash($pass);
     }
     foreach my $f(qw/billing_data call_data is_active is_master is_superuser lawful_intercept read_only show_passwords/) {
-        $resource->{$f} = (ref $resource->{$f} eq 'JSON::true' ) ? 1 : 0;
-        #|| $resource->{$f} eq 'true'
+        $resource->{$f} = (ref $resource->{$f} eq 'JSON::true' || $resource->{$f} eq 'true') ? 1 : 0;
     }
     return $resource;
 }
