5b5b45b5a8cc0ad0685b4aeb8cf334210a4d38f6
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-05-07 05:15:48 +0200

    TT#36055 WIP Add field device preferences web UI
    Change-Id: I0af9f55cca8c8292cc3b18aeedf8fbcfb58a88f1

---
+++ lib/NGCP/Panel/Form/Device/Preference.pm
@@ -0,0 +1,160 @@
+package NGCP::Panel::Form::Device::Preference;
+
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+with 'NGCP::Panel::Render::RepeatableJs';
+
+use HTML::FormHandler::Widget::Block::Bootstrap;
+
+has '+widget_wrapper' => ( default => 'Bootstrap' );
+sub build_render_list {[qw/fields actions/]}
+sub build_form_element_class {[qw(form-horizontal)]}
+
+has_field 'attribute' => (
+    type => 'Text',
+    required => 1,
+    label => 'Name',
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Name will be prefixed with double underscore.']
+    },
+);
+
+has_field 'label' => (
+    type => 'Text',
+    required => 1,
+    label => 'Label',
+);
+
+has_field 'description' => (
+    type => 'Text',
+    required => 1,
+    label => 'Description',
+);
+
+has_field 'fielddev_pref' => (
+    type => 'Boolean',
+    default => 1,
+    label => 'Override on deployed device',
+);
+
+has_field 'max_occur' => (
+    type => 'Boolean',
+    default => '0',
+    label => 'Only one is allowed.',
+);
+
+has_field 'data_type' => (
+    type => 'Select',
+    required => 1,
+    label => 'Data type',
+    options => [
+        { value => '',        label => 'none' },
+        { value => 'boolean', label => 'Boolean' },
+        { value => 'string',  label => 'String' },
+        { value => 'int',     label => 'Integer' },
+        { value => 'enum',    label => 'Enum' },
+    ],
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Options to lock customer if the monthly limit is exceeded.']
+    },
+);
+
+has_field 'enum' => (
+    type => 'Repeatable',
+    required => 0,
+    setup_for_js => 1,
+    num_when_empty => 0,
+    do_wrapper => 1,
+    do_label => 1,
+    label => 'Enum values',
+    tags => { 
+        controls_div => 1,
+    },
+    wrapper_class => [qw/hfh-rep-block/],
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['An Array of enum.'],
+    },
+);
+
+has_field 'enum.id' => (
+    type => 'Hidden',
+);
+
+has_field 'enum.label' => (
+    type => 'Text',
+    wrapper_class => [qw/hfh-rep-field/],
+);
+
+has_field 'enum.value' => (
+    type => 'Text',
+    wrapper_class => [qw/hfh-rep-field/],
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Name will be prefixed with double underscore.']
+    },
+);
+
+has_field 'enum.default_val' => (
+    type => 'Boolean',
+    wrapper_class => [qw/hfh-rep-field/],
+);
+
+has_field 'enum.rm' => (
+    type => 'RmElement',
+    value => 'Remove enum value description',
+    order => 100,
+    element_class => [qw/btn btn-primary pull-right/],
+);
+
+has_field 'enum_add' => (
+    type => 'AddElement',
+    repeatable => 'enum',
+    value => 'Add enum value',
+    element_class => [qw/btn btn-primary pull-right/],
+);
+
+has_field 'save' => (
+    type => 'Submit',
+    value => 'Save',
+    element_class => [qw/btn btn-primary/],
+    label => '',
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/attribute fielddev_pref label max_occur description data_type enum enum_add/],
+);
+
+has_block 'actions' => (
+    tag => 'div',
+    class => [qw/modal-footer/],
+    render_list => [qw/save/],
+);
+
+sub validate_attribute {
+    my ($self, $field) = @_;
+    my $c = $self->form->ctx;
+    return unless $c;
+
+    unless($field->value =~ /^[a-z0-9_]+$/) {
+        my $err_msg = 'Only lower-case, digits and _ allowed';
+        $field->add_error($err_msg);
+    }
+}
+
+sub validate_enum_value {
+    my ($self, $field) = @_;
+    my $c = $self->form->ctx;
+    return unless $c;
+
+    unless($field->value =~ /^[a-z0-9_]+$/) {
+        my $err_msg = 'Only lower-case, digits and _ allowed';
+        $field->add_error($err_msg);
+    }
+}
+1;
+# vim: set tabstop=4 expandtab:
