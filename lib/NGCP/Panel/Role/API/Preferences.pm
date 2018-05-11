9b10bc58ae59616ba10560c55e1d5f6fb8029b3e
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-05-11 10:48:23 +0200

    TT#36055 Remove common code from preferences
    Change-Id: Ib5770b10dd8dcf4ca7a327345c9a78e9b1e22452

--- lib/NGCP/Panel/Role/API/Preferences.pm
+++ lib/NGCP/Panel/Role/API/Preferences.pm
@@ -22,9 +22,9 @@ sub get_form {
 }
 
 sub hal_from_item {
-    my ($self, $c, $item, $type) = @_;
+    my ($self, $c, $item) = @_;
 
-    $type //= $self->container_resource_type;
+    my $type = $self->container_resource_type;
     my $print_type = $type;
     $print_type = "customers" if $print_type eq "contracts";
     my $hal = Data::HAL->new(
@@ -58,10 +58,16 @@ sub _check_profile {
     return 1;
 }
 
-sub get_resource {
-    my ($self, $c, $item, $type) = @_;
+sub resource_from_item{
+    my($self, $c, $item) = @_;
+    return $self->get_resource($c, $item);
+}
 
-    $type //= $self->container_resource_type;
+sub get_resource {
+    my ($self, $c, $item) = @_;
+
+    my $type = $self->container_resource_type;
+
 
     my $prefs;
     my %profile_attrs = (); # for filtering profiles based list
@@ -274,9 +280,9 @@ sub get_resource {
 }
 
 sub _item_rs {
-    my ($self, $c, $type) = @_;
+    my ($self, $c) = @_;
     my $item_rs;
-    $type //= $self->container_resource_type;
+    my $type = $self->container_resource_type;
 
     if($type eq "domains") {
         # we actually return the domain rs here, as we can easily
@@ -381,14 +387,6 @@ sub _item_rs {
     return $item_rs;
 }
 
-sub item_by_id {
-    my ($self, $c, $id, $type) = @_;
-
-    $type //= $self->container_resource_type;
-    my $item_rs = $self->item_rs($c, $type);
-    return $item_rs->find($id);
-}
-
 sub get_preference_rs {
     my ($self, $c, $type, $elem, $attr) = @_;
 
@@ -442,14 +440,11 @@ sub get_preference_rs {
 }
 
 sub update_item {
-    my ($self, $c, $item, $old_resource, $resource, $replace, $type) = @_;
+    my ($self, $c, $item, $old_resource, $resource) = @_;
 
-    if (ref $replace || !defined $replace) {
-        $replace = uc($c->request->method) eq 'PUT';
-    }
-    if (ref $type || !defined $type) {
-        $type = $self->container_resource_type;
-    }
+    my $type = $self->container_resource_type;
+    my $replace = uc($c->request->method) eq 'PUT';
+
     delete $resource->{id};
     my $accessor;
     my $elem;
