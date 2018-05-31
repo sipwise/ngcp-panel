a5e38b6dfe2be07f2daa7658e4959b188b833529
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-05-31 21:23:06 +0200

    TT#36007 Remove /api/sipcaptures standard GET method
    Change-Id: I6b318df167056486181542460217de3b2b221773

--- lib/NGCP/Panel/Controller/API/SIPCaptures.pm
+++ lib/NGCP/Panel/Controller/API/SIPCaptures.pm
@@ -3,12 +3,6 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use boolean qw(true);
-use Data::HAL qw();
-use Data::HAL::Link qw();
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
-use DateTime::TimeZone;
 use NGCP::Panel::Utils::DateTime;
 
 use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::SIPCaptures/;
@@ -88,53 +82,6 @@ sub query_params {
             description => 'Format start_time according to the optional time zone provided here, e.g. Europe/Berlin.',
         },
     ];
-}
-
-sub GET :Allow {
-    my ($self, $c) = @_;
-    my $page = $c->request->params->{page} // 1;
-    my $rows = $c->request->params->{rows} // 10;
-    {
-        if($c->req->param('tz') && !DateTime::TimeZone->is_valid_name($c->req->param('tz'))) {
-            $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Query parameter 'tz' value is not a valid time zone");
-            return;
-        }
-
-        my $items = $self->item_rs($c);
-        (my $total_count, $items) = $self->paginate_order_collection($c, $items);
-        my (@embedded, @links);
-        my $form = $self->get_form($c);
-        for my $item ($items->all) {
-            push @embedded, $self->hal_from_item($c, $item, $form);
-            push @links, Data::HAL::Link->new(
-                relation => 'ngcp:'.$self->resource_name,
-                href     => sprintf('/%s%s', $c->request->path, $item->call_id),
-            );
-        }
-        push @links,
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            $self->collection_nav_links($c, $page, $rows, $total_count, $c->request->path, $c->request->query_params);
-
-        my $hal = Data::HAL->new(
-            embedded => [@embedded],
-            links => [@links],
-        );
-        $hal->resource({
-            total_count => $total_count,
-        });
-        my $response = HTTP::Response->new(HTTP_OK, undef,
-            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
 }
 
 1;
