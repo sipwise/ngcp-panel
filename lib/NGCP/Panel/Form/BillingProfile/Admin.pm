8b8868c9dee9b0e348aec84441779d46b8b0b3ac
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-06-13 13:53:40 +0200

    TT#37458 Add billing profile aoc field
    Change-Id: I43ba35575f24f18ae1647da3a0e268fa63c1dd83

--- lib/NGCP/Panel/Form/BillingProfile/Admin.pm
+++ lib/NGCP/Panel/Form/BillingProfile/Admin.pm
@@ -15,7 +15,7 @@ has_field 'reseller' => (
 has_block 'fields' => (
     tag => 'div',
     class => [qw/modal-body/],
-    render_list => [qw/reseller handle name prepaid interval_charge interval_free_time interval_free_cash 
+    render_list => [qw/reseller handle name prepaid advice_of_charge interval_charge interval_free_time interval_free_cash 
         fraud_interval_limit fraud_interval_lock fraud_interval_notify
         fraud_daily_limit fraud_daily_lock fraud_daily_notify fraud_use_reseller_rates
         currency id
--- lib/NGCP/Panel/Form/BillingProfile/Reseller.pm
+++ lib/NGCP/Panel/Form/BillingProfile/Reseller.pm
@@ -45,6 +45,15 @@ has_field 'prepaid' => (
     },
 );
 
+has_field 'advice_of_charge' => (
+    type => 'Boolean',
+    default => 0,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Enable Advice of Charge support for the billing profile.']
+    },
+);
+
 has_field 'interval_charge' => (
     type => 'Money',
     element_attr => {
@@ -182,7 +191,7 @@ has_field 'save' => (
 has_block 'fields' => (
     tag => 'div',
     class => [qw/modal-body/],
-    render_list => [qw/handle name prepaid interval_charge interval_free_time interval_free_cash
+    render_list => [qw/handle name prepaid advice_of_charge interval_charge interval_free_time interval_free_cash
         fraud_interval_limit fraud_interval_lock fraud_interval_notify
         fraud_daily_limit fraud_daily_lock fraud_daily_notify fraud_use_reseller_rates
         currency id
