2228830317da430fedafd04c10a35d67a638f846
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-06-15 12:59:28 +0200

    TT#37459 Implement outbound lb_set/path for permanent registrations
    Change-Id: I5565eb20c61bd46dcdf888ee482a79b94c43813f

--- lib/NGCP/Panel/Form/Subscriber/RegisteredAPI.pm
+++ lib/NGCP/Panel/Form/Subscriber/RegisteredAPI.pm
@@ -1,13 +1,8 @@
 package NGCP::Panel::Form::Subscriber::RegisteredAPI;
 
 use HTML::FormHandler::Moose;
-extends 'HTML::FormHandler';
-
-use HTML::FormHandler::Widget::Block::Bootstrap;
-
-has '+widget_wrapper' => ( default => 'Bootstrap' );
+extends 'NGCP::Panel::Form::Subscriber::Registered';
 has '+use_fields_for_input_without_param' => ( default => 1 );
-has_field 'submitid' => ( type => 'Hidden' );
 
 has_field 'subscriber_id' => (
     type => 'PosInteger',
@@ -15,15 +10,6 @@ has_field 'subscriber_id' => (
     element_attr => {
         rel => ['tooltip'],
         title => ['The subscriber the contact belongs to.']
-    },
-);
-
-has_field 'contact' => (
-    type => 'Text',
-    required => 1,
-    element_attr => {
-        rel => ['tooltip'],
-        title => ['The SIP URI pointing to the current contact of the subscriber.']
     },
 );
 
@@ -36,20 +22,6 @@ has_field 'expires' => (
     },
 );
 
-has_field 'q' => (
-    type => 'Float',
-    required => 1,
-    range_start => -1,
-    range_end => 1,
-    decimal_symbol => '.',
-    default => 1,
-    element_attr => {
-        rel => ['tooltip'],
-        title => ['The priority (q-value) of the registration.']
-    },
-    #validate_method => \&validate_q,
-);
-
 has_field 'nat' => (
     type => 'Boolean',
     required => 0,
@@ -58,25 +30,6 @@ has_field 'nat' => (
         title => ['The registered contact is detected as behind NAT.']
     },
 );
-sub validate_q {
-    my ($self,$field) = @_;
-    if(($field->value < -1) || ($field->value > 1)){
-        $field->add_error('Value of "q" must be a float value between -1 and 1'); 
-        return;
-    }
-    return 1;
-}
-=pod
-sub validate {
-    my $self = shift;
-    my $attach = $self->field('attach')->value;
-    my $delete = $self->field('delete')->value;
-    if($delete && !$attach) {
-        $self->field('attach')->add_error('Must be set if delete is set');
-    }
-}
-=cut
-
 
 1;
 
