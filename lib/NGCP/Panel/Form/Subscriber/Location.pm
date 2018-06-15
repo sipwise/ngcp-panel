2ab1507b47b95f43ef10a1494b16c7d4a5515dcd
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-06-15 12:59:28 +0200

    TT#37459 Implement outbound lb_set/path for permanent registrations
    Change-Id: I5565eb20c61bd46dcdf888ee482a79b94c43813f

--- lib/NGCP/Panel/Form/Subscriber/Location.pm
+++ lib/NGCP/Panel/Form/Subscriber/Location.pm
@@ -1,38 +1,12 @@
 package NGCP::Panel::Form::Subscriber::Location;
 
 use HTML::FormHandler::Moose;
-extends 'HTML::FormHandler';
+extends 'NGCP::Panel::Form::Subscriber::Registered';
 
 use HTML::FormHandler::Widget::Block::Bootstrap;
 
-has '+widget_wrapper' => ( default => 'Bootstrap' );
-has_field 'submitid' => ( type => 'Hidden' );
 sub build_render_list {[qw/submitid fields actions/]}
 sub build_form_element_class { [qw/form-horizontal/] }
-
-has_field 'contact' => (
-    type => 'Text',
-    label => 'Contact URI',
-    required => 1,
-    element_attr => {
-        rel => ['tooltip'],
-        title => ['A full SIP URI like sip:user@ip:port']
-    },
-);
-
-has_field 'q' => (
-    type => 'Float',
-    label => 'Priority (q-value)',
-    required => 1,
-    range_start => -1,
-    range_end => 1,
-    decimal_symbol => '.',
-    default => 1,
-    element_attr => {
-        rel => ['tooltip'],
-        title => ['The contact priority for serial forking (float value, higher is stronger) between -1.00 to 1.00']
-    },
-);
 
 has_field 'save' => (
     type => 'Submit',
@@ -44,7 +18,7 @@ has_field 'save' => (
 has_block 'fields' => (
     tag => 'div',
     class => [qw/modal-body/],
-    render_list => [qw/contact q/],
+    render_list => [qw/contact q socket/],
 );
 
 has_block 'actions' => (
