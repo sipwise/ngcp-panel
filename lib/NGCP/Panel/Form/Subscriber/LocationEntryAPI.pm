a862753017c21d2cc318e84aa0a892523fcf79ef
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-06-15 12:59:28 +0200

    TT#37459 Implement outbound lb_set/path for permanent registrations
    Change-Id: I5565eb20c61bd46dcdf888ee482a79b94c43813f

---
+++ lib/NGCP/Panel/Form/Subscriber/LocationEntryAPI.pm
@@ -0,0 +1,58 @@
+package NGCP::Panel::Form::Subscriber::LocationEntryAPI;
+
+use HTML::FormHandler::Moose;
+extends 'NGCP::Panel::Form::Subscriber::LocationEntry';
+
+has '+use_fields_for_input_without_param' => ( default => 1 );
+
+has_field 'subscriber_id' => (
+    type => 'PosInteger',
+    required => 1,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The subscriber the contact belongs to.']
+    },
+);
+
+has_field 'expires' => (
+    type => 'Text',
+    required => 1,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The expire timestamp of the registered contact.']
+    },
+);
+
+has_field 'nat' => (
+    type => 'Boolean',
+    required => 0,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The registered contact is detected as behind NAT.']
+    },
+);
+
+1;
+
+__END__
+
+=head1 NAME
+
+NGCP::Panel::Form::Subscriber::RegisteredAPI
+
+=head1 DESCRIPTION
+
+A helper to manipulate the registered API subscriber form
+
+=head1 AUTHOR
+
+Sipwise Development Team
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+# vim: set tabstop=4 expandtab:
