32252b2d8e333fcf740a684540739dfd7724e2a1
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-06-15 12:59:28 +0200

    TT#37459 Implement outbound lb_set/path for permanent registrations
    Change-Id: I5565eb20c61bd46dcdf888ee482a79b94c43813f

--- lib/NGCP/Panel/Utils/Kamailio.pm
+++ lib/NGCP/Panel/Utils/Kamailio.pm
@@ -41,10 +41,10 @@ EOF
 }
 
 sub create_location {
-    my ($c, $prov_subscriber, $contact, $q, $expires, $flags, $cflags) = @_;
-
+    my ($c, $prov_subscriber, $params) = @_;
+    my($contact, $q, $expires, $flags, $cflags) = @$params{qw/contact q expires flags cflags/};
     my $aor = get_aor($c, $prov_subscriber);
-    my $path = $c->config->{sip}->{path} || '<sip:127.0.0.1:5060;lr>';
+    my $path = _compose_location_path($c, $prov_subscriber, $params);#compose path from path or socket from params
     if ($expires) {
         $expires = NGCP::Panel::Utils::DateTime::from_string($expires)->epoch;
         $expires //= 0;
@@ -106,6 +106,43 @@ sub get_aor{
     my ($c, $prov_subscriber) = @_;
     return $prov_subscriber->username . '@' . $prov_subscriber->domain->domain;
 }
+
+sub _compose_location_path {
+    my ($c, $prov_subscriber, $params) = @_;
+#path: <sip:lb@127.0.0.1;lr;received=sip:10.15.17.50:32997;socket=sip:10.15.17.198:5060>
+#The "path" uri points to the sip_int on the LB node to which this subscriber belongs.
+#The "received" parameter uri is equal to the contact provided in the registrations.
+#The "socket" points to the LB interface from which the incoming calls to this registration should be sent out.
+#See detailed description of the implemented logic in the workfron tasks 37459, 14589 
+    my $socket = $params->{socket};
+    my $path_default = $c->config->{sip}->{path} || '<sip:127.0.0.1:5060;lr>';
+    if (!$socket) {
+        #user selected default outbound option
+        $c->log->debug('_compose_location_path: socket is empty, return default path:'.$path_default);
+        return $path_default;
+    }
+    my $lb_clusters = $c->config->{cluster_sets};
+    my $subscriber_lb_ptr_preference_rs = NGCP::Panel::Utils::Preferences::get_nested_preference_rs(
+        $c,
+        'lbrtp_set',
+        $prov_subscriber,
+        {
+            type => 'usr',
+            'order' => [qw/usr prof dom/]
+        },
+    );
+    my ($subscriber_lb_ptr,$subscriber_lb);
+    if ($subscriber_lb_ptr_preference_rs && $subscriber_lb_ptr_preference_rs->first) {
+        $c->log->debug('_compose_location_path: lbrtp_set:'.$subscriber_lb_ptr_preference_rs->first->value);
+        $subscriber_lb_ptr = $subscriber_lb_ptr_preference_rs->first->value // $lb_clusters->{default};
+    }
+    #TODO: is it ok to use default 'sip:lb@127.0.0.1:5060;lr' here?
+    $subscriber_lb = $lb_clusters->{$subscriber_lb_ptr} // 'sip:lb@127.0.0.1:5060;lr';
+    my $path = '<'.$subscriber_lb.';received=sip:'.$params->{contact}.';socket='.$socket.'>';
+    $c->log->debug('_compose_location_path: path:'.$path);
+    return $path;
+}
+
 1;
 
 # vim: set tabstop=4 expandtab:
