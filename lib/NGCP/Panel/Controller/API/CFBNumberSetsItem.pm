f5687b22947de9a2263e02d4a34fef4db570c776
Gerhard Jungwirth <gjungwirth@sipwise.com>
2018-06-26 18:19:51 +0200

    TT#37205 bnumber cf in API: complex /api/cf*
    * /api/cfmappings
    * /api/cfbnumbersets
    
    small fix: "anonymous" doesn't make sense for b-numbers
    
    Change-Id: I13fe97bb35c7c34f8919932391dea86bd492a291

---
+++ lib/NGCP/Panel/Controller/API/CFBNumberSetsItem.pm
@@ -0,0 +1,42 @@
+package NGCP::Panel::Controller::API::CFBNumberSetsItem;
+use NGCP::Panel::Utils::Generic qw(:all);
+
+use Sipwise::Base;
+
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
+
+use NGCP::Panel::Utils::ValidateJSON qw();
+require Catalyst::ActionRole::ACL;
+require NGCP::Panel::Role::HTTPMethods;
+require Catalyst::ActionRole::RequireSSL;
+
+sub allowed_methods{
+    return [qw/GET OPTIONS HEAD PATCH PUT DELETE/];
+}
+
+use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::CFBNumberSets/;
+
+sub journal_query_params {
+    my($self,$query_params) = @_;
+    return $self->get_journal_query_params($query_params);
+}
+
+__PACKAGE__->set_config({
+    allowed_roles => {
+        Default => [qw/admin reseller subscriberadmin subscriber/],
+        Journal => [qw/admin reseller/],
+    },
+    PATCH => { ops => [qw/add replace remove copy/] },
+});
+
+sub get_journal_methods{
+    return [qw/handle_item_base_journal handle_journals_get handle_journalsitem_get handle_journals_options handle_journalsitem_options handle_journals_head handle_journalsitem_head/];
+}
+
+1;
+
+# vim: set tabstop=4 expandtab:
