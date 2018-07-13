ae916560b49389fc4e23887b5db70e609bff9155
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-07-13 12:58:41 +0200

    TT#28457 Remove inherited code from faxserversettings
    Change-Id: I697eb79ccd04ef6e5f3d0de5297be840cb23b731

--- lib/NGCP/Panel/Controller/API/FaxserverSettings.pm
+++ lib/NGCP/Panel/Controller/API/FaxserverSettings.pm
@@ -3,12 +3,11 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use boolean qw(true);
-use Data::HAL qw();
-use Data::HAL::Link qw();
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::FaxserverSettings/;
 
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin reseller subscriber subscriberadmin/],
+});
 
 sub allowed_methods{
     return [qw/GET OPTIONS HEAD/];
@@ -16,7 +15,7 @@ sub allowed_methods{
 
 sub api_description {
     return 'Specifies faxserver settings for a specific subscriber.';
-};
+}
 
 sub query_params {
     return [
@@ -54,67 +53,6 @@ sub query_params {
             },
         },
     ];
-}
-
-use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::FaxserverSettings/;
-
-sub resource_name{
-    return 'faxserversettings';
-}
-
-sub dispatch_path{
-    return '/api/faxserversettings/';
-}
-
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-faxserversettings';
-}
-
-__PACKAGE__->set_config({
-    allowed_roles => [qw/admin reseller subscriber subscriberadmin/],
-});
-
-sub GET :Allow {
-    my ($self, $c) = @_;
-    my $page = $c->request->params->{page} // 1;
-    my $rows = $c->request->params->{rows} // 10;
-    {
-        my $cfs = $self->item_rs($c);
-        (my $total_count, $cfs) = $self->paginate_order_collection($c, $cfs);
-        my (@embedded, @links);
-        for my $cf ($cfs->all) {
-            try {
-                push @embedded, $self->hal_from_item($c, $cf);
-                push @links, Data::HAL::Link->new(
-                    relation => 'ngcp:'.$self->resource_name,
-                    href     => sprintf('%s%s', $self->dispatch_path, $cf->id),
-                );
-            }
-        }
-        push @links,
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            $self->collection_nav_links($c, $page, $rows, $total_count, $c->request->path, $c->request->query_params);
-
-        my $hal = Data::HAL->new(
-            embedded => [@embedded],
-            links => [@links],
-        );
-        $hal->resource({
-            total_count => $total_count,
-        });
-        my $response = HTTP::Response->new(HTTP_OK, undef, 
-            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
 }
 
 1;
