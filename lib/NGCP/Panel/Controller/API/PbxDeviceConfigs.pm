a058911e182ed2b53010ee7cb171dcc12ae87bad
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-07-23 01:37:28 +0200

    clear pbxdeviceconfig
    Change-Id: I183d1dc952ab3d967ea1266dab6b3e0ba913f096

--- lib/NGCP/Panel/Controller/API/PbxDeviceConfigs.pm
+++ lib/NGCP/Panel/Controller/API/PbxDeviceConfigs.pm
@@ -9,6 +9,11 @@ use Data::HAL::Link qw();
 use HTTP::Headers qw();
 use HTTP::Status qw(:constants);
 
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::PbxDeviceConfigs/;
+
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin reseller/],
+});
 
 sub allowed_methods{
     return [qw/GET POST OPTIONS HEAD/];
@@ -23,97 +28,19 @@ sub query_params {
         {
             param => 'device_id',
             description => 'Filter for configs of a specific device model',
-            query => {
-                first => sub {
-                    my $q = shift;
-                    return { 'device_id' => $q };
-                },
-                second => sub { },
-            },
-		},
-		{
+            query_type => 'string_eq',
+        },
+        {
             param => 'version',
             description => 'Filter for configs by a specific version',
-            query => {
-                first => sub {
-                    my $q = shift;
-                    return { 'version' => $q };
-                },
-                second => sub { },
-            },
-		},
-		{
+            query_type => 'string_eq',
+        },
+        {
             param => 'content_type',
             description => 'Filter for configs by a specific content type',
-            query => {
-                first => sub {
-                    my $q = shift;
-                    return { 'content_type' => $q };
-                },
-                second => sub { },
-            },
+            query_type => 'string_eq',
         },
     ];
-}
-
-use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::PbxDeviceConfigs/;
-
-sub resource_name{
-    return 'pbxdeviceconfigs';
-}
-
-sub dispatch_path{
-    return '/api/pbxdeviceconfigs/';
-}
-
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-pbxdeviceconfigs';
-}
-
-__PACKAGE__->set_config({
-    allowed_roles => [qw/admin reseller/],
-});
-
-sub GET :Allow {
-    my ($self, $c) = @_;
-    my $page = $c->request->params->{page} // 1;
-    my $rows = $c->request->params->{rows} // 10;
-    {
-        my $items = $self->item_rs($c);
-        (my $total_count, $items) = $self->paginate_order_collection($c, $items);
-        my (@embedded, @links);
-        my $form = $self->get_form($c);
-        for my $item ($items->all) {
-            push @embedded, $self->hal_from_item($c, $item, $form);
-            push @links, Data::HAL::Link->new(
-                relation => 'ngcp:'.$self->resource_name,
-                href     => sprintf('/%s%d', $c->request->path, $item->id),
-            );
-        }
-        push @links,
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            $self->collection_nav_links($c, $page, $rows, $total_count, $c->request->path, $c->request->query_params);
-
-        my $hal = Data::HAL->new(
-            embedded => [@embedded],
-            links => [@links],
-        );
-        $hal->resource({
-            total_count => $total_count,
-        });
-        my $response = HTTP::Response->new(HTTP_OK, undef, 
-            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
 }
 
 sub POST :Allow {
