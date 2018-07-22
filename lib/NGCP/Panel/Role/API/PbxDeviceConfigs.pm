22ec6b8d58d876c9b1fd6e96ad694b5221839b05
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-07-23 01:37:28 +0200

    clear pbxdeviceconfig
    Change-Id: I183d1dc952ab3d967ea1266dab6b3e0ba913f096

--- lib/NGCP/Panel/Role/API/PbxDeviceConfigs.pm
+++ lib/NGCP/Panel/Role/API/PbxDeviceConfigs.pm
@@ -5,11 +5,14 @@ use Sipwise::Base;
 
 use parent 'NGCP::Panel::Role::API';
 
-
 use boolean qw(true);
 use Data::HAL qw();
 use Data::HAL::Link qw();
 use HTTP::Status qw(:constants);
+
+sub resource_name{
+    return 'pbxdeviceconfigs';
+}
 
 sub _item_rs {
     my ($self, $c) = @_;
@@ -31,43 +34,6 @@ sub get_form {
     return NGCP::Panel::Form::get("NGCP::Panel::Form::Device::ConfigAPI", $c);
 }
 
-sub hal_from_item {
-    my ($self, $c, $item, $form) = @_;
-
-    $form //= $self->get_form($c);
-    my $resource = $self->resource_from_item($c, $item, $form);
-
-    my $hal = Data::HAL->new(
-        links => [
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            Data::HAL::Link->new(relation => 'collection', href => sprintf("/api/%s/", $self->resource_name)),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            Data::HAL::Link->new(relation => 'self', href => sprintf("%s%d", $self->dispatch_path, $item->id)),
-            Data::HAL::Link->new(relation => 'ngcp:pbxdevicemodels', href => sprintf("/api/pbxdevicemodels/%d", $item->device_id)),
-            Data::HAL::Link->new(relation => 'ngcp:pbxdeviceconfigfiles', href => sprintf("/api/pbxdeviceconfigfiles/%d", $item->id)),
-        ],
-        relation => 'ngcp:'.$self->resource_name,
-    );
-
-
-    $self->validate_form(
-        c => $c,
-        resource => $resource,
-        form => $form,
-        run => 0,
-    );
-
-    $resource->{id} = int($item->id);
-    delete $resource->{data};
-    $hal->resource($resource);
-    return $hal;
-}
-
 sub resource_from_item {
     my ($self, $c, $item, $form) = @_;
 
@@ -75,12 +41,6 @@ sub resource_from_item {
     delete $resource->{data};
 
     return $resource;
-}
-
-sub item_by_id {
-    my ($self, $c, $id) = @_;
-    my $item_rs = $self->item_rs($c);
-    return $item_rs->find($id);
 }
 
 sub update_item {
