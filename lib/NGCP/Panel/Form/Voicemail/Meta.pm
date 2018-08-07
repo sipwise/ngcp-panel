f860a3d1860d24719e4181558a76433d3ceb556e
Gerhard Jungwirth <gjungwirth@sipwise.com>
2018-08-07 16:15:12 +0200

    TT#38811 API documentation show nested fields
    plus fix/improve some forms
    
    Change-Id: Ibdecee0e77acfa57d7cb234b9781f98011fc75ad

--- lib/NGCP/Panel/Form/Voicemail/Meta.pm
+++ lib/NGCP/Panel/Form/Voicemail/Meta.pm
@@ -3,13 +3,6 @@ package NGCP::Panel::Form::Voicemail::Meta;
 use HTML::FormHandler::Moose;
 extends 'HTML::FormHandler';
 
-use HTML::FormHandler::Widget::Block::Bootstrap;
-
-has '+widget_wrapper' => ( default => 'Bootstrap' );
-has_field 'submitid' => ( type => 'Hidden' );
-sub build_render_list {[qw/submitid fields actions/]}
-sub build_form_element_class { [qw/form-horizontal/] }
-
 has_field 'subscriber_id' => (
     type => 'PosInteger',
     label => 'Subscriber ID',
@@ -71,25 +64,6 @@ has_field 'folder' => (
         { label => 'Cust5', value => 'Cust5' },
         { label => 'Cust6', value => 'Cust6' },
     ]
-);
-
-has_field 'save' => (
-    type => 'Submit',
-    value => 'Save',
-    element_class => [qw/btn btn-primary/],
-    label => '',
-);
-
-has_block 'fields' => (
-    tag => 'div',
-    class => [qw/modal-body/],
-    render_list => [qw/folder/],
-);
-
-has_block 'actions' => (
-    tag => 'div',
-    class => [qw/modal-footer/],
-    render_list => [qw/save/],
 );
 
 1;
