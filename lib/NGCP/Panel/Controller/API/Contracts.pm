a0ec0ccf86aee74addfd14f1ae53fd5179412c47
Rene Krenn <rkrenn@sipwise.com>
2018-09-12 05:35:50 +0200

    TT#44095 set isolation level when creating contracts
    Change-Id: Ie7944be85ea60626050896988f49917249ccc3f5
    (cherry picked from commit 7b5c9d9fa617ecba814421d82efd1418153dbd95)

--- lib/NGCP/Panel/Controller/API/Contracts.pm
+++ lib/NGCP/Panel/Controller/API/Contracts.pm
@@ -131,6 +131,7 @@ sub GET :Allow {
 sub POST :Allow {
     my ($self, $c) = @_;
 
+    $c->model('DB')->set_transaction_isolation('READ COMMITTED');
     my $guard = $c->model('DB')->txn_scope_guard;
     {
         my $schema = $c->model('DB');
