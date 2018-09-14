16529f98317abd470e97c377c948b28e0d0e4b00
Rene Krenn <rkrenn@sipwise.com>
2018-09-14 17:21:39 +0200

    TT#44404 datatables refactoring for large tables
    while limiting the count queries is sufficient for basic
    queries, a plethora of perfromance cosniderations arise
    when running compound queries (queries with set operations)
    against large tables. this is the particular case with the
    call history UI.
    
    + multi-column search must not hit any un-indexed column.
      when limit is providied, it seems ok for the optimizer
      even when no appropriate multi-col index is present.
    + 'col LIKE "%xyz%"' must be turned to 'col LIKE "xyz%"'
      - otherwise and idnex is useless. if fulltextsearch is
      desired, this needs to be done separately, including all
      the pain of maintaining the keyword/substring index etc.
    
    + each partial query of a compound query must have a LIMIT
      clause. if not, the db tends to try building an intermediate
      table (which gets large -> slow). this is the only option when sorting,
      but it must be prevent if only displaying unsorted or filtered rows.
      BIx::Class::Helper::ResultSet::SetOperations generates malformend
      SQL when limiting the partial queries. Therefore, the generated
      SQL will therefore be parsed and patched.
    
    + considering the LIMIT for the partial queries, it is also
      mandatory to inject the filter/joins to each. this is done
      properly by ReseultSet::SetOperations; but needs to be done
      explicitly when building compound queries.
    
    + aggregation footers must be prevented when clipped - it wont
      show useful data (sum, count, ..) anyway.
    
    Change-Id: Ie256877d368747ad6bfe74f5b6ac5dae88be9e58

--- lib/NGCP/Panel/Field/E164.pm
+++ lib/NGCP/Panel/Field/E164.pm
@@ -7,10 +7,10 @@ extends 'HTML::FormHandler::Field::Compound';
 
 has_field 'cc' => (
     type => '+NGCP::Panel::Field::PosInteger',
-    element_attr => { 
-        class => ['ngcp_e164_cc'], 
-        rel => ['tooltip'], 
-        title => ['Country Code, e.g. 1 for US or 43 for Austria'] 
+    element_attr => {
+        class => ['ngcp_e164_cc'],
+        rel => ['tooltip'],
+        title => ['Country Code, e.g. 1 for US or 43 for Austria']
     },
     do_label => 0,
     do_wrapper => 0,
@@ -19,10 +19,10 @@ has_field 'cc' => (
 
 has_field 'ac' => (
     type => '+NGCP::Panel::Field::PosInteger',
-    element_attr => { 
-        class => ['ngcp_e164_ac'], 
-        rel => ['tooltip'], 
-        title => ['Area Code, e.g. 212 for NYC or 1 for Vienna'] 
+    element_attr => {
+        class => ['ngcp_e164_ac'],
+        rel => ['tooltip'],
+        title => ['Area Code, e.g. 212 for NYC or 1 for Vienna']
     },
     do_label => 0,
     do_wrapper => 0,
@@ -31,10 +31,10 @@ has_field 'ac' => (
 
 has_field 'sn' => (
     type => '+NGCP::Panel::Field::PosInteger',
-    element_attr => { 
-        class => ['ngcp_e164_sn'], 
-        rel => ['tooltip'], 
-        title => ['Subscriber Number, e.g. 12345678'] 
+    element_attr => {
+        class => ['ngcp_e164_sn'],
+        rel => ['tooltip'],
+        title => ['Subscriber Number, e.g. 12345678']
     },
     do_label => 0,
     do_wrapper => 0,
@@ -50,7 +50,7 @@ sub validate {
     my $sn = $self->field('sn')->value;
 
     my @sub_fields = (qw/cc ac sn/);
-    my %sub_errors = 
+    my %sub_errors =
         map { $_ => 1 }
             map { ($self->field($_) && $self->field($_)->result ) ? @{$self->field($_)->errors} : () } 
                 @sub_fields;
