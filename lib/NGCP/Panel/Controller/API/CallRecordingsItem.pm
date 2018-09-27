213649428278f5ff80f0e4b94619632c9593c644
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-09-27 10:39:06 +0200

    TT#40453 Remove excrescent code from /api/callrecordings/
    Change-Id: I4eb14b3f33abe63d8d26ad4513514d1b196661f6

--- lib/NGCP/Panel/Controller/API/CallRecordingsItem.pm
+++ lib/NGCP/Panel/Controller/API/CallRecordingsItem.pm
@@ -3,82 +3,21 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
-
-use NGCP::Panel::Utils::ValidateJSON qw();
-use NGCP::Panel::Utils::Subscriber;
-require Catalyst::ActionRole::ACL;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
-
-sub allowed_methods{
-    return [qw/GET OPTIONS HEAD DELETE/];
-}
-
 use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::CallRecordings/;
 
-sub resource_name{
-    return 'callrecordings';
-}
-
-sub dispatch_path{
-    return '/api/callrecordings/';
-}
-
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-callrecordings';
-}
 use NGCP::Panel::Utils::Subscriber;
 
 __PACKAGE__->set_config({
     allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
 });
 
-sub GET :Allow {
-    my ($self, $c, $id) = @_;
-    {
-        last unless $self->valid_id($c, $id);
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, callrecording => $item);
-
-        my $hal = $self->hal_from_item($c, $item);
-
-        my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-            (map { # XXX Data::HAL must be able to generate links with multiple relations
-                s|rel="(http://purl.org/sipwise/ngcp-api/#rel-\w+)"|rel="item $1"|r =~
-                s/rel=self/rel="item self"/r;
-            } $hal->http_headers),
-        ), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
+sub allowed_methods{
+    return [qw/GET OPTIONS HEAD DELETE/];
 }
 
-sub DELETE :Allow {
-    my ($self, $c, $id) = @_;
-
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, callrecording => $item);
-
-        try {
-            NGCP::Panel::Utils::Subscriber::delete_callrecording( c => $c, recording => $item );
-        } catch($e) {
-            $c->log->error("Failed to delete recording: $e");
-            $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to delete recording.");
-            last;
-        }
-
-
-        $guard->commit;
-
-        $c->response->status(HTTP_NO_CONTENT);
-        $c->response->body(q());
-    }
+sub delete_item {
+    my ($self, $c, $item) = @_;
+    NGCP::Panel::Utils::Subscriber::delete_callrecording( c => $c, recording => $item );
     return;
 }
 
