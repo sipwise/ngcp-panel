9b4060a45bfd797b3a0d38ab0e3f0ed87ce59ebd
Gerhard Jungwirth <gjungwirth@sipwise.com>
2018-09-27 12:56:29 +0200

    TT#29300 fix singular for Faxes in OpenAPI/Swagger
    Change-Id: I5cb1e3b4cb24ba785ce771e02210a20c58cec0f1

--- lib/NGCP/Panel/Controller/API/Root.pm
+++ lib/NGCP/Panel/Controller/API/Root.pm
@@ -212,7 +212,7 @@ sub GET : Allow {
 
         $c->stash->{collections}->{$rel} = {
             name => $mod,
-            entity_name => $mod =~ s/ies$/y/r =~ s/s$//r,
+            entity_name => $mod =~ s/^Faxes$/Fax/r =~ s/ies$/y/r =~ s/s$//r,
             description => $full_mod->api_description,
             fields => $form_fields,
             uploads => $form_fields_upload,
