2aaed20156bfb7ae3a375e2aac08c37d8021abfd
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-09-27 10:39:06 +0200

    TT#40453 Remove excrescent code from /api/callrecordings/
    Change-Id: I4eb14b3f33abe63d8d26ad4513514d1b196661f6

--- lib/NGCP/Panel/Controller/API/CallRecordings.pm
+++ lib/NGCP/Panel/Controller/API/CallRecordings.pm
@@ -3,13 +3,11 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use boolean qw(true);
-use Data::HAL qw();
-use Data::HAL::Link qw();
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::CallRecordings/;
 
-use DateTime::TimeZone;
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
+});
 
 sub allowed_methods{
     return [qw/GET OPTIONS HEAD/];
@@ -37,13 +35,7 @@ sub query_params {
         {
             param => 'status',
             description => 'Filter for callrecordings with a specific status',
-            query => {
-                first => sub {
-                    my $q = shift;
-                    { 'status' => $q };
-                },
-                second => sub {},
-            },
+            query_type => 'string_eq',
         },
         {
             # we handle that separately/manually in the role
@@ -56,72 +48,6 @@ sub query_params {
             description => 'Format start_time according to the optional time zone provided here, e.g. Europe/Berlin.',
         },
     ];
-}
-
-use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::CallRecordings/;
-
-sub resource_name{
-    return 'callrecordings';
-}
-
-sub dispatch_path{
-    return '/api/callrecordings/';
-}
-
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-callrecordings';
-}
-
-__PACKAGE__->set_config({
-    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
-});
-
-sub GET :Allow {
-    my ($self, $c) = @_;
-    my $page = $c->request->params->{page} // 1;
-    my $rows = $c->request->params->{rows} // 10;
-    {
-        if($c->req->param('tz') && !DateTime::TimeZone->is_valid_name($c->req->param('tz'))) {
-            $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Query parameter 'tz' value is not a valid time zone");
-            return;
-        }
-        my $callrecordings = $self->item_rs($c);
-        (my $total_count, $callrecordings) = $self->paginate_order_collection($c, $callrecordings);
-        my (@embedded, @links);
-        my $form = $self->get_form($c);
-        for my $domain ($callrecordings->all) {
-            push @embedded, $self->hal_from_item($c, $domain, $form);
-            push @links, Data::HAL::Link->new(
-                relation => 'ngcp:'.$self->resource_name,
-                href     => sprintf('/%s%d', $c->request->path, $domain->id),
-            );
-        }
-        push @links,
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            $self->collection_nav_links($c, $page, $rows, $total_count, $c->request->path, $c->request->query_params);
-
-        my $hal = Data::HAL->new(
-            embedded => [@embedded],
-            links => [@links],
-        );
-        $hal->resource({
-            total_count => $total_count,
-        });
-        my $rname = $self->resource_name;
-
-        my $response = HTTP::Response->new(HTTP_OK, undef,
-            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
 }
 
 1;
