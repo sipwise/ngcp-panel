ba8bf9ea39fb337b4972f55c45284215206c058b
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-09-28 11:55:35 +0200

    remove code from recordingfiles
    Change-Id: I2ad3e756f012299af5236c57b02a09ff7b93da29

--- lib/NGCP/Panel/Controller/API/CallRecordingFiles.pm
+++ lib/NGCP/Panel/Controller/API/CallRecordingFiles.pm
@@ -3,38 +3,24 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use boolean qw(true);
-use Data::HAL qw();
-use Data::HAL::Link qw();
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::CallRecordingStreams/;
 
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
+});
 
 sub allowed_methods{
     return [qw/OPTIONS/];
 }
 
-sub api_description {
-    return 'Defines the actual recording of a recorded call stream. It is referred to by the <a href="#callrecordingstreams">CallRecordingStreams</a> relation. A GET on an item returns the binary blob of the recording with the content type depending on the output format given in the related stream.';
-};
-
-use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::CallRecordingStreams/;
-
+#we don't have role package, so method will be here
 sub resource_name{
     return 'callrecordingfiles';
 }
 
-sub dispatch_path{
-    return '/api/callrecordingfiles/';
-}
-
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-callrecordingfiles';
-}
-
-__PACKAGE__->set_config({
-    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
-});
+sub api_description {
+    return 'Defines the actual recording of a recorded call stream. It is referred to by the <a href="#callrecordingstreams">CallRecordingStreams</a> relation. A GET on an item returns the binary blob of the recording with the content type depending on the output format given in the related stream.';
+};
 
 1;
 
