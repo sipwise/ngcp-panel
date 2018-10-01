32615cddacbfb5538643353c5ed586d424d84b86
Rene Krenn <rkrenn@sipwise.com>
2018-10-01 20:36:26 +0200

    TT#44963 fix getting contract in /api/capabilities
    Change-Id: I9aa8d5fe40bd4092d88ba65a93f08f7656292c83

--- lib/NGCP/Panel/Role/API/Capabilities.pm
+++ lib/NGCP/Panel/Role/API/Capabilities.pm
@@ -70,20 +70,7 @@ sub _item_rs {
 
         $rtcengine &= ($c->user->voip_subscriber->contract->contact->reseller->rtc_user // 0);
 
-        my $customer_rs = NGCP::Panel::Utils::Contract::get_customer_rs(
-            c => $c,
-            contract_id => $c->user->account_id,
-        );
-        $customer_rs = $customer_rs->search({
-            '-or' => [
-                'product.class' => 'sipaccount',
-                'product.class' => 'pbxaccount',
-            ],
-        },{
-            '+select' => [ 'product.class' ],
-            '+as' => [ 'product_class' ],
-        });
-		my $customer = $customer_rs->first;
+		my $customer = $c->user->contract;
 
         my $cpbx = ($customer->product->class eq 'pbxaccount') ? 1 : 0;
         $cloudpbx &= $cpbx;
