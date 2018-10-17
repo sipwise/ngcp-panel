d9ec91a1b78435ffe717fa2f80927ea3fa98a4bd
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-10-17 07:48:32 +0200

    clear voicemailrecordings api alittle
    Change-Id: I6bb6258011fb66352fc3d9daf2ac9ff112d331af

--- lib/NGCP/Panel/Controller/API/VoicemailRecordings.pm
+++ lib/NGCP/Panel/Controller/API/VoicemailRecordings.pm
@@ -3,38 +3,23 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use boolean qw(true);
-use Data::HAL qw();
-use Data::HAL::Link qw();
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
+use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::VoicemailRecordings/;
 
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
+});
 
 sub allowed_methods{
     return [qw/OPTIONS/];
 }
 
-sub api_description {
-    return 'Defines the actual recording of voicemail messages. It is referred to by the <a href="#voicemails">Voicemails</a> relation. A GET on an item returns the binary blob of the recording with Content-Type "audio/x-wav".';
-};
-
-use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::VoicemailRecordings/;
-
 sub resource_name{
     return 'voicemailrecordings';
 }
 
-sub dispatch_path{
-    return '/api/voicemailrecordings/';
-}
-
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-voicemailrecordings';
-}
-
-__PACKAGE__->set_config({
-    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
-});
+sub api_description {
+    return 'Defines the actual recording of voicemail messages. It is referred to by the <a href="#voicemails">Voicemails</a> relation. A GET on an item returns the binary blob of the recording with Content-Type "audio/x-wav".';
+};
 
 sub query_params {
     return [
--- lib/NGCP/Panel/Controller/API/VoicemailRecordingsItem.pm
+++ lib/NGCP/Panel/Controller/API/VoicemailRecordingsItem.pm
@@ -4,64 +4,44 @@ use NGCP::Panel::Utils::Generic qw(:all);
 use Sipwise::Base;
 
 use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
 
-use NGCP::Panel::Utils::ValidateJSON qw();
 use NGCP::Panel::Utils::Subscriber;
 use NGCP::Panel::Utils::Sounds;
-require Catalyst::ActionRole::ACL;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
+
+use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::VoicemailRecordings/;
+
+__PACKAGE__->set_config({
+    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
+    log_response => 0,
+    GET => {
+        #'application/json' is default, if no accept header was recieved.
+        'ReturnContentType' => ['application/json', 'audio/x-wav', 'audio/mpeg', 'audio/ogg'],#,
+    },
+});
 
 sub allowed_methods{
     return [qw/GET OPTIONS HEAD/];
 }
 
-use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::VoicemailRecordings/;
-
 sub resource_name{
     return 'voicemailrecordings';
 }
 
-sub dispatch_path{
-    return '/api/voicemailrecordings/';
-}
-
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-voicemailrecordings';
-}
-
-__PACKAGE__->set_config({
-    allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
-});
-
-sub GET :Allow {
-    my ($self, $c, $id) = @_;
-    {
-        last unless $self->valid_id($c, $id);
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, voicemailrecording => $item);
-        my $format = $c->request->params->{format} // 'wav';
-        unless ($format && $format =~ /^(wav|mp3|ogg)$/) {
-            $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Unknown format '$format', supported: wav,mp3,ogg.");
-            last;
-        }
-        my $sr = \%NGCP::Panel::Utils::Subscriber::;
-        my $ss = \%NGCP::Panel::Utils::Sounds::;
-        my $filename = $sr->{get_voicemail_filename}($c,$item,$format);
-        $c->response->header ('Content-Disposition' => 'attachment; filename="'.$filename.'"');
-        $c->response->content_type($sr->{get_voicemail_content_type}($c,$format));
-        $c->response->body(${$ss->{transcode_data}(\$item->recording, 'WAV', uc($format))});
-        return;
+sub get_item_binary_data{
+    my($self, $c, $id, $item, $return_type) = @_;
+    #caller waits for: $data_ref,$mime_type,$filename
+    #while we will not strictly check Accepted header, if item can return only one type of the binary data
+    my $format = mime_type_to_extension($return_type);
+    my $data_ref;
+    my $sr = \%NGCP::Panel::Utils::Subscriber::;
+    my $ss = \%NGCP::Panel::Utils::Sounds::;
+    if ($format ne 'wav') {
+        $data_ref = $ss->{transcode_data}(\$item->recording, 'WAV', uc($format));
+    } else {
+        $data_ref = \$item->recording;
     }
-    return;
-}
-
-sub end : Private {
-    my ($self, $c) = @_;
-
-    #$self->log_response($c);
-    return;
+    my $filename = $sr->{get_voicemail_filename}($c, $item, $format);
+    return $data_ref, $return_type, $filename;
 }
 
 1;
