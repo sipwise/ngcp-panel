0830b3fdd1671b92659da23a892df4ba32f71397
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-11-14 10:15:46 +0100

    TT#45484 Add "force_delete" option to callrecordings
    Change-Id: I49c736a833583028f0a3f5382c93e4a08c81ed00
    (cherry picked from commit d003c66e7e1a0c50cd482a2745739b83c79cdb68)

--- lib/NGCP/Panel/Controller/API/CallRecordingsItem.pm
+++ lib/NGCP/Panel/Controller/API/CallRecordingsItem.pm
@@ -6,6 +6,7 @@ use Sipwise::Base;
 use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::CallRecordings/;
 
 use NGCP::Panel::Utils::Subscriber;
+use HTTP::Status qw(:constants);
 
 __PACKAGE__->set_config({
     allowed_roles => [qw/admin reseller subscriberadmin subscriber/],
@@ -17,7 +18,18 @@ sub allowed_methods{
 
 sub delete_item {
     my ($self, $c, $item) = @_;
-    NGCP::Panel::Utils::Subscriber::delete_callrecording( c => $c, recording => $item );
+    try {
+        NGCP::Panel::Utils::Subscriber::delete_callrecording( 
+            c => $c, 
+            recording => $item,
+            #TODO: Now we don't use any way to document such parameters, need to be created
+            force_delete => $c->request->params->{force_delete},
+        );
+    } catch($e) {
+        $c->log->error("failed to delete callrecording: $e");
+        $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to delete callrecording.");
+        return;
+    }
     return 1;
 }
 
---
+++ lib/NGCP/Panel/Form/Subscriber/CallRecordingDelete.pm
@@ -0,0 +1,45 @@
+package NGCP::Panel::Form::Subscriber::CallRecordingDelete;
+use Sipwise::Base;
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+
+use HTML::FormHandler::Widget::Block::Bootstrap;
+
+has '+widget_wrapper' => ( default => 'Bootstrap' );
+has_field 'submitid' => ( type => 'Hidden' );
+sub build_render_list {[qw/submitid fields actions/]}
+sub build_form_element_class { [qw/form-horizontal/] }
+
+
+has_field 'force_delete' => (
+    type => 'Boolean',
+    label => 'Force delete',
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Call recording infromation will be removed from database even in case of recording files absence or other impossibility to remove recording files.']
+    },
+
+);
+
+has_field 'save' => (
+    type => 'Submit',
+    value => 'Delete',
+    element_class => [qw/btn btn-primary/],
+    do_label => 0,
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/force_delete/],
+);
+
+has_block 'actions' => (
+    tag => 'div',
+    class => [qw/modal-footer/],
+    render_list => [qw/save/],
+);
+
+1;
+
+# vim: set tabstop=4 expandtab:
