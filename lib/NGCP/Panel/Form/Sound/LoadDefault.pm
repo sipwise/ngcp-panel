22d4d795369866962a8cff696baa4bbe23f92477
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-11-19 06:00:48 +0100

    TT#47569 Add possibility to load default files in sounsets api
    Change-Id: I1bf9a90ca3655772d9c671f9de725c974ebdd0bd

--- lib/NGCP/Panel/Form/Sound/LoadDefault.pm
+++ lib/NGCP/Panel/Form/Sound/LoadDefault.pm
@@ -1,7 +1,7 @@
 package NGCP::Panel::Form::Sound::LoadDefault;
 
 use HTML::FormHandler::Moose;
-extends 'HTML::FormHandler';
+extends 'NGCP::Panel::Form::Sound::SoundSetBase';
 use File::Find::Rule;
 
 use HTML::FormHandler::Widget::Block::Bootstrap;
@@ -11,51 +11,6 @@ has_field 'submitid' => ( type => 'Hidden' );
 sub build_render_list {[qw/submitid fields actions/]}
 sub build_form_element_class {[qw(form-horizontal)]}
 
-has_field 'language' => (
-    type => 'Select',
-    label => 'Language',
-    options_method => \&build_langs,
-    default => 'en',
-    required => 1,
-    element_attr => {
-        rel => ['tooltip'],
-        title => ['The language of the default sound files.']
-    },
-);
-
-sub build_langs {
-    my ($self) = @_;
-
-    my @options = ();
-    my @dirs = File::Find::Rule
-        ->directory
-        ->maxdepth(1)
-        ->not(File::Find::Rule->new->name(qr/^\.\.?$/))
-        ->in('/var/lib/ngcp-soundsets/system');
-
-    @options = map { my $val = s/^.+\///r; { label => $val, value => $val } } @dirs;
-        
-    return \@options;
-}
-
-
-has_field 'loopplay' => (
-    type => 'Boolean',
-    label => 'Play in Loop',
-    element_attr => {
-        rel => ['tooltip'],
-        title => ['Whether to play files in a loop where applicable.']
-    },
-);
-
-has_field 'override' => (
-    type => 'Boolean',
-    label => 'Replace existing',
-    element_attr => {
-        rel => ['tooltip'],
-        title => ['Replace existing sound files in set by default files.']
-    },
-);
 
 has_field 'save' => (
     type => 'Submit',
