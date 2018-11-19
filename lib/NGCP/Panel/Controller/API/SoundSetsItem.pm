fc73a49f0800005ef29d1b5f4c4d372e0673f9cb
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-11-19 06:01:36 +0100

    ss: remove code
    Change-Id: Ied28b5c93f2f6c528af4dba070054c828404e84e

--- lib/NGCP/Panel/Controller/API/SoundSetsItem.pm
+++ lib/NGCP/Panel/Controller/API/SoundSetsItem.pm
@@ -3,36 +3,7 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use HTTP::Headers qw();
-use HTTP::Status qw(:constants);
-
-use NGCP::Panel::Utils::ValidateJSON qw();
-require Catalyst::ActionRole::ACL;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
-
-sub allowed_methods{
-    return [qw/GET OPTIONS HEAD PATCH PUT DELETE/];
-}
-
 use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::SoundSets/;
-
-sub resource_name{
-    return 'soundsets';
-}
-
-sub dispatch_path{
-    return '/api/soundsets/';
-}
-
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-soundsets';
-}
-
-sub journal_query_params {
-    my($self,$query_params) = @_;
-    return $self->get_journal_query_params($query_params);
-}
 
 __PACKAGE__->set_config({
     allowed_roles => {
@@ -41,90 +12,13 @@ __PACKAGE__->set_config({
     }
 });
 
-sub GET :Allow {
-    my ($self, $c, $id) = @_;
-    {
-        last unless $self->valid_id($c, $id);
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, soundset => $item);
-
-        my $hal = $self->hal_from_item($c, $item);
-
-        my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-            (map { # XXX Data::HAL must be able to generate links with multiple relations
-                s|rel="(http://purl.org/sipwise/ngcp-api/#rel-resellers)"|rel="item $1"|r =~
-                s/rel=self/rel="item self"/r;
-            } $hal->http_headers),
-        ), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
+sub allowed_methods{
+    return [qw/GET OPTIONS HEAD PATCH PUT DELETE/];
 }
 
-sub PATCH :Allow {
-    my ($self, $c, $id) = @_;
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $preference = $self->require_preference($c);
-        last unless $preference;
-
-        my $json = $self->get_valid_patch_data(
-            c => $c, 
-            id => $id,
-            media_type => 'application/json-patch+json',
-        );
-        last unless $json;
-
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, soundset => $item);
-        my $form = $self->get_form($c);
-        my $old_resource = $self->resource_from_item($c, $item, $form);
-        my $resource = $self->apply_patch($c, $old_resource, $json);
-        last unless $resource;
-
-        $item = $self->update_item($c, $item, $old_resource, $resource, $form);
-        last unless $item;
-        
-        my $hal = $self->hal_from_item($c, $item, $form);
-        last unless $self->add_update_journal_item_hal($c,$hal);
-        
-        $guard->commit;
-
-        $self->return_representation($c, 'hal' => $hal, 'preference' => $preference );
-    }
-    return;
-}
-
-sub PUT :Allow {
-    my ($self, $c, $id) = @_;
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $preference = $self->require_preference($c);
-        last unless $preference;
-
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, soundset => $item);
-        my $resource = $self->get_valid_put_data(
-            c => $c,
-            id => $id,
-            media_type => 'application/json',
-        );
-        last unless $resource;
-        my $form = $self->get_form($c);
-        my $old_resource = $self->resource_from_item($c, $item, $form);
-
-        $item = $self->update_item($c, $item, $old_resource, $resource, $form);
-        last unless $item;
-        
-        my $hal = $self->hal_from_item($c, $item, $form);
-        last unless $self->add_update_journal_item_hal($c,$hal);
-
-        $guard->commit;
-        $self->return_representation($c, 'hal' => $hal, 'preference' => $preference );
-    }
-    return;
+sub journal_query_params {
+    my($self,$query_params) = @_;
+    return $self->get_journal_query_params($query_params);
 }
 
 sub DELETE :Allow {
