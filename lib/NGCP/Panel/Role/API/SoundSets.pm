17a23232beff5de6ab39ee41682ceaef29b8c4ae
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-11-21 23:04:56 +0100

    TT#47906 Remove unnecessary code from /api/soundsets/
    And fix discrepancy between doc and accepted parameter
    
    Change-Id: Ied28b5c93f2f6c528af4dba070054c828404e84e

--- lib/NGCP/Panel/Role/API/SoundSets.pm
+++ lib/NGCP/Panel/Role/API/SoundSets.pm
@@ -5,11 +5,14 @@ use Sipwise::Base;
 
 use parent 'NGCP::Panel::Role::API';
 
-
 use boolean qw(true);
 use Data::HAL qw();
 use Data::HAL::Link qw();
 use HTTP::Status qw(:constants);
+
+sub resource_name{
+    return 'soundsets';
+}
 
 sub _item_rs {
     my ($self, $c) = @_;
@@ -41,72 +44,26 @@ sub get_form {
     }
 }
 
-sub hal_from_item {
-    my ($self, $c, $item, $form) = @_;
-
-    $form //= $self->get_form($c);
-    my $resource = $self->resource_from_item($c, $item, $form);
-
-    my $hal = Data::HAL->new(
-        links => [
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            # nth: these should also be adapted/adaptable when using subscriber(admin) roles
-            Data::HAL::Link->new(relation => 'collection', href => sprintf("/api/%s/", $self->resource_name)),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            Data::HAL::Link->new(relation => 'self', href => sprintf("%s%d", $self->dispatch_path, $item->id)),
-            Data::HAL::Link->new(relation => 'ngcp:resellers', href => sprintf("/api/resellers/%d", $item->reseller_id)),
-            $item->contract_id ? Data::HAL::Link->new(relation => 'ngcp:customers', href => sprintf("/api/customers/%d", $item->contract_id)) : (),
-            Data::HAL::Link->new(relation => 'ngcp:soundfiles', href => sprintf("/api/soundfiles/?set_id=%d", $item->id)),
-            $self->get_journal_relation_link($c, $item->id),
-        ],
-        relation => 'ngcp:'.$self->resource_name,
-    );
-
-
-    $self->validate_form(
-        c => $c,
-        resource => $resource,
-        form => $form,
-        run => 0,
-    );
-    if (exists $resource->{contract_id}) {
-        $resource->{customer_id} = delete $resource->{contract_id};
-    }
-
-    $resource->{id} = int($item->id);
-    $hal->resource($resource);
-    return $hal;
+sub hal_links {
+    my($self, $c, $item, $resource, $form) = @_;
+    my $adm = $c->user->roles eq "admin";
+    return [
+        Data::HAL::Link->new(relation => 'ngcp:resellers', href => sprintf("/api/resellers/%d", $item->reseller_id)),
+        $item->contract_id ? Data::HAL::Link->new(relation => 'ngcp:customers', href => sprintf("/api/customers/%d", $item->contract_id)) : (),
+        Data::HAL::Link->new(relation => 'ngcp:soundfiles', href => sprintf("/api/soundfiles/?set_id=%d", $item->id)),
+    ];
 }
 
-sub resource_from_item {
-    my ($self, $c, $item, $form) = @_;
-
-    my $resource = { $item->get_inflated_columns };
-
-    return $resource;
+sub pre_process_form_resource{
+    my($self,$c, $item, $old_resource, $resource, $form, $process_extras) = @_;
+    #For backward compatibility. We always showed contract_id in generated API docs, as we used datatable field contract
+    #but considered only customer_id value.
+    #So now we allow both, and one documented (contract_id) has higher priority
+    $resource->{contract_id} //= delete $resource->{customer_id};
 }
 
-sub item_by_id {
-    my ($self, $c, $id) = @_;
-    my $item_rs = $self->item_rs($c);
-    return $item_rs->find($id);
-}
-
-sub update_item {
-    my ($self, $c, $item, $old_resource, $resource, $form) = @_;
-
-    $resource->{contract_id} = delete $resource->{customer_id};
-    $form //= $self->get_form($c);
-    return unless $self->validate_form(
-        c => $c,
-        form => $form,
-        resource => $resource,
-    );
+sub process_form_resource{
+    my($self,$c, $item, $old_resource, $resource, $form, $process_extras) = @_;
 
     if ($c->user->roles eq "admin") {
     } elsif ($c->user->roles eq "reseller") {
@@ -115,6 +72,14 @@ sub update_item {
         $resource->{contract_id} = $c->user->account_id;
         $resource->{reseller_id} = $c->user->contract->contact->reseller_id;
     }
+    $resource->{contract_default} //= 0;
+    return $resource;
+}
+
+sub check_resource{
+    my($self, $c, $item, $old_resource, $resource, $form, $process_extras) = @_;
+    my $schema = $c->model('DB');
+
     my $reseller = $c->model('DB')->resultset('resellers')->find({
         id => $resource->{reseller_id},
     });
@@ -142,47 +107,7 @@ sub update_item {
             return;
         }
     }
-
-    $resource->{contract_default} //= 0;
-
-    my $copy_from_default_params =  { map {$_ => delete $resource->{$_}} (qw/copy_from_default loopplay replace_existing language/) };
-    $item->update($resource);
-    if ($copy_from_default_params->{copy_from_default}) {
-        my $error;
-        my $handles_rs = NGCP::Panel::Utils::Sounds::get_handles_rs(c => $c, set_rs => $item);
-        NGCP::Panel::Utils::Sounds::apply_default_soundset_files(
-            c          => $c,
-            lang       => $copy_from_default_params->{language},
-            set_id     => $item->id,
-            handles_rs => $handles_rs,
-            loopplay   => $copy_from_default_params->{loopplay},
-            override   => $copy_from_default_params->{replace_existing},
-            error_ref  => \$error,
-        );
-    }
-
-    if($item->contract_id && $item->contract_default && !$old_resource->{contract_default}) {
-        $c->model('DB')->resultset('voip_sound_sets')->search({
-            reseller_id => $item->reseller_id,
-            contract_id => $item->contract_id,
-            contract_default => 1,
-            id => { '!=' => $item->id },
-        })->update({ contract_default => 0 });
-
-        foreach my $bill_subscriber($item->contract->voip_subscribers->all) {
-            my $prov_subscriber = $bill_subscriber->provisioning_voip_subscriber;
-            if($prov_subscriber) {
-                my $pref_rs = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
-                    c => $c, prov_subscriber => $prov_subscriber, attribute => 'contract_sound_set',
-                );
-                unless($pref_rs->first) {
-                    $pref_rs->create({ value => $item->id });
-                }
-            }
-        }
-    }
-
-    return $item;
+    return 1;
 }
 
 1;
