3de43f6aab5599a707060b7375bc58c5c4461331
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-11-23 18:41:40 +0100

    Revert "Revert "TT#47906 Remove unnecessary code from /api/soundsets/""
    This reverts commit 04500cbc40541bb7f1c23d9d9e2d87a1c988c350.
    
    Change-Id: Id13dc9c487420371bf5e69a519fd6a135d9f0d92

--- lib/NGCP/Panel/Controller/API/SoundSetsItem.pm
+++ lib/NGCP/Panel/Controller/API/SoundSetsItem.pm
@@ -2,37 +2,9 @@ package NGCP::Panel::Controller::API::SoundSetsItem;
 use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
-
-use HTTP::Headers qw();
 use HTTP::Status qw(:constants);
 
-use NGCP::Panel::Utils::ValidateJSON qw();
-require Catalyst::ActionRole::ACL;
-require NGCP::Panel::Role::HTTPMethods;
-require Catalyst::ActionRole::RequireSSL;
-
-sub allowed_methods{
-    return [qw/GET OPTIONS HEAD PATCH PUT DELETE/];
-}
-
 use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::SoundSets/;
-
-sub resource_name{
-    return 'soundsets';
-}
-
-sub dispatch_path{
-    return '/api/soundsets/';
-}
-
-sub relation{
-    return 'http://purl.org/sipwise/ngcp-api/#rel-soundsets';
-}
-
-sub journal_query_params {
-    my($self,$query_params) = @_;
-    return $self->get_journal_query_params($query_params);
-}
 
 __PACKAGE__->set_config({
     allowed_roles => {
@@ -41,134 +13,84 @@ __PACKAGE__->set_config({
     }
 });
 
-sub GET :Allow {
-    my ($self, $c, $id) = @_;
-    {
-        last unless $self->valid_id($c, $id);
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, soundset => $item);
-
-        my $hal = $self->hal_from_item($c, $item);
-
-        my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
-            (map { # XXX Data::HAL must be able to generate links with multiple relations
-                s|rel="(http://purl.org/sipwise/ngcp-api/#rel-resellers)"|rel="item $1"|r =~
-                s/rel=self/rel="item self"/r;
-            } $hal->http_headers),
-        ), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
+sub allowed_methods{
+    return [qw/GET OPTIONS HEAD PATCH PUT DELETE/];
 }
 
-sub PATCH :Allow {
-    my ($self, $c, $id) = @_;
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $preference = $self->require_preference($c);
-        last unless $preference;
-
-        my $json = $self->get_valid_patch_data(
-            c => $c, 
-            id => $id,
-            media_type => 'application/json-patch+json',
-        );
-        last unless $json;
-
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, soundset => $item);
-        my $form = $self->get_form($c);
-        my $old_resource = $self->resource_from_item($c, $item, $form);
-        my $resource = $self->apply_patch($c, $old_resource, $json);
-        last unless $resource;
-
-        $item = $self->update_item($c, $item, $old_resource, $resource, $form);
-        last unless $item;
-        
-        my $hal = $self->hal_from_item($c, $item, $form);
-        last unless $self->add_update_journal_item_hal($c,$hal);
-        
-        $guard->commit;
-
-        $self->return_representation($c, 'hal' => $hal, 'preference' => $preference );
-    }
-    return;
-}
-
-sub PUT :Allow {
-    my ($self, $c, $id) = @_;
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $preference = $self->require_preference($c);
-        last unless $preference;
-
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, soundset => $item);
-        my $resource = $self->get_valid_put_data(
-            c => $c,
-            id => $id,
-            media_type => 'application/json',
-        );
-        last unless $resource;
-        my $form = $self->get_form($c);
-        my $old_resource = $self->resource_from_item($c, $item, $form);
-
-        $item = $self->update_item($c, $item, $old_resource, $resource, $form);
-        last unless $item;
-        
-        my $hal = $self->hal_from_item($c, $item, $form);
-        last unless $self->add_update_journal_item_hal($c,$hal);
-
-        $guard->commit;
-        $self->return_representation($c, 'hal' => $hal, 'preference' => $preference );
-    }
-    return;
-}
-
-sub DELETE :Allow {
-    my ($self, $c, $id) = @_;
-
-    my $guard = $c->model('DB')->txn_scope_guard;
-    {
-        my $item = $self->item_by_id($c, $id);
-        last unless $self->resource_exists($c, soundset => $item);
-
-        last unless $self->add_delete_journal_item_hal($c,sub {
-            my $self = shift;
-            my ($c) = @_;
-            my $_form = $self->get_form($c);
-            return $self->hal_from_item($c, $item, $_form); });        
-        
-        if($item->contract_id) {
-            my $pref_rs = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
-                c => $c, attribute => 'contract_sound_set',
-            );
-            $pref_rs->search({ value => $item->id })->delete;
-        }
-
-        foreach my $p(qw/usr dom peer/) {
-            $c->model('DB')->resultset("voip_".$p."_preferences")->search({
-                'attribute.attribute' => 'sound_set',
-                value => $item->id,
-            },{
-                join => 'attribute',
-            })->delete_all; # explicit delete_all, otherwise query fails
-        }
-        
-        $item->delete;
-
-        $guard->commit;
-
-        $c->response->status(HTTP_NO_CONTENT);
-        $c->response->body(q());
-    }
-    return;
+sub journal_query_params {
+    my($self,$query_params) = @_;
+    return $self->get_journal_query_params($query_params);
 }
 
 sub get_journal_methods{
     return [qw/handle_item_base_journal handle_journals_get handle_journalsitem_get handle_journals_options handle_journalsitem_options handle_journals_head handle_journalsitem_head/];
+}
+
+sub update_item_model {
+    my ($self, $c, $item, $old_resource, $resource, $form) = @_;
+
+    my $copy_from_default_params =  { map {$_ => delete $resource->{$_}} (qw/copy_from_default loopplay replace_existing language/) };
+    $item->update($resource);
+    if ($copy_from_default_params->{copy_from_default}) {
+        my $error;
+        my $handles_rs = NGCP::Panel::Utils::Sounds::get_handles_rs(c => $c, set_rs => $item);
+        NGCP::Panel::Utils::Sounds::apply_default_soundset_files(
+            c          => $c,
+            lang       => $copy_from_default_params->{language},
+            set_id     => $item->id,
+            handles_rs => $handles_rs,
+            loopplay   => $copy_from_default_params->{loopplay},
+            override   => $copy_from_default_params->{replace_existing},
+            error_ref  => \$error,
+        );
+    }
+
+    if($item->contract_id && $item->contract_default && !$old_resource->{contract_default}) {
+        $c->model('DB')->resultset('voip_sound_sets')->search({
+            reseller_id => $item->reseller_id,
+            contract_id => $item->contract_id,
+            contract_default => 1,
+            id => { '!=' => $item->id },
+        })->update({ contract_default => 0 });
+
+        foreach my $bill_subscriber($item->contract->voip_subscribers->all) {
+            my $prov_subscriber = $bill_subscriber->provisioning_voip_subscriber;
+            if($prov_subscriber) {
+                my $pref_rs = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
+                    c => $c, prov_subscriber => $prov_subscriber, attribute => 'contract_sound_set',
+                );
+                unless($pref_rs->first) {
+                    $pref_rs->create({ value => $item->id });
+                }
+            }
+        }
+    }
+
+    return $item;
+}
+
+sub delete_item {
+    my ($self, $c, $item) = @_;
+
+    if($item->contract_id) {
+        my $pref_rs = NGCP::Panel::Utils::Preferences::get_usr_preference_rs(
+            c => $c, attribute => 'contract_sound_set',
+        );
+        $pref_rs->search({ value => $item->id })->delete;
+    }
+
+    foreach my $p(qw/usr dom peer/) {
+        $c->model('DB')->resultset("voip_".$p."_preferences")->search({
+            'attribute.attribute' => 'sound_set',
+            value => $item->id,
+        },{
+            join => 'attribute',
+        })->delete_all; # explicit delete_all, otherwise query fails
+    }
+    
+    $item->delete;
+
+    return 1;
 }
 
 1;
