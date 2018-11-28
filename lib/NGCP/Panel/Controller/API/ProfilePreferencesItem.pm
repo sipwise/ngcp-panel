3ebff19c67684777fda39c736222d58c6ce83849
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-11-28 10:30:39 +0100

    Revert "TT#36055 Remove common code from preferences"
    This reverts commit 9b10bc58ae59616ba10560c55e1d5f6fb8029b3e.
    
    Change-Id: I4504f1432870e1928f83d52f5ed864b7167534fc

--- lib/NGCP/Panel/Controller/API/ProfilePreferencesItem.pm
+++ lib/NGCP/Panel/Controller/API/ProfilePreferencesItem.pm
@@ -3,23 +3,22 @@ use NGCP::Panel::Utils::Generic qw(:all);
 
 use Sipwise::Base;
 
-use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::Preferences/;
+use boolean qw(true);
+use Data::HAL qw();
+use Data::HAL::Link qw();
+use HTTP::Headers qw();
+use HTTP::Status qw(:constants);
 
-__PACKAGE__->set_config({
-    PATCH => { ops => [qw/add replace remove copy/] },
-    allowed_roles => {
-        Default => [qw/admin reseller/],
-        Journal => [qw/admin reseller/],
-    }
-});
+use NGCP::Panel::Utils::ValidateJSON qw();
+require Catalyst::ActionRole::ACL;
+require NGCP::Panel::Role::HTTPMethods;
+require Catalyst::ActionRole::RequireSSL;
 
 sub allowed_methods{
     return [qw/GET OPTIONS HEAD PATCH PUT/];
 }
 
-sub item_name{
-    return 'profilepreference';
-}
+use parent qw/NGCP::Panel::Role::EntitiesItem NGCP::Panel::Role::API::Preferences/;
 
 sub resource_name{
     return 'profilepreferences';
@@ -29,9 +28,140 @@ sub container_resource_type{
     return 'profiles';
 }
 
+sub dispatch_path{
+    return '/api/profilepreferences/';
+}
+
+sub relation{
+    return 'http://purl.org/sipwise/ngcp-api/#rel-profilepreferences';
+}
+
 sub journal_query_params {
     my($self,$query_params) = @_;
     return $self->get_journal_query_params($query_params);
+}
+
+__PACKAGE__->set_config({
+    allowed_roles => {
+        Default => [qw/admin reseller/],
+        Journal => [qw/admin reseller/],
+    }
+});
+
+sub GET :Allow {
+    my ($self, $c, $id) = @_;
+    {
+        last unless $self->valid_id($c, $id);
+        my $profile = $self->item_by_id($c, $id, "profiles");
+        last unless $self->resource_exists($c, profilepreference => $profile);
+
+        my $hal = $self->hal_from_item($c, $profile, "profiles");
+
+        my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
+            (map { # XXX Data::HAL must be able to generate links with multiple relations
+                s|rel="(http://purl.org/sipwise/ngcp-api/#rel-resellers)"|rel="item $1"|;
+                s/rel=self/rel="item self"/;
+                $_
+            } $hal->http_headers),
+        ), $hal->as_json);
+        $c->response->headers($response->headers);
+        $c->response->body($response->content);
+        return;
+    }
+    return;
+}
+
+sub PATCH :Allow {
+    my ($self, $c, $id) = @_;
+    my $guard = $c->model('DB')->txn_scope_guard;
+    {
+        my $preference = $self->require_preference($c);
+        last unless $preference;
+
+        my $json = $self->get_valid_patch_data(
+            c => $c,
+            id => $id,
+            media_type => 'application/json-patch+json',
+            ops => [qw/add replace remove copy/],
+        );
+        last unless $json;
+
+        my $profile = $self->item_by_id($c, $id, "profiles");
+        last unless $self->resource_exists($c, profilepreferences => $profile);
+        my $old_resource = $self->get_resource($c, $profile, "profiles");
+        my $resource = $self->apply_patch($c, $old_resource, $json);
+        last unless $resource;
+
+        # last param is "no replace" to NOT delete existing prefs
+        # for proper PATCH behavior
+        $profile = $self->update_item($c, $profile, $old_resource, $resource, 0, "profiles");
+        last unless $profile;
+
+        my $hal = $self->hal_from_item($c, $profile, "profiles");
+        last unless $self->add_update_journal_item_hal($c,$hal);
+        
+        $guard->commit; 
+
+        if ('minimal' eq $preference) {
+            $c->response->status(HTTP_NO_CONTENT);
+            $c->response->header(Preference_Applied => 'return=minimal');
+            $c->response->body(q());
+        } else {
+            #my $hal = $self->hal_from_item($c, $profile, "profiles");
+            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
+                $hal->http_headers,
+            ), $hal->as_json);
+            $c->response->headers($response->headers);
+            $c->response->header(Preference_Applied => 'return=representation');
+            $c->response->body($response->content);
+        }
+    }
+    return;
+}
+
+sub PUT :Allow {
+    my ($self, $c, $id) = @_;
+    my $guard = $c->model('DB')->txn_scope_guard;
+    {
+        my $preference = $self->require_preference($c);
+        last unless $preference;
+
+        my $profile = $self->item_by_id($c, $id, "profiles");
+        # TODO: systemcontact?
+        last unless $self->resource_exists($c, systemcontact => $profile);
+        my $resource = $self->get_valid_put_data(
+            c => $c,
+            id => $id,
+            media_type => 'application/json',
+        );
+        last unless $resource;
+        my $old_resource = $self->get_resource($c, $profile, "profiles");
+
+        # last param is "replace" to delete all existing prefs
+        # for proper PUT behavior
+        $profile = $self->update_item($c, $profile, $old_resource, $resource, 1, "profiles");
+        last unless $profile;
+
+        my $hal = $self->hal_from_item($c, $profile, "profiles");
+        last unless $self->add_update_journal_item_hal($c,$hal);
+        
+        $guard->commit; 
+
+        if ('minimal' eq $preference) {
+            $c->response->status(HTTP_NO_CONTENT);
+            $c->response->header(Preference_Applied => 'return=minimal');
+            $c->response->body(q());
+        } else {
+            #my $hal = $self->hal_from_item($c, $profile, "profiles");
+            my $response = HTTP::Response->new(HTTP_OK, undef, HTTP::Headers->new(
+                $hal->http_headers,
+            ), $hal->as_json);
+            $c->response->headers($response->headers);
+            $c->response->header(Preference_Applied => 'return=representation');
+            $c->response->body($response->content);
+        }
+    }
+    return;
 }
 
 sub get_journal_methods{
