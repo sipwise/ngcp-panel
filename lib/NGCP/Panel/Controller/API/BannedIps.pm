211e537facae7bbd6ecf0f7697f759bb85a6798b
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2018-12-04 11:10:11 +0100

    TT#47268 Use order_by_cols to avoid item_rs call for api doc gen
    Change-Id: Ia2fccedeaa4ee513fb4ef9d77c4ea724f49820f7
    (cherry picked from commit 667522fafc8289876c101d13becf731fb7d38ee5)

--- lib/NGCP/Panel/Controller/API/BannedIps.pm
+++ lib/NGCP/Panel/Controller/API/BannedIps.pm
@@ -21,6 +21,15 @@ sub get_list{
     my ($self, $c) = @_;
     return NGCP::Panel::Utils::Security::list_banned_ips($c);
 }
+
+sub order_by_cols {
+    my ($self, $c) = @_;
+    my $cols = {
+        'ip' => 'ip',
+        'id' => 'id',
+    };
+    return $cols;
+}
 1;
 
 # vim: set tabstop=4 expandtab:
