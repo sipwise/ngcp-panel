3518c4a197a59219238325d413e6638a9160881f
Kirill Solomko <ksolomko@sipwise.com>
2018-12-12 17:56:59 +0100

    TT#48855 fix UI billing zones
        * fix relative path use in billing zones ajax
          causing an error when rendering the datatable
        * fix BillingFee form field to correctly
          capture ajax datatable billing profile id
        * fix typo when stashing billing_zone_id
        * move billing zone datatable fields
          from base to zones_list
    
    Change-Id: Ibbeb776a3bc6bfb26798fc13c1183ddb68a9d2fd
    (cherry picked from commit b364746d1a05d6e50a8c429924034f713ab5da03)
    (cherry picked from commit c07d64dc221f4511de4c00c7ceba2e11e53f4ee3)

--- lib/NGCP/Panel/Field/BillingZone.pm
+++ lib/NGCP/Panel/Field/BillingZone.pm
@@ -9,7 +9,8 @@ has_field 'id' => (
     do_wrapper => 0,
     required => 1,
     template => 'helpers/datatables_field.tt',
-    ajax_src => '../zones/ajax', # /billing/<id>/zones/ajax
+    ajax_src => '/billing/zones/ajax',
+    adjust_datatable_vars => \&adjust_datatable_vars,
     table_titles => ['#', 'Zone', 'Zone Detail'],
     table_fields => ['id', 'zone', 'detail'],
 );
@@ -20,6 +21,17 @@ has_field 'create' => (
     value => 'Create Zone',
     element_class => [qw/btn btn-tertiary pull-right/],
 );
+
+sub adjust_datatable_vars {
+    my ($self, $vars) = @_;
+    my $form = $self->form;
+    my $ctx = $form->ctx;
+    return unless $ctx;
+    my $billing_profile_id = $ctx->stash->{profile}->{id};
+    $vars->{ajax_src} = (
+        $ctx->uri_for_action('/billing/zones_ajax', [$billing_profile_id])->as_string
+    );
+}
 
 no Moose;
 1;
