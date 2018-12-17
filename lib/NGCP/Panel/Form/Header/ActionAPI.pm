ac732c336b07d382d08648f85a22501a827eb2d1
Kirill Solomko <ksolomko@sipwise.com>
2018-12-17 22:49:44 +0100

    TT#47004 Implement Header Manipulation API endpoints
    Change-Id: Id15f087511e6c71b4507a73cdb03caf10d3661e6

---
+++ lib/NGCP/Panel/Form/Header/ActionAPI.pm
@@ -0,0 +1,26 @@
+package NGCP::Panel::Form::Header::ActionAPI;
+
+use HTML::FormHandler::Moose;
+extends 'NGCP::Panel::Form::Header::Action';
+
+has_field 'rwr_dp' => (
+    type => 'Select',
+    options => [
+        { value => '' },
+        { value => 'caller_in' },
+        { value => 'callee_in' },
+        { value => 'caller_out' },
+        { value => 'callee_out' },
+    ],
+    required => 0,
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/rule_id header header_part action_type value_part value rwr_set_id rwr_dp enabled/ ],
+);
+
+1;
+
+# vim: set tabstop=4 expandtab:
