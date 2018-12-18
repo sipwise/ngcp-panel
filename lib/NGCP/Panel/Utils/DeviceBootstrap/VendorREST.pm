d8014e37666f2810d3c0b7cd2a0f990070aa5d8c
Andreas Granig <agranig@sipwise.com>
2018-12-18 12:49:55 +0100

    TT#49037 Implement rds connector to sipwise eds
    Change-Id: Ib10c44dce69ce7a1f7c1017ce25b85c6590f9b0c

---
+++ lib/NGCP/Panel/Utils/DeviceBootstrap/VendorREST.pm
@@ -0,0 +1,32 @@
+package NGCP::Panel::Utils::DeviceBootstrap::VendorREST;
+
+use strict;
+use HTTP::Request;
+use JSON qw/encode_json decode_json/;
+extends 'NGCP::Panel::Utils::DeviceBootstrap::VendorRPC';
+use Data::Dumper;
+
+sub redirect_server_call {
+    my ($self, $action) = @_;
+    my $c = $self->params->{c};
+    mu $ret;
+    $self->init_content_params();
+
+    my $method = $action.'_content';
+	my $data = $self->rest_prepare_request($method);
+    return "Failed to perform '$action'" unless $data;
+
+	my $req = HTTP::Request->new($data->{method} => $data->{url});
+    if (defined $data->{body}) {
+        $req->content(encode_json($data->{body}));
+        $req->content_type('application/json');
+    }
+    my $res = $self->_ua->request($req);
+    if ($res->is_success) {
+        return "Success";
+    } else {
+        return "Failed";
+    }
+}
+
+# vim: set tabstop=4 expandtab:
