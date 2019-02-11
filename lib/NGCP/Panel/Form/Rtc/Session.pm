24fe8aef75e71de48949c2853512ff1579bd336f
Irina Peshinskaya <ipeshinskaya@sipwise.com>
2019-02-11 06:14:27 +0100

    TT#47562 Fix rtc session subscriber parameter handling
    Note: removed reseller_id assignment in resource.
              Reseller info is taken from subscriber later.
    Added form to provide documentation
    
    Change-Id: I3847eb4778d350862812f7834437c0a95404dfca

---
+++ lib/NGCP/Panel/Form/Rtc/Session.pm
@@ -0,0 +1,27 @@
+package NGCP::Panel::Form::Rtc::Session;
+
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+
+has_field 'subscriber_id' => (
+    type => 'PosInteger',
+    required => 1,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The ID of the rtc session subscriber']
+    },
+);
+
+has_field 'rtc_app_name' => (
+    type => 'Text',
+    required => 0,
+    label => 'RTC application',
+    maxlength => 255,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Rtc application name']
+    },
+);
+
+1;
+# vim: set tabstop=4 expandtab:
