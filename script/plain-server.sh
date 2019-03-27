6c816dfe9350635dfa5e8adf6daede2ed5510f53
Guillem Jover <gjover@sipwise.com>
2019-03-27 23:39:23 +0100

    TT#53701 Line-wrap helper scripts
    Change-Id: Ia5352b21a807a47ac816c07d0c74b0416a26b7b2

--- script/plain-server.sh
+++ script/plain-server.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-CATALYST_DEBUG=1 DBIC_TRACE=1 DBIC_TRACE_PROFILE=console DEVEL_CONFESS_OPTIONS='objects builtin dump color source' perl -I ../data-hal/lib -I ../ngcp-schema/lib -I lib -I ../sipwise-base/lib/ script/ngcp_panel_server.pl --port 1444 -r
+
+export CATALYST_DEBUG=1
+export DBIC_TRACE=1
+export DBIC_TRACE_PROFILE=console
+export DEVEL_CONFESS_OPTIONS='objects builtin dump color source'
+
+perl \
+  -I ../data-hal/lib \
+  -I ../ngcp-schema/lib \
+  -I lib \
+  -I ../sipwise-base/lib/ \
+  script/ngcp_panel_server.pl --port 1444 -r
