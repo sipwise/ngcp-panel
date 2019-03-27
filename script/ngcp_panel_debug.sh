ba84aeec9313d82d1d8be9e7b5424652663e29ef
Guillem Jover <gjover@sipwise.com>
2019-03-27 23:39:23 +0100

    TT#53701 Line-wrap helper scripts
    Change-Id: Ia5352b21a807a47ac816c07d0c74b0416a26b7b2

--- script/ngcp_panel_debug.sh
+++ script/ngcp_panel_debug.sh
@@ -1,1 +1,16 @@
-PERL5LIB=/opt/Komodo-IDE-9/remote_debugging PERLDB_OPTS="RemotePort=127.0.0.1:9000" DBGP_IDEKEY="jdoe" CATALYST_DEBUG=1 DBIC_TRACE=1 DBIC_TRACE_PROFILE=console DEVEL_CONFESS_OPTIONS='objects builtin dump color source' perl -d `which plackup` -I ../data-hal/lib -I ../ngcp-schema/lib -I lib -I ../sipwise-base/lib/ ngcp_panel.psgi --listen /tmp/ngcp_panel_sock --nproc 2 -s FCGI -r
+#!/bin/sh
+
+export PERL5LIB=/opt/Komodo-IDE-9/remote_debugging
+export PERLDB_OPTS="RemotePort=127.0.0.1:9000"
+export DBGP_IDEKEY="jdoe"
+export CATALYST_DEBUG=1
+export DBIC_TRACE=1
+export DBIC_TRACE_PROFILE=console
+export DEVEL_CONFESS_OPTIONS='objects builtin dump color source'
+
+perl -d `which plackup` \
+  -I ../data-hal/lib \
+  -I ../ngcp-schema/lib \
+  -I lib \
+  -I ../sipwise-base/lib/ \
+  ngcp_panel.psgi --listen /tmp/ngcp_panel_sock --nproc 2 -s FCGI -r
