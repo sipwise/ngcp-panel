0a6a4cc5d5e6889ee9f5e44cbe8365b2062cef79
Kirill Solomko <ksolomko@sipwise.com>
2019-06-28 15:49:55 +0200

    TT#58950 RedisLocationResult improvements
        * switch from Moose to Moo reduced memory consumption
        * rework RedisLocationResultSource to use AUTOLOAD
          instead of creating accessors in BUILD (that is very expensive
          considering the amount of rows, multiplied by the overall amount
          of entries). Now the object creation takes sub 0.0001 sec from
          0.017 sec as before.
    
    Change-Id: I9917ff38266ce89297adf55d75c40dd5f16a435b

--- lib/NGCP/Panel/Utils/RedisLocationResultSource.pm
+++ lib/NGCP/Panel/Utils/RedisLocationResultSource.pm
@@ -1,21 +1,20 @@
 package NGCP::Panel::Utils::RedisLocationResultSource;
 
-use Moose;
+use Moo;
+
+our $AUTOLOAD;
 
 has _data => (
     is => 'ro',
-    isa => 'HashRef',
+    isa => sub { die "$_[0] must be HASHREF" unless $_[0] && ref $_[0] eq 'HASH' },
     default => sub {{}},
 );
 
-sub BUILD {
-    my ($self) = @_;
-    foreach my $k (keys %{ $self->_data }) {
-        $self->meta->add_attribute(
-            $k => (accessor => $k)
-        );
-        $self->$k($self->_data->{$k});
-    }
+sub AUTOLOAD {
+    my $self = shift;
+    my $col = $AUTOLOAD;
+    $col = (split '::', $col)[-1];
+    return $self->_data->{$col};
 }
 
 sub get_inflated_columns {
@@ -23,9 +22,6 @@ sub get_inflated_columns {
     return %{ $self->_data };
 }
 
-sub columns {
-    
-}
-
+sub columns { }
 
 1;
