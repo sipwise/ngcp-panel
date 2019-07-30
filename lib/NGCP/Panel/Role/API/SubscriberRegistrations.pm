4db31270b2c923a244caf02a6fcfa0385b32b1de
Kirill Solomko <ksolomko@sipwise.com>
2019-07-30 15:11:24 +0200

    TT#63542 API/SubscriberRegistrations respect redis usrloc
        * _item_by_aor search now includes redis usrloc
          state as well as ignore_auth_realm (multidomain)
          to build correct search queries in both redis
          and db locations
    
    Change-Id: I5c08a299c88592c63f3778dd0cf384a56cde3316

--- lib/NGCP/Panel/Role/API/SubscriberRegistrations.pm
+++ lib/NGCP/Panel/Role/API/SubscriberRegistrations.pm
@@ -175,12 +175,18 @@ sub subscriber_from_id {
 sub _item_by_aor {
     my ($self, $c, $sub, $contact) = @_;
 
+    my $domain = $sub->provisioning_voip_subscriber->domain->domain;
+
     return $self->item_rs($c)->search({
         'me.contact'  => $contact,
         'me.username' => $sub->provisioning_voip_subscriber->username,
-        'me.domain'   => $c->config->{features}->{multidomain}
-                            ? undef
-                            : $sub->provisioning_voip_subscriber->domain->domain,
+        $c->config->{redis}->{usrloc}
+            ? ($c->config->{features}->{multidomain}
+                ? ('me.domain' => $domain)
+                : ())
+            : ($c->config->{features}->{multidomain}
+                ? ('me.domain' => $domain)
+                : ('me.domain' => undef))
     })->first;
 }
 
