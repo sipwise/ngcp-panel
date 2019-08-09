6e77eff53e42d916fd1781528e372bca094ef667
Rene Krenn <rkrenn@sipwise.com>
2019-08-09 17:30:19 +0200

    TT#64701 remove customer_fraud_events.pm virtualview
    Change-Id: I09b3c36945e9234aec330066ae8cb29cb8ccb831
    (cherry picked from commit ed3ea31e074e5c566ba9e25204ff78126c5d5ac4)

--- lib/NGCP/Panel/Role/API/CustomerFraudEvents.pm
+++ lib/NGCP/Panel/Role/API/CustomerFraudEvents.pm
@@ -8,27 +8,84 @@ use boolean qw(true);
 use Data::HAL qw();
 use Data::HAL::Link qw();
 use HTTP::Status qw(:constants);
+use NGCP::Panel::Utils::BillingMappings qw();
+use NGCP::Panel::Utils::DateTime qw();
+use DateTime::Format::Strptime qw();
 
 sub _item_rs {
-    my ($self, $c) = @_;
+    my ($self, $c, $id) = @_;
 
-    my $item_rs = $c->model('DB')->resultset('contract_fraud_events')->search({
-    });
-
-    if (my $interval = $c->request->param('interval')) {
-        $item_rs = $item_rs->search({ interval => $interval });
+    my %cond = ();
+    if ($c->user->roles eq "admin") {
+        if (my $reseller_id = $c->request->param('reseller_id')) {
+            $cond{'contact.reseller_id'} = $reseller_id;
+        }
+    } elsif ($c->user->roles eq "reseller") {
+        $cond{'contact.reseller_id'} = $c->user->reseller_id;
+    }
+    if (my $contract_id = $c->request->param('contract_id')) {
+        $cond{'me.contract_id'} = $contract_id;
     }
     if (my $notify_status = $c->request->param('notify_status')) {
-        $item_rs = $item_rs->search({ notify_status => $notify_status });
+        $cond{'notify_status'} = $notify_status;
+    }
+    if ($id) {
+        $cond{'me.id'} = $id;
+    }
+    my $attr = {
+        join => { 'contract' => 'contact' },
+    };
+
+    my $dtf = $c->model('DB')->storage->datetime_parser;
+    my $now;
+    my $datetime_fmt = DateTime::Format::Strptime->new(
+        pattern   => '%Y-%m-%d',
+    );
+    unless ($now = $datetime_fmt->parse_datetime($c->request->param('date'))) {
+        $now = NGCP::Panel::Utils::DateTime::current_local();
+    }
+    my $first_of_month = $now->clone;
+    $first_of_month->set_day(1);
+    my $day_rs = $c->model('DB')->resultset('cdr_period_costs')->search_rs({
+        'period' => 'day',
+        'period_date' => $dtf->format_date($now),
+        'direction' => 'out',
+        'fraud_limit_exceeded' => 1,
+        'contract.status' => 'active',
+        %cond,
+    },$attr);
+    $now->subtract(days => 1);
+    my $previous_day_rs = $c->model('DB')->resultset('cdr_period_costs')->search_rs({
+        'period' => 'day',
+        'period_date' => $dtf->format_date($now),
+        'direction' => 'out',
+        'fraud_limit_exceeded' => 1,
+        'contract.status' => 'active',
+        %cond,
+    },$attr);
+    my $month_rs = $c->model('DB')->resultset('cdr_period_costs')->search_rs({
+        'period' => 'month',
+        'period_date' => $dtf->format_date($first_of_month),
+        'direction' => 'out',
+        'fraud_limit_exceeded' => 1,
+        'contract.status' => 'active',
+        %cond,
+    },$attr);
+
+    my $interval = $c->request->param('interval');
+    my $rs;
+    if (defined $interval and $interval eq 'day') {
+        $rs = $day_rs->union_all($previous_day_rs);
+    } elsif (defined $interval and $interval eq 'month') {
+        $rs = $month_rs;
+    } elsif (not defined $interval) {
+        $rs = $day_rs->union_all([$previous_day_rs, $month_rs]);
+    } else {
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Invalid interval '$interval'");
+        return;
     }
 
-    if($c->user->roles eq "admin") {
-        #
-    } elsif($c->user->roles eq "reseller") {
-        $item_rs = $item_rs->search({ reseller_id => $c->user->reseller_id });
-    }
-
-    return $item_rs;
+    return $rs;
 }
 
 sub get_form {
@@ -40,49 +97,91 @@ sub get_form {
     }
 }
 
-sub hal_from_item {
-    my ($self, $c, $item, $form) = @_;
+sub resource_from_item {
+    my ($self, $c, $item) = @_; #$form
 
-    my %resource = $item->get_inflated_columns;
-
-    my $hal = Data::HAL->new(
-        links => [
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            Data::HAL::Link->new(relation => 'collection', href => sprintf('/api/%s/', $self->resource_name)),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            Data::HAL::Link->new(relation => 'self', href => sprintf("%s%d", $self->dispatch_path, $item->id)),
-        ],
-        relation => 'ngcp:'.$self->resource_name,
+    my %cpc = $item->get_inflated_columns;
+    my %resource;
+    my $billing_mapping = NGCP::Panel::Utils::BillingMappings::get_actual_billing_mapping(c => $c,
+        now => NGCP::Panel::Utils::DateTime::epoch_local($item->last_cdr_start_time),
+        contract => $item->contract,
     );
+    my $billing_profile = $billing_mapping->billing_profile;
+    my $contract_fraud_preference = $item->contract->contract_fraud_preference;
+    $resource{'contract_id'} = $item->contract->id;
+    $resource{'reseller_id'} = $item->contract->contact->reseller_id;
+    $resource{'interval'} = $cpc{'period'};
+    $resource{'type'} = $cpc{'fraud_limit_type'};
+    $resource{'type'} = 'account_limit' if $resource{'type'} eq 'contract';
+    $resource{'type'} = 'profile_limit' if $resource{'type'} eq 'billing_profile';
 
-    $form //= $self->get_form($c);
-    return unless $self->validate_form(
-        c => $c,
-        form => $form,
-        resource => \%resource,
-        run => 0,
+    if ($billing_profile->fraud_use_reseller_rates) {
+        $resource{'interval_cost'} = $cpc{'customer_cost'};
+    } else {
+        $resource{'interval_cost'} = $cpc{'reseller_cost'};
+    }
+    if ('month' eq $cpc{'period'}) {
+        if ('contract' eq $cpc{'fraud_limit_type'}) {
+            if ($contract_fraud_preference) {
+                $resource{'interval_limit'} = $contract_fraud_preference->fraud_interval_limit;
+                $resource{'interval_lock'} = $contract_fraud_preference->fraud_interval_lock;
+                $resource{'interval_notify'} = $contract_fraud_preference->fraud_interval_notify;
+            } else {
+                $self->debug($c, "no contract fraud preference any more for contract ID $cpc{contract_id}");
+                $resource{'interval_limit'} = undef;
+                $resource{'interval_lock'} = undef;
+                $resource{'interval_notify'} = undef;
+            }
+        } elsif ('billing_profile' eq $cpc{'fraud_limit_type'}) {
+            $resource{'interval_limit'} = $billing_profile->fraud_interval_limit;
+            $resource{'interval_lock'} = $billing_profile->fraud_interval_lock;
+            $resource{'interval_notify'} = $billing_profile->fraud_interval_notify;
+        } else {
+            $self->debug($c, "unsupported fraud limit type '$cpc{fraud_limit_type}'");
+            $resource{'interval_limit'} = undef;
+            $resource{'interval_lock'} = undef;
+            $resource{'interval_notify'} = undef;
+        }
+    } elsif ('day' eq $cpc{'period'}) {
+        if ('contract' eq $cpc{'fraud_limit_type'}) {
+            if ($contract_fraud_preference) {
+                $resource{'interval_limit'} = $contract_fraud_preference->fraud_daily_limit;
+                $resource{'interval_lock'} = $contract_fraud_preference->fraud_daily_lock;
+                $resource{'interval_notify'} = $contract_fraud_preference->fraud_daily_notify;
+            } else {
+
+            }
+        } elsif ('billing_profile' eq $cpc{'fraud_limit_type'}) {
+            $resource{'interval_limit'} = $billing_profile->fraud_daily_limit;
+            $resource{'interval_lock'} = $billing_profile->fraud_daily_lock;
+            $resource{'interval_notify'} = $billing_profile->fraud_daily_notify;
+        } else {
+            $self->debug($c, "unsupported fraud limit type '$cpc{fraud_limit_type}'");
+            $resource{'interval_limit'} = undef;
+            $resource{'interval_lock'} = undef;
+            $resource{'interval_notify'} = undef;
+        }
+    } else {
+        $self->debug($c, "unsupported fraud interval '$cpc{'period'}'");
+        $resource{'interval_limit'} = undef;
+        $resource{'interval_lock'} = undef;
+        $resource{'interval_notify'} = undef;
+    }
+    $resource{'use_reseller_rates'} = $billing_profile->fraud_use_reseller_rates;
+    $resource{'notify_status'} = $cpc{'notify_status'};
+    my $datetime_fmt = DateTime::Format::Strptime->new(
+        pattern => '%F %T',
     );
+    $resource{'notified_at'} = undef;
+    $resource{'notified_at'} = $datetime_fmt->format_datetime($cpc{notified_at}) if defined $cpc{notified_at};
 
-    $resource{id} = int($item->id);
-    $hal->resource({%resource});
-    return $hal;
+    return \%resource;
+
 }
 
 sub item_by_id {
     my ($self, $c, $id) = @_;
-    my $item_rs = $self->item_rs($c);
-    my ($contract_id, $period, $period_date) = split(/-/, $id, 3);
-
-    return $item_rs->search_rs({
-        id => $contract_id,
-        interval => $period,
-        interval_date => $period_date
-    })->first;
+    return $self->item_rs($c,$id)->first;
 }
 
 sub update_item {
@@ -95,24 +194,18 @@ sub update_item {
         resource => $resource,
     );
 
-    my $cpc_rs = $c->model('DB')->resultset('cdr_period_costs')->search({
-        contract_id => $item->id,
-        period => $item->interval,
-        period_date => $item->interval_date
-    });
-    my $cpc = $cpc_rs->first;
-    unless ($cpc) {
-        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Customer fraud event does not exist");
+    try {
+        # only update r/w fields
+        $item->update({
+            map { $_ => $resource->{$_} } qw(notify_status notified_at)
+        })->discard_changes;
+    } catch($e) {
+        $c->log->error("failed to update customer fraud event: $e");
+        $self->error($c, HTTP_INTERNAL_SERVER_ERROR, "Failed to update customer fraud event.");
         return;
-    }
-
-    # only update r/w fields
-    $cpc->update({
-        map { $_ => $resource->{$_} } qw(notify_status notified_at)
-    });
+    };
 
     return $item;
 }
 
 1;
-# vim: set tabstop=4 expandtab:
