37d4b8e35a7cb8eae2e2b018c854861e7311b112
Rene Krenn <rkrenn@sipwise.com>
2019-08-09 17:30:19 +0200

    TT#64701 remove customer_fraud_events.pm virtualview
    Change-Id: I09b3c36945e9234aec330066ae8cb29cb8ccb831

--- lib/NGCP/Panel/Controller/API/CustomerFraudEvents.pm
+++ lib/NGCP/Panel/Controller/API/CustomerFraudEvents.pm
@@ -14,7 +14,7 @@ sub allowed_methods{
 }
 
 sub api_description {
-    return 'Defines a list of customers with fraud limits above defined thresholds for a specific interval.';
+    return 'Defines a list of current fraud limit violations (the threshold for outgoing call costs per day/month was exceeded) of customers.';
 };
 
 sub query_params {
@@ -22,13 +22,10 @@ sub query_params {
         {
             param => 'reseller_id',
             description => 'Filter for fraud events belonging to a specific reseller',
-            query => {
-                first => sub {
-                    my $q = shift;
-                    return { reseller_id => $q };
-                },
-                second => sub {},
-            },
+        },
+        {
+            param => 'contract_id',
+            description => 'Filter for fraud events of a specific contract',
         },
         {
             param => 'interval',
@@ -39,6 +36,19 @@ sub query_params {
             description => 'Notify status filter. values: ["new", "notified"].',
         },
     ];
+}
+
+sub order_by_cols {
+    my ($self, $c) = @_;
+    my $cols = {
+        'customer_id' => 'customer_id',
+        'id' => 'id',
+        'interval' => 'interval',
+        'notified_at' => 'notified_at',
+        'notify_status' => 'notify_status',
+        'reseller_id' => 'reseller_id',
+    };
+    return $cols;
 }
 
 use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::CustomerFraudEvents/;
@@ -60,48 +70,4 @@ __PACKAGE__->set_config({
     allowed_roles => [qw/admin reseller/],
 });
 
-sub GET :Allow {
-    my ($self, $c) = @_;
-    my $page = $c->request->params->{page} // 1;
-    my $rows = $c->request->params->{rows} // 10;
-    {
-        my $items = $self->item_rs($c);
-        (my $total_count, $items, my $items_rows) = $self->paginate_order_collection($c, $items);
-        my (@embedded, @links);
-        my $form = $self->get_form($c);
-        for my $item (@$items_rows) {
-            push @embedded, $self->hal_from_item($c, $item, $form);
-            push @links, Data::HAL::Link->new(
-                relation => 'ngcp:'.$self->resource_name,
-                href     => sprintf('/%s%d-%s-%s', $c->request->path, $item->id, $item->interval, $item->interval_date),
-            );
-        }
-        push @links,
-            Data::HAL::Link->new(
-                relation => 'curies',
-                href => 'http://purl.org/sipwise/ngcp-api/#rel-{rel}',
-                name => 'ngcp',
-                templated => true,
-            ),
-            Data::HAL::Link->new(relation => 'profile', href => 'http://purl.org/sipwise/ngcp-api/'),
-            $self->collection_nav_links($c, $page, $rows, $total_count, $c->request->path, $c->request->query_params);
-
-        my $hal = Data::HAL->new(
-            embedded => [@embedded],
-            links => [@links],
-        );
-        $hal->resource({
-            total_count => $total_count,
-        });
-        my $response = HTTP::Response->new(HTTP_OK, undef,
-            HTTP::Headers->new($hal->http_headers(skip_links => 1)), $hal->as_json);
-        $c->response->headers($response->headers);
-        $c->response->body($response->content);
-        return;
-    }
-    return;
-}
-
 1;
-
-# vim: set tabstop=4 expandtab:
