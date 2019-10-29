467321e2f3aef7ea5e17deb48b0c225fbba113dd
Flaviu Mates <fmates@sipwise.com>
2019-10-29 18:14:36 +0200

    TT#69360 - Fix 500 errors when issuing POST to HeaderRule(Actions|Conditions)
             * DBIx could not resolve the relation 'rwr_set'
               when post processing hal resource because,
    	   if not 'rwr_set_id', parameter is sent, the
    	   field is not set in the DB
    	 * Add error message when sending 'rwr_dp' param
    	   with no 'rwr_set_id' which was causing error
    	   as well
    
    Change-Id: I90c2d5314b9e8a5389cc606bb0be72533614f8d5

--- lib/NGCP/Panel/Role/API/HeaderRuleActions.pm
+++ lib/NGCP/Panel/Role/API/HeaderRuleActions.pm
@@ -51,7 +51,7 @@ sub hal_links {
 sub post_process_hal_resource {
     my ($self, $c, $item, $resource, $form) = @_;
     my $dp_id = delete $resource->{rwr_dp_id};
-    if ($item->rwr_set) {
+    if ($item->rwr_set_id && $item->rwr_set) {
         my %rwr_set_cols = $item->rwr_set->get_inflated_columns;
         foreach my $dp_t (qw(callee_in callee_out caller_in caller_out)) {
             my $c_dp_id = $rwr_set_cols{$dp_t.'_dpid'} // next;
@@ -70,6 +70,11 @@ sub check_resource {
 
     unless (defined $resource->{rule_id}) {
         $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Required: 'rule_id'");
+        return;
+    }
+
+    if (!defined $resource->{rwr_set_id} && (defined $resource->{rwr_dp} || defined $resource->{rwr_dp_id})) {
+        $self->error($c, HTTP_UNPROCESSABLE_ENTITY, "Missing 'rwr_set_id' (when rwr_dp is set).");
         return;
     }
 
