58ff2d18a0f2a559c128125772ac5ee8736a4776
Nico Schedel <nschedel@sipwise.com>
2019-12-17 12:29:16 +0100

    TT#68016 selenium: add proper file type to alert screenshots
    FirefoxExtensions.pm:
    - add .png file type to screenshots so you can view it directly
    in your browser instead of having to download the file
    
    imagine needing 3 commits to get a line of code working properly (:
    
    Change-Id: I864fb06ae7adcd686a9af1fe06ce70d3c90fbba5

--- t/lib/Selenium/Remote/Driver/FirefoxExtensions.pm
+++ t/lib/Selenium/Remote/Driver/FirefoxExtensions.pm
@@ -113,7 +113,7 @@ sub get_text_safe {
         my $element = $self->find_element($path, $type);
         return $element->get_text();
     } catch {
-        $self->capture_screenshot('/results/alert' . int(rand(1000)));
+        $self->capture_screenshot('/results/alert' . int(rand(1000)) . '.png');
         return 'Element not found';
     };
 }
