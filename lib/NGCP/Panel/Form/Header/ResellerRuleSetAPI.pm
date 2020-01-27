ffd070cd242d0526eacf1df9c7318453469d5ecb
Flaviu Mates <fmates@sipwise.com>
2020-01-27 17:45:23 +0200

    TT#74525 - Updgrade /api/headerrulesets and /api/callforwards
    	 * Introduce posibility to provision rule sets,
    	   rules, condition and actions at the same time
    	   using only the /api/headerrulesets endpoint;
    	   also rules can be modified with PUT/PATCH on
    	   /api/headerrulesets
    
    	 * Introduce posibility to provision source sets,
    	   time sets, b number sets and destination sets
    	   in the same request with defining call forwards
    
    Change-Id: I8c054f72a2632d45fec76166774521f8c22aea05

--- lib/NGCP/Panel/Form/Header/ResellerRuleSetAPI.pm
+++ lib/NGCP/Panel/Form/Header/ResellerRuleSetAPI.pm
@@ -12,10 +12,19 @@ has_field 'subscriber_id' => (
     fif_from_value => 1,
 );
 
+has_field 'rules' => (
+    type => 'Compound',
+    required => 0,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['The list of rules in the set.'],
+    },
+);
+
 has_block 'fields' => (
     tag => 'div',
     class => [qw/modal-body/],
-    render_list => [qw/subscriber_id name description/],
+    render_list => [qw/subscriber_id name description rules/],
 );
 
 1;
