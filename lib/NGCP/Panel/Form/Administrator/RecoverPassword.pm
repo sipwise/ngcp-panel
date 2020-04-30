93caac6ede505a661721657f957be1d3be09e900
Flaviu Mates <fmates@sipwise.com>
2020-04-30 13:00:07 +0300

    TT#76108 - Implement password reset mechanism for admins
    	 * Introduce endopint '/resetpassword' for asking for
    	   password reset using admin username
    	 * Create form for introducing username
    	 * Create url with unique token pointing to '/recoverpassword'
    	   where admin user can introduce new password and email
    	   said url to admin's email address
    	 * Create form for setting new password
    	 * Store username and unique token in Redis expiring
    	   in 5 minutes to store password reset attempt
    	   and identify it when user accesses url in email
    	 * Limit admin access to be able to only change own password
    	   due to new password reset possibility as requested in
    	   TT#76110
    
    Change-Id: Ie3acb961444398afa5b2fdc85e3ca8ceccf9244a
    (cherry picked from commit 9907936d4711e29e752f78d82585866ead3f3dd1)

---
+++ lib/NGCP/Panel/Form/Administrator/RecoverPassword.pm
@@ -0,0 +1,56 @@
+package NGCP::Panel::Form::Administrator::RecoverPassword;
+
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+
+use HTML::FormHandler::Widget::Block::Bootstrap;
+use NGCP::Panel::Field::PosInteger;
+use NGCP::Panel::Utils::Form;
+
+has '+widget_wrapper' => ( default => 'Bootstrap' );
+has_field 'submitid' => ( type => 'Hidden' );
+sub build_render_list {[qw/submitid fields actions/]}
+sub build_form_element_class {[qw(form-horizontal)]}
+
+has_field 'password' => (
+    type => 'Password',
+    required => 1,
+    label => 'Password',
+);
+
+has_field 'password_conf' => (
+    type => 'PasswordConf',
+    required => 1,
+    label => 'Repeat Password',
+    password_field => 'password',
+);
+
+has_field 'save' => (
+    type => 'Submit',
+    value => 'Save',
+    element_class => [qw/btn btn-primary/],
+    label => '',
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/password password_conf/],
+);
+
+has_block 'actions' => (
+    tag => 'div',
+    class => [qw/modal-footer/],
+    render_list => [qw/save/],
+);
+
+sub validate_password {
+    my ($self, $field) = @_;
+    my $c = $self->form->ctx;
+    return unless $c;
+
+    NGCP::Panel::Utils::Form::validate_password(c => $c, field => $field);
+}
+
+1;
+# vim: set tabstop=4 expandtab:
