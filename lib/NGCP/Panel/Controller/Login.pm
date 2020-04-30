0f120be0c47d20a4063509acb4ca46f9db881e29
Flaviu Mates <fmates@sipwise.com>
2020-04-30 13:00:07 +0300

    TT#76108 - Implement password reset mechanism for admins
    	 * Introduce endopint '/resetpassword' for asking for
    	   password reset using admin username
    	 * Create form for introducing username
    	 * Create url with unique token pointing to '/recoverpassword'
    	   where admin user can introduce new password and email
    	   said url to admin's email address
    	 * Create form for setting new password
    	 * Store username, unique token, and user's IP in Redis
    	   epiring in 5 minutes to store password reset attempt
    	   and identify it when user accesses url in email
    	 * Limit admin access to be able to only change own password
    	   due to new password reset possibility as requested in
    	   TT#76110
    
    Change-Id: Ie3acb961444398afa5b2fdc85e3ca8ceccf9244a

--- lib/NGCP/Panel/Controller/Login.pm
+++ lib/NGCP/Panel/Controller/Login.pm
@@ -4,6 +4,9 @@ use warnings;
 use strict;
 
 use parent 'Catalyst::Controller';
+use Redis;
+use TryCatch;
+use UUID;
 
 use NGCP::Panel::Form;
 
@@ -90,6 +93,178 @@ sub login_index :Path Form {
     $c->stash(template => 'login/login.tt');
 }
 
+sub reset_password :Chained('/') :PathPart('resetpassword') :Args(0) {
+    my ($self, $c) = @_;
+
+    my $posted = $c->req->method eq "POST";
+    my $form = NGCP::Panel::Form::get("NGCP::Panel::Form::Administrator::ResetPassword", $c);
+    my $params = {};
+    $form->process(
+        posted => $posted,
+        params => $c->req->params,
+        item => $params,
+    );
+    NGCP::Panel::Utils::Navigation::check_form_buttons(
+        c => $c,
+        form => $form,
+        fields => {},
+        back_uri => $c->req->uri,
+    );
+
+    if($posted && $form->validated) {
+        try {
+            my $schema = $c->model('DB');
+            $schema->txn_do(sub {
+                my $username = $form->params->{username};
+                my $admin = $schema->resultset('admins')->search({
+                    login => $username,
+                })->first;
+
+                # don't inform about unknown users
+                if($admin) {
+                    if ($admin->can_reset_password) {
+                        # don't clear web password, a user might just have guessed it and
+                        # could then block the legit user out
+                        my ($uuid_bin, $uuid_string);
+                        UUID::generate($uuid_bin);
+                        UUID::unparse($uuid_bin, $uuid_string);
+                        my $redis = Redis->new(
+                            server => $c->config->{redis}->{central_url},
+                            reconnect => 10, every => 500000, # 500ms
+                            cnx_timeout => 3,
+                        );
+                        unless ($redis) {
+                            $c->log->error("Failed to connect to central redis url " . $c->config->{redis}->{central_url});
+                            return;
+                        }
+                        $redis->select(30);
+                        $redis->hset($admin->login, 'token', $uuid_string);
+                        $redis->hset($admin->login, 'issue_time', time);
+                        $redis->hset($admin->login, 'ip', $c->req->address);
+                        $redis->expire($admin->login, 300);
+                        my $url = $c->uri_for_action('/login/recover_password')->as_string . '?uuid=' . $uuid_string;
+                        NGCP::Panel::Utils::Email::admin_password_reset($c, $admin, $url);
+
+                        NGCP::Panel::Utils::Message::info(
+                            c    => $c,
+                            desc => $c->loc('Successfully reset password, please check your email'),
+                        );
+                    }
+                    else {
+                        NGCP::Panel::Utils::Message::error(
+                            c    => $c,
+                            desc => $c->loc('Admin is not allowed to reset password'),
+                        );
+                    }
+                }
+            });
+        } catch($e) {
+            NGCP::Panel::Utils::Message::error(
+                c     => $c,
+                error => $e,
+                desc  => $c->loc('Failed to reset password'),
+            );
+        }
+        $c->res->redirect($c->uri_for('/login/admin'));
+    }
+
+    $c->stash(
+        form => $form,
+        edit_flag => 1,
+        template => 'administrator/reset_password.tt',
+        close_target => $c->uri_for('/login/admin'),
+    );
+}
+
+sub recover_password :Chained('/') :PathPart('recoverpassword') :Args(0) {
+    my ($self, $c) = @_;
+
+    $c->user->logout if($c->user);
+
+    my $posted = $c->req->method eq "POST";
+    my ($uuid_bin, $uuid_string);
+    $uuid_string = $c->req->params->{uuid} // '';
+
+    unless($uuid_string && UUID::parse($uuid_string, $uuid_bin) != -1) {
+        $c->log->warn("invalid password recovery attempt for uuid '$uuid_string' from '".$c->qs($c->req->address)."'");
+        $c->detach('/denied_page')
+    }
+
+    my $redis = Redis->new(
+        server => $c->config->{redis}->{central_url},
+        reconnect => 10, every => 500000, # 500ms
+        cnx_timeout => 3,
+    );
+    unless ($redis) {
+        $c->log->error("Failed to connect to central redis url " . $c->config->{redis}->{central_url});
+        return;
+    }
+    $redis->select(30);
+    my @keys = $redis->keys('*');
+    my ($admin) = grep { $redis->hget($_, "token") eq $uuid_string } @keys;
+    my $administrator;
+    if ($admin) {
+        $administrator = $c->model('DB')->resultset('admins')->search({login => $admin})->first;
+        unless ($administrator) {
+            $c->log->warn("invalid password recovery attempt for uuid '$uuid_string' from '".$c->qs($c->req->address)."'");
+            $c->detach('/denied_page');
+        }
+    }
+    else {
+        NGCP::Panel::Utils::Message::error(
+            c    => $c,
+            desc => $c->loc('Invalid UUID'),
+        );
+    }
+
+    my $form = NGCP::Panel::Form::get("NGCP::Panel::Form::Administrator::RecoverPassword", $c);
+    my $params = {
+        uuid => $uuid_string,
+    };
+    $form->process(
+        posted => $posted,
+        params => $c->req->params,
+        item => $params,
+    );
+    NGCP::Panel::Utils::Navigation::check_form_buttons(
+        c => $c,
+        form => $form,
+        fields => {},
+        back_uri => $c->req->uri,
+    );
+    if($posted && $form->validated) {
+        try {
+                $administrator->update({
+                    saltedpass => NGCP::Panel::Utils::Admin::generate_salted_hash($form->params->{password}),
+                });
+        } catch($e) {
+            NGCP::Panel::Utils::Message::error(
+                c     => $c,
+                error => $e,
+                type  => 'internal',
+                desc  => $c->loc('Failed to recover password'),
+            );
+            $c->detach('/denied_page');
+        }
+
+        NGCP::Panel::Utils::Message::info(
+            c    => $c,
+            data => { username => $administrator->login },
+            desc => $c->loc('Password successfully recovered, please re-login.'),
+        );
+        $c->flash(username => $administrator->login);
+        $c->res->redirect($c->uri_for('/login/admin'));
+        return;
+
+    }
+
+    $c->stash(
+        form => $form,
+        edit_flag => 1,
+        template => 'administrator/reset_password.tt',
+        close_target => $c->uri_for('/login/admin'),
+    );
+}
 
 1;
 
---
+++ lib/NGCP/Panel/Form/Administrator/ResetPassword.pm
@@ -0,0 +1,39 @@
+package NGCP::Panel::Form::Administrator::ResetPassword;
+
+use HTML::FormHandler::Moose;
+extends 'HTML::FormHandler';
+
+use HTML::FormHandler::Widget::Block::Bootstrap;
+use NGCP::Panel::Field::PosInteger;
+
+has '+widget_wrapper' => ( default => 'Bootstrap' );
+has_field 'submitid' => ( type => 'Hidden' );
+sub build_render_list {[qw/submitid fields actions/]}
+sub build_form_element_class {[qw(form-horizontal)]}
+
+has_field 'username' => (
+    type => 'Text',
+    required => 1,
+    label => 'Username',
+);
+
+has_field 'save' => (
+    type => 'Submit',
+    value => 'Recover',
+    element_class => [qw/btn btn-primary/],
+    label => '',
+);
+
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw/modal-body/],
+    render_list => [qw/username/],
+);
+
+has_block 'actions' => (
+    tag => 'div',
+    class => [qw/modal-footer/],
+    render_list => [qw/save/],
+);
+
+1;
