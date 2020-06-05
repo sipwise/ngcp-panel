b5b02e71d141aa64b20b70ef185e5942cad8b43d
Kirill Solomko <ksolomko@sipwise.com>
2020-06-05 18:13:25 +0200

    TT#76111 add lintercept role
        * new c.users.role 'lintercept', that set to when an admin user has
    enabled 'lawful_intercept' flag
        * only Administrator page /api/admins and /api/interceptions are available for
    the role
        * 'lintercept' role can only see own user and only change password
    and email
    
    Change-Id: Iadcb022a124afbd77b224e734026f380af0170e8
    (cherry picked from commit 7862a87639b3beacc7dc7452e9a1d2ed587cb967)

---
+++ lib/NGCP/Panel/Form/Administrator/LIntercept.pm
@@ -0,0 +1,32 @@
+package NGCP::Panel::Form::Administrator::LIntercept;
+use HTML::FormHandler::Moose;
+use HTML::FormHandler::Widget::Block::Bootstrap;
+use NGCP::Panel::Utils::Form;
+extends 'HTML::FormHandler';
+
+has '+widget_wrapper' => (default => 'Bootstrap');
+has_field 'submitid' => ( type => 'Hidden' );
+sub build_render_list {[qw/submitid fields actions/]}
+sub build_form_element_class {[qw(form-horizontal)]}
+
+has_field 'password' => (type => 'Password', required => 1, label => 'Password');
+has_field 'email' => (type => 'Email', required => 0, label => 'Email', maxlength => 255);
+has_field 'save' => (type => 'Submit', element_class => [qw(btn btn-primary)],);
+has_block 'fields' => (
+    tag => 'div',
+    class => [qw(modal-body)],
+    render_list => [qw(
+        password email
+    )],
+);
+has_block 'actions' => (tag => 'div', class => [qw(modal-footer)], render_list => [qw(save)],);
+
+sub validate_password {
+    my ($self, $field) = @_;
+    my $c = $self->form->ctx;
+    return unless $c;
+
+    NGCP::Panel::Utils::Form::validate_password(c => $c, field => $field);
+}
+
+1;
