4155971dd513f2f56d9fef615557b13513abcde0
Rene Krenn <rkrenn@sipwise.com>
2021-08-16 12:28:37 +0200

    TT#134807 "ignore domain" option for billing profiles
    fee matching works in 2 attempts:
    1. try to find a fee matching <user@domain>
    2. if none found, try to find a fee matching <user>
    
    the billing_profile record will get a new field
    "ignore_domain". if set to "1", step#1 above
    should be skipped.
    
    Change-Id: I7f0a8f1dd8d5699ad4e2b8242c68fb9f75fc1bff
    (cherry picked from commit 48f8938dfd301c5a9dd1194ece7f74c8b98bd174)

--- lib/NGCP/Panel/Form/BillingProfile/Reseller.pm
+++ lib/NGCP/Panel/Form/BillingProfile/Reseller.pm
@@ -36,6 +36,15 @@ has_field 'handle' => (
     },
 );
 
+has_field 'ignore_domain' => (
+    type => 'Boolean',
+    default => 0,
+    element_attr => {
+        rel => ['tooltip'],
+        title => ['Ignore SIP domain part when matching fees.']
+    },
+);
+
 has_field 'prepaid' => (
     type => 'Boolean',
     default => 0,
@@ -203,7 +212,7 @@ has_field 'save' => (
 has_block 'fields' => (
     tag => 'div',
     class => [qw/modal-body/],
-    render_list => [qw/handle name prepaid prepaid_library advice_of_charge interval_charge interval_free_time interval_free_cash
+    render_list => [qw/handle name ignore_domain prepaid prepaid_library advice_of_charge interval_charge interval_free_time interval_free_cash
         fraud_interval_limit fraud_interval_lock fraud_interval_notify
         fraud_daily_limit fraud_daily_lock fraud_daily_notify fraud_use_reseller_rates
         currency id
