c9e56364a4e545352ce2130dac9438ffe7a393d7
Victor Tsvetov <vtsvetov@sipwise.com>
2021-08-21 13:17:28 +0300

    TT#137552 Update German translations
    Import new translations from Crowdin.
    99% translated
    81% approved
    
    Change-Id: I78db4d313fb1ae8481fe82a8aa15cf8bdedba8c5

--- share/static/js/i18n/dataTables.de.js
+++ share/static/js/i18n/dataTables.de.js
@@ -1,23 +1,23 @@
 {
     "oAria": {
-        "sSortAscending":  ": aktivieren, um die Spalte aufsteigend zu sortieren",
+        "sSortAscending": ": aktivieren, um die Spalte aufsteigend zu sortieren",
         "sSortDescending": ": aktivieren, um die Spalte absteigend zu sortieren"
     },
-    "sEmptyTable":     "Keine Daten verfügbar",
+    "sEmptyTable": "Keine Daten verfügbar",
     "sInfo": "_START_ bis _END_ von _TOTAL_ Einträgen",
     "sInfoEmpty": "0 bis 0 von 0 Einträgen",
     "sInfoFiltered": "(gefiltert von _MAX_  Einträgen)",
-    "sInfoPostFix":    "",
-    "sInfoThousands":  ",",
+    "sInfoPostFix": "",
+    "sInfoThousands": ",",
     "sLengthMenu": "_MENU_ Einträge anzeigen",
-    "sLoadingRecords": "Wird geladen...",
+    "sLoadingRecords": "Lädt …",
     "oPaginate": {
-        "sFirst":    "Erster",
-        "sLast":     "Letzter",
-        "sNext":     "Nächster",
+        "sFirst": "Erster",
+        "sLast": "Letzter",
+        "sNext": "Weiter",
         "sPrevious": "Zurück"
     },
-    "sProcessing":     "Bitte warten...",
-    "sSearch":         "Suchen:",
+    "sProcessing": "Bitte warten...",
+    "sSearch": "Suchen:",
     "sZeroRecords": "Keine Einträge vorhanden"
 }
