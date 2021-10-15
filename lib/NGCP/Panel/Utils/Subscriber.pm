e6034427e9e9a5e3b714b02a5fddb5e47224e6ff
Kirill Solomko <ksolomko@sipwise.com>
2021-10-15 15:13:19 +0200

    TT#139550 vmnotify use correct uuid
    * wrong value was used in the vmnotify select query, now the
      correct uuid is used
    
    Change-Id: I311cc7ea9cce01014dd40761d0bc7f594ecca7b2
    (cherry picked from commit 67bc755276dba5b76ab27dc77888e7cc8d8833a3)

--- lib/NGCP/Panel/Utils/Subscriber.pm
+++ lib/NGCP/Panel/Utils/Subscriber.pm
@@ -2285,7 +2285,7 @@ sub vmnotify{
     $data->{new_messages} = 0;
 
     my $msg_rs = $c->model('DB')->resultset('voicemail_spool')->search({
-        'mailboxuser' => $data->{mailboxuser},
+        'mailboxuser' => $data->{uuid},
     },{
         'select'      => [
             'dir',
