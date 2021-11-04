sipwise/ngcp-panel

commit 133bd43df4e99c17952cc9faf7ab88094dc7315f

TT#147151 fast loading/paging/searching panel datatables
query refactoring an rowcount clipping for UI datatables
that are slow when using millions of subscribers:

 contacts
 customers
 contracts
 subscribers
 billing profiles
 billing networks
 billing profile packages

Change-Id: Ia50e3aa52684772548569b6908f0cbc08395a5a7

lib/NGCP/Panel/Utils/Contract.pm
@@ -131,9 +131,7 @@ sub get_contract_rs {
     $schema //= $c->model('DB');
     my $rs = $schema->resultset('contracts')->search({
         $include_terminated ? () : ('me.status' => { '!=' => 'terminated' }),  ## no critic (ProhibitCommaSeparatedStatements)
-    },{
-        join => 'product',
-    });
+    }, undef);
     return $rs;
 
 }
@@ -142,12 +140,15 @@ sub get_customer_rs {
     my %params = @_;
     my ($c,$schema,$include_terminated) = @params{qw/c schema include_terminated/};
 
+    $schema //= $c->model('DB');
+    my @product_ids = map { $_->id; } $schema->resultset('products')->search_rs({ 'class' => ['sipaccount','pbxaccount'] })->all;
+
     my $rs = get_contract_rs(
         c => $c,
         schema => $schema,
         include_terminated => $include_terminated,
     )->search_rs({
-        'product.class' => { -in => [ 'sipaccount', 'pbxaccount' ] },
+        'product_id' => { -in => [ @product_ids ] },
     },{
         join => 'contact',
     });

lib/NGCP/Panel/Controller/Contract.pm
@@ -25,13 +25,13 @@ sub contract_list :Chained('/') :PathPart('contract') :CaptureArgs(0) {
 
     my $now = NGCP::Panel::Utils::DateTime::current_local;
     $c->stash->{contract_dt_columns} = NGCP::Panel::Utils::Datatables::set_columns($c, [
-        { name => "id", search => 1, title => $c->loc("#") },
-        { name => "external_id", search => 1, title => $c->loc("External #") },
-        { name => "contact.email", search => 1, title => $c->loc("Contact Email") },
-        { name => "product.name", search => 1, title => $c->loc("Product") },
+        { name => "id", int_search => 1, title => $c->loc("#") },
+        { name => "external_id", strict_search => 1, title => $c->loc("External #") },
+        { name => "contact.email", search => 0, title => $c->loc("Contact Email") },
+        { name => "product.name", search => 0, title => $c->loc("Product") },
         { name => 'billing_profile_name', accessor => "billing_profile_name", search => 0, title => $c->loc('Billing Profile'),
           literal_sql => NGCP::Panel::Utils::BillingMappings::get_actual_billing_mapping_stmt(c => $c, now => $now, projection => 'billing_profile.name' ) },
-        { name => "status", search => 1, title => $c->loc("Status") },
+        { name => "status", search => 0, title => $c->loc("Status") },
     ]);
 
     my $rs = NGCP::Panel::Utils::Contract::get_contract_rs(
@@ -50,13 +50,11 @@ sub contract_list :Chained('/') :PathPart('contract') :CaptureArgs(0) {
             join => 'contact',
         });
     }
+
+    my @product_ids = map { $_->id; } $c->model('DB')->resultset('products')->search_rs({ 'class' => ['pstnpeering','sippeering','reseller'] })->all;
     $rs = $rs->search({
-            '-or' => [
-                'product.class' => 'pstnpeering',
-                'product.class' => 'sippeering',
-                'product.class' => 'reseller',
-            ],
-        });
+        'product_id' => { -in => [ @product_ids ] },
+    });
     $c->stash(contract_select_rs => $rs);
     $c->stash(contract_select_all_rs => $rs_all);
     $c->stash(now => $now);
@@ -297,7 +295,18 @@ sub ajax :Chained('contract_list') :PathPart('ajax') :Args(0) {
     my ($self, $c) = @_;
 
     my $res = $c->stash->{contract_select_rs};
-    NGCP::Panel::Utils::Datatables::process($c, $res, $c->stash->{contract_dt_columns});
+    NGCP::Panel::Utils::Datatables::process($c, $res, $c->stash->{contract_dt_columns}, undef, {
+        'count_limit' => 1000,
+        'extra_or' => [ {
+            'me.id' => { in => [ map { $_->id } $res->search({
+                'contact.email' => { like => [ NGCP::Panel::Utils::Datatables::get_search_string_pattern($c) ]->[0] },
+            },{
+                rows => 1001,
+                page => 1,
+                join => 'contact',
+            })->all ] },
+        }, ],
+    });
     $c->detach( $c->view("JSON") );
 }
 

lib/NGCP/Panel/Controller/Billing.pm
@@ -32,7 +32,6 @@ sub profile_list :Chained('/') :PathPart('billing') :CaptureArgs(0) :Does(ACL) :
         { name => "id", "search" => 1, "title" => $c->loc("#") },
         { name => "name", "search" => 1, "title" => $c->loc("Name") },
         { name => "reseller.name", "search" => 1, "title" => $c->loc("Reseller") },
-        #{ name => "v_count_used", "search" => 0, "title" => $c->loc("Used") },
         NGCP::Panel::Utils::Billing::get_datatable_cols($c),
     ]);
 
@@ -48,7 +47,7 @@ sub _profile_resultset_admin {
     my $rs = $c->model('DB')->resultset('billing_profiles')->search({
             'me.status' => { '!=' => 'terminated' },
             },
-            { '+select' => [ { '' => \[ NGCP::Panel::Utils::Billing::get_contract_count_stmt() ] , -as => 'contract_cnt' },
+            { '+select' => [ { '' => \[ NGCP::Panel::Utils::Billing::get_contract_count_stmt(1000) ] , -as => 'contract_cnt' },
                            { '' => \[ NGCP::Panel::Utils::Billing::get_contract_exists_stmt() ] , -as => 'contract_exists' },
                            { '' => \[ NGCP::Panel::Utils::Billing::get_package_count_stmt() ] , -as => 'package_cnt' }, ],
             });
@@ -62,7 +61,7 @@ sub _profile_resultset_reseller {
         ->search_rs({
             'me.status' => { '!=' => 'terminated' },
             },
-            { '+select' => [ { '' => \[ NGCP::Panel::Utils::Billing::get_contract_count_stmt() ] , -as => 'contract_cnt' },
+            { '+select' => [ { '' => \[ NGCP::Panel::Utils::Billing::get_contract_count_stmt(1000) ] , -as => 'contract_cnt' },
                            { '' => \[ NGCP::Panel::Utils::Billing::get_contract_exists_stmt() ] , -as => 'contract_exists' },
                            { '' => \[ NGCP::Panel::Utils::Billing::get_package_count_stmt() ] , -as => 'package_cnt' }, ],
             });

lib/NGCP/Panel/Controller/Contact.pm
@@ -31,11 +31,11 @@ sub list_contact :Chained('/') :PathPart('contact') :CaptureArgs(0) :Does(ACL) :
     $c->stash(template => 'contact/list.tt');
 
     $c->stash->{contact_dt_columns} = NGCP::Panel::Utils::Datatables::set_columns($c, [
-        { name => "id", search => 1, title => $c->loc("#") },
-        { name => "reseller.name", search => 1, title => $c->loc("Reseller") },
-        { name => "firstname", search => 1, title => $c->loc("First Name") },
-        { name => "lastname", search => 1, title => $c->loc("Last Name") },
-        { name => "company", search => 1, title => $c->loc("Company") },
+        { name => "id", int_search => 1, title => $c->loc("#") },
+        { name => "reseller.name", search => 0, title => $c->loc("Reseller") },
+        { name => "firstname", search => 0, title => $c->loc("First Name") },
+        { name => "lastname", search => 0, title => $c->loc("Last Name") },
+        { name => "company", search => 0, title => $c->loc("Company") },
         { name => "email", search => 1, title => $c->loc("Email") },
     ]);
 }
@@ -311,9 +311,7 @@ sub ajax_list_contacts{
         $c,
         $c->stash->{contacts}->search_rs(
             $reseller_query->[0],
-            $reseller_query->[1] ? $reseller_query->[1] : {
-                prefetch=>"contracts"
-            }
+            $reseller_query->[1],
         ),
         $c->stash->{contact_dt_columns},
         sub {
@@ -328,6 +326,7 @@ sub ajax_list_contacts{
             my %data = (deletable => ($contract_rs->first or $subscriber_rs->first) ? 0 : 1);
             return %data
         },
+	{ 'count_limit' => 1000, },
     );
 
 }

lib/NGCP/Panel/Controller/Customer.pm
@@ -45,18 +45,18 @@ sub list_customer :Chained('/') :PathPart('customer') :CaptureArgs(0) {
 
     my $now = NGCP::Panel::Utils::DateTime::current_local;
     $c->stash->{contract_dt_columns} = NGCP::Panel::Utils::Datatables::set_columns($c, [
-        { name => "id", search => 1, title => $c->loc("#") },
-        { name => "external_id", search => 1, title => $c->loc("External #") },
-        { name => "contact.reseller.name", search => 1, title => $c->loc("Reseller") },
-        { name => "contact.email", search => 1, title => $c->loc("Contact Email") },
-        { name => "contact.firstname", search => 1, title => '' },
-        { name => "contact.lastname", search => 1, title => $c->loc("Name"),
+        { name => "id", int_search => 1, title => $c->loc("#") },
+        { name => "external_id", strict_search => 1, title => $c->loc("External #") },
+        { name => "contact.reseller.name", search => 0, title => $c->loc("Reseller") },
+        { name => "contact.email", search => 0, title => $c->loc("Contact Email") },
+        { name => "contact.firstname", search => 0, title => '' },
+        { name => "contact.lastname", search => 0, title => $c->loc("Name"),
             custom_renderer => 'function ( data, type, full, opt ) { var sep = (full.contact_firstname && full.contact_lastname) ? " " : ""; return (full.contact_firstname || "") + sep + (full.contact_lastname || ""); }' },
-        { name => "product.name", search => 1, title => $c->loc("Product") },
+        { name => "product.name", search => 0, title => $c->loc("Product") },
         { name => 'billing_profile_name', accessor => "billing_profile_name", search => 0, title => $c->loc('Billing Profile'),
           literal_sql => '""' },
-        { name => "status", search => 1, title => $c->loc("Status") },
-        { name => "max_subscribers", search => 1, title => $c->loc("Max. Subscribers") },
+        { name => "status", search => 0, title => $c->loc("Status") },
+        { name => "max_subscribers", search => 0, title => $c->loc("Max. Subscribers") },
     ]);
 
     my $rs = NGCP::Panel::Utils::Contract::get_customer_rs(c => $c); #, now => $now);
@@ -88,7 +88,18 @@ sub ajax :Chained('list_customer') :PathPart('ajax') :Args(0) {
         my $bm_actual = NGCP::Panel::Utils::BillingMappings::get_actual_billing_mapping(c => $c, contract => $item, now => $now, );
         $result{'billing_profile_name'} = $bm_actual->billing_profile->name if $bm_actual;
         return %result;
-    },);
+    }, { 
+        'count_limit' => 1000,
+        'extra_or' => [ {
+            'me.id' => { in => [ map { $_->id } $res->search({
+                'contact.email' => { like => [ NGCP::Panel::Utils::Datatables::get_search_string_pattern($c) ]->[0] },
+            },{
+                rows => 1001,
+                page => 1,
+                join => 'contact',
+            })->all ] },
+        }, ],
+    });
     $c->detach( $c->view("JSON") );
 }
 

lib/NGCP/Panel/Controller/Network.pm
@@ -47,7 +47,7 @@ sub _network_resultset_admin {
          })->search_rs({
                 'me.status' => { '!=' => 'terminated' },
                 },
-                { '+select' => [ { '' => \[ NGCP::Panel::Utils::BillingNetworks::get_contract_count_stmt() ] , -as => 'contract_cnt' },
+                { '+select' => [ { '' => \[ NGCP::Panel::Utils::BillingNetworks::get_contract_count_stmt(1000) ] , -as => 'contract_cnt' },
                 { '' => \[ NGCP::Panel::Utils::BillingNetworks::get_contract_exists_stmt() ] , -as => 'contract_exists' },
                 { '' => \[ NGCP::Panel::Utils::BillingNetworks::get_package_count_stmt() ] , -as => 'package_cnt' }, ],
             });
@@ -65,7 +65,7 @@ sub _network_resultset_reseller {
          })->search_rs({
                 'me.status' => { '!=' => 'terminated' },
                 },
-                { '+select' => [ { '' => \[ NGCP::Panel::Utils::BillingNetworks::get_contract_count_stmt() ] , -as => 'contract_cnt' },
+                { '+select' => [ { '' => \[ NGCP::Panel::Utils::BillingNetworks::get_contract_count_stmt(1000) ] , -as => 'contract_cnt' },
                 { '' => \[ NGCP::Panel::Utils::BillingNetworks::get_contract_exists_stmt() ] , -as => 'contract_exists' },
                 { '' => \[ NGCP::Panel::Utils::BillingNetworks::get_package_count_stmt() ] , -as => 'package_cnt' }, ],
             });

lib/NGCP/Panel/Controller/Package.pm
@@ -47,7 +47,7 @@ sub _package_resultset_admin {
         { group_by => 'me.id',
          })->search_rs(
             undef,
-            { '+select' => [ { '' => \[ NGCP::Panel::Utils::ProfilePackages::get_contract_count_stmt() ] , -as => 'contract_cnt' },
+            { '+select' => [ { '' => \[ NGCP::Panel::Utils::ProfilePackages::get_contract_count_stmt(1000) ] , -as => 'contract_cnt' },
                              { '' => \[ NGCP::Panel::Utils::ProfilePackages::get_voucher_count_stmt() ] , -as => 'voucher_cnt' },
                              ],
             });
@@ -64,7 +64,7 @@ sub _package_resultset_reseller {
         { group_by => 'me.id',
          })->search_rs(
             undef,
-            { '+select' => [ { '' => \[ NGCP::Panel::Utils::ProfilePackages::get_contract_count_stmt() ] , -as => 'contract_cnt' },
+            { '+select' => [ { '' => \[ NGCP::Panel::Utils::ProfilePackages::get_contract_count_stmt(1000) ] , -as => 'contract_cnt' },
                              { '' => \[ NGCP::Panel::Utils::ProfilePackages::get_voucher_count_stmt() ] , -as => 'voucher_cnt' },
                              ],
             });

lib/NGCP/Panel/Controller/Subscriber.pm
@@ -91,16 +91,16 @@ sub sub_list :Chained('/') :PathPart('subscriber') :CaptureArgs(0) {
     }
 
     $c->stash->{dt_columns} = NGCP::Panel::Utils::Datatables::set_columns($c, [
-        { name => "id", search => 1, title => $c->loc('#') },
-        { name => "contract_id", search => 1, title => $c->loc('Contract #') },
-        { name => "contract.contact.email", search => 1, title => $c->loc('Contact Email') },
-        { name => "username", search => 1, title => $c->loc('Username') },
-        { name => "domain.domain", search => 1, title => $c->loc('Domain') },
-        { name => "uuid", search => 1, title => $c->loc('UUID') },
-        { name => "status", search => 1, title => $c->loc('Status') },
+        { name => "id", int_search => 1, title => $c->loc('#') },
+        { name => "contract_id", int_search => 1, title => $c->loc('Contract #') },
+        { name => "contract.contact.email", search => 0, title => $c->loc('Contact Email') },
+        { name => "username", strict_search => 1, title => $c->loc('Username') },
+        { name => "domain.domain", search => 0, title => $c->loc('Domain') },
+        { name => "uuid", strict_search => 1, title => $c->loc('UUID') },
+        { name => "status", search => 0, title => $c->loc('Status') },
         { name => "number", search => 0, title => $c->loc('Number'), literal_sql => "concat(primary_number.cc, primary_number.ac, primary_number.sn)" },
         #{ name => "provisioning_voip_subscriber.voip_dbaliases.username", search => 1, 'no_column' => 1 },
-        { name => "provisioning_voip_subscriber.voip_subscriber_profile.name", search => 1, title => $c->loc('Profile') },
+        { name => "provisioning_voip_subscriber.voip_subscriber_profile.name", search => 0, title => $c->loc('Profile') },
     ]);
 }
 
@@ -365,7 +365,18 @@ sub ajax :Chained('sub_list') :PathPart('ajax') :Args(0) :Does(ACL) :ACLDetachTo
     my ($self, $c) = @_;
 
     my $resultset = $c->stash->{subscribers_rs};
-    NGCP::Panel::Utils::Datatables::process($c, $resultset, $c->stash->{dt_columns}, undef, { 'count_limit' => 1000, });
+    NGCP::Panel::Utils::Datatables::process($c, $resultset, $c->stash->{dt_columns}, undef, {
+        'count_limit' => 1000,
+        'extra_or' => [ {
+            'me.id' => { in => [ map { $_->id } $resultset->search({
+                'contact.email' => { like => [ NGCP::Panel::Utils::Datatables::get_search_string_pattern($c) ]->[0] },
+            },{
+                rows => 1001,
+                page => 1,
+                join => { 'contract' => 'contact'},
+            })->all ] },
+        }, ],
+    });
     $c->detach( $c->view("JSON") );
 }
 

commit f8d982708b04c15f7a7e689de277b71168aad1dc

TT#147151 restore get_contract_rs() product join
* restore the removed product table join in
  get_contract_rs() as get_contract_rs() and product.class are used
  in many places in the code that have their own filters by
  product.class

Fixes: 133bd43df TT#147151 fast loading/paging/searching panel datatables

Change-Id: I56e20f240ccc08cf1c9a25947f67990691425549

lib/NGCP/Panel/Utils/Contract.pm
@@ -131,7 +131,9 @@ sub get_contract_rs {
     $schema //= $c->model('DB');
     my $rs = $schema->resultset('contracts')->search({
         $include_terminated ? () : ('me.status' => { '!=' => 'terminated' }),  ## no critic (ProhibitCommaSeparatedStatements)
-    }, undef);
+    },{
+        join => 'product',
+    });
     return $rs;
 
 }
@@ -140,15 +142,12 @@ sub get_customer_rs {
     my %params = @_;
     my ($c,$schema,$include_terminated) = @params{qw/c schema include_terminated/};
 
-    $schema //= $c->model('DB');
-    my @product_ids = map { $_->id; } $schema->resultset('products')->search_rs({ 'class' => ['sipaccount','pbxaccount'] })->all;
-
     my $rs = get_contract_rs(
         c => $c,
         schema => $schema,
         include_terminated => $include_terminated,
     )->search_rs({
-        'product_id' => { -in => [ @product_ids ] },
+        'product.class' => { -in => [ 'sipaccount', 'pbxaccount' ] },
     },{
         join => 'contact',
     });