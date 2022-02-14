78c93e31c305a85650a38d8a14ecea98f9a6d786
Javier Rodriguez <jrodriguez@sipwise.com>
2022-02-14 18:58:51 -0300

    TT#115002 Update Spanish Translations for ngcp-panel
    Change-Id: I4b9b5dedca02ee417471623e69c296382e2e3eab

--- share/static/js/i18n/dataTables.es.js
+++ share/static/js/i18n/dataTables.es.js
@@ -1,23 +1,23 @@
 {
     "oAria": {
-        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        "sSortAscending": ": activar para ordenar las columnas de forma ascendente",
+        "sSortDescending": ": activar para ordenar la columna de modo descendente"
     },
-    "sEmptyTable":     "Ningún dato disponible en esta tabla",
-    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-    "sInfoPostFix":    "",
-    "sInfoThousands":  ",",
-    "sLengthMenu":     "Mostrar _MENU_ registros",
+    "sEmptyTable": "No hay datos disponibles en la tabla",
+    "sInfo": "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+    "sInfoEmpty": "Mostrando 0 a 0 de 0 entradas",
+    "sInfoFiltered": "(filtrado de _MAX_ entradas totales)",
+    "sInfoPostFix": "",
+    "sInfoThousands": ",",
+    "sLengthMenu": "Mostrar las entradas del _MENU_",
     "sLoadingRecords": "Cargando...",
     "oPaginate": {
-        "sFirst":    "Primero",
-        "sLast":     "Último",
-        "sNext":     "Siguiente",
+        "sFirst": "Primero",
+        "sLast": "Último",
+        "sNext": "Siguiente",
         "sPrevious": "Anterior"
     },
-    "sProcessing":     "Procesando...",
-    "sSearch":         "Buscar:",
-    "sZeroRecords":    "No se encontraron resultados"
+    "sProcessing": "Procesando...",
+    "sSearch": "Buscar:",
+    "sZeroRecords": "No hay entradas disponibles"
 }
