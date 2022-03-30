26c868bcb34c1e898563143ba36fd71f98a6a7d2
Kirill Solomko <ksolomko@sipwise.com>
2022-03-30 15:24:17 +0200

    Revert "TT#158252 API GET subscriberpreferences - rm acquire_contract_rowlocks"
    This reverts commit 9d77ddb15de40aecb22b2e84d02a5b9b77ea9d5c.
    
    Change-Id: I649fa2b01498c55f1f27811cda1f882d3192d0bc

--- lib/NGCP/Panel/Controller/API/SubscriberPreferences.pm
+++ lib/NGCP/Panel/Controller/API/SubscriberPreferences.pm
@@ -90,11 +90,12 @@ sub GET :Allow {
 
     my $subscribers_rs = $self->item_rs($c, "subscribers");
     (my $total_count, $subscribers_rs, my $subscribers_rows) = $self->paginate_order_collection($c, $subscribers_rs);
-
+    my $subscribers = NGCP::Panel::Utils::Contract::acquire_contract_rowlocks(c => $c,
+        rs => $subscribers_rs,
+        contract_id_field => 'contract_id');
     my $now = NGCP::Panel::Utils::DateTime::current_local;
-
     my (@embedded, @links);
-    for my $subscriber ($subscribers_rs->all) {
+    for my $subscriber (@$subscribers) {
         next unless($subscriber->provisioning_voip_subscriber);
         push @embedded, $self->hal_from_item($c, $subscriber, "subscribers");
         push @links, Data::HAL::Link->new(
