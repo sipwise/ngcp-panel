f7d57da547f35dd6e0eba80d8cb78abf4e9b1268
Oleksandr Duts <oduts@sipwise.com>
2022-04-29 15:06:58 +0300

    TT#134050 API POST subscribers - doc customer_id field
      * Fix the missed customer_id field in API doc.
    
    Change-Id: I81831ed6fb01dbf723a95e7846bc54b496a51f2e

--- lib/NGCP/Panel/Form/Subscriber/SubscriberAPI.pm
+++ lib/NGCP/Panel/Form/Subscriber/SubscriberAPI.pm
@@ -205,7 +205,7 @@ has_field 'save' => (
 has_block 'fields' => (
     tag => 'div',
     class => [qw/modal-body/],
-    render_list => [qw/customer domain pbx_extension e164 alias_numbers email webusername webpassword username password status lock external_id administrative is_pbx_group pbx_group_ids pbx_groupmember_ids is_pbx_pilot display_name profile_set profile timezone/ ],
+    render_list => [qw/customer domain pbx_extension e164 alias_numbers email webusername webpassword username password status lock external_id administrative is_pbx_group pbx_group_ids pbx_groupmember_ids is_pbx_pilot display_name profile_set profile timezone customer_id/ ],
 );
 
 has_block 'actions' => (
