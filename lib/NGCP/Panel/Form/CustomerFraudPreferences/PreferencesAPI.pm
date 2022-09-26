74e2477173111a2341d023576f31feeaa5f5c94f
Rene Krenn <rkrenn@sipwise.com>
2022-09-26 16:27:31 +0200

    MT#55436 mark concurrent_* fraudpref fields as readonly
    Change-Id: I47f3a42163703c57cecdbcf4dd84e9e7a9cbe2e7
    (cherry picked from commit d4d924e146fe9c57cae87f1836c032aafb3cc140)

--- lib/NGCP/Panel/Form/CustomerFraudPreferences/PreferencesAPI.pm
+++ lib/NGCP/Panel/Form/CustomerFraudPreferences/PreferencesAPI.pm
@@ -4,9 +4,10 @@ use HTML::FormHandler::Widget::Block::Bootstrap;
 extends 'HTML::FormHandler';
 
 has_field 'current_fraud_interval_source' => (
+    readonly => 1,
     required => 0,
     type => 'Select',
-    label => 'Fraud preferences interval source (customer or billing_profile)',
+    label => 'Fraud preferences interval source (customer or billing_profile) (readonly)',
     options => [
         { value => 'customer', label => 'Customer' },
         { value => 'billng_profile', label => 'Billing Profile' },
@@ -19,9 +20,10 @@ has_field 'fraud_interval_limit' => (
 );
 
 has_field 'current_fraud_interval_limit' => (
+    readonly => 1,
     type => 'Integer',
     required => 0,
-    label => 'Current fraud detection threshold per month in cents',
+    label => 'Current fraud detection threshold per month in cents (readonly)',
 );
 
 has_field 'fraud_interval_lock' => (
@@ -38,9 +40,10 @@ has_field 'fraud_interval_lock' => (
 );
 
 has_field 'current_fraud_interval_lock' => (
+    readonly => 1,
     type => 'Select',
     required => 0,
-    label => 'Current lock level',
+    label => 'Current lock level (readonly)',
     options => [
         { value => 0, label => 'none' },
         { value => 1, label => 'foreign calls' },
@@ -58,16 +61,18 @@ has_field 'fraud_interval_notify' => (
 );
 
 has_field 'current_fraud_interval_notify' => (
+    readonly => 1,
     type => '+NGCP::Panel::Field::EmailList',
     required => 0,
     maxlength => 255,
-    label => 'Current comma-separated list of e-mail addresses for notification'
+    label => 'Current comma-separated list of e-mail addresses for notification (readonly)'
 );
 
 has_field 'current_fraud_daily_source' => (
+    readonly => 1,
     required => 0,
     type => 'Select',
-    label => 'Fraud daily preferences source (customer or billing_profile)',
+    label => 'Fraud daily preferences source (customer or billing_profile) (readonly)',
     options => [
         { value => 'customer', label => 'Customer' },
         { value => 'billng_profile', label => 'Billing Profile' },
@@ -80,8 +85,9 @@ has_field 'fraud_daily_limit' => (
 );
 
 has_field 'current_fraud_daily_limit' => (
+    readonly => 1,
     type => 'Integer',
-    label => 'Current fraud detection threshold per day in cents',
+    label => 'Current fraud detection threshold per day in cents (readonly)',
 );
 
 has_field 'fraud_daily_lock' => (
@@ -98,9 +104,10 @@ has_field 'fraud_daily_lock' => (
 );
 
 has_field 'current_fraud_daily_lock' => (
+    readonly => 1,
     type => 'Select',
     required => 0,
-    label => 'Current daily lock level',
+    label => 'Current daily lock level (readonly)',
     options => [
         { value => 0, label => 'none' },
         { value => 1, label => 'foreign calls' },
@@ -118,10 +125,11 @@ has_field 'fraud_daily_notify' => (
 );
 
 has_field 'current_fraud_daily_notify' => (
+    readonly => 1,
     type => '+NGCP::Panel::Field::EmailList',
     required => 0,
     maxlength => 255,
-    label => 'Current comma-separated list of e-mail addresses for notification'
+    label => 'Current comma-separated list of e-mail addresses for notification (readonly)'
 );
 
 1;
