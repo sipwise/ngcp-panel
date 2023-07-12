31f450272b340ada0a7c1f97f061b709b7cae9cf
Kirill Solomko <ksolomko@sipwise.com>
2023-07-12 13:39:34 +0200

    MT#57708 sound set/file use READ COMMITTED
    * as changes to sound sets and sound files cause
      internal triggers + SP to be invoked, the gap
      locking should be reduced to avoid possible
      deadlocks, hence READ COMMITTED transaction
      isolation level is used for all sound sets
      and sound files change operations.
    * fix a condition that caused an evaluation error
      when both old_parent_id and parent_id are undef.
    
    Change-Id: I7e69a0a0e5878c03b086977933315e532aaa2a29
    (cherry picked from commit 22a8e95c29e4817c3cafed223449c2d76bcff4f1)

--- lib/NGCP/Panel/Controller/API/SoundSets.pm
+++ lib/NGCP/Panel/Controller/API/SoundSets.pm
@@ -8,6 +8,7 @@ use parent qw/NGCP::Panel::Role::Entities NGCP::Panel::Role::API::SoundSets/;
 
 __PACKAGE__->set_config({
     allowed_roles => [qw/admin reseller subscriberadmin/],
+    set_transaction_isolation => 'READ COMMITTED',
 });
 
 sub allowed_methods{
