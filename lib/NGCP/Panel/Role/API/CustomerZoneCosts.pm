e4a0067cefe66f4e772b3e08bc5d188efcfdb684
Rene Krenn <rkrenn@sipwise.com>
2023-12-07 21:35:21 +0100

    MT#58936 fix customerzonecosts resourcename
    Change-Id: I22b052bc6931e5426e1aa982396c446cb1c67883
    (cherry picked from commit 2101e2246d30b029f4611f5b432d0f62ece73589)

--- lib/NGCP/Panel/Role/API/CustomerZoneCosts.pm
+++ lib/NGCP/Panel/Role/API/CustomerZoneCosts.pm
@@ -81,8 +81,7 @@ sub resource_from_item {
         stime => $stime,
         etime => $etime,
         subscriber_uuid => $subscriber_uuid,
-        direction => ($c->request->query_params->{direction} ? $c->request->query_params->{direction} : 'in_out'),
-        category => 'customer',
+        call_direction => ($c->request->query_params->{direction} ? $c->request->query_params->{direction} : 'in_out'),
         group_by_detail => 0,
     );
 
